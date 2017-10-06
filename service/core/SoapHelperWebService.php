ecb136741fe462c65e57ba4ce2729532ecfc0918
maretval <valerie.maret@twentyreasons.com>
2017-10-06 21:58:35 +0200

    BugFix PHP7 Compatibility in default rest service

--- service/core/SoapHelperWebService.php
+++ service/core/SoapHelperWebService.php
@@ -1,39 +1,6 @@
 <?php
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-/*********************************************************************************
-* SugarCRM Community Edition is a customer relationship management program developed by
-* SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
-* 
-* This program is free software; you can redistribute it and/or modify it under
-* the terms of the GNU Affero General Public License version 3 as published by the
-* Free Software Foundation with the addition of the following permission added
-* to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
-* IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
-* OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
-* 
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-* FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
-* details.
-* 
-* You should have received a copy of the GNU Affero General Public License along with
-* this program; if not, see http://www.gnu.org/licenses or write to the Free
-* Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-* 02110-1301 USA.
-* 
-* You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
-* SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
-* 
-* The interactive user interfaces in modified source and object code versions
-* of this program must display Appropriate Legal Notices, as required under
-* Section 5 of the GNU Affero General Public License version 3.
-* 
-* In accordance with Section 7(b) of the GNU Affero General Public License version 3,
-* these Appropriate Legal Notices must retain the display of the "Powered by
-* SugarCRM" logo. If the display of the logo is not reasonably feasible for
-* technical reasons, the Appropriate Legal Notices must display the words
-* "Powered by SugarCRM".
-********************************************************************************/
+/***** SPICE-SUGAR-HEADER-SPACEHOLDER *****/
 
 
 global $disable_date_format;
@@ -402,8 +369,8 @@ function validate_user($user_name, $password){
 			foreach($value->field_defs as $var){
 				if(isset($var['source']) && ($var['source'] != 'db' && $var['source'] != 'custom_fields') && $var['name'] != 'email1' && $var['name'] != 'email2' && (!isset($var['type'])|| $var['type'] != 'relate'))continue;
 
-				if(isset($value->$var['name'])){
-					$val = $value->$var['name'];
+				if(isset($value->{$var['name']})){ //PHP7 COMPAT //$var['name']
+					$val = $value->{$var['name']};
 					$type = $var['type'];
 
 					if(strcmp($type, 'date') == 0){
@@ -471,8 +438,8 @@ function validate_user($user_name, $password){
 
 			foreach($filterFields as $field){
 				$var = $value->field_defs[$field];
-				if(isset($value->$var['name'])){
-					$val = $value->$var['name'];
+				if(isset($value->{$var['name']})){ //PHP7 COMPAT //$value->$var['name']
+					$val = $value->{$var['name']}; //PHP7 COMPAT //$value->$var['name']
 					$type = $var['type'];
 
 					if(strcmp($type, 'date') == 0){
@@ -778,7 +745,10 @@ function validate_user($user_name, $password){
                 if(!empty($seed->field_name_map[$value['name']]['sensitive'])) {
                     continue;
                 }
-				$seed->$value['name'] = $val;
+                // begin PHP7 COMPAT
+				// $seed->$value['name']= $value['value'];
+				$seed->{$value['name']} = $val;
+				//end
 			}
 
 			if($count == $total){
