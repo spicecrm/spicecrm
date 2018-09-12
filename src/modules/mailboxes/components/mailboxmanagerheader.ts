12be344d4e6fd86e5a944d51a88518d3d86d5cef
Dawid Bestrzynski <dawid.bestrzynski@twentyreasons.com>
2018-09-12 09:41:44 +0200

    Routes to specific Mailboxes

--- src/modules/mailboxes/components/mailboxmanagerheader.ts
+++ src/modules/mailboxes/components/mailboxmanagerheader.ts
@@ -1,23 +1,42 @@
-import {Component, Input, ViewChild, ViewContainerRef} from "@angular/core";
-import {Subject} from "rxjs";
-import {backend} from "../../../services/backend.service";
-import {language} from "../../../services/language.service";
-import {navigation} from "../../../services/navigation.service";
-import {mailboxesEmails} from "../services/mailboxesemail.service";
+import {Component, Input, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {Subject} from 'rxjs';
+import {backend} from '../../../services/backend.service';
+import {language} from '../../../services/language.service';
+import {navigation} from '../../../services/navigation.service';
+import {mailboxesEmails} from '../services/mailboxesemail.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
     selector: 'mailbox-manager-header',
     templateUrl: './src/modules/mailboxes/templates/mailboxmanagerheader.html',
 })
-export class MailboxManagerHeader {
+export class MailboxManagerHeader implements OnInit {
 
     private mailboxselection: string;
-    private emailopenness: string = "";
+    private emailopenness: string = '';
 
     constructor(
+        private activatedRoute: ActivatedRoute,
         private language: language,
         private mailboxesEmails: mailboxesEmails,
     ) {}
+
+    public ngOnInit() {
+        let routeSubscribe = this.activatedRoute.params.subscribe(
+            (params) => {
+                this.mailboxselection = params['id'];
+
+                // catch an event from mailboxesEmails service once the mailboxes are actually loaded
+                this.mailboxesEmails.mailboxesLoaded$.subscribe(
+                    (loaded) => {
+                        if (loaded === true) {
+                            this.selectMailbox();
+                        }
+                    }
+                );
+            }
+        );
+    }
 
     private selectMailbox() {
         this.mailboxesEmails.activeMailBox = {};
