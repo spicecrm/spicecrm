456cee17e4041ae8941ee7157b9a90c25bf20099
Christian Knoll <christian.g.knoll@gmail.com>
2018-09-16 18:45:56 +0200

    resolving modulecomponents module .. created emails and projects module

---
+++ src/modules/emails/components/emailtoobjectbutton.ts
@@ -0,0 +1,49 @@
+import {Component, EventEmitter, Output} from "@angular/core";
+import {model} from "../../../services/model.service";
+import {language} from "../../../services/language.service";
+import {modal} from "../../../services/modal.service";
+
+@Component({
+    selector: "email-to-object-button",
+    templateUrl: "./src/modules/emails/templates/emailtoobjectbutton.html",
+    host: {
+        "class": "slds-button slds-button--neutral",
+        "(click)" : "open()"
+    },
+    styles: [
+        ":host {cursor:pointer;}"
+    ]
+})
+export class EmailToObjectButton {
+    private object_module_name: string;
+    private actionconfig; // can be set inside actionsets...
+    @Output() public actionemitter = new EventEmitter();
+
+    constructor(
+        private language: language,
+        private model: model,
+        private modal: modal,
+    ) {
+
+    }
+
+    public ngOnInit() {
+        this.object_module_name = this.actionconfig.module;
+    }
+
+    public open() {
+        this.modal.openModal("EmailToObjectModal", true).subscribe(
+            cmpref => {
+                cmpref.instance.email_model = this.model;
+                cmpref.instance.object_module_name = this.object_module_name;
+                cmpref.instance.object_relation_link_name = this.actionconfig.relation_link_name;
+                cmpref.instance.object_predefined_fields = this.actionconfig.predefined_fields;
+                cmpref.instance.save$.subscribe(
+                    data => {
+                        this.actionemitter.emit("save");
+                    }
+                );
+            }
+        );
+    }
+}
---
+++ src/modules/projects/components/projectwbshierarchynode.ts
@@ -0,0 +1,57 @@
+import {Component, AfterViewInit, OnInit, OnDestroy, Input} from "@angular/core";
+import {model} from "../../../services/model.service";
+import {view} from "../../../services/view.service";
+import {metadata} from "../../../services/metadata.service";
+import {language} from "../../../services/language.service";
+
+import {projectwbsHierarchy} from "../services/projectwbshierarchy.service";
+
+@Component({
+    selector: "[projectwbs-hierarchy-node]",
+    templateUrl: "./src/modules/projects/templates/projectwbshierarchynode.html",
+    providers: [model, view],
+    host: {
+        "[attr.aria-level]": "nodedata.level"
+    }
+})
+export class ProjectWBSHierarchyNode implements OnInit {
+    @Input() public nodedata: any = {};
+    @Input() public fields: Array<any> = [];
+    private loading: boolean = false;
+
+    constructor(private language: language, private metadata: metadata, private projectwbsHierarchy: projectwbsHierarchy, private model: model) {
+
+    }
+
+    public ngOnInit() {
+        this.model.module = "ProjectWBSs";
+        this.model.id = this.nodedata.id;
+        this.model.data.summary_text = this.nodedata.summary_text;
+
+        // copy fields
+        for (let field of this.fields) {
+            this.model.data[field.field] = this.nodedata.data[field.field];
+        }
+
+        // copy acl
+        this.model.data.acl = this.nodedata.data.acl;
+    }
+
+    private eexpandNode() {
+        if (this.nodedata.expanded) {
+            this.projectwbsHierarchy.collapse(this.nodedata.id);
+        } else {
+            this.loading = true;
+            this.projectwbsHierarchy.expand(this.nodedata.id);
+        }
+    }
+
+    private getIcon() {
+        switch (this.nodedata.expanded) {
+            case false:
+                return "chevronright";
+            case true:
+                return "chevrondown";
+        }
+    }
+}
