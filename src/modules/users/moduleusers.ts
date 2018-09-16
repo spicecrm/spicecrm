1f4e88534b0d358d3ad05e0f0d577cf548ff4b1a
Christian Knoll <christian.g.knoll@gmail.com>
2018-09-16 14:56:16 +0200

    resolving modulecomponents module .. created users module

---
+++ src/modules/users/moduleusers.ts
@@ -0,0 +1,60 @@
+import {CommonModule} from "@angular/common";
+import {NgModule, Renderer2, Output, Component, Injectable, EventEmitter, OnInit, AfterViewInit, OnDestroy, OnChanges, ViewChild, ViewContainerRef, Input} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+
+import {VersionManagerService} from "../../services/versionmanager.service";
+import {DirectivesModule} from "../../directives/directives";
+
+import {language} from "../../services/language.service";
+import {view} from "../../services/view.service";
+import {metadata} from "../../services/metadata.service";
+import {backend} from "../../services/backend.service";
+import {model} from "../../services/model.service";
+import {toast} from "../../services/toast.service";
+import {relatedmodels} from "../../services/relatedmodels.service";
+
+import {ObjectFields} from "../../objectfields/objectfields";
+import {GlobalComponents} from "../../globalcomponents/globalcomponents";
+import {ObjectComponents} from "../../objectcomponents/objectcomponents";
+import {SystemComponents} from "../../systemcomponents/systemcomponents";
+import {GlobalUtilityComponents} from "../../globalutilitycomponents/globalutilitycomponents";
+
+import /*embed*/ {UserChangePasswordButton} from "./components/userchangepasswordbutton";
+import /*embed*/ {UserChangePasswordModal} from "./components/userchangepasswordmodal";
+import /*embed*/ {UserPreferences} from "./components/userpreferences";
+import /*embed*/ {UserRoles} from "./components/userroles";
+import /*embed*/ {UserRolesAddModal} from "./components/userrolesaddmodal";
+import /*embed*/ {UserAddButton} from "./components/useraddbutton";
+import /*embed*/ {UserAddModal} from "./components/useraddmodal";
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ObjectFields,
+        GlobalComponents,
+        ObjectComponents,
+        SystemComponents,
+        GlobalUtilityComponents,
+        DirectivesModule,
+    ],
+    declarations: [
+        UserChangePasswordButton,
+        UserChangePasswordModal,
+        UserPreferences,
+        UserRoles,
+        UserRolesAddModal,
+        UserAddButton,
+        UserAddModal,
+    ]
+})
+export class ModuleUsers {
+    public readonly version = "1.0";
+    public readonly build_date = "/*build_date*/";
+
+    constructor(
+        private vms: VersionManagerService,
+    ) {
+        this.vms.registerModule(this);
+    }
+}
