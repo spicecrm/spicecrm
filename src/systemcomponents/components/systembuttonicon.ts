290d9b69d57b618f5af8827102e4b3521bf97a5a
MehyarSawas <mehyar.sawas@outlook.com>
2018-09-24 11:48:17 +0200

    Tslint correction

--- src/systemcomponents/components/systembuttonicon.ts
+++ src/systemcomponents/components/systembuttonicon.ts
@@ -1,35 +1,40 @@
-import {Component, Input} from '@angular/core';
-import {metadata} from '../../services/metadata.service';
+import {Component, Input} from "@angular/core";
+import {metadata} from "../../services/metadata.service";
 
 @Component({
-    selector: 'system-button-icon',
-    templateUrl: './src/systemcomponents/templates/systembuttonicon.html'
+    selector: "system-button-icon",
+    templateUrl: "./src/systemcomponents/templates/systembuttonicon.html"
 })
 export class SystemButtonIcon {
-    @Input() icon: string = '';
-    @Input() size: string = '';
-    @Input() position: string = '';
-    @Input() inverse: boolean = false;
-    @Input() title: string = undefined;
+    @Input() private icon: string = "";
+    @Input() private size: string = "";
+    @Input() private position: string = "";
+    @Input() private inverse: boolean = false;
+    @Input() private title: string = undefined;
 
-    constructor(private metadata: metadata) {}
-
-    getSvgHRef() {
-        return './sldassets/icons/utility-sprite/svg/symbols.svg#' + this.icon;
+    constructor(private metadata: metadata) {
     }
 
-    getClass(){
-        let classList: Array<string> = [];
-        if(this.size != '')
-            classList.push('slds-button__icon--' + this.size);
-        else
-            classList.push('slds-button__icon');
-        if ( this.position != '' )
-            classList.push('slds-button__icon_' + this.position);
+    private getSvgHRef() {
+        return "./sldassets/icons/utility-sprite/svg/symbols.svg#" + this.icon;
+    }
 
-        if(this.inverse)
-            classList.push('slds-button_icon-inverse');
+    private getClass() {
+        let classList: Array<string> = [];
+        if (this.size !== "") {
+            classList.push("slds-button__icon--" + this.size);
+        } else {
+            classList.push("slds-button__icon");
+        }
+        if (this.position !== "") {
+            classList.push("slds-button__icon_" + this.position);
+        }
+
+        if (this.inverse) {
+            classList.push("slds-button_icon-inverse");
+        }
 
         return classList;
     }
 }
+
