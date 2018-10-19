239f147f71683bdd54606eb39eee2bfe96e71511
Dawid Bestrzynski <dawid.bestrzynski@twentyreasons.com>
2018-10-19 14:27:13 +0200

    [cr1000062gcnl] Mailbox load more fix

--- src/modules/mailboxes/services/mailboxesemail.service.ts
+++ src/modules/mailboxes/services/mailboxesemail.service.ts
@@ -137,7 +137,8 @@ export class mailboxesEmails {
         let parameters = {};
         if (this.unreadonly) {
             parameters = {
-                fields: JSON.stringify(["name", "id", "from_addr_name", "date_sent", "status", "openness"]),
+                fields: JSON.stringify(["name", "id", "from_addr_name", "date_sent", "status", "openness",
+                "sentiment", "magnitude"]),
                 limit: this.limit,
                 offset: this.emails.length,
                 searchfields: JSON.stringify({
