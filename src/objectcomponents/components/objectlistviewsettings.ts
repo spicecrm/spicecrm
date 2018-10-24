4132310841f0520fdf5daff43a8da58a0b30d6be
MehyarSawas <mehyar.sawas@outlook.com>
2018-10-24 16:16:28 +0200

    Ticket SPICEUI-117: Resolved, Metadata updateModuleListType method added

--- src/objectcomponents/components/objectlistviewsettings.ts
+++ src/objectcomponents/components/objectlistviewsettings.ts
@@ -1,7 +1,7 @@
-import {ElementRef, Component, Renderer2} from '@angular/core';
-import { modellist } from '../../services/modellist.service';
-import { modal } from '../../services/modal.service';
-import { language } from '../../services/language.service';
+import {Component, ElementRef, Renderer2} from '@angular/core';
+import {modellist} from '../../services/modellist.service';
+import {modal} from '../../services/modal.service';
+import {language} from '../../services/language.service';
 import {ObjectListViewSettingsAddlistModal} from "./objectlistviewsettingsaddlistmodal";
 import {ObjectListViewSettingsSetfieldsModal} from "./objectlistviewsettingssetfieldsmodal";
 
@@ -10,13 +10,9 @@ import {ObjectListViewSettingsSetfieldsModal} from "./objectlistviewsettingssetf
     templateUrl: './src/objectcomponents/templates/objectlistviewsettings.html',
 
 })
-export class ObjectListViewSettings{
-    showMenu: boolean = false;
-    showAddModal: boolean = false;
-    showDeleteModal: boolean = false;
-    showDisplayfieldsModal: boolean = false;
-    modalMode: string = 'add';
-    clickListener: any;
+export class ObjectListViewSettings {
+    private showMenu: boolean = false;
+    private clickListener: any;
 
     constructor(
         private language: language,
@@ -24,17 +20,16 @@ export class ObjectListViewSettings{
         private modal: modal,
         private modellist: modellist,
         private renderer: Renderer2
-    ){
+    ) {}
 
-    }
-
-    toggleMenu(){
+    private toggleMenu() {
         this.showMenu = !this.showMenu;
 
         if (this.showMenu) {
             this.clickListener = this.renderer.listen('document', 'click', (event) => this.onClick(event));
-        } else if (this.clickListener)
+        } else if (this.clickListener) {
             this.clickListener();
+        }
     }
 
     public onClick(event: MouseEvent): void {
@@ -45,42 +40,42 @@ export class ObjectListViewSettings{
         }
     }
 
-    add(){
+    private add() {
         this.modal.openModal('ObjectListViewSettingsAddlistModal').subscribe(modalref => {
             modalref.instance.modellist = this.modellist;
             modalref.instance.modalmode = 'add';
-        })
+        });
     }
 
-    edit()
-    {
-        if(!this.modellist.checkAccess('edit'))
+    private edit() {
+        if (!this.modellist.checkAccess('edit')) {
             return false;
+        }
 
         this.modal.openModal('ObjectListViewSettingsAddlistModal').subscribe(modalref => {
             modalref.instance.modellist = this.modellist;
             modalref.instance.modalmode = 'edit';
-        })
+        });
     }
 
-    setfields()
-    {
-        if(!this.modellist.checkAccess('edit'))
+    private setfields() {
+        if (!this.modellist.checkAccess('edit')) {
             return false;
+        }
 
         this.modal.openModal('ObjectListViewSettingsSetfieldsModal').subscribe(modalref => {
             modalref.instance.modellist = this.modellist;
-        })
+        });
     }
 
-    delete()
-    {
-        if(!this.modellist.checkAccess('delete'))
+    private delete() {
+        if (!this.modellist.checkAccess('delete')) {
             return false;
+        }
 
         this.modal.openModal('ObjectListViewSettingsDeletelistModal').subscribe(modalref => {
             modalref.instance.modellist = this.modellist;
-        })
+        });
     }
 
 }
