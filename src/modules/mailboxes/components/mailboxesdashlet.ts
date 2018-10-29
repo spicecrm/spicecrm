fd49d8f8be99fdb5086a2353da1d9c97094d064a
MehyarSawas <mehyar.sawas@outlook.com>
2018-10-29 12:54:24 +0100

    Mailboxes Dashlet: Getter Interval Added. TSLint

--- src/modules/mailboxes/components/mailboxesdashlet.ts
+++ src/modules/mailboxes/components/mailboxesdashlet.ts
@@ -1,4 +1,4 @@
-import {Component, AfterViewInit, OnInit, ViewChild, ViewContainerRef, ElementRef} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import {model} from '../../../services/model.service';
 import {view} from '../../../services/view.service';
 import {metadata} from '../../../services/metadata.service';
@@ -11,14 +11,15 @@ import {Router} from '@angular/router';
     templateUrl: './src/modules/mailboxes/templates/mailboxesdashlet.html',
     providers: [model, view]
 })
-export class MailboxesDashlet implements OnInit {
-    isLoading: boolean = true;
-    mailboxes: Array<any> = [];
-    canLoadMore: boolean = true;
-    loadLimit: number = 20;
+export class MailboxesDashlet implements OnInit, OnDestroy {
+    private isLoading: boolean = true;
+    private mailboxes: Array<any> = [];
+    private canLoadMore: boolean = true;
+    private loadLimit: number = 20;
+    private getMailBoxesInterval: any = undefined;
 
-    @ViewChild('tablecontainer', {read: ViewContainerRef}) tablecontainer: ViewContainerRef;
-    @ViewChild('headercontainer', {read: ViewContainerRef}) headercontainer: ViewContainerRef;
+    @ViewChild('tablecontainer', {read: ViewContainerRef}) private tablecontainer: ViewContainerRef;
+    @ViewChild('headercontainer', {read: ViewContainerRef}) private headercontainer: ViewContainerRef;
 
     constructor(private language: language,
                 private metadata: metadata,
@@ -29,50 +30,63 @@ export class MailboxesDashlet implements OnInit {
 
     }
 
-    ngOnInit() {
+    get tablestyle() {
+        let element = this.headercontainer.element.nativeElement;
+        return {height: `calc(98% - ${element.clientHeight}px`};
+
+    }
+
+    public ngOnInit() {
         this.model.module = 'Mailboxes';
         this.getMailboxes();
+        this.getMailBoxesInterval = this.getMailboxesInterval();
     }
 
-    getMailboxes(){
-        this.backend.getRequest('/modules/Mailboxes/dashlet').subscribe((mailboxes: any[]) => {
+    public ngOnDestroy() {
+        if (this.getMailBoxesInterval) {
+            clearInterval(this.getMailBoxesInterval);
+        }
+    }
+
+    private getMailboxes() {
+            this.backend.getRequest('/modules/Mailboxes/dashlet').subscribe((mailboxes: any[]) => {
                 this.mailboxes = mailboxes;
-                if (mailboxes.length < this.loadLimit)
+                if (mailboxes.length < this.loadLimit) {
                     this.canLoadMore = false;
-            this.isLoading = false;
-        });
-        if (this.mailboxes.length > 0)
-            this.mailboxes = this.mailboxes.map(mailbox => mailbox.emailsread = mailbox.emailsread - mailbox.emailsclosed);
+                }
+                this.isLoading = false;
+            });
+            if (this.mailboxes.length > 0) {
+                this.mailboxes = this.mailboxes.map(mailbox => mailbox.emailsread = mailbox.emailsread - mailbox.emailsclosed);
+            }
     }
 
-    get tablestyle(){
-        let element = this.headercontainer.element.nativeElement;
-        return {height: `calc(98% - ${element.clientHeight}px` }
-
+    private getMailboxesInterval() {
+         return setInterval(() => this.getMailboxes(), 60000);
     }
 
-    onScroll() {
+    private onScroll() {
         let element = this.tablecontainer.element.nativeElement;
-        if (element.scrollTop + element.clientHeight >= element.scrollHeight)
+        if (element.scrollTop + element.clientHeight >= element.scrollHeight) {
             this.loadMore();
+        }
     }
 
-    goToRecord(id){
+    private goToRecord(id) {
         this.router.navigate([`/module/${this.model.module}/${id}`]);
 
     }
 
-    loadMore(){
-        if (this.canLoadMore){
+    private loadMore() {
+        if (this.canLoadMore) {
             this.isLoading = true;
             this.backend.getRequest('/modules/Mailboxes/dashlet').subscribe((mailboxes: any[]) => {
                 this.mailboxes = this.mailboxes.concat(mailboxes);
-                if (mailboxes.length < this.loadLimit)
+                if (mailboxes.length < this.loadLimit) {
                     this.canLoadMore = false;
+                }
                 this.isLoading = false;
             });
         }
     }
-
-
 }
