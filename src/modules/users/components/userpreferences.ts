bbeb02af1752f6317cfc88f8a4e28f3df6ec2f17
strombo <andreas@gloeckl.name>
2018-10-30 15:46:31 +0100

    revision user prefs

--- src/modules/users/components/userpreferences.ts
+++ src/modules/users/components/userpreferences.ts
@@ -135,8 +135,8 @@ export class UserPreferences {
     }
 
     private setFormattingOfNumbers(val: number | string) {
-        if (val === "") {
-            this.preferences.num_grp_sep = this.preferences.dec_sep = "";
+        if (val === '-') {
+            this.preferences.num_grp_sep = this.preferences.dec_sep = null;
         } else {
             this.preferences.num_grp_sep = this.formattingsOfNumbers[val].num_grp_sep;
             this.preferences.dec_sep = this.formattingsOfNumbers[val].dec_sep;
@@ -148,7 +148,7 @@ export class UserPreferences {
     }
 
     private save() {
-        this.prefservice.setPreferences(this.preferences, true).subscribe(() => {
+        this.prefservice.setPreferences( this.preferences ).subscribe(() => {
             this.toast.sendToast(this.language.getLabel("LBL_DATA_SAVED"), "success");
             this.preferences = _.pick(this.prefservice.unchangedPreferences.global, this.names);
         });
@@ -179,6 +179,10 @@ export class UserPreferences {
             });
         }
         return exampleText;
+    }
+
+    private change( event, pref ) {
+        this.preferences[pref] = ( event.srcElement.value === '-' ? null : event.srcElement.value );
     }
 
 }
