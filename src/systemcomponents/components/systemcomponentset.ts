918d3f97f1189779fbbed1f4805ae6092f51bbca
Christian Knoll <christian.g.knoll@gmail.com>
2018-11-04 13:01:12 +0100

    new component system-componentset

--- src/systemcomponents/components/systemcomponentset.ts
+++ src/systemcomponents/components/systemcomponentset.ts
@@ -1,23 +1,55 @@
-import {Component, Input, AfterViewInit, ViewContainerRef, ViewChild} from '@angular/core';
+import {Component, Input, AfterViewInit, ViewContainerRef, ViewChild, OnChanges} from '@angular/core';
 import {metadata} from '../../services/metadata.service';
 
 @Component({
     selector: 'system-componentset',
     templateUrl: './src/systemcomponents/templates/systemcomponentset.html'
 })
-export class SystemComponentSet implements AfterViewInit {
+export class SystemComponentSet implements AfterViewInit, OnChanges {
     @ViewChild('componentcontainer', {read: ViewContainerRef}) private componentcontainer: ViewContainerRef;
     @Input() private componentset: string = '';
 
-    constructor(private metadata: metadata) {}
+    private viewInitialized: boolean = false;
+    private _componentset: string = '';
+    private _componentRefs: Array<any> = [];
+
+    constructor(private metadata: metadata) {
+    }
 
     public ngAfterViewInit() {
+        // render the componentset
+        this.renderComnponentset();
+
+        // set the view to initialized
+        this.viewInitialized = true;
+    }
+
+    public ngOnChanges() {
+        if (this.viewInitialized && this.componentset != this._componentset) {
+            // destroy all components if the componentset has changed
+            for (let _componentRef of this._componentRefs) {
+                _componentRef.destroy();
+            }
+            this._componentRefs = [];
+
+            // render the componentset
+            this.renderComnponentset();
+        }
+    }
+
+    private renderComnponentset() {
         if (this.componentset) {
             for (let component of this.metadata.getComponentSetObjects(this.componentset)) {
                 this.metadata.addComponent(component.component, this.componentcontainer).subscribe(componentRef => {
                     componentRef.instance.componentconfig = component.componentconfig;
+
+                    // keep in the stack so we can destroy it when the set changes
+                    this._componentRefs.push(componentRef);
                 });
             }
         }
+
+        // keep the componentset we have rendered in teh object so to onyl reredner on change
+        this._componentset = this.componentset;
     }
 }
