2c80cf45e5cffd6b26a487f5e3c452779ab06363
Christian Knoll <christian.g.knoll@gmail.com>
2018-11-10 09:05:27 +0100

    cleanup

--- src/systemcomponents/components/systembuttongroup.ts
+++ src/systemcomponents/components/systembuttongroup.ts
@@ -6,15 +6,15 @@ import {metadata} from '../../services/metadata.service';
     templateUrl: './src/systemcomponents/templates/systembuttongroup.html'
 })
 export class SystemButtonGroup{
-    @Input() items: Array<any> = [];
-    @Output() action: EventEmitter<string> = new EventEmitter<string>();
+    @Input() private items: any[] = [];
+    @Output() private action: EventEmitter<string> = new EventEmitter<string>();
 
-    isOpen = false;
+    private isopen = false;
 
     constructor(private metadata: metadata) {}
 
     toggleOpen(){
-        this.isOpen = !this.isOpen;
+        this.isopen = !this.isopen;
     }
 
 }
