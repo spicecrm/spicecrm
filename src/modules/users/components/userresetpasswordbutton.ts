9f3f5783aa09078e8a48603c252cb7f8aaba804e
Christian Knoll <christian.g.knoll@gmail.com>
2018-11-12 17:08:48 +0100

    rework actionsets buttons

--- src/modules/users/components/userresetpasswordbutton.ts
+++ src/modules/users/components/userresetpasswordbutton.ts
@@ -10,12 +10,18 @@ import {language} from "../../../services/language.service";
 
 export class UserResetPasswordButton {
 
-    constructor(private modal: modal, private language: language, private model: model, private session: session) {}
+    public disabled: boolean = true;
 
-    private resetPassword() {
-        if (!this.session.isAdmin) {return;}
+    constructor(private modal: modal, private language: language, private model: model, private session: session) {
+        if (this.session.isAdmin) this.disabled = false;
+    }
+
+    private execute() {
+        if (!this.session.isAdmin) {
+            return;
+        }
         this.modal.openModal("UserResetPasswordModal")
-            .subscribe( modalRef => {
+            .subscribe(modalRef => {
                 modalRef.instance.userId = this.model.id;
             });
     }
