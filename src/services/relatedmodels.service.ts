173435c656d1481b70ada4056f226cc292286dd2
Christian Knoll <christian.g.knoll@gmail.com>
2018-11-26 00:08:26 +0100

    module filters

--- src/services/relatedmodels.service.ts
+++ src/services/relatedmodels.service.ts
@@ -18,6 +18,7 @@ export class relatedmodels {
     public module: string = "";
     public relatedModule: string = "";
     public linkName: string = "";
+    public modulefilter: string = "";
     public id: string = "";
     public items: any = [];
     public items$ = new EventEmitter();
@@ -134,6 +135,7 @@ export class relatedmodels {
             getcount: true,
             offset: 0,
             limit: this.loaditems,
+            modulefilter: this.modulefilter,
             relationshipFields: JSON.stringify(this.relationshipFields),
             sort: this.sort.sortfield ? JSON.stringify(this.sort) : ""
         };
