c20818e65cdf8fbaa27424b1a8d1ede85ddd23eb
Christian Knoll <christian.g.knoll@gmail.com>
2018-12-05 13:10:04 +0100

    asterisk and Phone Components

--- src/globalcomponents/components/globaldockedcomposercall.ts
+++ src/globalcomponents/components/globaldockedcomposercall.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/core';
 import {dockedComposer} from '../../services/dockedcomposer.service';
 import {language} from '../../services/language.service';
-import {model} from '../../services/model.service';
+import {backend} from '../../services/backend.service';
 import {view} from '../../services/view.service';
 import {metadata} from '../../services/metadata.service';
 import {modal} from '../../services/modal.service';
@@ -16,17 +16,34 @@ import {modal} from '../../services/modal.service';
     selector: 'global-docked-composer-call',
     templateUrl: './src/globalcomponents/templates/globaldockedcomposercall.html'
 })
-export class GlobalDockedComposerCall {
+export class GlobalDockedComposerCall implements OnInit {
 
     @ViewChild('containercontent', {read: ViewContainerRef}) private containercontent: ViewContainerRef;
 
     @Input() public calldata: any = {};
 
+    private searching: boolean = true;
+    private contact: any = {};
+
 
     private isClosed: boolean = false;
 
-    constructor(private metadata: metadata, private dockedComposer: dockedComposer, private language: language, private ViewContainerRef: ViewContainerRef) {
+    constructor(private backend: backend, private dockedComposer: dockedComposer, private language: language, private ViewContainerRef: ViewContainerRef) {
 
+    }
+
+    public ngOnInit() {
+        this.backend.postRequest('search', {}, {
+            modules: 'Contacts',
+            searchterm: this.calldata.callnumber
+        }).subscribe(results => {
+            try {
+                this.contact = results.Contacts.hits[0]._source;
+                this.searching = false;
+            } catch (err) {
+                this.searching = false;
+            }
+        });
     }
 
     private closeComposer() {
