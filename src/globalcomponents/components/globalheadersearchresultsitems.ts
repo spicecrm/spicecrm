fe2ed0da7a2e99a56c3320f9e72c154be1cae722
Christian Knoll <christian.g.knoll@gmail.com>
2018-12-06 11:11:32 +0100

    asterisk

--- src/globalcomponents/components/globalheadersearchresultsitems.ts
+++ src/globalcomponents/components/globalheadersearchresultsitems.ts
@@ -1,25 +1,33 @@
 /**
  * Created by christian on 08.11.2016.
  */
-import {AfterViewInit, ComponentFactoryResolver, Component, Input, NgModule, ViewChild, ViewContainerRef} from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { fts } from '../../services/fts.service';
-import { popup } from '../../services/popup.service';
-import { language } from '../../services/language.service';
-import { broadcast } from '../../services/broadcast.service';
+import {
+    AfterViewInit,
+    ComponentFactoryResolver,
+    Component,
+    Input,
+    NgModule,
+    ViewChild,
+    ViewContainerRef
+} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {fts} from '../../services/fts.service';
+import {popup} from '../../services/popup.service';
+import {language} from '../../services/language.service';
+import {broadcast} from '../../services/broadcast.service';
 
 @Component({
     selector: 'global-header-search-results-items',
     templateUrl: './src/globalcomponents/templates/globalheadersearchresultsitems.html'
 })
 export class GlobalHeaderSearchResultsItems {
-    @Input() searchTerm: string = '';
+    @Input() private searchTerm: string = '';
 
-    constructor(private broadcast: broadcast, private fts: fts, private popup: popup, private router: Router, private language: language){
+    constructor(private broadcast: broadcast, private fts: fts, private popup: popup, private router: Router, private language: language) {
 
     }
 
-    goSearch(){
+    private goSearch() {
         // set the searchterm .. the timeout might not have gotten it
         this.fts.searchTerm = this.searchTerm;
 
