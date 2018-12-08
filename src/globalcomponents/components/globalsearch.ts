40f476ccd2b290b406459579d7222dd85d6919f2
Christian Knoll <christian.g.knoll@gmail.com>
2018-12-08 14:16:24 +0100

    Global Search Update CR 1000102

--- src/globalcomponents/components/globalsearch.ts
+++ src/globalcomponents/components/globalsearch.ts
@@ -1,45 +1,39 @@
-import {ElementRef, Component, NgModule, ViewChild, ViewContainerRef} from '@angular/core';
-import {Router} from '@angular/router';
+import {ElementRef, Component, NgModule, ViewChild, ViewContainerRef, OnDestroy} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
 import {fts} from '../../services/fts.service';
 import {language} from '../../services/language.service';
-import {broadcast} from '../../services/broadcast.service';
 import {navigation} from '../../services/navigation.service';
 
 @Component({
     selector: 'global-search',
-    templateUrl: './src/globalcomponents/templates/globalsearch.html'
+    templateUrl: './src/globalcomponents/templates/globalsearch.html',
+    providers: [fts]
 })
-export class GlobalSearch {
+export class GlobalSearch implements OnDestroy {
 
     private searchScope: string = '*';
     private searchTimeOut: any = undefined;
     private searchTerm: string = '';
-    private searchTermUntrimmed: string = '';
+    private routeSubscription: any;
 
-    constructor(navigation: navigation, private broadcast: broadcast, private elementref: ElementRef, router: Router, private fts: fts, private language: language) {
-        // set the navigation
-        navigation.setActiveModule('search', 'search: ' + fts.searchTerm);
+    constructor(navigation: navigation, private elementref: ElementRef, router: Router, private activatedRoute: ActivatedRoute, private fts: fts, private language: language) {
+        this.routeSubscription = this.activatedRoute.params.subscribe(params => {
+            if (params.searchterm) {
+                // try to base 64 decode .. but can also be plain string
+                try {
+                    this.searchTerm = atob(params.searchterm);
+                } catch (e) {
+                    this.searchTerm = params.searchterm;
+                }
 
-        // set the searchterm
-        this.searchTerm = fts.searchTerm;
-
-        // start the general search
-        this.fts.searchByModules(fts.searchTerm);
-
-        // subscribe to the broadcast message
-        this.broadcast.message$.subscribe(message => this.handleMessage(message));
+                this.doSearch();
+                navigation.setActiveModule('search', 'search: ' + this.searchTerm);
+            }
+        });
     }
 
-    private handleMessage(message): void {
-        switch (message.messagetype) {
-            case 'fts.search':
-                this.searchTerm = message.messagedata;
-                this.doSearch();
-                break;
-            case 'fts.setscope':
-                this.setSearchScope(message.messagedata);
-                break;
-        }
+    public ngOnDestroy(): void {
+        this.routeSubscription.unsubscribe();
     }
 
     private search(_e) {
@@ -47,8 +41,8 @@ export class GlobalSearch {
         switch (_e.key) {
             case 'Enter':
                 if (this.searchTimeOut) window.clearTimeout(this.searchTimeOut);
-                // set the searchterm .. the timeout might not have gotten it
                 this.fts.searchTerm = this.searchTerm;
+                this.doSearch();
                 break;
             default:
                 if (this.searchTimeOut) window.clearTimeout(this.searchTimeOut);
