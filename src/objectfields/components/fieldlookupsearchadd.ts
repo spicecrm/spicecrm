ea1a76dd17e3e6e2568f53ff9172b64bd434bbd4
strombo <andreas@gloeckl.name>
2018-12-15 14:28:43 +0100

    fix: adding record via fieldlookup returns id of new record (and going to record is prevented)

--- src/objectfields/components/fieldlookupsearchadd.ts
+++ src/objectfields/components/fieldlookupsearchadd.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { model } from '../../services/model.service';
 import { language } from '../../services/language.service';
 import { popup } from '../../services/popup.service';
