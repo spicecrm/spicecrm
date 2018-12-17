b50413f2acb488ec8f30898ab103d24d5c648c9f
Christian Knoll <christian.g.knoll@gmail.com>
2018-12-17 11:03:36 +0100

    Angular 7.1.3 update

--- vendor/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js
+++ vendor/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.1
+ * @license Angular v7.1.3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -495,7 +495,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.0.1');
+    var VERSION = new core.Version('7.1.3');
 
     /**
      * @license
