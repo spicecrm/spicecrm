4fbef9fa8e468a8d3e959919bccb343d366dd383
Christian Knoll <christian.g.knoll@gmail.com>
2019-01-02 16:19:04 +0100

    no records and no data visualization

---
+++ src/systemcomponents/components/systemillustrationnorecords.ts
@@ -0,0 +1,8 @@
+import {Component, Input} from '@angular/core';
+
+
+@Component({
+    selector: 'system-illustration-no-records',
+    templateUrl: './src/systemcomponents/templates/systemillustrationnorecords.html'
+})
+export class SystemIllustrationNoRecords {}
