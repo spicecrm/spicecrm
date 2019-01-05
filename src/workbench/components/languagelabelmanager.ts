6306e85da4734aefe390163636b2d348339d7b28
Christian Knoll <christian.g.knoll@gmail.com>
2019-01-05 07:30:01 +0100

    system component for label selector

--- src/workbench/components/languagelabelmanager.ts
+++ src/workbench/components/languagelabelmanager.ts
@@ -7,25 +7,26 @@ import {metadata} from '../../services/metadata.service';
 import {language} from '../../services/language.service';
 import {toast} from "../../services/toast.service";
 import {footer} from "../../services/footer.service";
-import { modal } from '../../services/modal.service';
+import {modal} from '../../services/modal.service';
 
 @Component({
     selector: 'language-label-manager',
     templateUrl: './src/workbench/templates/languagelabelmanager.html',
 })
-export class LanguageLabelManagerComponent
-{
+export class LanguageLabelManagerComponent {
     public search_term: string = '';
     public labels = [];
     public languages = [];
     private _selected_label = null;
-    get selected_label() { return this._selected_label; }
-    set selected_label(val)
-    {
+    get selected_label() {
+        return this._selected_label;
+    }
+
+    set selected_label(val) {
         this._selected_label = val;
-        if(val) {
+        if (val) {
             // if no translation is found for the current translation scope, find the right one...
-            if(val[this.translation_scope + '_translations'].length == 0) {
+            if (val[this.translation_scope + '_translations'].length == 0) {
                 for (let scope of this.scopes) {
                     if (val[scope + '_translations'].length > 0) {
                         this.translation_scope = scope;
@@ -33,20 +34,21 @@ export class LanguageLabelManagerComponent
                     }
                 }
                 val[this.translation_scope + '_translations'].sort(
-                    (a,b) => {
-                        if(a.syslanguage == this.language.languagedata.languages.default || a.syslanguage < b.syslanguage)
+                    (a, b) => {
+                        if (a.syslanguage == this.language.languagedata.languages.default || a.syslanguage < b.syslanguage)
                             return -1;
-                        else if(a.syslanguage > b.syslanguage)
+                        else if (a.syslanguage > b.syslanguage)
                             return +1;
                     }
                 );
             }
         }
     }
+
     public is_searching = false;
     public page = 1;
     public translation_scope = 'global';
-    public readonly scopes = ['global','custom'];
+    public readonly scopes = ['global', 'custom'];
 
     constructor(
         private backend: backend,
@@ -56,7 +58,7 @@ export class LanguageLabelManagerComponent
         private toast: toast,
         private footer: footer,
         private modalservice: modal
-    ){
+    ) {
         this.languages = this.language.getAvialableLanguages();
     }
 
@@ -64,22 +66,20 @@ export class LanguageLabelManagerComponent
         this.modalservice.openModal('LanguageLabelReferenceConfigModal');
     }
 
-    get translations()
-    {
+    get translations() {
         return this.selected_label[`${this.translation_scope}_translations`];
     }
 
-    public search(search_term = null)
-    {
+    public search(search_term = null) {
         this.page = 1;
-        if(!search_term)
+        if (!search_term)
             search_term = this.search_term;
-        if(!search_term)
+        if (!search_term)
             return false;
 
         this.selected_label = null;
         this.is_searching = true;
-        this.backend.getRequest('syslanguages/labels/search/'+this.search_term).subscribe(
+        this.backend.getRequest('syslanguages/labels/search/' + this.search_term).subscribe(
             (res) => {
                 this.labels = res;
                 this.is_searching = false;
@@ -87,42 +87,38 @@ export class LanguageLabelManagerComponent
         );
     }
 
-    public addTranslation(scope: string, language_name: string = null)
-    {
-        if( !this.selected_label[scope+'_translations'] )
-            this.selected_label[scope+'_translations'] = [];
+    public addTranslation(scope: string, language_name: string = null) {
+        if (!this.selected_label[scope + '_translations'])
+            this.selected_label[scope + '_translations'] = [];
 
-        if(!language_name)
-        {
+        if (!language_name) {
             let langs = this.getMissingLanguages(scope);
             language_name = langs[0];
         }
 
-        this.selected_label[scope+'_translations'].push({
+        this.selected_label[scope + '_translations'].push({
             id: this.utils.generateGuid(),
             syslanguagelabel_id: this.selected_label.id,
             syslanguage: language_name,
         });
     }
 
-    public getMissingLanguages(scope: string = null): any[]
-    {
-        if(!scope)
+    public getMissingLanguages(scope: string = null): any[] {
+        if (!scope)
             scope = this.translation_scope;
 
         let missing_langs = [];
-        for(let lang of this.languages)
-        {
-            if( !this.translations.find((e) => {return e.syslanguage == lang.language}) )
-            {
+        for (let lang of this.languages) {
+            if (!this.translations.find((e) => {
+                return e.syslanguage == lang.language
+            })) {
                 missing_langs.push(lang);
             }
         }
         return missing_langs;
     }
 
-    public addLabel()
-    {
+    public addLabel() {
         let label = {
             id: this.utils.generateGuid(),
             name: '',
@@ -134,7 +130,6 @@ export class LanguageLabelManagerComponent
             {
                 id: this.utils.generateGuid(),
                 syslanguagelabel_id: label.id,
-                //syslanguage: this.language.currentlanguage,
                 syslanguage: this.language.languagedata.languages.default,
             }
         );
@@ -142,26 +137,25 @@ export class LanguageLabelManagerComponent
         this.selected_label = label;
         this.labels.push(label);
 
-        this.modalservice.openModal('LanguageLabelModal').subscribe( modal => {
+        this.modalservice.openModal('LanguageLabelModal').subscribe(modal => {
             modal.instance.label = this.selected_label;
-            modal.instance.label$.subscribe( value => {
-               this.selected_label = value;
+            modal.instance.label$.subscribe(value => {
+                this.selected_label = value;
             });
-            modal.instance.close$.subscribe( event => { this.closeModal(event); });
+            modal.instance.close$.subscribe(event => {
+                this.closeModal(event);
+            });
         });
     }
 
-    public deleteLabel(label)
-    {
-        this.modalservice.confirm( this.language.getLabel('LBL_DELETE_LABEL_TEXT'), this.language.getLabel('LBL_DELETE_LABEL_TITLE')).subscribe( ( decision ) => {
+    public deleteLabel(label) {
+        this.modalservice.confirm(this.language.getLabel('LBL_DELETE_LABEL_TEXT'), this.language.getLabel('LBL_DELETE_LABEL_TITLE')).subscribe((decision) => {
             if (decision) {
-                this.backend.deleteRequest('/syslanguages/labels/'+label.id+'/'+label.source).subscribe(
+                this.backend.deleteRequest('/syslanguages/labels/' + label.id + '/' + label.source).subscribe(
                     (res) => {
-                        for(let i = 0; i < this.labels.length; i++)
-                        {
+                        for (let i = 0; i < this.labels.length; i++) {
                             let lbl = this.labels[i];
-                            if(lbl.id == label.id)
-                            {
+                            if (lbl.id == label.id) {
                                 this.labels.splice(i, 1);
                             }
                         }
@@ -172,23 +166,37 @@ export class LanguageLabelManagerComponent
         });
     }
 
-    public save()
-    {
-        //this.backend.postRequest('/syslanguages/labels', null, this.labels).subscribe(
+    public save() {
         this.backend.postRequest('/syslanguages/labels', null, [this.selected_label]).subscribe(
             (res) => {
-                //console.log(res);
                 // reload labels to show up in the application...
-                this.language.loadLanguage();
+                // this.language.loadLanguage();
+                // add/update to language service
+                let currenttranslation = this.getcurrentLanguageTranslations();
+                this.language.addLabel(this.selected_label.name, currenttranslation.default, currenttranslation.short, currenttranslation.long);
+
                 this.toast.sendToast('Label saved!');
             }
         );
     }
 
-    public closeModal(event)
-    {
-        switch(event)
-        {
+
+    private getcurrentLanguageTranslations() {
+        let translations = this.translations;
+        let currenttranslation = {default: '', short: '', long: ''};
+        translations.some(translation => {
+            if (translation.syslanguage == this.language.currentlanguage) {
+                currenttranslation.default = translation.translation_default;
+                currenttranslation.short = translation.translation_short;
+                currenttranslation.long = translation.translation_long;
+                return true;
+            }
+        });
+        return currenttranslation;
+    }
+
+    public closeModal(event) {
+        switch (event) {
             case 'cancel':
                 // remove empty or selected label from results...
                 for (let i = 0; i < this.labels.length; i++) {
@@ -205,11 +213,10 @@ export class LanguageLabelManagerComponent
 
     }
 
-    public sortTranslations(a,b)
-    {
-        if(a.syslanguage == this.language.languagedata.languages.default || a.syslanguage < b.syslanguage)
+    public sortTranslations(a, b) {
+        if (a.syslanguage == this.language.languagedata.languages.default || a.syslanguage < b.syslanguage)
             return -1;
-        else if(a.syslanguage > b.syslanguage)
+        else if (a.syslanguage > b.syslanguage)
             return +1;
         else
             return 0;
@@ -221,7 +228,7 @@ export class LanguageLabelManagerComponent
      * @param language
      * @returns {any}
      */
-    public getLangText(language){
+    public getLangText(language) {
         return this.language.getLangText(language);
     }
 
@@ -232,7 +239,7 @@ export class LanguageLabelManagerComponent
     name: 'sort'
 })
 export class SortPipe implements PipeTransform {
-    public transform(ary: any, fn: Function = (a,b) => a > b ? 1 : -1): any {
+    public transform(ary: any, fn: Function = (a, b) => a > b ? 1 : -1): any {
         return ary.sort(fn)
     }
 }
