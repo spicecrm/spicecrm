16aa86ff45b80450c53424dc2e9a808ae01bd639
Christian Knoll <christian.g.knoll@gmail.com>
2019-01-06 22:15:41 +0100

    acl manager

--- src/modules/acl/components/aclprofilesmanagerprofile.ts
+++ src/modules/acl/components/aclprofilesmanagerprofile.ts
@@ -11,23 +11,48 @@ import {backend} from '../../../services/backend.service';
 
 @Component({
     selector: 'aclprofiles-manager-profile',
-    templateUrl: './src/modules/acl/templates/aclprofilesmanagerprofile.html',
+    templateUrl: './src/modules/acl/templates/aclprofilesmanagerprofile.html'
 })
 export class ACLProfilesManagerProfile implements OnChanges {
 
+    @Input() private profile: any;
     @Input() private profileid: string = '';
     private loaded: boolean = false;
     private loadingobjects: boolean = false;
     private loadingusers: boolean = false;
     private activetab: string = 'profiles';
 
-    @ViewChild('header', {read: ViewContainerRef}) private header: ViewContainerRef;
-
     private aclobjects: any[] = [];
     private aclusers: any[] = [];
 
     constructor(private modal: modal, private language: language, private backend: backend) {
 
+    }
+
+    get aclallusers() {
+        let allusers = this.aclusers.filter(acluser => acluser.id == '*');
+        return allusers.length > 0;
+    }
+
+    set aclallusers(value) {
+        if (value) {
+            this.backend.postRequest('spiceaclprofiles/' + this.profileid + '/aclusers', {}, {userids: ['*']}).subscribe(status => {
+                this.aclusers.push({
+                    id: '*',
+                    user_name: ''
+                });
+            });
+        } else {
+            this.backend.deleteRequest('spiceaclprofiles/' + this.profileid + '/aclusers/*').subscribe(status => {
+                let i = 0;
+                for (let aclusers of this.aclusers) {
+                    if (aclusers.id == '*') {
+                        this.aclusers.splice(i, 1);
+                    }
+                    i++;
+                }
+            });
+        }
     }
 
     public ngOnChanges() {
@@ -40,11 +65,13 @@ export class ACLProfilesManagerProfile implements OnChanges {
             this.backend.getRequest('spiceaclprofiles/' + this.profileid + '/aclobjects').subscribe(aclobjects => {
                 this.aclobjects = aclobjects;
                 this.loadingobjects = false;
+                this.sortobjects();
             });
 
             this.backend.getRequest('spiceaclprofiles/' + this.profileid + '/aclusers').subscribe(aclusers => {
                 this.aclusers = aclusers;
                 this.loadingusers = false;
+                this.sortusers();
             });
         }
     }
@@ -54,6 +81,7 @@ export class ACLProfilesManagerProfile implements OnChanges {
             modalRef.instance.aclobject.subscribe(aclobject => {
                 this.backend.postRequest('spiceaclprofiles/' + this.profileid + '/aclobjects/' + aclobject.id).subscribe(status => {
                     this.aclobjects.push(aclobject);
+                    this.sortobjects();
                 });
             });
         });
@@ -72,6 +100,13 @@ export class ACLProfilesManagerProfile implements OnChanges {
         });
     }
 
+    private save() {
+        this.backend.save('SpiceACLProfiles', this.profileid, {
+            name: this.profile.name,
+            description: this.profile.description
+        });
+    }
+
     private removeUser(userId) {
         this.backend.deleteRequest('spiceaclprofiles/' + this.profileid + '/aclusers/' + userId).subscribe(status => {
             let i = 0;
@@ -86,14 +121,42 @@ export class ACLProfilesManagerProfile implements OnChanges {
     }
 
     private selectUser() {
-        /*
-        this.modal.openModal('ACLProfilesManagerAddObjectModal').subscribe(modalRef => {
-            modalRef.instance.aclobject.subscribe(aclobject => {
-                this.backend.postRequest('spiceaclprofiles/'+this.profileid+'/aclobjects/'+aclobject.id).subscribe(status => {
-                    this.aclobjects.push(aclobject);
-                });
+        this.modal.openModal('ObjectModalModuleLookup').subscribe(selectModal => {
+            selectModal.instance.module = 'Users';
+            selectModal.instance.multiselect = true;
+            selectModal.instance.selectedItems.subscribe(items => {
+                if (items.length) {
+                    let newusers = [];
+                    items.forEach(user => {
+                        newusers.push(user.id);
+                    });
+                    this.backend.postRequest('spiceaclprofiles/' + this.profileid + '/aclusers', {}, {userids: newusers}).subscribe(status => {
+                        items.forEach(user => {
+                            this.aclusers.push({
+                                id: user.id,
+                                user_name: user.user_name
+                            });
+                        });
+                        this.sortusers();
+                    });
+
+                }
             });
         });
-        */
+    }
+
+    private sortobjects() {
+        this.aclobjects.sort((a, b) => {
+            return a.name > b.name ? 1 : -1;
+        });
+    }
+
+    private sortusers() {
+        this.aclusers.sort((a, b) => {
+            if (a.id == '*') return -1;
+            if (b.id == '*') return 1;
+
+            return a.user_name > b.user_name ? 1 : -1;
+        });
     }
 }
