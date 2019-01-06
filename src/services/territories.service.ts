3af4706f1a62466292b7235e0cb5d79c325131f7
Christian Knoll <christian.g.knoll@gmail.com>
2019-01-06 22:16:15 +0100

    territory management fields

--- src/services/territories.service.ts
+++ src/services/territories.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpResponse} from "@angular/common/http";
 import {Subject, Observable} from 'rxjs';
-import {CanActivate}    from '@angular/router';
+import {CanActivate} from '@angular/router';
 
 import {configurationService} from './configuration.service';
 import {session} from './session.service';
@@ -11,15 +11,15 @@ import {backend} from './backend.service';
 @Injectable()
 export class territories {
 
-    userTerritories: any = {};
-    addTerritories: any = {};
+    private userTerritories: any = {};
+    private addTerritories: any = {};
 
     constructor(private backend: backend, private metadata: metadata, private configurationService: configurationService, private session: session) {
 
     }
 
-    getTerritories(loadhandler: Subject<string>) {
-        if (sessionStorage[window.btoa('territorries'+this.session.authData.sessionId)] && sessionStorage[window.btoa('territorries'+this.session.authData.sessionId)].length > 0 && !this.configurationService.data.developerMode) {
+    public getTerritories(loadhandler: Subject<string>) {
+        if (sessionStorage[window.btoa('territorries' + this.session.authData.sessionId)] && sessionStorage[window.btoa('territorries' + this.session.authData.sessionId)].length > 0 && !this.configurationService.data.developerMode) {
             this.userTerritories = this.session.getSessionData('territorries');
             loadhandler.next('getTerritorries');
         } else {
@@ -29,18 +29,19 @@ export class territories {
         }
     }
 
-    loadTerritories() {
+    public loadTerritories() {
         let retSubject = new Subject();
 
-        let modules: Array<String> = [];
+        let modules: string[] = [];
         for (let module of this.metadata.getModules()) {
-            if (module !== 'Home')
+            if (module !== 'Home') {
                 modules.push(module);
+            }
         }
 
         this.backend.getRequest('territories')
             .subscribe(response => {
-                this.session.setSessionData('territorries',response);
+                this.session.setSessionData('territorries', response);
                 this.userTerritories = response;
 
                 retSubject.next(true);
@@ -50,57 +51,58 @@ export class territories {
         return retSubject.asObservable();
     }
 
-    loadTerritoryName(territory) {
+    public loadTerritoryName(territory) {
         this.addTerritories[territory] = '...';
         this.backend.getRequest('territories/' + territory)
             .subscribe((response: any) => {
-                if (response.id === territory)
+                if (response.id === territory) {
                     this.addTerritories[territory] = response.name;
-                else
+                } else {
                     this.addTerritories[territory] = 'error';
+                }
             });
     }
 
-    searchTerritories(module, searchterm, items = 5, activeterritories = []){
+    public searchTerritories(module, searchterm, items = 5, activeterritories = []) {
         let retArray = [];
 
-        for(let territory of this.userTerritories[module]){
-            if(territory.name.indexOf(searchterm) >= 0 && activeterritories.indexOf(territory.id) < 0){
+        for (let territory of this.userTerritories[module]) {
+            if (territory.name.toLowerCase().indexOf(searchterm.toLowerCase()) >= 0 && activeterritories.indexOf(territory.id) < 0) {
                 retArray.push(territory);
             }
 
             // check if we did reach the number we are looking for
-            if(retArray.length >= items){
+            if (retArray.length >= items) {
                 return retArray;
             }
         }
 
-        return retArray
+        return retArray;
     }
 
-    isUserTerritory(module, territory){
+    public isUserTerritory(module, territory) {
         let userTerritory: boolean = false;
         this.userTerritories[module].some(thisTerritorry => {
             if (thisTerritorry.id === territory) {
                 userTerritory = true;
                 return true;
             }
-        })
+        });
 
         return userTerritory;
     }
 
-    getTerritoryName(module, territory) {
+    public getTerritoryName(module, territory) {
         let territoryName = '';
         this.userTerritories[module].some(thisTerritorry => {
             if (thisTerritorry.id === territory) {
                 territoryName = thisTerritorry.name;
                 return true;
             }
-        })
+        });
 
         if (territoryName === '') {
-            if (!this.addTerritories[territory]){
+            if (!this.addTerritories[territory]) {
                 this.loadTerritoryName(territory);
             }
             territoryName = this.addTerritories[territory];
