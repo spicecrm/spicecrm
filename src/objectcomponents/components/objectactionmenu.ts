3e22d62de8225634a13742cb8f0c762c1dd54455
strombo <andreas@gloeckl.name>
2019-01-09 16:21:20 +0100

    action item "open" for object-list-item

--- src/objectcomponents/components/objectactionmenu.ts
+++ src/objectcomponents/components/objectactionmenu.ts
@@ -116,6 +116,10 @@ export class ObjectActionMenu implements OnDestroy {
         })
     }
 
+    private viewModel() {
+        this.model.goDetail();
+    }
+
     doCustomAction(action) {
         this.action.emit(action);
         this.isOpen = false;
