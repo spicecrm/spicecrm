e7da8e52ff8766f62f700e2f469b1c7fd49240d8
MehyarSawas <mehyar.sawas@outlook.com>
2019-01-11 13:04:38 +0100

    Schedulers: UI Platform built.

---
+++ src/admincomponents/components/administrationschedulerrunbutton.ts
@@ -0,0 +1,44 @@
+import {Component} from '@angular/core';
+import {metadata} from '../../services/metadata.service';
+import {language} from '../../services/language.service';
+import {model} from "../../services/model.service";
+import {backend} from "../../services/backend.service";
+import {modal} from "../../services/modal.service";
+import {toast} from "../../services/toast.service";
+import {broadcast} from "../../services/broadcast.service";
+
+declare var _;
+
+@Component({
+    selector: 'administration-scheduler-run-button',
+    templateUrl: './src/admincomponents/templates/administrationschedulerrunbutton.html'
+})
+export class AdministrationSchedulerRunButton {
+
+    constructor(public model: model,
+                public language: language,
+                public metadata: metadata,
+                public broadcast: broadcast,
+                public toast: toast,
+                public modal: modal,
+                public backend: backend) {
+    }
+
+    public execute() {
+        this.modal.openModal('SystemLoadingModal', false).subscribe(modalRef => {
+            this.backend.postRequest('module/Scheduler/'+ this.model.id +'/runjob').subscribe(res => {
+                modalRef.instance.self.destroy();
+                if (res) {
+                    this.toast.sendToast(this.language.getLabel('MSG_SUCCESSFULLY_EXECUTED'), 'success');
+                } else {
+                    this.toast.sendToast(this.language.getLabel('ERR_FAILED_TO_EXECUTE'), 'error');
+                }
+                this.broadcast.broadcastMessage('scheduler.run');
+            }, err => {
+                modalRef.instance.self.destroy();
+                this.toast.sendToast(this.language.getLabel('ERR_FAILED_TO_EXECUTE'), 'error');
+                this.broadcast.broadcastMessage('scheduler.run');
+            });
+        });
+    }
+}
