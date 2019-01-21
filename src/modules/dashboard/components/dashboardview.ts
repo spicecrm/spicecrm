7a375b4000e81babc22708f27022bc28efedab36
MehyarSawas <mehyar.sawas@outlook.com>
2019-01-21 10:26:48 +0100

    Dashboards: Dashboards module name corrected.

--- src/modules/dashboard/components/dashboardview.ts
+++ src/modules/dashboard/components/dashboardview.ts
@@ -41,7 +41,7 @@ export class DashboardView implements OnInit {
             }
         });
 
-        this.navigation.setActiveModule('Dashbords');
+        this.navigation.setActiveModule('Dashboards');
     }
 
     get ismobile() {
