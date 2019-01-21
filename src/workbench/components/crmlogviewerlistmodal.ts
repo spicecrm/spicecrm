9a18ca9d0512ade9cd30e21a7cd94a492dafffeb
strombo <andreas@gloeckl.name>
2019-01-21 09:52:31 +0100

    CR1000109: KREST Log Viewer CR1000095: CRM Log Viewer

---
+++ src/workbench/components/crmlogviewerlistmodal.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { language } from '../../services/language.service';
+
+@Component({
+    templateUrl: './src/workbench/templates/crmlogviewerlistmodal.html',
+})
+export class CRMLogViewerListModal {
+
+    @Input() filter: any;
+    private self;
+
+    constructor( private language: language ) { }
+
+    // Close the modal.
+    private closeModal() {
+        this.self.destroy();
+    }
+
+    // Escape pressed or [x] clicked.
+    public onModalEscX() {
+        this.closeModal();
+    }
+
+}
