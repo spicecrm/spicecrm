f32aab14451b5a9fab07109d676a8db48b0c8d56
MehyarSawas <mehyar.sawas@outlook.com>
2019-02-07 11:40:00 +0100

    Dashboards: Cleaned up. trackByFn added.

--- src/modules/dashboard/components/dashboardaddelement.ts
+++ src/modules/dashboard/components/dashboardaddelement.ts
@@ -13,17 +13,16 @@ import {metadata} from '../../../services/metadata.service';
 })
 export class DashboardAddElement {
 
-    dashboardDashlets: Array<any> = [];
-    kreports: Array<any> = [];
-    dashletName: string = '';
-    dashletComponent: string = '';
-    reportFilterKey: string = '';
-    dashlettype: string = 'Generic';
-    dashletmodule: string = '*';
-    isLoading: boolean = false;
-    self: any = {};
-
-    addDashlet: EventEmitter<any> = new EventEmitter<any>();
+    public self: any = {};
+    private dashboardDashlets: Array<any> = [];
+    private kreports: Array<any> = [];
+    private dashletName: string = '';
+    private dashletComponent: string = '';
+    private reportFilterKey: string = '';
+    private dashlettype: string = 'Generic';
+    private dashletmodule: string = '*';
+    private isLoading: boolean = false;
+    private addDashlet: EventEmitter<any> = new EventEmitter<any>();
 
     constructor(
         private language: language,
@@ -32,9 +31,10 @@ export class DashboardAddElement {
         private recent: recent,
         private metadata: metadata,
         private backend: backend
-    ) {}
+    ) {
+    }
 
-    get noData(){
+    get noData() {
         let noData = false;
         if (this.getType() == 'Generic' && this.dashboardDashletsFiltered.length == 0)
             noData = true;
@@ -53,22 +53,6 @@ export class DashboardAddElement {
         this.dashlettype = value;
         if (this.getType() == 'Reporter' && this.noData) this.getKreports();
         if (this.getType() == 'Generic' && this.noData) this.getDashlets();
-    }
-
-    getDashlets(){
-        this.isLoading = true;
-        this.backend.getRequest('dashboards/dashlets').subscribe((dashboardDashlets: any) => {
-            this.dashboardDashlets = dashboardDashlets;
-            this.isLoading = false;
-        });
-    }
-    getKreports(){
-        this.isLoading = true;
-        let fields = ["id", "name", "description", "report_module", "integration_params"];
-        this.backend.getRequest('module/KReports', {fields: fields}).subscribe((kreports: any) => {
-            this.kreports = kreports.list;
-            this.isLoading = false;
-        });
     }
 
     get dashboardDashletsFiltered(): any[] {
@@ -102,7 +86,7 @@ export class DashboardAddElement {
         if (this.reportFilterKey.length > 0) {
             filteredKreports = filteredKreports.filter(kreport => {
                 return kreport.name.toLowerCase().indexOf(this.reportFilterKey.toLowerCase()) !== -1 ||
-                    kreport.description.toLowerCase().indexOf(this.reportFilterKey.toLowerCase()) !== -1
+                    kreport.description.toLowerCase().indexOf(this.reportFilterKey.toLowerCase()) !== -1;
             });
         }
 
@@ -132,7 +116,7 @@ export class DashboardAddElement {
         this.dashletmodule = value;
     }
 
-    ngOnInit() {
+    public ngOnInit() {
         switch (this.getType()) {
             case 'Reporter':
                 this.dashletType = this.dashletComponent;
@@ -140,19 +124,46 @@ export class DashboardAddElement {
                 break;
             case 'Generic':
                 this.getDashlets();
-            break;
+                break;
         }
     }
 
-    getIcon(icon){
+    private trackByFn(index, item) {
+        return item.id;
+    }
+
+    private trackByFnModule(index, item) {
+        return index;
+    }
+
+    private getDashlets() {
+        this.isLoading = true;
+        this.backend.getRequest('dashboards/dashlets')
+            .subscribe((dashboardDashlets: any) => {
+                this.dashboardDashlets = dashboardDashlets;
+                this.isLoading = false;
+            });
+    }
+
+    private getKreports() {
+        this.isLoading = true;
+        let fields = ["id", "name", "description", "report_module", "integration_params"];
+        this.backend.getRequest('module/KReports', {fields: fields})
+            .subscribe((kreports: any) => {
+                this.kreports = kreports.list;
+                this.isLoading = false;
+            });
+    }
+
+    private getIcon(icon) {
         return (icon && icon.split(':')[1]) ? icon.split(':')[1] : icon;
     }
 
-    getSprite(icon){
+    private getSprite(icon) {
         return (icon && icon.split(':')[1]) ? icon.split(':')[0] : 'standard';
     }
 
-    filterReportsType(type) {
+    private filterReportsType(type) {
         let filteredKreports;
         switch (type) {
             case 'ReporterVisualizationDashlet':
@@ -165,7 +176,7 @@ export class DashboardAddElement {
         return filteredKreports;
     }
 
-    filteredKreports(property) {
+    private filteredKreports(property) {
         let filteredKreports;
         filteredKreports = this.kreports.filter(kreport => {
             if (kreport.integration_params) {
@@ -179,17 +190,17 @@ export class DashboardAddElement {
         return filteredKreports;
     }
 
-    getType() {
+    private getType() {
         return this.dashletType ? this.dashletType.indexOf('Reporter') === 0 ? 'Reporter' : 'Generic' : '';
     }
 
-    close() {
+    private close() {
         this.addDashlet.emit(false);
         this.self.destroy();
 
     }
 
-    save(id) {
+    private save(id) {
 
         if (!id) return false;
         let component = '';
