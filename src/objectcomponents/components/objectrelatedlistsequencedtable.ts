580ada7bbaa4342e623ed7327ec45ada1c297f03
strombo <andreas@gloeckl.name>
2019-02-13 08:59:03 +0100

    bugfix

--- src/objectcomponents/components/objectrelatedlistsequencedtable.ts
+++ src/objectcomponents/components/objectrelatedlistsequencedtable.ts
@@ -6,7 +6,6 @@ import {broadcast} from '../../services/broadcast.service';
 import {metadata} from '../../services/metadata.service';
 import {language} from '../../services/language.service';
 import {layout} from "../../services/layout.service";
-import {Router}   from '@angular/router';
 import {ObjectRelatedlistTable} from './objectrelatedlisttable';
 
 @Component({
@@ -15,39 +14,40 @@ import {ObjectRelatedlistTable} from './objectrelatedlisttable';
 })
 export class ObjectRelatedlistSequencedTable extends ObjectRelatedlistTable {
 
-    @Input() sequencefield: string = 'sequence_number';
+    @Input() private sequencefield: string = 'sequence_number';
 
     constructor(public language: language, public metadata: metadata, public relatedmodels: relatedmodels, public model: model, public layout: layout, private backend: backend, private broadcast: broadcast) {
         super(language, metadata, relatedmodels, model, layout);
     }
 
-    get displayfields(){
+    get displayfields() {
         let displayfields = [];
-        for(let listfield of this.listfields){
-            if(listfield.fieldconfig.default){
+        for ( let listfield of this.listfields ) {
+            if ( listfield.fieldconfig.default ) {
                 displayfields.push(listfield);
             }
         }
         return displayfields;
     }
 
-    dragstart(event, item) {
-        event.dataTransfer.setData("text", item.id)
+    private dragstart( event, item ) {
+        event.dataTransfer.setData( "text", item.id );
     }
 
-    dragover(event, item) {
-        var data = event.dataTransfer.getData("text");
-        if (data != item.id)
-            event.preventDefault();
+    private dragover(event, item) {
+        event.stopPropagation();
+        let data = event.dataTransfer.getData("text");
+        if ( data != item.id ) event.preventDefault();
     }
 
-    drop(event, targetitem) {
-        var sourceID = event.dataTransfer.getData("text");
+    private drop( event, targetitem ) {
+
+        let sourceID = event.dataTransfer.getData("text");
 
         // build an internal array with ids and sequence
         let itemsArray = [];
-        for(let item of this.relatedmodels.items){
-            itemsArray.push({id: item.id, sequence: parseInt(item[this.sequencefield])});
+        for ( let item of this.relatedmodels.items ) {
+            itemsArray.push({id: item.id, sequence: parseInt( item[this.sequencefield], 10 )});
         }
 
         // sort the Array
@@ -58,42 +58,42 @@ export class ObjectRelatedlistSequencedTable extends ObjectRelatedlistTable {
         // get the indexes of the two records
         let sourceitem = {};
         itemsArray.some((item, index) => {
-            if(item.id == sourceID){
+            if ( item.id == sourceID ) {
                 sourceitem = itemsArray.splice(index, 1);
                 return true;
             }
-        })
+        });
 
         // get the current source element
         itemsArray.some((item, index) => {
-            if(item.id == targetitem.id){
+            if ( item.id == targetitem.id ) {
                 itemsArray.splice(index, 0, sourceitem[0]);
                 return true;
             }
-        })
+        });
 
         // get the droptarget and
         let currentIndex = 0; let indexObj = {}; let updateArray = [];
-        for(let item of itemsArray){
+        for ( let item of itemsArray ) {
             indexObj[item.id] = currentIndex;
             updateArray.push({id: item.id, sequence_number: currentIndex});
             currentIndex++;
         }
 
         // transverse array to object
-        for(let item of this.relatedmodels.items){
+        for ( let item of this.relatedmodels.items ) {
             item[this.sequencefield] = indexObj[item.id];
         }
 
         // resort the array
         this.relatedmodels.items.sort((a, b) => {
-            return parseInt(a[this.sequencefield]) > parseInt(b[this.sequencefield]) ? 1 : -1;
-        })
+            return parseInt( a[this.sequencefield], 10 ) > parseInt( b[this.sequencefield], 10 ) ? 1 : -1;
+        });
 
         // send to the backen saving the models
         this.backend.postRequest('/module/'+this.relatedmodels.relatedModule, {}, updateArray).subscribe(updated => {
             console.log('updated models');
-            for(let modeldata of updated){
+            for ( let modeldata of updated ) {
                 this.broadcast.broadcastMessage('model.save', {
                     id: modeldata.id,
                     module: this.relatedmodels.relatedModule,
