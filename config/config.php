338f2e7d0d3efd22fca53b4d8eec06fa18d7687b
Christian Knoll <christian.g.knoll@gmail.com>
2019-02-17 09:40:02 +0100

    outlook plugin adde get xml request from config route

--- config/config.php
+++ config/config.php
@@ -107,6 +107,14 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
                 echo json_encode(array('success' => $success, 'message' => $message));
                 break;
+            case 'outlookxml':
+                header('Content-Type: application/xml' );
+                $dir = dirname(__DIR__);
+                $serverurl = str_replace('/config/outlookxml', '', "{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}");
+                $filepath = "$dir/assets/outlook/spicecrmoutlookplugin.xml";
+                $file = file_get_contents($filepath);
+                echo( str_replace('<serverurl>', $serverurl, $file) );
+                break;
         }
         break;
     case 'POST':
