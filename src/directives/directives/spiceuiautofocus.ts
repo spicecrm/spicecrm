73a10678726a42212364258981e128fbc0845e3b
Christian Knoll <christian.g.knoll@gmail.com>
2019-02-24 14:39:15 +0100

    fix directives

--- src/directives/directives/spiceuiautofocus.ts
+++ src/directives/directives/spiceuiautofocus.ts
@@ -1,21 +1,35 @@
+/**
+ * @module directives
+ */
 import {AfterViewInit, Directive, ElementRef, Input} from '@angular/core';
 
+/**
+ * a directive that can be added to an input element and then will ensure when it is rendered it is focused automatically
+ *
+ * ```html
+ * <input spiceuiautofocus type="search" class="slds-input" [(ngModel)]="searchTerm" [placeholder]="language.getLabel('LBL_SEARCH')">
+ * ```
+ */
 @Directive({
     selector: '[spiceuiautofocus]'
 })
 export class SpiceUIAutofocusDirective implements AfterViewInit {
 
-
+    /**
+     * @ignore
+     */
     constructor(
         private elementRef: ElementRef
     ) {
     }
 
+    /**
+     * @ignore
+     */
     public ngAfterViewInit() {
         setTimeout(() => {
             if (!this.elementRef.nativeElement.tabIndex) this.elementRef.nativeElement.tabIndex = '-1';
             this.elementRef.nativeElement.focus();
         });
     }
-
 }
