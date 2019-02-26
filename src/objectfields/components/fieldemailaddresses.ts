cb8152841b6f6d5760aa167249466538dbaf0453
Christian Knoll <christian.g.knoll@gmail.com>
2019-02-26 21:02:28 +0100

    fixed fields to update using setter function on model

--- src/objectfields/components/fieldemailaddresses.ts
+++ src/objectfields/components/fieldemailaddresses.ts
@@ -21,19 +21,23 @@ export class fieldEmailAddresses extends fieldGeneric implements OnInit {
     }
 
     public ngOnInit() {
-        if (!this.model.data.emailaddresses) {
-            this.model.data.emailaddresses = [{
-                id: this.model.generateGuid(),
-                bean_id: this.model.id,
-                bean_module: this.model.module,
-                email_address: '',
-                email_address_id: '',
-                primary_address: '1'
-            }];
+        if (!this.model.getField('emailaddresses')) {
+            this.model.setField(
+                'emailaddresses',
+                [{
+                    id: this.model.generateGuid(),
+                    bean_id: this.model.id,
+                    bean_module: this.model.module,
+                    email_address: '',
+                    email_address_id: '',
+                    primary_address: '1'
+                }]
+            );
         }
     }
+
     get emailAddresses() {
-        return this.model.data[this.fieldname];
+        return this.model.getField(this.fieldname);
     }
 
     private sendEmail(emailaddress) {
@@ -57,7 +61,7 @@ export class fieldEmailAddresses extends fieldGeneric implements OnInit {
     }
 
     private addEmailAddress() {
-        this.model.data.emailaddresses.push({
+        this.model.getField('emailaddresses').push({
             id: this.model.generateGuid(),
             bean_id: this.model.id,
             bean_module: this.model.module,
@@ -68,7 +72,15 @@ export class fieldEmailAddresses extends fieldGeneric implements OnInit {
     }
 
     private handleOnBlur() {
-        this.model.data.emailaddresses = this.model.data.emailaddresses.filter(emailaddress => emailaddress.email_address != '');
+        // get the email addresses
+        let emailAddresses = this.model.getField('emailaddresses').filter(emailaddress => emailaddress.email_address != '');
+
+        // get the primary email address
+        let email1 = '';
+        let primaryEmailAddress = emailAddresses.find(emailaddress => emailaddress.primary_address == '1');
+        if(primaryEmailAddress) email1 = primaryEmailAddress.email_address;
+
+        this.model.setFields({emailAddresses, email1});
     }
 }
 
