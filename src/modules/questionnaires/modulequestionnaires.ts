c191ae4f800a42a5819ee442fc627bb4cf094120
Christian Knoll <christian.g.knoll@gmail.com>
2019-02-27 19:05:44 +0100

    update path so build process works for questionnaires

--- src/modules/questionnaires/modulequestionnaires.ts
+++ src/modules/questionnaires/modulequestionnaires.ts
@@ -34,7 +34,7 @@ import {ObjectFields}      from '../../objectfields/objectfields';
 import {GlobalComponents}      from '../../globalcomponents/globalcomponents';
 import {ObjectComponents}      from '../../objectcomponents/objectcomponents';
 import {SystemComponents}      from '../../systemcomponents/systemcomponents';
-import {ModuleMediaFiles}      from '../mediafiles/modulemediafiles';
+import {ModuleMediaFiles}      from '../../modules/mediafiles/modulemediafiles';
 
 import /*embed*/ {QuestionsManager} from './components/questionsmanager';
 import /*embed*/ {QuestionsManagerEditMulti} from "./components/questionsmanagereditmulti";
