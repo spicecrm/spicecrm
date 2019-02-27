cc7da59508c850eafa19a69d47f975fb395f9b5f
MehyarSawas <mehyar.sawas@outlook.com>
2019-02-27 15:22:52 +0100

    SPICEUI-210: ObjectActionModal: model and parent provieded.

--- src/objectcomponents/components/objectactionbeantomailbutton.ts
+++ src/objectcomponents/components/objectactionbeantomailbutton.ts
@@ -19,6 +19,7 @@ export class ObjectActionBeanToMailButton {
     }
     // should be inject the parent to the modal???
     public execute() {
-        this.modal.openModal('ObjectActionMailModal', true, this.viewContainerRef.injector);
+        this.modal.openModal('ObjectActionMailModal', true, this.viewContainerRef.injector)
+            .subscribe(ref => ref.instance.parent = this.model);
     }
 }
