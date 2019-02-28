06fb9d09cfdfd08b651927a2d08b9a004a4519f7
MehyarSawas <mehyar.sawas@outlook.com>
2019-02-28 09:27:49 +0100

    Product Groups Manager: Add button added.

--- src/modules/products/components/productgroupmanager.ts
+++ src/modules/products/components/productgroupmanager.ts
@@ -2,6 +2,7 @@ import {Component, ViewChild, ViewContainerRef} from '@angular/core';
 import {language} from '../../../services/language.service';
 import {navigation} from '../../../services/navigation.service';
 import {productfinder} from "../services/productfinder.service";
+import {metadata} from "../../../services/metadata.service";
 
 @Component({
     templateUrl: './src/modules/products/templates/productgroupmanager.html',
@@ -10,19 +11,26 @@ import {productfinder} from "../services/productfinder.service";
 
 export class ProductGroupManager {
 
-    @ViewChild("bodycontainer", {read: ViewContainerRef}) private bodyContainer: ViewContainerRef;
+    @ViewChild("actionSetContainer", {read: ViewContainerRef}) private actionSetContainer: ViewContainerRef;
 
     private selectedGroupId: string;
+    private actionSet: string = '';
 
-    constructor(private language: language, private navigation: navigation) {
+    constructor(private language: language, private navigation: navigation, private metadata: metadata) {
         this.navigation.setActiveModule('ProductGroups');
+        this.getActionSet();
     }
 
-    get bodyStyle() {
-        return {height: `calc(100vh - ${this.bodyContainer.element.nativeElement.offsetHeight}`};
+    get actionSetModel() {
+        return {module: 'ProductGroups'};
     }
 
-    selectionChanged(data) {
+    private getActionSet() {
+        let conf = this.metadata.getComponentConfig('ProductGroupManager', 'ProductGroups');
+        this.actionSet = conf && conf.actionset ? conf.actionset : '';
+    }
+
+    private selectionChanged(data) {
         this.selectedGroupId = data.object.id;
     }
 }
