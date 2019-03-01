785ecc4cf28b80ebe03fb19e0626d77b532d3ef2
Christian Knoll <christian.g.knoll@gmail.com>
2019-03-01 10:53:21 +0100

    update history summary

---
+++ src/objectcomponents/components/objectactivitiytimelinesummaryitemview.ts
@@ -0,0 +1,53 @@
+import {
+    AfterViewInit, ComponentFactoryResolver, Component, NgModule, ViewChild, ViewContainerRef,
+    Input, OnInit, OnDestroy, OnChanges
+} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpResponse} from "@angular/common/http";
+import {ActivatedRoute} from '@angular/router';
+import {metadata} from '../../services/metadata.service';
+import {model} from '../../services/model.service';
+import {language} from '../../services/language.service';
+import {activitiyTimeLineService} from '../../services/activitiytimeline.service';
+
+declare var moment: any;
+
+@Component({
+    selector: 'object-activitiy-timeline-summary-item-view',
+    templateUrl: './src/objectcomponents/templates/objectactivitiytimelinesummaryitemview.html',
+    providers: [model]
+})
+export class ObjectActivitiyTimelineSummaryItemView implements OnChanges{
+    @ViewChild('detailContainer', {read: ViewContainerRef}) private detailContainer: ViewContainerRef;
+
+    @Input() private module: '';
+    @Input() private id: '';
+    @Input() private data: '';
+    private componentRefs: any[] = [];
+
+    constructor(private metadata: metadata, private parent: model, private model: model, private language: language, private activitiyTimeLineService: activitiyTimeLineService, private activatedRoute: ActivatedRoute) {
+
+    }
+
+    public ngOnChanges() {
+        this.model.module = this.module;
+        this.model.id = this.id;
+        this.model.data = this.data;
+
+        for (let component of this.componentRefs) {
+            component.destroy();
+        }
+
+        let componentconfig = this.metadata.getComponentConfig('ObjectActivitiyTimelineSummary', this.module);
+        if (componentconfig && componentconfig.componentsets) {
+            for (let componentSet of componentconfig.componentsets) {
+                for (let view of this.metadata.getComponentSetObjects(componentSet)) {
+                    this.metadata.addComponent(view.component, this.detailContainer).subscribe(componentRef => {
+                        componentRef.instance.componentconfig = view.componentconfig;
+                        this.componentRefs.push(componentRef);
+                    });
+                }
+            }
+        }
+
+    }
+}
