0cc8dae42608e8fad6fa6ef81effa537180d0c8c
Christian Knoll <christian.g.knoll@gmail.com>
2019-03-01 10:51:33 +0100

    logger documentation

--- src/services/logger.service.ts
+++ src/services/logger.service.ts
@@ -1,38 +1,60 @@
-import { Injectable, isDevMode } from '@angular/core';
-import { session } from './session.service';
+/**
+ * @module services
+ */
+import {Injectable, isDevMode} from '@angular/core';
+import {session} from './session.service';
 
+/**
+ * no operation ...
+ */
 const noop = (): any => undefined;
 
+/**
+ * general service for logging
+ */
 @Injectable()
 export class loggerService {
 
     private sessionService: session;
 
+    /**
+     * getter for
+     */
     get info() {
-        if ( this.isDev() ) return console.info.bind( console );
+        if (this.isDev()) return console.info.bind(console);
         else return noop;
     }
 
     get warn() {
-        if ( this.isDev() ) return console.warn.bind( console );
+        if (this.isDev()) return console.warn.bind(console);
         else return noop;
     }
 
     get log() {
-        if ( this.isDev() ) return console.log.bind( console );
+        if (this.isDev()) return console.log.bind(console);
         else return noop;
     }
 
     get error() {
-        if ( this.isDev() ) return console.error.bind( console );
+        if (this.isDev()) return console.error.bind(console);
         else return noop;
     }
 
+    /**
+     * @ignore
+     *
+     * returns if we are in angular in the developer mode
+     */
     private isDev() {
-        return isDevMode() || ( this.sessionService && this.sessionService.isDev );
+        return isDevMode() || (this.sessionService && this.sessionService.isDev);
     }
 
-    public setSession( service ) {
+    /**
+     * set the session service. This is needed because the logger is instantiated before the session service. The sessionservice finds the logger service then and sets the session there.
+     *
+     * @param service the service itself
+     */
+    public setSession(service) {
         this.sessionService = service;
     }
 
