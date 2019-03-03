feb568795094271ef18c642b9f0e94181bcb8e30
Christian Knoll <christian.g.knoll@gmail.com>
2019-03-03 16:42:18 +0100

    language fix for lables, details tab update to uzse system component

--- src/objectfields/components/fieldlabel.ts
+++ src/objectfields/components/fieldlabel.ts
@@ -68,7 +68,7 @@ export class fieldLabel {
         if (this.fieldconfig.label) {
             if (this.fieldconfig.label.indexOf(':') > 0) {
                 let fielddetails = this.fieldconfig.label.split(':');
-                return this.language.getLabel(fielddetails[1], fielddetails[0], this.view.labels)
+                return this.language.getLabel(fielddetails[1], fielddetails[0], this.view.labels);
             } else {
                 return this.language.getLabel(this.fieldconfig.label, this.model.module, this.view.labels)
             }
