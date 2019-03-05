df0f9c9b7b6b4a3ad46cef0aab3efead4d01e6b1
Christian Knoll <christian.g.knoll@gmail.com>
2019-03-05 21:34:13 +0100

    documentation toast service

--- src/services/toast.service.ts
+++ src/services/toast.service.ts
@@ -4,9 +4,15 @@
 import {Injectable} from "@angular/core";
 import {modelutilities} from "./modelutilities.service";
 
+/**
+ * a generic service that handles the toasts. The service is also injected in the footer component that renders the toas on top of the complete UI. In the DOM hierarchy Toasts are high in the index overlyaing all other elements.
+ */
 @Injectable()
 export class toast {
 
+    /**
+     * array holding the current active toasts that are rendered in the toas container
+     */
     private activeToasts: any[] = [];
 
     constructor(private modelutilities: modelutilities) {
@@ -26,10 +32,28 @@ export class toast {
         return this.addToast(text, 'toast', type, description, autoClose, uniqueMessageCode);
     }
 
+    /**
+     * a generic function to send an alert. This difers from the regular toast by the texture
+     *
+     * @param text the text of the message to be sent
+     * @param type the type of toast
+     * @param description an optional longtext to be displayed with the toast
+     * @param autoClose if set to true the toast will automatically close itself after 5 seconds
+     * @param uniqueMessageCode a string that unuqely identifies the type oif a message. If that is sent this ensuires that this type of message is sent to the user only once. This makes e.g. sense if the user is logged off .. then the toast that the user has been logged out might be sent several times. With the unique id this is only sent once to the user
+     */
     public sendAlert(text: string, type: "default" | "warning" | "info" | "success" | "error" = "default", description: string = "", autoClose: boolean | number = true, uniqueMessageCode?: string): string {
         return this.addToast(text, 'alert', type, description, autoClose, uniqueMessageCode);
     }
 
+    /**
+     * the internal funtion handling the toast adding
+     *
+     * @param text the text of the message to be sent
+     * @param theme the theme to be used
+     * @param description an optional longtext to be displayed with the toast
+     * @param autoClose if set to true the toast will automatically close itself after 5 seconds
+     * @param uniqueMessageCode a string that unuqely identifies the type oif a message. If that is sent this ensuires that this type of message is sent to the user only once. This makes e.g. sense if the user is logged off .. then the toast that the user has been logged out might be sent several times. With the unique id this is only sent once to the user
+     */
     private addToast(text: string, theme: 'toast' | 'alert' = 'toast', type: "default" | "warning" | "info" | "success" | "error" = "default", description: string = "", autoClose: boolean | number = true, uniqueMessageCode?: string): string {
         // check if a unique message code has been passed in .. if that is the case check if a toast with this unique id is active already
         if (uniqueMessageCode && this.activeToasts.filter(toast => toast.code == uniqueMessageCode).length > 0) {
@@ -59,6 +83,11 @@ export class toast {
         return messageId;
     }
 
+    /**
+     * a public function to clear the toast by the unique toast id that is returned when the toast is added
+     *
+     * @param messageId
+     */
     public clearToast(messageId) {
         if (!messageId) return;
         this.activeToasts.some((item, index) => {
@@ -69,6 +98,9 @@ export class toast {
         });
     }
 
+    /**
+     * clears all toasts
+     */
     public clearAll() {
         this.activeToasts = [];
     }
