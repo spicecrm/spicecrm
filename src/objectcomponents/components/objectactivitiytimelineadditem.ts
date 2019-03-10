f1a20a81f9a6fdb1e8f9ec916f02b3a5e9078eab
Christian Knoll <christian.g.knoll@gmail.com>
2019-03-10 11:00:06 +0100

    cleanup activityadditem

--- src/objectcomponents/components/objectactivitiytimelineadditem.ts
+++ src/objectcomponents/components/objectactivitiytimelineadditem.ts
@@ -1,7 +1,7 @@
 /**
  * @module ObjectComponents
  */
-import {AfterViewInit, Component, OnInit, ViewContainerRef} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewContainerRef} from '@angular/core';
 import {metadata} from '../../services/metadata.service';
 import {language} from '../../services/language.service';
 import {model} from '../../services/model.service';
@@ -11,86 +11,151 @@ import {dockedComposer} from '../../services/dockedcomposer.service';
 import {activitiyTimeLineService} from '../../services/activitiytimeline.service';
 
 /**
-* @ignore
-*/
+ * @ignore
+ */
 declare var moment: any;
 
-
+/**
+ * a component that is a generic container for adding items as part of the activitiy add container
+ */
 @Component({
     selector: 'object-activitiytimeline-add-item',
     templateUrl: './src/objectcomponents/templates/objectactivitytimelineadditem.html',
     providers: [model, view]
 })
-export class ObjectActivitiyTimelineAddItem implements OnInit {
+export class ObjectActivitiyTimelineAddItem implements OnInit, OnDestroy {
 
-    componentconfig: any = {};
-    headerFieldSet: string = '';
-    bodyFieldSet: string = '';
+    /**
+     * @ignore
+     *
+     * the componentconfig that is added whent eh component is added
+     */
+    private componentconfig: any = {};
 
-    isExpanded: boolean = false;
+    /**
+     * the fieldset for the header. Pulled from the componentnconfig for the component and the module
+     */
+    private headerFieldSet: string = '';
 
-    constructor(private metadata: metadata, private activitiyTimeLineService: activitiyTimeLineService, private model: model, private view: view, private language: language, private modal: modal, private dockedComposer: dockedComposer, private ViewContainerRef: ViewContainerRef) {}
+    /**
+     * the fieldset for the body. Pulled from the componentnconfig for the component and the module
+     */
+    private bodyFieldSet: string = '';
 
-    ngOnInit() {
+    /**
+     * @ignore
+     *
+     * indicator if the panel is expanded or not
+     */
+    private isExpanded: boolean = false;
+
+    /**
+     * @ignore
+     *
+     * a handler to the parent subscription
+     */
+    private parentSubscription: any;
+
+    constructor(private metadata: metadata, private activitiyTimeLineService: activitiyTimeLineService, private model: model, private view: view, private language: language, private modal: modal, private dockedComposer: dockedComposer, private ViewContainerRef: ViewContainerRef) {
+    }
+
+    /**
+     * @ignore
+     */
+    public ngOnInit() {
         // initialize the model
         this.model.module = this.module;
 
         // subscribe to the parent models data Observable
         // name is not necessarily loaded
-        this.activitiyTimeLineService.parent.data$.subscribe(data => {
+        this.parentSubscription = this.activitiyTimeLineService.parent.data$.subscribe(data => {
             // if we still have the same model .. update
-            if (data.id = this.model.data.parent_id)
+            if (data.id == this.model.data.parent_id) {
                 this.model.data.parent_name = data.summary_text;
-        })
+            }
+        });
 
         // set view to editbale and edit mode
         this.view.isEditable = true;
         this.view.setEditMode();
 
         // get the fields
-        //let componentconfig = this.metadata.getComponentConfig('ObjectActivitiyTimelineAddCall', this.model.module);
         this.headerFieldSet = this.componentconfig.headerfieldset;
         this.bodyFieldSet = this.componentconfig.bodyfieldset;
     }
 
-    get module(){
+    /**
+     * @ignore
+     *
+     * cancels the subscription on the parent
+     */
+    public ngOnDestroy(): void {
+        this.parentSubscription.unsubscribe();
+    }
+
+    /**
+     * getter for the module from the componentconfig
+     */
+    get module() {
         return this.componentconfig.module;
     }
 
-    get actionset(){
+    /**
+     * getter for the actionset fromn the config
+     */
+    get actionset() {
         return this.componentconfig.actionset;
     }
 
-    initializeModule(){
+    /**
+     * initializes the model when the item is expanded
+     */
+    private initializeModule() {
         this.model.module = this.module;
         // SPICEUI-2
         this.model.id = this.model.generateGuid();
         this.model.initializeModel(this.activitiyTimeLineService.parent);
     }
 
-    onHeaderClick() {
-        if(!this.isExpanded) {
+    /**
+     * the trigger when the header fieldset or any item therein in focused and the item is expanded
+     */
+    private onHeaderClick() {
+        if (!this.isExpanded) {
             this.isExpanded = true;
             this.initializeModule();
         }
     }
 
-    expand(){
+    /**
+     * expands the item and renders it in a modal undocking it from the activity tiemline container
+     */
+    private expand() {
         this.modal.openModal('GlobalDockedComposerModal', true, this.ViewContainerRef.injector);
     }
 
-    dock(){
+    /**
+     * docks the model to the docked composer and removes it from the activity timeline container
+     */
+    private dock() {
         this.dockedComposer.addComposer(this.model.module, this.model);
         this.isExpanded = false;
     }
 
-    cancel(){
+    /**
+     * cancels and collapses the container
+     */
+    private cancel() {
         this.isExpanded = false;
     }
 
-    handleaction(event){
+    /**
+     * catches when a custom action has been fired.Closes the container and resets the model
+     *
+     * @param event the event fired from teh custom action
+     */
+    handleaction(event) {
         this.initializeModule();
         this.isExpanded = false;
     }
-
 }
