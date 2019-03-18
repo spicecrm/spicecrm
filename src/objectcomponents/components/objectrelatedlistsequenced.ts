3f53a25d89e13876b612ff24ab77ee444303e36e
strombo <andreas@gloeckl.name>
2019-03-18 16:29:44 +0100

    CR1000170: Sequenced ObjectRelatedListList (Drag & Drop)

--- src/objectcomponents/components/objectrelatedlistsequenced.ts
+++ src/objectcomponents/components/objectrelatedlistsequenced.ts
@@ -1,17 +1,14 @@
 /**
  * @module ObjectComponents
  */
-import {Component} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {relatedmodels} from '../../services/relatedmodels.service';
 import {ObjectRelatedlistList} from './objectrelatedlistlist';
 
 @Component({
-    selector: 'object-relatedlist-list',
-    templateUrl: './src/objectcomponents/templates/objectrelatedlistsequenced.html',
-    providers: [relatedmodels]
+    selector: 'object-relatedlist-sequenced',
+    templateUrl: './src/objectcomponents/templates/objectrelatedlistsequenced.html'
 })
-export class ObjectRelatedlistSequenced extends ObjectRelatedlistList {
-    get sequencefield() {
-        return this.componentconfig.sequencefield;
-    }
+export class ObjectRelatedlistSequenced {
+    @Input() private componentconfig: any = {};
 }
