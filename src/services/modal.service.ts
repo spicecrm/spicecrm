9891521f4bb08534476cf0861c4177a7783a1c0d
strombo <andreas@gloeckl.name>
2019-03-30 00:56:59 +0100

    CR1000183: Select field for modal input prompt

--- src/services/modal.service.ts
+++ src/services/modal.service.ts
@@ -107,15 +107,15 @@ export class modal {
         return this.modalsArray.length * 2;
     }
 
-    public prompt(type: string, text: string, headertext: string = null, theme: string, defaultvalue: string = null): Observable<any> {
+    public prompt( type: string, text: string, headertext: string = null, theme: string = 'shade', defaultvalue: string|number = null, options: string[] = null ): Observable<any> {
         let responseSubject = new Subject();
         this.openModal("SystemPrompt").subscribe(component => {
-            // todo: abhängig von type: esc ein/aus via component.instance["wrapper"]
             component.instance.type = type;
             component.instance.text = text;
             component.instance.headertext = headertext;
             component.instance.theme = theme;
-            component.instance.defaultvalue = defaultvalue;
+            component.instance.value = defaultvalue;
+            component.instance.options = options;
             component.instance.answer.subscribe(answervalue => {
                 responseSubject.next(answervalue); // return the answer
                 responseSubject.complete();
