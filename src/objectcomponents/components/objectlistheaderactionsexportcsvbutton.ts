955fae6b4275685d3b43b45152e7b149f81a0693
Christian Knoll <christian.g.knoll@gmail.com>
2019-03-29 10:06:38 +0100

    object list export

--- src/objectcomponents/components/objectlistheaderactionsexportcsvbutton.ts
+++ src/objectcomponents/components/objectlistheaderactionsexportcsvbutton.ts
@@ -23,10 +23,10 @@ import {modellist} from '../../services/modellist.service';
  * either downloads the full list with all entries or the selected entries
  */
 @Component({
-    selector: 'object-list-header-actions-export-button',
-    templateUrl: './src/objectcomponents/templates/objectlistheaderactionsexportbutton.html',
+    selector: 'object-list-header-actions-export-csv-button',
+    templateUrl: './src/objectcomponents/templates/objectlistheaderactionsexportcsvbutton.html',
 })
-export class ObjectListHeaderActionsExportButton {
+export class ObjectListHeaderActionsExportCSVButton {
 
     constructor(private language: language, private metadata: metadata, private modellist: modellist, private model: model, private modal: modal, private backend: backend) {
     }
