4ddc311c329a4fd7c4d16ddf52d1e10a3549abec
Dawid Bestrzynski <dawid.bestrzynski@twentyreasons.com>
2019-04-05 14:48:46 +0200

    Outlook add-in actions

--- src/include/outlook/components/outlookpane.ts
+++ src/include/outlook/components/outlookpane.ts
@@ -3,6 +3,9 @@ import {Router} from '@angular/router';
 
 import {OutlookConfiguration} from '../services/outlookconfiguration.service';
 import {GroupwareService} from "../../groupware/services/groupware.service";
+import {session} from "../../../services/session.service";
+import {metadata} from "../../../services/metadata.service";
+import {language} from "../../../services/language.service";
 
 declare var Office: any;
 
@@ -12,12 +15,38 @@ declare var Office: any;
 })
 export class OutlookPane implements OnInit {
 
+    private _currentroute: string = 'mailitem';
+    // private actions: any = [];
+
     constructor(
         private configuration: OutlookConfiguration,
         private groupware: GroupwareService,
         private router: Router,
+        private session: session,
+        private metadata: metadata,
+        private language: language
     ) {
+    }
 
+
+    get currentroute() {
+        return this._currentroute;
+    }
+
+    set currentroute(route) {
+        if (route) {
+            this._currentroute = route;
+
+            this.router.navigate([route]);
+        }
+    }
+
+    get actions() {
+        let componentConfig = this.metadata.getComponentConfig('OutlookPane');
+        if (componentConfig.actionset) {
+            return this.metadata.getActionSetItems(componentConfig.actionset);
+        }
+        return [];
     }
 
     public ngOnInit(): void {
@@ -27,6 +56,18 @@ export class OutlookPane implements OnInit {
         // } else {
         //     this.router.navigate(['settings']);
         // }
+
+        // load the actions
+        /*
+        let componentConfig = this.metadata.getComponentConfig('OutlookPane');
+        if (componentConfig.actionset) {
+            this.actions = this.metadata.getActionSetItems(componentConfig.actionset);
+        }
+         */
+    }
+
+    private openSettings() {
+        this.router.navigate(['settings']);
     }
 
 }
