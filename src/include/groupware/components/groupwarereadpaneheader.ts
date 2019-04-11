0413b8783f8f61c777f13990756d860e83993773
Christian Knoll <christian.g.knoll@gmail.com>
2019-04-11 12:59:01 +0200

    groupware and outlook update styling

---
+++ src/include/groupware/components/groupwarereadpaneheader.ts
@@ -0,0 +1,37 @@
+import {Component} from '@angular/core';
+
+import {GroupwareService} from '../services/groupware.service';
+import {language} from "../../../services/language.service";
+
+@Component({
+    selector: 'groupware-read-pane-header',
+    templateUrl: './src/include/groupware/templates/groupwarereadpaneheader.html'
+})
+export class GroupwareReadPaneHeader {
+
+    constructor(
+        private language: language,
+        private groupware: GroupwareService
+    ) {
+
+    }
+
+    private archive() {
+        this.groupware.archiveEmail().subscribe(
+            next => {
+                // update message
+            },
+            error => {
+                // display error
+            },
+            () => {
+                // Office.context.ui.closeContainer();
+                // todo move that into the specific module
+            });
+    }
+
+    get canArchive() {
+        return this.groupware.archiveto.length > 0;
+    }
+
+}
