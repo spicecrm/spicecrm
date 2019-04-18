57ffc1fb3d10e627d06a10c8cd36d07fd3fb4d4f
Mehyar <mehyar.sawas@twentyreasons.com>
2019-04-18 14:17:14 +0200

    Module: "Components import" sorted correctly.

--- src/modules/knowledge/moduleknowledge.ts
+++ src/modules/knowledge/moduleknowledge.ts
@@ -15,16 +15,16 @@ import {AddComponentsModule} from "../../addcomponents/addcomponents.module";
 
 import /*embed*/ {KnowledgeService} from "./services/knowledge.service";
 
-import /*embed*/ {KnowledgeManager} from "./components/knowledgemanager";
-import /*embed*/ {KnowledgeManagerDetails} from "./components/knowledgemanagerdetails";
-import /*embed*/ {KnowledgeBrowser} from "./components/knowledgebrowser";
-import /*embed*/ {KnowledgeBrowserDetails} from "./components/knowledgebrowserdetails";
-import /*embed*/ {KnowledgeBrowserDetailsContainerLeft} from "./components/knowledgebrowserdetailscontainerleft";
-import /*embed*/ {KnowledgeBrowserDetailsContainerRight} from "./components/knowledgebrowserdetailscontainerright";
 import /*embed*/ {KnowledgeBookSelector} from "./components/knowledgebookselector";
 import /*embed*/ {KnowledgeDocumentsSearch} from "./components/knowledgedocumentssearch";
 import /*embed*/ {KnowledgeDocumentRelatedList} from "./components/knowledgedocumentrelatedlist";
 import /*embed*/ {KnowledgeDocumentFavorites} from "./components/knowledgedocumentfavorites";
+import /*embed*/ {KnowledgeBrowserDetailsContainerRight} from "./components/knowledgebrowserdetailscontainerright";
+import /*embed*/ {KnowledgeBrowserDetailsContainerLeft} from "./components/knowledgebrowserdetailscontainerleft";
+import /*embed*/ {KnowledgeBrowserDetails} from "./components/knowledgebrowserdetails";
+import /*embed*/ {KnowledgeBrowser} from "./components/knowledgebrowser";
+import /*embed*/ {KnowledgeManagerDetails} from "./components/knowledgemanagerdetails";
+import /*embed*/ {KnowledgeManager} from "./components/knowledgemanager";
 
 @NgModule({
     imports: [
