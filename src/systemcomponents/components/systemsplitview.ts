1de9bf8c9f4b02b0b0e71692ff9e9b3bd8ad7927
Christian Knoll <christian.g.knoll@gmail.com>
2019-04-22 15:16:29 +0200

    generic components SystemSplitView & SystemViewContainer

---
+++ src/systemcomponents/components/systemsplitview.ts
@@ -0,0 +1,23 @@
+/**
+ * @module SystemComponents
+ */
+import {Component, Input} from "@angular/core";
+
+/**
+ * a simple container that renders a split view with two componentsets rendered
+ */
+@Component({
+    selector: "system-split-view",
+    templateUrl: "./src/systemcomponents/templates/systemsplitview.html"
+})
+export class SystemSplitView {
+    @Input() public componentconfig: any = {};
+
+    get componentSetLeft() {
+        return this.componentconfig.left;
+    }
+
+    get componentSetRight() {
+        return this.componentconfig.right;
+    }
+}
