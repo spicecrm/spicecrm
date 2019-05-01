be7d135a39a54c57e16cbbf496c9b557a180e113
Christian Knoll <christian.g.knoll@gmail.com>
2019-05-01 07:08:04 +0200

    opportunity revenue lines

---
+++ src/modules/opportunities/moduleopportunities.ts
@@ -0,0 +1,41 @@
+/**
+ * @module ModuleOpportunities
+ */
+import {CommonModule} from "@angular/common";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+
+import {VersionManagerService} from "../../services/versionmanager.service";
+import {DirectivesModule} from "../../directives/directives";
+
+import {ObjectFields} from "../../objectfields/objectfields";
+import {GlobalComponents} from "../../globalcomponents/globalcomponents";
+import {ObjectComponents} from "../../objectcomponents/objectcomponents";
+import {SystemComponents} from "../../systemcomponents/systemcomponents";
+
+import /*embed*/ {OpportunityRevenueLines} from "./components/opportunityrevenuelines";
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ObjectFields,
+        GlobalComponents,
+        ObjectComponents,
+        SystemComponents,
+        DirectivesModule,
+    ],
+    declarations: [
+        OpportunityRevenueLines
+    ]
+})
+export class ModuleOpportunities {
+    public readonly version = "1.0";
+    public readonly build_date = "/*build_date*/";
+
+    constructor(
+        private vms: VersionManagerService,
+    ) {
+        this.vms.registerModule(this);
+    }
+}
