0e94c82f6848917d460b455900607b1610b75636
strombo <andreas@gloeckl.name>
2019-05-02 08:46:07 +0200

    CR1000196: Configuration Transfer (update)

--- src/workbench/components/configtransfer.ts
+++ src/workbench/components/configtransfer.ts
@@ -11,8 +11,8 @@ import { toast } from '../../services/toast.service';
 import { Observable, Subject } from 'rxjs';
 
 /**
-* @ignore
-*/
+ * @ignore
+ */
 declare var moment: any;
 
 @Component({
@@ -51,6 +51,7 @@ export class ConfigTransfer {
     private importErrorID: string;
     private dontEmptyTables = false;
     @ViewChild('fileupload', {read: ViewContainerRef}) private fileupload: ViewContainerRef;
+    private isDragOver = false;
 
     constructor( private backend: backend, private metadata: metadata, private lang: language, private prefs: userpreferences, private modalservice: modal, private toast: toast ) { }
 
@@ -85,32 +86,32 @@ export class ConfigTransfer {
         let selectedTables: string[] = [];
         this.selectableTables.forEach( ( table => {
             if ( table.include ) selectedTables.push(table.name);
-       }));
+        }));
         this.fileName = 'spicecrm.cfg.' + moment().format('YYYYMMDD_HHmm') + '.bz2';
         this.backend.getDownloadPostRequestFile('configtransfer/data/export', {}, { selectedTables: selectedTables, additionalTables: this.additionalTables } ).subscribe(
-           url => {
-               this.downloadlink.element.nativeElement.href = url;
-               this.downloadlink.element.nativeElement.click();
-               this.isDownloading = false;
-           },
-           err => {
+            url => {
+                this.downloadlink.element.nativeElement.href = url;
+                this.downloadlink.element.nativeElement.click();
+                this.isDownloading = false;
+            },
+            err => {
                 this.isDownloading = false;
                 this.exportErrorID = this.toast.sendToast( this.lang.getLabel('ERR_EXPORT_FAILED'), 'error', this.exportErrorMessage = err.error.message );
             }
         );
-   }
+    }
 
-   private selectAll( status: boolean ) {
+    private selectAll( status: boolean ) {
         this.selectableTables.forEach( table => {
             table.include = status;
         });
-   }
+    }
 
-   private get numberOfSelectedTables() {
+    private get numberOfSelectedTables() {
         let number = 0;
         this.selectableTables.forEach( table => table.include && number++ );
         return number;
-   }
+    }
 
     private uploadFile() {
         this.modalservice.confirm('You are about to upload a file and import its data into the database. This will change the existing database. Do you really want to do this?', this.lang.getLabel('LBL_WARNING'), 'warning').subscribe(
@@ -125,17 +126,16 @@ export class ConfigTransfer {
                     });
                     this.readFileFromFilesystem( this.fileFromBrowser ).subscribe( fileContent => {
                         this.backend.postRequestWithProgress( '/configtransfer/data/import', null, { file: fileContent, dontEmptyTables: this.dontEmptyTables }, null, progress ).subscribe( response => {
-                            this.isAfterUpload = true;
-                            this.isImporting = this.isUploading = false;
-                            if ( response.success ) this.importOK = true; else this.importOK = false;
-                            this.importResponse = response;
-                        },
-                        error => {
-                            this.isAfterUpload = true;
-                            this.importOK = false;
-                            this.isImporting = this.isUploading = false;
-                            this.importErrorID = this.toast.sendToast( this.lang.getLabel('ERR_IMPORT_FAILED'), 'error', this.importErrorMessage = error.error.error.message );
-                        });
+                                this.isAfterUpload = this.importOK = true;
+                                this.isImporting = this.isUploading = false;
+                                this.importResponse = response;
+                            },
+                            error => {
+                                this.isAfterUpload = true;
+                                this.importOK = false;
+                                this.isImporting = this.isUploading = false;
+                                this.importErrorID = this.toast.sendToast( this.lang.getLabel('ERR_IMPORT_FAILED'), 'error', this.importErrorMessage = error.error.error.message );
+                            });
                     } );
                 }
             }
@@ -171,7 +171,7 @@ export class ConfigTransfer {
     private readFileFromFilesystem( file: File ): Observable<string> {
         let responseSubject = new Subject<string>();
         let reader = new FileReader();
-        reader.onloadend = (e) => {
+        reader.onloadend = () => {
             let fileContent = reader.result.toString();
             fileContent = fileContent.substring( fileContent.indexOf('base64,') + 7 );
             responseSubject.next( fileContent );
@@ -183,6 +183,7 @@ export class ConfigTransfer {
 
     private onDrop( event: DragEvent ) {
         event.preventDefault(); // Turn off the browser's default drag and drop handler.
+        this.isDragOver = false;
         if ( event.dataTransfer.items ) {
             // Use DataTransferItemList interface to access the file
             if ( event.dataTransfer.items.length !== 1 || event.dataTransfer.items[0].kind !== 'file' ) { // We only accept a file, one single file.
@@ -217,6 +218,11 @@ export class ConfigTransfer {
         event.preventDefault();
         event.stopPropagation();
         event.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+        this.isDragOver = true;
+    }
+
+    private onDragLeave() {
+        this.isDragOver = false;
     }
 
 }
