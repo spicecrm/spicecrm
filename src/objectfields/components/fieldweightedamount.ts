b8101cba21b0c416d4b6e43e384be9e586b969f2
Christian Knoll <christian.g.knoll@gmail.com>
2019-05-20 17:31:52 +0200

    field updates

--- src/objectfields/components/fieldweightedamount.ts
+++ src/objectfields/components/fieldweightedamount.ts
@@ -8,8 +8,8 @@ import {language} from '../../services/language.service';
 import {metadata} from '../../services/metadata.service';
 import {currency} from '../../services/currency.service';
 import {fieldCurrency} from './fieldcurrency';
-import {Router}   from '@angular/router';
-import { userpreferences } from '../../services/userpreferences.service';
+import {Router} from '@angular/router';
+import {userpreferences} from '../../services/userpreferences.service';
 
 @Component({
     selector: 'field-currency',
@@ -17,30 +17,36 @@ import { userpreferences } from '../../services/userpreferences.service';
 })
 export class fieldWeightedAmount extends fieldCurrency {
 
-    currencies: Array<any> = [];
 
-    constructor(public model: model, public view: view, public language: language, public metadata: metadata, public router: Router, public currency: currency, public userpreferences: userpreferences ) {
-        super(model, view, language, metadata, router, currency, userpreferences );
+    constructor(public model: model, public view: view, public language: language, public metadata: metadata, public router: Router, public currency: currency, public userpreferences: userpreferences) {
+        super(model, view, language, metadata, router, currency, userpreferences);
 
         this.currencies = this.currency.getCurrencies();
     }
 
-    get amountfield(){
-        return this.fieldconfig.field_amount ?  this.fieldconfig.field_amount : 'amount';
+    /**
+     * a simple getter to get the amount field that is the base for the weighted amount
+     */
+    get amountfield() {
+        return this.fieldconfig.field_amount ? this.fieldconfig.field_amount : 'amount';
     }
-    get probabilityfield(){
-        return this.fieldconfig.field_probability ?  this.fieldconfig.field_probability : 'probability';
+
+    /**
+     * a simple getter to get the probability field
+     */
+    get probabilityfield() {
+        return this.fieldconfig.field_probability ? this.fieldconfig.field_probability : 'probability';
     }
 
 
-    getValue() {
+    private getValue() {
 
-        try{
-            let value = this.model.getFieldValue('amount') ? parseFloat(this.model.getFieldValue(this.amountfield))  : 0;
-            let probability = this.model.getFieldValue('amount') ? parseFloat(this.model.getFieldValue(this.probabilityfield))  : 0;
+        try {
+            let value = this.model.getFieldValue('amount') ? parseFloat(this.model.getFieldValue(this.amountfield)) : 0;
+            let probability = this.model.getFieldValue('amount') ? parseFloat(this.model.getFieldValue(this.probabilityfield)) : 0;
             return this.userpreferences.formatMoney(value * probability / 100);
 
-        } catch(e){
+        } catch (e) {
             return 0;
         }
 
