c4390ca8a0e2d43c7a55b5f6c660cb2e3c1f269a
Christian Knoll <christian.g.knoll@gmail.com>
2019-05-20 09:18:17 +0200

    field updates

--- src/objectfields/components/fieldcurrency.ts
+++ src/objectfields/components/fieldcurrency.ts
@@ -8,8 +8,8 @@ import {language} from '../../services/language.service';
 import {metadata} from '../../services/metadata.service';
 import {currency} from '../../services/currency.service';
 import {fieldGeneric} from './fieldgeneric';
-import {Router}   from '@angular/router';
-import { userpreferences } from '../../services/userpreferences.service';
+import {Router} from '@angular/router';
+import {userpreferences} from '../../services/userpreferences.service';
 
 @Component({
     selector: 'field-currency',
@@ -17,32 +17,40 @@ import { userpreferences } from '../../services/userpreferences.service';
 })
 export class fieldCurrency extends fieldGeneric implements OnInit {
 
-    currencies: Array<any> = [];
-    currencyidfield: string = '';
-    textvalue = '';
+    /**
+     * holds an array of currencies
+     */
+    private currencies: any[] = [];
 
-    constructor(public model: model, public view: view, public language: language, public metadata: metadata, public router: Router, public currency: currency, public userpreferences: userpreferences ) {
+    /**
+     * the reference to the field with the currency id
+     */
+    private currencyidfield: string = '';
+
+    constructor(public model: model, public view: view, public language: language, public metadata: metadata, public router: Router, public currency: currency, public userpreferences: userpreferences) {
         super(model, view, language, metadata, router);
         this.currencies = this.currency.getCurrencies();
     }
 
-    ngOnInit(){
+    /**
+     * @ignore
+     */
+    public ngOnInit() {
         this.currencyidfield = this.fieldconfig.field_currencyid;
-        this.textvalue = this.getValAsText();
-        this.model.data$.subscribe( () => {
-            this.textvalue = this.getValAsText();
-        });
     }
 
-    getCurrencySymbol(): string {
+    /**
+     * helper to get the currency symbol
+     */
+    private getCurrencySymbol(): string {
         let currencySymbol: string;
         let currencyid = -99;
-        if ( this.currencyidfield ) {
-            if( !this.model.data[this.currencyidfield] ) return '';
+        if (this.currencyidfield) {
+            if (!this.model.data[this.currencyidfield]) return '';
             else currencyid = this.model.data[this.currencyidfield];
         }
         this.currencies.some(currency => {
-            if(currency.id == currencyid){
+            if (currency.id == currencyid) {
                 currencySymbol = currency.symbol;
                 return true;
             }
@@ -50,23 +58,31 @@ export class fieldCurrency extends fieldGeneric implements OnInit {
         return currencySymbol;
     }
 
-    getValAsText() {
-        if ( this.value === undefined ) return '';
-        let val = parseFloat( this.value );
-        if ( isNaN( val )) return '';
-        return this.userpreferences.formatMoney( val );
+    /**
+     * override the getter for the value
+     */
+    get value() {
+        let fieldval = this.model.getFieldValue(this.fieldname);
+
+        if (fieldval === undefined) return '';
+        let val = parseFloat(fieldval);
+        if (isNaN(val)) return '';
+        return this.userpreferences.formatMoney(val);
     }
 
-    changed() {
-        let val: any = this.textvalue;
-        val = val.split( this.userpreferences.toUse.num_grp_sep ).join('');
-        val = val.split( this.userpreferences.toUse.dec_sep ).join('.');
-        if ( isNaN( val = parseFloat( val ))) {
-            this.value = '';
+    /**
+     * override the setter for the value
+     *
+     * @param val
+     */
+    set value(val) {
+        val = val.split(this.userpreferences.toUse.num_grp_sep).join('');
+        val = val.split(this.userpreferences.toUse.dec_sep).join('.');
+        if (isNaN(val = parseFloat(val))) {
+            this.model.setField(this.fieldname, '');
         } else {
-            this.value = Math.floor( val * Math.pow( 10, this.userpreferences.toUse.default_currency_significant_digits )) / Math.pow( 10, this.userpreferences.toUse.default_currency_significant_digits );
+            // this.value = Math.floor(val * Math.pow(10, this.userpreferences.toUse.default_currency_significant_digits)) / Math.pow(10, this.userpreferences.toUse.default_currency_significant_digits);
+            this.model.setField(this.fieldname, Math.floor(val * Math.pow(10, this.userpreferences.toUse.default_currency_significant_digits)) / Math.pow(10, this.userpreferences.toUse.default_currency_significant_digits))
         }
-        this.textvalue = this.getValAsText();
     }
-
 }
