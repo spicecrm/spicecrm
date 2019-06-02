99fcf2bcf39090e475c96d0df1014d77cac75e46
Christian Knoll <christian.g.knoll@gmail.com>
2019-06-02 13:59:03 +0200

    added userimage to objectnote as well as documentation & Cleanup

--- src/objectcomponents/components/objectnote.ts
+++ src/objectcomponents/components/objectnote.ts
@@ -7,31 +7,44 @@ import {
 import {objectnote} from '../services/objectnote.service';
 
 /**
-* @ignore
-*/
+ * @ignore
+ */
 declare var moment: any;
 
+/**
+ * displays a quicknote that is read in teh stream
+ */
 @Component({
     selector: 'object-note',
     templateUrl: './src/objectcomponents/templates/objectnote.html'
 })
-export class ObjectNote implements OnInit {
+export class ObjectNote {
 
-    @Input() note: any = {};
+    /**
+     * the note in the for loop
+     */
+    @Input() private note: any = {};
 
-    constructor(private objectnote: objectnote ) {
+    /**
+     * @ignore
+     *
+     * @param objectnote
+     */
+    constructor(private objectnote: objectnote) {
 
     }
 
-    ngOnInit(){
-
-    }
-
-    getNoteTimeFromNow(){
+    /**
+     * get the timestamp and vonverts into a relative one
+     */
+    private getNoteTimeFromNow() {
         return moment(this.note.date).fromNow();
     }
 
-    deleteNote(){
+    /**
+     * delete the note
+     */
+    private deleteNote() {
         this.objectnote.deleteNote(this.note.id);
     }
 }
