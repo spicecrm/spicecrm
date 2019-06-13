c62e31d5c355f59037fc5ea620ddb24331bedd77
Mehyar <mehyar.sawas@twentyreasons.com>
2019-06-13 10:44:56 +0200

    Module DUNS: created.

---
+++ src/include/duns/moduleduns.ts
@@ -0,0 +1,43 @@
+/**
+ * @module ModuleDuns
+ */
+import {CommonModule} from "@angular/common";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+
+import {VersionManagerService} from "../../services/versionmanager.service";
+import {DirectivesModule} from "../../directives/directives";
+
+import {ObjectFields} from "../../objectfields/objectfields";
+import {GlobalComponents} from "../../globalcomponents/globalcomponents";
+import {ObjectComponents} from "../../objectcomponents/objectcomponents";
+import {SystemComponents} from "../../systemcomponents/systemcomponents";
+
+import /*embed*/ {FieldDunsNumber} from "./components/fielddunsnumber";
+import /*embed*/ {DunsNumberModal} from "./components/dunsnumbermodal";
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ObjectFields,
+        GlobalComponents,
+        ObjectComponents,
+        SystemComponents,
+        DirectivesModule,
+    ],
+    declarations: [
+        FieldDunsNumber,
+        DunsNumberModal
+    ]
+})
+export class ModuleDuns {
+    public readonly version = "1.0";
+    public readonly build_date = "/*build_date*/";
+
+    constructor(
+        private vms: VersionManagerService,
+    ) {
+        this.vms.registerModule(this);
+    }
+}
