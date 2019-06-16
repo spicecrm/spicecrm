69a43506ae7b6e9f1bcb69f033a60acaf832c201
Christian Knoll <christian.g.knoll@gmail.com>
2019-06-16 16:38:40 +0200

    CR 1000223 - tranlsation using labels in beangudes (Path)

--- src/include/spicepath/components/spicepathwithcoaching.ts
+++ src/include/spicepath/components/spicepathwithcoaching.ts
@@ -10,6 +10,7 @@ import {
     transition
 } from '@angular/animations';
 import {model} from "../../../services/model.service";
+import {language} from "../../../services/language.service";
 import {configurationService} from "../../../services/configuration.service";
 import {broadcast} from "../../../services/broadcast.service";
 
@@ -24,18 +25,18 @@ import {broadcast} from "../../../services/broadcast.service";
     animations: [
         trigger('displaycoaching', [
             transition(':enter', [
-                style({ opacity: 0 , height: '0px',overflow: 'hidden'}),
-                animate('.5s', style({ height: '*', opacity: 1 })),
-                style({ overflow: 'unset'})
+                style({opacity: 0, height: '0px', overflow: 'hidden'}),
+                animate('.5s', style({height: '*', opacity: 1})),
+                style({overflow: 'unset'})
             ]),
             transition(':leave', [
-                style({ overflow: 'hidden'}),
-                animate('.5s', style({ height: '0px', opacity: 0 }))
+                style({overflow: 'hidden'}),
+                animate('.5s', style({height: '0px', opacity: 0}))
             ])
         ]),
         trigger('coachingicon', [
-            state('open', style({ transform: 'rotate(90deg)'})),
-            state('closed', style({ transform: 'rotate(0deg)'})),
+            state('open', style({transform: 'rotate(90deg)'})),
+            state('closed', style({transform: 'rotate(0deg)'})),
             transition('open => closed', [
                 animate('.5s'),
             ]),
@@ -57,7 +58,7 @@ export class SpicePathWithCoaching {
      */
     private activeStage: string;
 
-    constructor(private configuration: configurationService, private model: model) {
+    constructor(private configuration: configurationService, private model: model, private language: language) {
     }
 
     /**
@@ -125,13 +126,14 @@ export class SpicePathWithCoaching {
 
     // gets the current stage description
     get stageDescription() {
-        let stagetext: string = "";
-        this.stages.some(stage => {
-            if (this.displayStage === stage.stage) {
-                stagetext = stage.stagedata.stage_description;
-                return true;
-            }
-        });
-        return stagetext;
+        let stage = this.stages.find(el => el.stage == this.displayStage);
+
+        if (!stage) return '';
+
+        if (stage.stagedata.stage_label) {
+            return this.language.getLabel(stage.stagedata.stage_label, '', 'long');
+        } else {
+            return stage.stagedata.stage_description;
+        }
     }
 }
