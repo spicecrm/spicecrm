3037a76bddf82e14e524ac0e20e4807c9d5f6caf
Mehyar <mehyar.sawas@twentyreasons.com>
2019-06-24 16:26:05 +0200

    System Tree : "CDK Drag Drop" applied.

--- src/systemcomponents/components/systemtreeitem.ts
+++ src/systemcomponents/components/systemtreeitem.ts
@@ -1,46 +1,49 @@
 /**
  * @module SystemComponents
  */
-import {Component, EventEmitter, Input, Output, Renderer2} from "@angular/core";
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {AfterViewInit, Component, EventEmitter, Input, OnDestroy, Output, ViewChild} from "@angular/core";
 
 @Component({
     selector: "system-tree-item",
     templateUrl: "./src/systemcomponents/templates/systemtreeitem.html"
 })
 
-export class SystemTreeItem {
-
+export class SystemTreeItem implements AfterViewInit, OnDestroy {
     @Output() public selectedItem$: EventEmitter<any> = new EventEmitter<any>();
-    @Output() public treelistChange$: EventEmitter<any> = new EventEmitter<any>();
     @Output() public addItem$: EventEmitter<any> = new EventEmitter<any>();
-    @Output() public changeItemPosition_item$: EventEmitter<any> = new EventEmitter<any>();
-  /*  @Output() public changeItemPosition_item_two$: EventEmitter<any> = new EventEmitter<any>();*/
-
-
+    @Output() public itemPosition$: EventEmitter<any> = new EventEmitter<any>();
+    @Output() public dropListId$: EventEmitter<any> = new EventEmitter<any>();
     @Input() public selectedItem: string = "";
     @Input() public items: any = [];
+    @Input() private dropListIds: any = [];
     @Input() private config: any = {};
+    @Input() private hasChildren: boolean = false;
+    @ViewChild('dropList') private dropList;
 
-    constructor(private renderer: Renderer2) {
+    get connectionList() {
+        return (this.dropList && this.hasChildren) ? this.dropListIds.filter(i => i != this.dropList.id) : this.dropListIds;
     }
 
-    public expand(e, item) {
-        if (!item) {
-            return;
+    public ngAfterViewInit() {
+        if (this.hasChildren && this.dropList && this.config.draggable) {
+            window.setTimeout(() => this.dropListId$.emit({id: this.dropList.id, action: 'add'}), 100);
         }
+    }
+
+    public ngOnDestroy() {
+        if (this.dropList && this.config.draggable) {
+            window.setTimeout(() => this.dropListId$.emit({id: this.dropList.id, action: 'remove'}), 100);
+        }
+    }
+
+    public expand(item, e?) {
         item.expanded = !item.expanded;
         e.stopPropagation();
-
     }
 
     public addItem(e, parentId, parentName) {
-        this.handleAddEvent({id: parentId, name: parentName});
+        this.addItem$.emit({id: parentId, name: parentName});
         e.stopPropagation();
-    }
-
-    public handleAddEvent(parent) {
-        this.addItem$.emit(parent);
     }
 
     private selectItem(e, id) {
@@ -48,75 +51,7 @@ export class SystemTreeItem {
         e.stopPropagation();
     }
 
-    private handleSelectItemEvent(id) {
-        this.selectedItem$.emit(id);
+    private trackByFn(i, item) {
+        return item.id;
     }
-
-/*    private dragItem(e, id) {
-        if (!this.config.draggable) {
-            return;
-        }
-        e.dataTransfer.setDragImage(this.renderer.createElement("div"), 0, 0);
-        e.dataTransfer.setData("itemId", id);
-        e.stopPropagation();
-    }
-
-    private onDragging(e) {
-        if (!this.config.draggable) {
-            return;
-        }
-        if (e.currentTarget.firstChild.tagName == 'ARTICLE') {
-            this.renderer.setStyle(e.currentTarget.firstChild, "border", "1px solid red");
-        }
-        e.preventDefault();
-        e.stopPropagation();
-    }
-
-    private onDragLeave(e) {
-        if (!this.config.draggable) {
-            return;
-        }
-        if (e.currentTarget.firstChild.tagName == 'ARTICLE') {
-            this.renderer.removeStyle(e.currentTarget.firstChild, "border");
-        }
-    }
-
-    // Pass to emitter: Ids of moved and targeted item
-    private dropItem(e, dropitemId) {
-        if (!this.config.draggable) {
-            return;
-        }
-        if (e.currentTarget.firstChild.tagName == 'ARTICLE') {
-            this.renderer.removeStyle(e.currentTarget.firstChild, "border");
-        }
-        let itemId: string = e.dataTransfer.getData("itemId");
-        if (itemId && itemId !== dropitemId) {
-            this.handleDragDropEvent({child: itemId, parent: dropitemId});
-        }
-
-        e.dataTransfer.clearData();
-        e.preventDefault();
-        e.stopPropagation();
-    }
-
-    private handleDragDropEvent(obj: any) {
-        this.treelistChange$.emit(obj);
-    }*/
-
-
-    public drop(event: CdkDragDrop<string[]>) {
-        let new_pos_el = {
-            new_position: event.currentIndex,
-            id: event.item.element.nativeElement.id,
-            parent_id: event.container.data
-        };
-        console.log("HEY1: ", new_pos_el);
-        this.changeItemPosition_item$.emit(new_pos_el);
-    }
-
-    public changeItemPosition(el: any) {
-        console.log("HEY3: ", el);
-        this.changeItemPosition_item$.emit(el);
-    }
-
 }
