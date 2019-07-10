aeb369354b16edc9d619e3f03c97dffa9b52f4f7
maretval <valerie.maret@twentyreasons.com>
2019-07-10 11:52:45 +0200

    Package for knowledgebase / 20reasons update

--- src/spiceui.ts
+++ src/spiceui.ts
@@ -52,7 +52,7 @@ import {VersionManagerService} from "./services/versionmanager.service";
 import {modal} from "./services/modal.service";
 import {layout} from "./services/layout.service";
 import {GlobalLogin} from "./globalcomponents/components/globallogin";
-import {modelregister} from './services/modelregister.service';
+import {modelregister} from "./services/modelregister.service";
 
 // declarations for TS
 /**
