2c8bbb5fa7404d128987db0872da1d5876b0461e
Rainer Grohmann <rainer.grohmann@twentyreasons.com>
2019-07-31 09:43:43 +0200

    actionset_menus_refactored
    Change Request ID: 1000244

--- src/objectcomponents/objectcomponents.ts
+++ src/objectcomponents/objectcomponents.ts
@@ -34,6 +34,9 @@ import /*embed*/ {ObjectListHeaderActionMenu} from './components/objectlistheade
 import /*embed*/ {ObjectListHeaderActionsExportCSVButton} from './components/objectlistheaderactionsexportcsvbutton';
 import /*embed*/ {ObjectListHeaderActionsExportTargetlistButton} from './components/objectlistheaderactionsexporttargetlistbutton';
 import /*embed*/ {ObjectListHeaderActionsExportTargetlistModal} from './components/objectlistheaderactionsexporttargetlistmodal';
+import /*embed*/ {ObjectListHeaderActionsSelectAllButton} from "./components/objectlistheaderactionsselectallbutton";
+import /*embed*/ {ObjectListHeaderActionsUnselectAllButton} from "./components/objectlistheaderactionsunselectallbutton";
+
 import /*embed*/ {ObjectListItem} from './components/objectlistitem';
 import /*embed*/ {ObjectListItemField} from './components/objectlistitemfield';
 import /*embed*/ {ObjectActionMenu} from './components/objectactionmenu';
@@ -48,6 +51,9 @@ import /*embed*/ {ObjectActionContainer} from './components/objectactioncontaine
 import /*embed*/ {ObjectActionEditButton} from './components/objectactioneditbutton';
 import /*embed*/ {ObjectActionDeleteButton} from './components/objectactiondeletebutton';
 import /*embed*/ {ObjectActionAuditlogButton} from './components/objectactionauditlogbutton';
+import /*embed*/ {ObjectActionOpenButton} from './components/objectactionopenbutton';
+import /*embed*/ {ObjectActionCancelButton} from './components/objectactioncancelbutton';
+import /*embed*/ {ObjectActionRemoveButton} from "./components/objectactionremovebutton";
 import /*embed*/ {ObjectActionAuditlogModal} from './components/objectactionauditlogmodal';
 import /*embed*/ {ObjectActionNewButton} from './components/objectactionnewbutton';
 import /*embed*/ {ObjectActionDuplicateButton} from './components/objectactionduplicatebutton';
@@ -214,6 +220,7 @@ import /*embed*/ {ObjectTextsAddModal} from "./components/objecttextsaddmodal";
 
 import /*embed*/ {ObjectRecordMessagesBadge} from "./components/objectrecordmessagesbadge";
 
+
 /**
  * This module encapsulates various components that are used related to an object or the handling of multiple objects
  */
@@ -266,6 +273,8 @@ import /*embed*/ {ObjectRecordMessagesBadge} from "./components/objectrecordmess
         ObjectListHeaderActionsExportCSVButton,
         ObjectListHeaderActionsExportTargetlistButton,
         ObjectListHeaderActionsExportTargetlistModal,
+        ObjectListHeaderActionsSelectAllButton,
+        ObjectListHeaderActionsUnselectAllButton,
         ObjectListItem,
         ObjectListItemField,
         ObjectActionMenu,
@@ -298,6 +307,9 @@ import /*embed*/ {ObjectRecordMessagesBadge} from "./components/objectrecordmess
         ObjectActionSaveButton,
         ObjectActionDeleteButton,
         ObjectActionAuditlogButton,
+        ObjectActionOpenButton,
+        ObjectActionCancelButton,
+        ObjectActionRemoveButton,
         ObjectActionAuditlogModal,
         ObjectGDPRModal,
         ObjectActionNewButton,
