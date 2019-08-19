0512991dab95e38287d40ac9d11e0d7e29afdb67
Rainer Grohmann <rainer.grohmann@twentyreasons.com>
2019-08-19 07:32:15 +0200

    Zwischencommit

---
+++ src/objectcomponents/components/objectactionnewbeanfromrelatedbutton.ts
@@ -0,0 +1,55 @@
+/**
+ * @module ObjectComponents
+ */
+import {Component, OnInit} from "@angular/core";
+import {metadata} from "../../services/metadata.service";
+import {model} from "../../services/model.service";
+import {language} from "../../services/language.service";
+
+@Component({
+    selector: "object-action-new-bean-from-related-button",
+    templateUrl: "./src/objectcomponents/templates/objectactionnewbeanfromrelatedbutton.html"
+})
+export class ObjectActionNewBeanFromRelatedButton implements OnInit {
+
+    public parent: any = {};
+    public disabled: boolean = false;
+    public actionconfig: any = {};
+
+    constructor(private language: language, private metadata: metadata, private model: model) {
+
+    }
+
+    public ngOnInit() {
+
+
+        // this.relatedmodels
+        // this.model.module = this.relatedmodels.relatedModule;
+        // if (this.model.module && this.metadata.checkModuleAcl(this.model.module, "create")) {
+        //     this.disabled = false;
+        // }
+    }
+
+    public execute() {
+
+
+
+        console.log("this.model_", this.model);
+        console.log("this.actionconfig.module_", this.actionconfig.module);
+
+        // if (!this.parent.data.id) {
+        //     this.parent.data.id = this.parent.id;
+        // }
+        //
+        // // make sure we have no id so a new on gets issues
+        // this.model.id = "";
+        //
+        // // add the model
+        // this.model.addModel("", this.model).subscribe(response => {
+        //     if (response != false) {
+        //         this.relatedmodels.addItems([response]);
+        //     }
+        // });
+    }
+
+}
