42c0fd9674115d42bc06cf5b62c9fb5407c9f35a
Rainer Grohmann <rainer.grohmann@twentyreasons.com>
2019-08-29 12:22:28 +0200

    - create new from related button

--- src/objectcomponents/components/objectactionnewbeanfromrelatedbutton.ts
+++ src/objectcomponents/components/objectactionnewbeanfromrelatedbutton.ts
@@ -1,60 +1,52 @@
 /**
  * @module ObjectComponents
  */
-import {Component, Inject, OnInit} from "@angular/core";
+import {Component, Directive, Inject, OnInit, ViewChild, ViewContainerRef} from "@angular/core";
 import {metadata} from "../../services/metadata.service";
 import {model} from "../../services/model.service";
+import {modal} from "../../services/modal.service";
 import {language} from "../../services/language.service";
+
+
+@Directive({
+    selector: "object-action-new-bean-from-related-button-model-helper",
+    providers: [model]
+})
+export class ObjectActionNewBeanFromRelatedButtonModelHelper {
+    constructor(public model: model) {}
+}
+
 
 @Component({
     selector: "object-action-new-bean-from-related-button",
     templateUrl: "./src/objectcomponents/templates/objectactionnewbeanfromrelatedbutton.html",
-    providers: [{provide: 'modelnew', useClass: model}]
 })
 export class ObjectActionNewBeanFromRelatedButton implements OnInit {
 
+    // To provide a new model, we need a child component!
+    @ViewChild(ObjectActionNewBeanFromRelatedButtonModelHelper, {static: true}) private child;
+
     public parent: any = {};
-    public disabled: boolean = false;
+    public disabled: boolean = true;
     public actionconfig: any = {};
 
-    constructor(private language: language, private metadata: metadata, private model: model, private modelnew: model) {
+    constructor(private language: language, private metadata: metadata, private model: model, private modal: modal) {
 
     }
 
     public ngOnInit() {
-
-
-        // this.relatedmodels
-        // this.model.module = this.relatedmodels.relatedModule;
-        // if (this.model.module && this.metadata.checkModuleAcl(this.model.module, "create")) {
-        //     this.disabled = false;
-        // }
+        // check acl
+        if (this.actionconfig.module && this.metadata.checkModuleAcl(this.actionconfig.module, "create")) {
+            this.disabled = false;
+        }
     }
 
     public execute() {
-
-    this.modelnew.module = this.actionconfig.module;
-    this.modelnew.addModel("", this.model);
-
-
-    console.log("this.model_", this.model);
-    console.log("this.actionconfig.module", this.actionconfig.module);
-    console.log("parent", parent);
-
-
-
-        // if (!this.parent.data.id) {
-        //     this.parent.data.id = this.parent.id;
-        // }
-        //
-        // // make sure we have no id so a new on gets issues
-        // this.model.id = "";
-        //
-        // // add the model
-        // this.model.addModel("", this.model).subscribe(response => {
-        //     if (response != false) {
-        //         this.relatedmodels.addItems([response]);
-        //     }
-        // });
+        // Set the module of the new model and open a modal with copy rules
+        this.child.model.module = this.actionconfig.module;
+        this.child.model.addModel("", this.model);
     }
 }
+
+
+
