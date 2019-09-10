eebde07739da90304b1ad93aca7537dc9d1d37cb
christianknoll <christian.g.knoll@gmail.com>
2019-09-10 09:58:27 +0200

    new fields for quanity and units of measure, updated currency and currencies field

---
+++ src/objectfields/components/fieldunitsofmeasure.ts
@@ -0,0 +1,40 @@
+/**
+ * @module ObjectFields
+ */
+import {Component} from '@angular/core';
+import {model} from '../../services/model.service';
+import {view} from '../../services/view.service';
+import {language} from '../../services/language.service';
+import {metadata} from '../../services/metadata.service';
+import {configurationService} from '../../services/configuration.service';
+import {fieldGeneric} from './fieldgeneric';
+import {Router} from '@angular/router';
+
+/**
+ * renders a field that allows selection of currencies
+ */
+@Component({
+    selector: 'field-units-of-measure',
+    templateUrl: './src/objectfields/templates/fieldunitsofmeasure.html'
+})
+export class fieldUnitsOfMeasure extends fieldGeneric {
+
+    private uoms: any[] = [];
+
+    constructor(public model: model, public view: view, public language: language, public metadata: metadata, public router: Router, private configuration: configurationService) {
+        super(model, view, language, metadata, router);
+
+        this.uoms = this.configuration.getData('uomunits');
+    }
+
+    private getUOMLabel() {
+        if (this.value) {
+            let uom = this.uoms.find(u => u.id == this.value);
+            if (uom) return this.language.getLabel(uom.label);
+        }
+
+        // if no found return the value
+        return this.value;
+
+    }
+}
