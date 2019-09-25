8ed915c9ec7725e5fda57cae3cc32afc59297bc9
maretval <valerie.maret@twentyreasons.com>
2019-09-25 21:07:30 +0200

    SpiceDuns improvements

--- src/include/duns/components/fielddunsnumber.ts
+++ src/include/duns/components/fielddunsnumber.ts
@@ -24,6 +24,7 @@ export class FieldDunsNumber extends fieldGeneric {
     private fieldCountry: string = 'billing_address_country';
     private fieldHouseNumber: string = 'billing_address_hsnm';
     private neverOverwriteAddress: boolean = false;
+    private guessHouseNumber: boolean = false;
 
     constructor(public model: model,
                 public view: view,
@@ -47,6 +48,7 @@ export class FieldDunsNumber extends fieldGeneric {
     */
     private loadComponentConfig() {
         if (this.fieldconfig.never_overwrite_address) this.neverOverwriteAddress = true;
+        if (this.fieldconfig.guess_house_number) this.guessHouseNumber = true;
         if (!this.fieldconfig.address_type || this.fieldconfig.address_type.length == 0) return;
         this.fieldStreet = this.fieldconfig.address_type + '_address_street';
         this.fieldCity = this.fieldconfig.address_type + '_address_city';
@@ -96,22 +98,48 @@ export class FieldDunsNumber extends fieldGeneric {
         });
     }
 
-    /*
-    * @param res
-    * @return void
-    */
+    /**
+     * set field values in form.
+     * consider overwriting current address in form with DUNS results
+     * do not overwrite if user selected "nothing found"
+     * check on street / house number split
+     * @param res
+     * @return void
+     */
     private setAddressFields(res) {
         if (!this.model.getField(this.fieldStreet) || this.model.getField(this.fieldStreet).length == 0 || !this.neverOverwriteAddress) {
-            this.model.setField(this.fieldStreet, res.street || '');
+            if(res.street.length > 0) {
+                this.model.setField(this.fieldStreet, res.street || '');
+            }
         }
+        // overwrite street when guess house number is on
+        if(this.guessHouseNumber) {
+            if (!this.model.getField(this.fieldStreet) || this.model.getField(this.fieldStreet).length == 0 || !this.neverOverwriteAddress) {
+                if(res.street_only.length > 0) {
+                    this.model.setField(this.fieldStreet, res.street_only || '');
+                }
+            }
+            if (!this.model.getField(this.fieldHouseNumber) || this.model.getField(this.fieldHouseNumber).length == 0 || !this.neverOverwriteAddress) {
+                if(res.hsnm.length > 0) {
+                    this.model.setField(this.fieldHouseNumber, res.hsnm || '');
+                }
+            }
+        }
+
         if (!this.model.getField(this.fieldCity) || this.model.getField(this.fieldCity).length == 0 || !this.neverOverwriteAddress) {
-            this.model.setField(this.fieldCity, res.city || '');
+            if(res.city.length > 0) {
+                this.model.setField(this.fieldCity, res.city || '');
+            }
         }
         if (!this.model.getField(this.fieldPostalCode) || this.model.getField(this.fieldPostalCode).length == 0 || !this.neverOverwriteAddress) {
-            this.model.setField(this.fieldPostalCode, res.postalcode || '');
+            if(res.postalcode.length > 0) {
+                this.model.setField(this.fieldPostalCode, res.postalcode || '');
+            }
         }
         if (!this.model.getField(this.fieldCountry) || this.model.getField(this.fieldCountry).length == 0 || !this.neverOverwriteAddress) {
-            this.model.setField(this.fieldCountry, res.country || '');
+            if(res.country.length > 0) {
+                this.model.setField(this.fieldCountry, res.country || '');
+            }
         }
     }
 }
