d278e93c59f56f3e33932f1529029e4229ba36b4
Christian Knoll <christian.knoll@twentyreasons.com>
2019-10-01 08:55:35 +0200

    user access check .. aloow users to access own user record

--- src/services/metadata.service.ts
+++ src/services/metadata.service.ts
@@ -1446,9 +1446,10 @@ export class aclCheck implements CanActivate {
         }
 
         // otherwise check here
-        if (route.params.module === 'Users' && !this.session.authData.admin) {
+        if (route.params.module === 'Users' && !route.params.id && !this.session.authData.admin) {
             return false;
         } // prevents non-admins from listing the user list
+
         // if ( route.params.module === 'Users' && this.session.authData.portalOnly ) return false; // prevents "portal only users" from listing the user list
         if (route.params.module && route.params.module != "Home" && !this.metadata.checkModuleAcl(route.params.module, route.data.aclaction)) {
             this.router.navigate(["/modules/Home"]);
