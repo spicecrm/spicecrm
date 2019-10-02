d8084a7df0ac8a3ca51b823cdeb1964a40f2dfd2
Christian Knoll <christian.knoll@twentyreasons.com>
2019-10-02 22:52:58 +0200

    cleanup and documentation

--- src/modules/workflow/components/workflowpanelheader.ts
+++ src/modules/workflow/components/workflowpanelheader.ts
@@ -13,36 +13,54 @@ import {language} from '../../../services/language.service';
     templateUrl: './src/modules/workflow/templates/workflowpanelheader.html'
 
 })
-export class WorkflowPanelHeader implements OnDestroy{
+export class WorkflowPanelHeader implements OnDestroy {
 
-    broadcastSubscription: any = {};
-    workflowcount: number = 0;
+    /**
+     * subscroibe to the broadcast to catch when the panel issues the number
+     */
+    private broadcastSubscription: any = {};
+
+    /**
+     * the count recieved
+     */
+    private workflowcount: number = 0;
 
     constructor(private model: model, private language: language, private broadcast: broadcast) {
         this.broadcastSubscription = this.broadcast.message$.subscribe(message => {
             this.handleMessage(message);
-        })
+        });
     }
 
-    get hasWorkFlows(){
+    /**
+     * check if there are workflows
+     */
+    get hasWorkFlows() {
         return this.workflowcount > 0 ? true : false;
     }
 
-    handleMessage(message: any) {
+    /**
+     * handle the broadcast message
+     *
+     * @param message
+     */
+    private handleMessage(message: any) {
         // only handle if the module is the list module
-        if (message.messagedata.module !== this.model.module && message.messagedata.id !== this.model.id)
+        if (message.messagedata.module !== this.model.module && message.messagedata.id !== this.model.id){
             return;
+        }
 
         switch (message.messagetype) {
             case 'workflows.loaded':
-                    this.workflowcount = message.messagedata.workflowcount;
+                this.workflowcount = message.messagedata.workflowcount;
                 break;
 
         }
     }
 
-    ngOnDestroy(){
+    /**
+     * make sure on destroy to unsubscribe from the broadcast
+     */
+    public ngOnDestroy() {
         this.broadcastSubscription.unsubscribe();
     }
-
 }
