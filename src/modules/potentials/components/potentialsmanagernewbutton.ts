91e3327c381ce66b131dd38f710e94fa1894a883
Christian Knoll <christian.knoll@twentyreasons.com>
2019-10-03 20:43:45 +0200

    add potentials button in potentials manager

--- src/modules/potentials/components/potentialsmanagernewbutton.ts
+++ src/modules/potentials/components/potentialsmanagernewbutton.ts
@@ -13,14 +13,36 @@ import {language} from "../../../services/language.service";
 })
 export class PotentialsManagerNewButton {
 
+    /**
+     * the parent model in which context we are
+     */
     @Input() private parent: any;
+
+    /**
+     * the input for the companycode. This is passed in when the new potential is created
+     */
     @Input() private companyCode: string;
+
+    /**
+     * set if the button is disabled
+     */
+    private disabled: boolean = true;
 
     constructor(public language: language, public metadata: metadata, public model: model) {
         this.model.module = 'Potentials';
+
+        // check if we can add a Poential
+        if (this.metadata.checkModuleAcl('Potentials', 'create')) {
+            this.disabled = false;
+        }
     }
 
+    /**
+     * executes the click
+     */
     public execute() {
+        if (this.disabled) return;
+
         // make sure we have no idea so a new on gets issues
         this.model.id = "";
         this.model.addModel("", this.parent, {companycode_id: this.companyCode});
