601ad345765bb99d158d331b8fa5642afd6ccbc6
Christian Knoll <christian.knoll@twentyreasons.com>
2019-10-06 22:19:58 +0200

    formatting fix for calendar with calendar weeks

--- src/modules/calendar/services/calendar.service.ts
+++ src/modules/calendar/services/calendar.service.ts
@@ -37,7 +37,7 @@ export class calendar implements OnDestroy {
     public calendars: any = {};
     public currentStart: any = {};
     public currentEnd: any = {};
-    public sidebarwidth: number = 350;
+    public sidebarwidth: number = 360;
     public sheetTimeWidth: number = 50;
     public sheetHourHeight: number = 80;
     public weekstartday: number = 0;
