7341e8d4f9d034ac0406b4b127f8e1552ca7ea94
Christian Knoll <christian.knoll@twentyreasons.com>
2019-10-08 15:33:14 +0200

    set default company code from user

--- src/modules/potentials/components/potentialsmanager.ts
+++ src/modules/potentials/components/potentialsmanager.ts
@@ -101,8 +101,11 @@ export class PotentialsManager extends ObjectRelatedList implements OnInit {
         let companyCodes = this.configuration.getData('companycodes');
         if (companyCodes && companyCodes.length > 0) {
             companyCodes.sort((a, b) => a.name > b.name ? -1 : 1);
-            this._companyCode = companyCodes[0].id;
-
+            if (this.userpreferences.companyCodeId) {
+                this._companyCode = this.userpreferences.companyCodeId;
+            } else {
+                this._companyCode = companyCodes[0].id;
+            }
             // set the related models filter
             this.relatedmodels.fieldfilters = {companycode_id: this._companyCode};
 
--- src/services/userpreferences.service.ts
+++ src/services/userpreferences.service.ts
@@ -54,7 +54,7 @@ export class userpreferences {
 
     public formats = {nameFormats: [], loaded: false};
 
-    constructor( private backend: backend, private toast: toast, private configuration: configurationService, private language: language, private broadcast: broadcast, private modalservice: modal, private session: session ) {
+    constructor(private backend: backend, private toast: toast, private configuration: configurationService, private language: language, private broadcast: broadcast, private modalservice: modal, private session: session) {
         this.toUse = this.preferences.global;
         this.retrievePrefsFromConfigService();
         this.broadcast.message$.subscribe(msg => {
@@ -66,10 +66,10 @@ export class userpreferences {
         let prefs = this.configuration.getData('globaluserpreferences');
         this.preferences.global = _.extendOwn(this.preferences.global, prefs);
         this.unchangedPreferences.global = _.clone(prefs);
-        this.defaults = _.extendOwn( this.defaults, this.configuration.getData('defaultuserpreferences'));
+        this.defaults = _.extendOwn(this.defaults, this.configuration.getData('defaultuserpreferences'));
         this.askForMissingPreferences();
         this.completePreferencesWithDefaults();
-        this.session.setTimezone( this.toUse.timezone ); // Tell the UI the current time zone.
+        this.session.setTimezone(this.toUse.timezone); // Tell the UI the current time zone.
     }
 
     public getPreferences(loadhandler: Subject<string>) {
@@ -81,12 +81,12 @@ export class userpreferences {
     public loadPreferences(category = 'global'): Observable<any> {
         let retSubject: Subject<any> = new Subject<any>();
 
-        this.backend.getRequest('user/'+this.session.authData.userId+'/preferences/' + category).subscribe((prefs) => {
+        this.backend.getRequest('user/' + this.session.authData.userId + '/preferences/' + category).subscribe((prefs) => {
             this.preferences[category] = _.extendOwn(this.preferences[category], prefs);
             if (category === 'global') {
                 this.unchangedPreferences.global = _.clone(prefs);
                 this.completePreferencesWithDefaults();
-                this.session.setTimezone( this.toUse.timezone ); // Tell the UI the current time zone.
+                this.session.setTimezone(this.toUse.timezone); // Tell the UI the current time zone.
             } else {
                 this.unchangedPreferences[category] = _.clone(prefs);
             }
@@ -130,7 +130,7 @@ export class userpreferences {
             let prefs = {};
             prefs[name] = value;
             const saved = new Subject();
-            this.backend.postRequest('user/'+this.session.authData.userId+'/preferences/' + category, {}, prefs).subscribe(response => {
+            this.backend.postRequest('user/' + this.session.authData.userId + '/preferences/' + category, {}, prefs).subscribe(response => {
 
                 // set the preference
                 if (!this.preferences[category]) this.preferences[category] = {};
@@ -141,24 +141,24 @@ export class userpreferences {
                 this.unchangedPreferences[category][name] = value;
 
                 this.completePreferencesWithDefaults();
-                if ( category === 'global' && name === 'timezone' ) this.session.setTimezone( this.toUse.timezone ); // Tell the UI the current time zone.
-                saved.next( response );
+                if (category === 'global' && name === 'timezone') this.session.setTimezone(this.toUse.timezone); // Tell the UI the current time zone.
+                saved.next(response);
             }, error => {
-                saved.error( error );
+                saved.error(error);
             });
             return saved;
         } else {
-            if(!this.preferences[category]) this.preferences[category] = {};
+            if (!this.preferences[category]) this.preferences[category] = {};
             this.preferences[category][name] = value;
             this.completePreferencesWithDefaults();
-            if ( category === 'global' && name === 'timezone' ) this.session.setTimezone( this.toUse.timezone ); // Tell the UI the current time zone.
+            if (category === 'global' && name === 'timezone') this.session.setTimezone(this.toUse.timezone); // Tell the UI the current time zone.
         }
         return null;
     }
 
     public setPreferences(prefs, category = 'global') {
         const saved = new Subject();
-        this.backend.postRequest('user/'+this.session.authData.userId+'/preferences/' + category, {}, prefs).subscribe(
+        this.backend.postRequest('user/' + this.session.authData.userId + '/preferences/' + category, {}, prefs).subscribe(
             (savedprefs) => {
                 for (let prop in this.preferences[category]) {
                     if (savedprefs.hasOwnProperty(prop)) this.preferences[category][prop] = savedprefs[prop];
@@ -166,7 +166,7 @@ export class userpreferences {
                 }
                 this.unchangedPreferences[category] = savedprefs;
                 this.completePreferencesWithDefaults();
-                this.session.setTimezone( this.toUse.timezone ); // Tell the UI the current time zone. It might got changed.
+                this.session.setTimezone(this.toUse.timezone); // Tell the UI the current time zone. It might got changed.
                 saved.next(true);
             },
             (error) => {
@@ -274,16 +274,16 @@ export class userpreferences {
 
         // Is there a timeshift between the configured user timezone and the timezone of the currently used client computer system?
         let timeshift = 0;
-        if ( this.unchangedPreferences.global && this.unchangedPreferences.global.timezone ) {
-            let a = moment.tz( moment.tz.guess() ).utcOffset();
-            let b = moment.tz( this.unchangedPreferences.global.timezone ).utcOffset();
-            if ( a !== b ) {
-                timeshift = ( a * b < 0 ? Math.abs( a ) + Math.abs( b ) : Math.abs( a - b )) / 60;
+        if (this.unchangedPreferences.global && this.unchangedPreferences.global.timezone) {
+            let a = moment.tz(moment.tz.guess()).utcOffset();
+            let b = moment.tz(this.unchangedPreferences.global.timezone).utcOffset();
+            if (a !== b) {
+                timeshift = (a * b < 0 ? Math.abs(a) + Math.abs(b) : Math.abs(a - b)) / 60;
             }
         }
 
         // No user preferences missing and no timeshift? Nothing to do!
-        if ( namesOfMissingPrefs.length === 0 && timeshift === 0 ) return;
+        if (namesOfMissingPrefs.length === 0 && timeshift === 0) return;
 
         // Otherwise open the modal window to obtain preferences:
         this.modalservice.openModal('GlobalObtainImportantPreferences').subscribe(modal => {
@@ -294,39 +294,48 @@ export class userpreferences {
 
     private getNamesOfMissingImportantPrefs(): string[] {
         let missing = [];
-        for ( let name of ['timezone','datef','timef']) {
-            if ( !this.unchangedPreferences.global[name] ) missing.push( name );
+        for (let name of ['timezone', 'datef', 'timef']) {
+            if (!this.unchangedPreferences.global[name]) missing.push(name);
         }
         return missing;
     }
 
     public getPossibleDateFormats(): object[] {
         return [
-            { name: moment().format( this.jsDateFormat2momentDateFormat( "Y-m-d" ) ), value: "Y-m-d" },
-            { name: moment().format( this.jsDateFormat2momentDateFormat( "m-d-Y" ) ), value: "m-d-Y" },
-            { name: moment().format( this.jsDateFormat2momentDateFormat( "d-m-Y" ) ), value: "d-m-Y" },
-            { name: moment().format( this.jsDateFormat2momentDateFormat( "Y/m/d" ) ), value: "Y/m/d" },
-            { name: moment().format( this.jsDateFormat2momentDateFormat( "m/d/Y" ) ), value: "m/d/Y" },
-            { name: moment().format( this.jsDateFormat2momentDateFormat( "d/m/Y" ) ), value: "d/m/Y" },
-            { name: moment().format( this.jsDateFormat2momentDateFormat( "Y.m.d" ) ), value: "Y.m.d" },
-            { name: moment().format( this.jsDateFormat2momentDateFormat( "d.m.Y" ) ), value: "d.m.Y" },
-            { name: moment().format( this.jsDateFormat2momentDateFormat( "m.d.Y" ) ), value: "m.d.Y" }
+            {name: moment().format(this.jsDateFormat2momentDateFormat("Y-m-d")), value: "Y-m-d"},
+            {name: moment().format(this.jsDateFormat2momentDateFormat("m-d-Y")), value: "m-d-Y"},
+            {name: moment().format(this.jsDateFormat2momentDateFormat("d-m-Y")), value: "d-m-Y"},
+            {name: moment().format(this.jsDateFormat2momentDateFormat("Y/m/d")), value: "Y/m/d"},
+            {name: moment().format(this.jsDateFormat2momentDateFormat("m/d/Y")), value: "m/d/Y"},
+            {name: moment().format(this.jsDateFormat2momentDateFormat("d/m/Y")), value: "d/m/Y"},
+            {name: moment().format(this.jsDateFormat2momentDateFormat("Y.m.d")), value: "Y.m.d"},
+            {name: moment().format(this.jsDateFormat2momentDateFormat("d.m.Y")), value: "d.m.Y"},
+            {name: moment().format(this.jsDateFormat2momentDateFormat("m.d.Y")), value: "m.d.Y"}
         ];
     }
 
     public getPossibleTimeFormats(): object[] {
         return [
-            { name: moment().format( this.jsTimeFormat2momentTimeFormat( "H:i" ) ), value: "H:i" },
-            { name: moment().format( this.jsTimeFormat2momentTimeFormat( "h:ia" ) ), value: "h:ia" },
-            { name: moment().format( this.jsTimeFormat2momentTimeFormat( "h:iA" ) ), value: "h:iA" },
-            { name: moment().format( this.jsTimeFormat2momentTimeFormat( "h:i a" ) ), value: "h:i a" },
-            { name: moment().format( this.jsTimeFormat2momentTimeFormat( "h:i A" ) ), value: "h:i A" },
-            { name: moment().format( this.jsTimeFormat2momentTimeFormat( "H.i" ) ), value: "H.i" },
-            { name: moment().format( this.jsTimeFormat2momentTimeFormat( "h.ia" ) ), value: "h.ia" },
-            { name: moment().format( this.jsTimeFormat2momentTimeFormat( "h.iA" ) ), value: "h.iA" },
-            { name: moment().format( this.jsTimeFormat2momentTimeFormat( "h.i a" ) ), value: "h.i a" },
-            { name: moment().format( this.jsTimeFormat2momentTimeFormat( "h.i A" ) ), value: "h.i A" }
+            {name: moment().format(this.jsTimeFormat2momentTimeFormat("H:i")), value: "H:i"},
+            {name: moment().format(this.jsTimeFormat2momentTimeFormat("h:ia")), value: "h:ia"},
+            {name: moment().format(this.jsTimeFormat2momentTimeFormat("h:iA")), value: "h:iA"},
+            {name: moment().format(this.jsTimeFormat2momentTimeFormat("h:i a")), value: "h:i a"},
+            {name: moment().format(this.jsTimeFormat2momentTimeFormat("h:i A")), value: "h:i A"},
+            {name: moment().format(this.jsTimeFormat2momentTimeFormat("H.i")), value: "H.i"},
+            {name: moment().format(this.jsTimeFormat2momentTimeFormat("h.ia")), value: "h.ia"},
+            {name: moment().format(this.jsTimeFormat2momentTimeFormat("h.iA")), value: "h.iA"},
+            {name: moment().format(this.jsTimeFormat2momentTimeFormat("h.i a")), value: "h.i a"},
+            {name: moment().format(this.jsTimeFormat2momentTimeFormat("h.i A")), value: "h.i A"}
         ];
+    }
+
+    /**
+     * returns the users default company code
+     *
+     * does not actually come from the user preferences but fromt eh user itself but routes this throug here via the session
+     */
+    get companyCodeId() {
+        return this.session.authData.companycode_id;
     }
 
 }
