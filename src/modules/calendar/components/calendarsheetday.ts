8bb7bc33d25821dd014c1ea435a8fd9f7ba31c42
Mehyar <mehyar.sawas@twentyreasons.com>
2019-10-18 12:07:53 +0200

    Calendar: TrackBy fixed. Cleaned up.

--- src/modules/calendar/components/calendarsheetday.ts
+++ src/modules/calendar/components/calendarsheetday.ts
@@ -125,9 +125,18 @@ export class CalendarSheetDay implements OnChanges {
     /*
     * @param index
     * @param item
-    * @return index|item
+    * @return item.id
     */
-    private trackByFn(index, item) {
+    private trackByItemFn(index, item) {
+        return item.id;
+    }
+
+    /*
+    * @param index
+    * @param item
+    * @return index
+    */
+    private trackByIndexFn(index, item) {
         return index;
     }
 
