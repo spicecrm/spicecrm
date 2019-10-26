9622f2849543e735f2e9ee42d19d6a0ddafe980a
Christian Knoll <christian.knoll@twentyreasons.com>
2019-10-26 14:48:15 +0200

    cleanup and reuse horizontal fields component

--- src/globalcomponents/globalcomponents.ts
+++ src/globalcomponents/globalcomponents.ts
@@ -8,6 +8,7 @@ import {NgModule} from "@angular/core";
 import {RouterModule,} from "@angular/router";
 
 import {ObjectFields}      from "../objectfields/objectfields";
+import {ObjectComponents}      from "../objectcomponents/objectcomponents";
 import {SystemComponents}      from "../systemcomponents/systemcomponents";
 
 /**
@@ -94,6 +95,7 @@ import /*embed*/ {GlobalObtainImportantPreferences} from './components/globalobt
         ObjectFields,
         DirectivesModule,
         SystemComponents,
+        ObjectComponents,
         RouterModule.forRoot([
             // {path: "login", component: GlobalLogin},
             {path: "setup", component: GlobalSetup},
