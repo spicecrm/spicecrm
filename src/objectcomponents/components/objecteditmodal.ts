541fa859fb731a695dd73c70a452dc82949c3d4f
Christian Knoll <christian.knoll@twentyreasons.com>
2019-10-27 16:08:25 +0100

    when creating new records auto add them to the last viewed .. tracker

--- src/objectcomponents/components/objecteditmodal.ts
+++ src/objectcomponents/components/objecteditmodal.ts
@@ -41,7 +41,11 @@ export class ObjectEditModal implements OnInit {
      * ToDo: add documentation what we need this for
      */
     private actionSubject: Subject<any> = new Subject<any>();
-    private action$: Observable<any> = new Observable<any>();
+
+    /**
+     * this emits the data ... is referenced from the modal save button that handles this
+     */
+    public action$: Observable<any> = new Observable<any>();
 
     /**
      * set to true (default) to have the modal check for duplicates
@@ -91,11 +95,12 @@ export class ObjectEditModal implements OnInit {
         this.componentconfig = this.metadata.getComponentConfig(this.constructor.name, this.model.module);
         this.actionSetItems = this.metadata.getActionSetItems(this.componentconfig.actionset);
 
-        // pass the self object to the modalwindow service for the actionsets
+        // set the reference to self ..
+        // helper service so buttons can destroy the window
         this.modalwindow.self = this.self;
     }
 
-    get actionset(){
+    get actionset() {
         return this.componentconfig.actionset;
     }
 
@@ -126,10 +131,38 @@ export class ObjectEditModal implements OnInit {
     }
 
     /**
+     * handles the event emitted by the actionset
+     *
+     * @param event
+     */
+    private handleAction(event) {
+        console.log(event);
+        switch (event) {
+            case 'savegodetail':
+                this.model.goDetail();
+                this.actionSubject.complete();
+                this.self.destroy();
+                break;
+            case 'save':
+                this.actionSubject.next(event);
+                this.actionSubject.complete();
+                this.self.destroy();
+                break;
+            default:
+                this.actionSubject.next(false);
+                this.actionSubject.complete();
+                this.self.destroy();
+                break;
+        }
+    }
+
+    /**
      * saves the data and if not done before does a duplicate check before saving
      *
      * @param goDetail if set to true the system will naviaget to the detail fo teh record after saving
      */
+
+    /*
     private save(goDetail: boolean = false) {
         if (this.preventGoingToRecord) goDetail = false;
         if (this.model.validate()) {
@@ -151,6 +184,8 @@ export class ObjectEditModal implements OnInit {
         }
     }
 
+     */
+
     /**
      * returns if the duplicate check iss enabled for the module. Used for the visiblity of he duplicates button in the view
      */
@@ -163,6 +198,7 @@ export class ObjectEditModal implements OnInit {
      *
      * @param goDetail if set to true the system will naviaget to the detail fo teh record after saving
      */
+    /*
     private saveModel(goDetail: boolean = false) {
         this.modal.openModal('SystemLoadingModal').subscribe(modalRef => {
             modalRef.instance.messagelabel = 'LBL_SAVING_DATA';
@@ -188,7 +224,9 @@ export class ObjectEditModal implements OnInit {
                 });
         });
     }
+    */
 
+    /*
     private saveToRelated(related_module: string) {
         if (!this.model.validate()) {
             return false;
@@ -220,6 +258,7 @@ export class ObjectEditModal implements OnInit {
             this.router.navigate(['/module/' + related_module + '/' + related_id]);
         });
     }
+    */
 
     /*
     private setModule(module) {
@@ -227,34 +266,4 @@ export class ObjectEditModal implements OnInit {
     }
     */
 
-    /*
-     style function for the duplicate overlay
-     */
-    get duplicateStyle() {
-        if (this.modalContent) {
-            let rect = this.modalContent.element.nativeElement.getClientRects();
-            return {
-                height: rect[0].height + 'px',
-                width: rect[0].width + 'px',
-                top: '0px'
-            };
-        } else {
-            return {
-                height: '0px',
-                width: '0px',
-                top: '0px'
-            };
-        }
-    }
-
-    /*
-     style function to prevent overflow to display scrollbar when duplicate check is displayed
-     */
-    get contentStyle() {
-        if (this.showDuplicates) {
-            return {
-                'overflow-y': 'hidden'
-            };
-        }
-    }
 }
