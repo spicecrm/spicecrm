e8d35273faf048e85b5b738c0467908719a89775
strombo <andreas@gloeckl.name>
2019-10-28 13:43:06 +0100

    removed SystemInputTags2

--- src/objectfields/components/fieldtags.ts
+++ src/objectfields/components/fieldtags.ts
@@ -19,4 +19,50 @@ export class fieldTags extends fieldGeneric {
         super(model, view, language, metadata, router);
     }
 
+    get displayTags() {
+        let tagList: string[];
+        let tags = this.model.getField( this.fieldname );
+        if ( !tags ) return '';
+        try {
+            tagList = JSON.parse( tags );
+        } catch (e) {
+            return '';
+        }
+        return tagList.join(', ');
+    }
+
+    /**
+     * parses the tags from teh model and returns an array to be handled in the display for loop
+     */
+    get objecttags() {
+        let tags = this.model.getField('tags');
+        if (!tags || tags === '') return [];
+        else {
+            try {
+                return JSON.parse(tags);
+            } catch (e) {
+                return [];
+            }
+        }
+    }
+
+    private removeByIndex(index) {
+        let tags = this.objecttags;
+        tags.splice(index, 1);
+        this.model.setField('tags', JSON.stringify(tags));
+    }
+
+    /**
+     * adds the tag. This is called fromt eh event emitter ont he input box in the component that fires the tag when a new tag shoudl be added
+     *
+     * @param tag the tag
+     */
+    private addTag(tag) {
+        if( tag !== '' ) {
+            let tags = this.objecttags;
+            tags.push( tag );
+            this.model.setField( 'tags', JSON.stringify( tags ) );
+        }
+    }
+
 }
