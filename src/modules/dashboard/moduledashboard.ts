02b793031a7e599495427afc21d3aaf9a6a67e37
Mehyar <mehyar.sawas@twentyreasons.com>
2019-10-31 13:27:30 +0100

    Dashboards: logic improved.

--- src/modules/dashboard/moduledashboard.ts
+++ src/modules/dashboard/moduledashboard.ts
@@ -31,6 +31,7 @@ import /*embed*/ {DashboardGenericDashlet} from "./components/dashboardgenericda
 import /*embed*/ {DashboardGenericDashletRow} from "./components/dashboardgenericdashletrow";
 import /*embed*/ {DashboardRemindersDashlet} from "./components/dashboardremindersdashlet";
 import /*embed*/ {DashboardComponentset} from "./components/dashboardcomponentset";
+import /*embed*/ {DashboardSelectPanelAddButton} from "./components/dashboardselectpaneladdbutton";
 
 @NgModule({
     imports: [
@@ -54,6 +55,7 @@ import /*embed*/ {DashboardComponentset} from "./components/dashboardcomponentse
         DashboardGenericDashletRow,
         DashboardRemindersDashlet,
         DashboardComponentset,
+        DashboardSelectPanelAddButton
     ],
     exports: [
         DashboardContainer,
