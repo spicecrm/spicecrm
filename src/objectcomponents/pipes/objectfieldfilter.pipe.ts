ef8dacf35002cd875a926a1083c319e19ddb6091
Christian Knoll <christian.knoll@twentyreasons.com>
2019-10-31 12:45:19 +0100

    feature 1000311 selection of fields in List Export

---
+++ src/objectcomponents/pipes/objectfieldfilter.pipe.ts
@@ -0,0 +1,30 @@
+/**
+ * @module ObjectComponents
+ */
+import {Pipe, PipeTransform} from "@angular/core";
+import {language} from '../../services/language.service';
+import {model} from '../../services/model.service';
+
+/**
+ * transforms an object with an attribute and a value into an aray with objects with attributes key and value
+ */
+@Pipe({
+    name: 'objectfieldfilter',
+    pure: false
+})
+export class ObjectFieldFilterPipe implements PipeTransform {
+
+    constructor(private languange: language, private model: model) {
+
+    }
+
+    public transform(fields, filter): any {
+        let retfields = [];
+        for (let field of fields) {
+            if (field.toLowerCase().indexOf(filter.toLowerCase()) >= 0 || this.languange.getFieldDisplayName(this.model.module, field).toLowerCase().indexOf(filter.toLowerCase()) > 0) {
+                retfields.push(field);
+            }
+        }
+        return retfields;
+    }
+}
