bd48d4eee473003da1548b6bb82d71b000bd35da
Christian Knoll <christian.knoll@twentyreasons.com>
2019-11-06 16:00:27 +0100

    Update email from Bean Modal

--- src/objectcomponents/components/objectactionmailmodal.ts
+++ src/objectcomponents/components/objectactionmailmodal.ts
@@ -1,7 +1,7 @@
 /**
  * @module ObjectComponents
  */
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {metadata} from '../../services/metadata.service';
 import {model} from '../../services/model.service';
 import {view} from '../../services/view.service';
@@ -17,9 +17,32 @@ import {userpreferences} from '../../services/userpreferences.service';
 })
 export class ObjectActionMailModal implements OnInit {
 
-    public parent: any = null;
+    /**
+     * the parent
+     */
+    @Input() public parent: any = null;
+
+    /**
+     * reference to the modal window self instance
+     */
     public self: any = null;
+
+    /**
+     * inidcates that we are sending
+     */
     private sending: boolean = false;
+
+    /**
+     * the title for the modal window to be displayed
+     */
+    @Input() private titlelabel: string = 'LBL_BEANTOMAIL';
+
+    /**
+     * an event emitter when the email ahs been sent and the modal window will destroy itself
+     */
+    @Output() private mailsent: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+    private fieldset: string;
 
     constructor(private language: language,
                 private metadata: metadata,
@@ -28,11 +51,28 @@ export class ObjectActionMailModal implements OnInit {
                 private backend: backend,
                 private prefs: userpreferences,
                 private modal: modal) {
+
+        // initialize the model and the view
         this.model.module = 'Emails';
         this.view.isEditable = true;
         this.view.setEditMode();
+
+        // get the fieldset
+        let componentconfig = this.metadata.getComponentConfig('ObjectActionMailModal');
+        this.fieldset = componentconfig.fieldset;
     }
 
+    /**
+     * initialize the model and try to get the parent data
+     */
+    public ngOnInit() {
+        this.model.initializeModel(this.parent);
+        this.setParentData();
+    }
+
+    /**
+     * check if the send button is disabled
+     */
     get isDisabled() {
         let recipientAddresses = this.model.getFieldValue('recipient_addresses');
         let mailbox = this.model.getFieldValue('mailbox_id');
@@ -43,21 +83,25 @@ export class ObjectActionMailModal implements OnInit {
         return (!name || !body || !mailbox || !recipientAddresses || !recipientTo) ? true : this.sending;
     }
 
-    public ngOnInit() {
-        this.model.initializeModel(this.parent);
-        this.setParentData();
-    }
-
+    /**
+     * sets the data from the parent
+     */
     private setParentData() {
         this.model.setField('parent_type', this.parent.module);
         this.model.setField('parent_id', this.parent.id);
         this.model.setField('parent_name', this.parent.data.summary_text);
     }
 
+    /**
+     * close the modal
+     */
     private close() {
         this.self.destroy();
     }
 
+    /**
+     * send the email
+     */
     private sendEmail() {
         this.modal.openModal('SystemLoadingModal', false).subscribe(modalRef => {
             modalRef.instance.messagelabel = 'LBL_SENDING';
