f4b3ed299b9b4969d5e5c539203cfecb72440823
Christian Knoll <christian.knoll@twentyreasons.com>
2019-11-19 09:18:46 +0100

    listupdate and KREST Cleanup

--- src/modules/users/components/userpreferences.ts
+++ src/modules/users/components/userpreferences.ts
@@ -154,12 +154,12 @@ export class UserPreferences {
             this.dayHoursList.push(i);
         }
 
-        this.backend.getList("Dashboards", "name", "DESC", ["name", "id"], {limit: -1})
+        this.backend.getList("Dashboards", [{sortfield: "name", sortdirection: "DESC"}], ["name", "id"], {limit: -1})
             .subscribe((dashboards: any) => {
                 this.dashboards = dashboards.list;
             });
 
-        this.backend.getList("DashboardSets", "name", "DESC", ["name", "id"], {limit: -1})
+        this.backend.getList("DashboardSets", [{sortfield: "name", sortdirection: "DESC"}], ["name", "id"], {limit: -1})
             .subscribe((dashboardSets: any) => {
                 this.dashboardSets = dashboardSets.list;
             });
