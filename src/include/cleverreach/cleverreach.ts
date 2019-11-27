346f160cefef43d09a33a0140df549852a09cdd7
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2019-11-27 09:19:12 +0100

    clever reach maillog panel + setup

---
+++ src/include/cleverreach/cleverreach.ts
@@ -0,0 +1,41 @@
+/**
+ * @module CleverReachModule
+ */
+import {CommonModule} from "@angular/common";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+
+import {VersionManagerService} from "../../services/versionmanager.service";
+import {DirectivesModule} from "../../directives/directives";
+
+import {ObjectFields} from "../../objectfields/objectfields";
+import {GlobalComponents} from "../../globalcomponents/globalcomponents";
+import {ObjectComponents} from "../../objectcomponents/objectcomponents";
+import {SystemComponents} from "../../systemcomponents/systemcomponents";
+
+import /*embed*/ {CleverReachPanel} from "./components/cleverreachpanel";
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ObjectFields,
+        GlobalComponents,
+        ObjectComponents,
+        SystemComponents,
+        DirectivesModule,
+    ],
+    declarations: [
+        CleverReachPanel,
+    ]
+})
+export class CleverReachModule {
+    public readonly version = "1.0";
+    public readonly build_date = "/*build_date*/";
+
+    constructor(
+        private vms: VersionManagerService,
+    ) {
+        this.vms.registerModule(this);
+    }
+}
---
+++ src/include/cleverreach/components/cleverreachpanel.ts
@@ -0,0 +1,31 @@
+/**
+ * @module CleverReachModule
+ */
+import {Component} from "@angular/core";
+import {model} from "../../../services/model.service";
+import {language} from "../../../services/language.service";
+import {metadata} from "../../../services/metadata.service";
+import {backend} from "../../../services/backend.service";
+
+@Component({
+    templateUrl: "./src/include/cleverreach/templates/cleverreachpanel.html"
+})
+export class CleverReachPanel {
+
+    public CRList: any[] = [];
+    private isLoading: boolean = false;
+
+    constructor(private language: language, private model: model, private metadata: metadata, private backend: backend) {
+        this.getUserMails();
+    }
+
+    private getUserMails() {
+        this.isLoading = true;
+        this.backend.getRequest(`/CleverReach/Contact/${this.model.id}/mails`)
+            .subscribe(res => {
+                this.CRList = res;
+                window.console.log(res);
+                this.isLoading = false;
+            });
+    }
+}
