fc9554ab56a7615de39c21d469dab2aa5eaaceb7
Christian Knoll <christian.knoll@twentyreasons.com>
2019-11-29 17:53:37 +0100

    EWS Contacts Exchange Sync Button

--- src/modules/contacts/modulecontacts.ts
+++ src/modules/contacts/modulecontacts.ts
@@ -17,6 +17,7 @@ import /*embed*/ {ContactNewslettersButton} from "./components/contactnewsletter
 import /*embed*/ {ContactNewsletters} from "./components/contactnewsletters";
 import /*embed*/ {ContactPortalButton} from "./components/contactportalbutton";
 import /*embed*/ {ContactPortalDetails} from "./components/contactportaldetails";
+import /*embed*/ {ContactExchangeSyncButton} from "./components/contactexchangesyncbutton";
 
 @NgModule({
     imports: [
@@ -33,6 +34,7 @@ import /*embed*/ {ContactPortalDetails} from "./components/contactportaldetails"
         ContactNewsletters,
         ContactPortalButton,
         ContactPortalDetails,
+        ContactExchangeSyncButton
     ]
 })
 export class ModuleContacts {
