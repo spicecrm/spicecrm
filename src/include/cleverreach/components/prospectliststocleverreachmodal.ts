b702595f845496fc496c58a40abd814a66e8b1c0
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2019-12-06 14:20:15 +0100

    mailing

---
+++ src/include/cleverreach/components/prospectliststocleverreachmodal.ts
@@ -0,0 +1,67 @@
+/**
+ * @module CleverReachModule
+ */
+
+import {Component, OnInit, Injector} from '@angular/core';
+import {metadata} from '../../../services/metadata.service';
+import {model} from '../../../services/model.service';
+import {language} from '../../../services/language.service';
+import {modal} from '../../../services/modal.service';
+import {Router} from "@angular/router";
+import {backend} from '../../../services/backend.service';
+import {toast} from "../../../services/toast.service";
+
+@Component({
+    selector: 'prospectlists-to-cleverreach-modal',
+    templateUrl: './src/include/cleverreach/templates/prospectliststocleverreachmodal.html'
+})
+export class ProspectListsToCleverReachModal {
+
+    private self: any = {};
+    public statistics: any[] = [];
+    private module: string = '';
+
+    constructor(
+        private language: language,
+        private router: Router,
+        private metadata: metadata,
+        private backend: backend,
+        private model: model,
+        private modal: modal,
+        private toast: toast
+    ) {
+    }
+
+    private initialize() {
+        this.modal.openModal('SystemLoadingModal').subscribe(loadingRef => {
+            loadingRef.instance.messagelabel = 'LBL_EXPORTING';
+
+            this.backend.getRequest(`CleverReach/${this.model.module}/${this.model.id}/initialize`).subscribe(result => {
+                this.statistics = result;
+                window.console.log(this.statistics);
+                loadingRef.instance.self.destroy();
+            });
+        });
+    }
+
+    private transferToCleverReach() {
+        this.backend.postRequest(`CleverReach/${this.model.module}/${this.model.id}/transferToCleverReach`).subscribe(result => {
+            if (result.status == 'success') {
+                this.router.navigate([`/module/${this.model.module}/${this.model.id}`]);
+                this.close();
+            } else {
+                this.toast.sendToast(result.msg, 'error');
+            }
+        });
+
+    }
+
+    private close() {
+        this.self.destroy();
+    }
+
+    public ngOnInit() {
+        this.initialize();
+    }
+
+}
