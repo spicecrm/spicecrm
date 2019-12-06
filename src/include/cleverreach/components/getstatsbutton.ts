64e40f2d1a0c2ce8afe1343f468487ec378500dd
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2019-12-06 15:53:34 +0100

    emit loading modal

--- src/include/cleverreach/components/getstatsbutton.ts
+++ src/include/cleverreach/components/getstatsbutton.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit, Injector} from '@angular/core';
 import {metadata} from '../../../services/metadata.service';
 import {model} from '../../../services/model.service';
+import {modal} from '../../../services/modal.service';
 import {language} from '../../../services/language.service';
 import {backend} from "../../../services/backend.service";
+import {toast} from "../../../services/toast.service";
 
 /**
  * gets the statistics of a mailing / campaigntask
@@ -19,16 +21,25 @@ export class GetStatsButton {
         private language: language,
         private metadata: metadata,
         private model: model,
-        private backend: backend
+        private modal: modal,
+        private backend: backend,
+        private toast: toast
     ) {
     }
 
     public execute() {
+        let stopper = this.modal.await('Loading')
         this.backend.getRequest(`/CleverReach/CampaignTasks/${this.model.id}/report`)
             .subscribe(response => {
-                this.report = response;
-                window.console.log(this.report);
-            });
+                    this.toast.sendToast(this.language.getLabel('LBL_COMPLETED'));
+                    this.report = response;
+                    // window.console.log(this.report);
+                    stopper.emit(true);
+                },
+                (error) => {
+                    this.toast.sendAlert(this.language.getLabel('LBL_ERROR'));
+                    stopper.emit(true);
+                });
 
     }
 }
