6e33149beee70ae5608e18a3df6e9cff2d0f28da
strombo <andreas@gloeckl.name>
2019-12-13 13:47:57 +0100

    New field: fieldPhone

---
+++ src/objectfields/components/fieldphone.ts
@@ -0,0 +1,23 @@
+/**
+ * @module ObjectFields
+ */
+import {Component} from '@angular/core';
+import {model} from '../../services/model.service';
+import {view} from '../../services/view.service';
+import {language} from '../../services/language.service';
+import {metadata} from '../../services/metadata.service';
+import {backend} from '../../services/backend.service';
+import {fieldGeneric} from './fieldgeneric';
+import {Router} from '@angular/router';
+
+@Component({
+    selector: 'field-phone',
+    templateUrl: './src/objectfields/templates/fieldphone.html'
+})
+export class fieldPhone extends fieldGeneric {
+
+    constructor(public model: model, public view: view, public l: language, public metadata: metadata, public router: Router, private backend: backend) {
+        super(model, view, l, metadata, router);
+    }
+
+}
