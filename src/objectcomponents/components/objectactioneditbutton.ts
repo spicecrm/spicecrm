9ecb8ab859bcc12d22781d34a4a04199364a457a
Mehyar <mehyar.sawas@twentyreasons.com>
2019-12-16 16:17:25 +0100

    Object Actions: TSLint code styled. Documentation added. Necessary unsubscribe call on destroy added.

--- src/objectcomponents/components/objectactioneditbutton.ts
+++ src/objectcomponents/components/objectactioneditbutton.ts
@@ -1,18 +1,20 @@
 /**
  * @module ObjectComponents
  */
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {metadata} from '../../services/metadata.service';
 import {model} from '../../services/model.service';
 import {language} from '../../services/language.service';
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'object-action-edit-button',
     templateUrl: './src/objectcomponents/templates/objectactioneditbutton.html'
 })
-export class ObjectActionEditButton implements OnInit {
+export class ObjectActionEditButton implements OnInit, OnDestroy {
 
     public disabled: boolean = true;
+    private subscriptions: Subscription = new Subscription();
 
     constructor(
         private language: language,
@@ -23,27 +25,47 @@ export class ObjectActionEditButton implements OnInit {
     }
 
     public ngOnInit() {
-        this.handleDisabled(this.model.isEditing ? 'edit' : 'display');
-        this.model.mode$.subscribe(mode => {
-            this.handleDisabled(mode);
-        });
 
-        this.model.data$.subscribe(data => {
-            this.handleDisabled(this.model.isEditing ? 'edit' : 'display');
-        });
+        this.handleDisabled(this.model.isEditing ? 'edit' : 'display');
+
+        // handleDisabled on on model.mode changes
+        this.subscriptions.add(
+            this.model.mode$.subscribe(mode => {
+                this.handleDisabled(mode);
+            })
+        );
+
+        // handleDisabled on on model.data changes
+        this.subscriptions.add(
+            this.model.data$.subscribe(data => {
+                this.handleDisabled(this.model.isEditing ? 'edit' : 'display');
+            })
+        );
     }
 
+    /*
+    * @call model.edit
+    */
     public execute() {
         this.model.edit();
     }
 
+    /*
+    * @unsubscribe subscriptions
+    */
+    public ngOnDestroy() {
+        this.subscriptions.unsubscribe();
+    }
+
+    /*
+    * @return boolean
+    */
     private handleDisabled(mode) {
         if (this.model.data.acl && !this.model.checkAccess('edit')) {
 
             this.disabled = true;
             return;
         }
-        this.disabled = mode == 'edit' ? true : false;
+        this.disabled = mode == 'edit';
     }
-
 }
