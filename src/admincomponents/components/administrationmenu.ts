ea7a5909eba1445fbc78b9d16dbceadffb5d3adc
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2019-12-20 18:47:29 +0100

    admin components to do adminhomescreencarditem render items of card block

--- src/admincomponents/components/administrationmenu.ts
+++ src/admincomponents/components/administrationmenu.ts
@@ -3,6 +3,8 @@
  */
 import {
     Component,
+    Output,
+    EventEmitter,
     ViewChild,
     ViewContainerRef,
     ElementRef, OnDestroy, AfterViewInit} from '@angular/core';
@@ -13,24 +15,16 @@ import {backend} from '../../services/backend.service';
 import {language} from '../../services/language.service';
 import {broadcast} from '../../services/broadcast.service';
 import {session} from '../../services/session.service';
-import { AdminHomeScreen } from './adminhomescreen';
+import {administration} from "../services/administration.service";
+
 
 @Component({
-    selector: '[administration-menu]',
+    selector: 'administration-menu',
     templateUrl: './src/admincomponents/templates/administrationmenu.html'
 })
-export class AdministrationMenu implements OnDestroy {
+export class AdministrationMenu {
 
-    @ViewChild('admincontentcontainer', {read: ViewContainerRef, static: true}) private admincontentcontainer: ViewContainerRef;
-    @ViewChild('adminhomecontainer', {read: ViewContainerRef, static: true}) private adminhomecontainer: ViewContainerRef;
-
-    private admincontentObject: any = null;
-    private adminNavigation: any = {};
     private itemfilter: string = '';
-    private opened_itemid: any = {};
-    private hidden: boolean = false;
-
-    private broadcastsubscription: any;
 
     constructor(
         private router: Router,
@@ -41,54 +35,21 @@ export class AdministrationMenu implements OnDestroy {
         private navigation: navigation,
         private elementref: ElementRef,
         private session: session,
+        private administration: administration,
     ) {
-        this.loadNavigation();
-        this.navigation.setActiveModule('Administration');
-
-        this.broadcastsubscription = this.broadcast.message$.subscribe(message => {
-            this.handleMessage(message);
-        });
-    }
-
-    public ngOnDestroy() {
-        this.broadcastsubscription.unsubscribe();
-    }
-
-    private handleMessage(message) {
-        switch (message.messagetype) {
-            case 'loader.reloaded':
-                this.loadNavigation();
-                break;
-        }
-    }
-
-    private loadNavigation() {
-        this.backend.getRequest('spiceui/admin/navigation').subscribe(
-            nav => {
-                this.adminNavigation = nav;
-                // default open version control...
-                // this.openContent('Versioning', 'Version Control');
-            },
-
-        );
 
     }
 
-    private getContainerStyle() {
-        return {
-            height: 'calc(100vh - ' + this.elementref.nativeElement.offsetTop + 'px)'
-        };
-    }
 
     private getNavigationBlocks() {
         let blocks = [];
-        for (let block in this.adminNavigation) {
+        for (let block in this.administration.adminNavigation) {
 
             let isRelevant = this.itemfilter == '';
 
             // check if we find an item
             if (!isRelevant) {
-                this.adminNavigation[block].some(item => {
+                this.administration.adminNavigation[block].some(item => {
                     let name = item.adminaction;
                     if (item.admin_label) {
                         name = this.language.getLabel(item.admin_label);
@@ -111,7 +72,7 @@ export class AdministrationMenu implements OnDestroy {
     private getNavigationItems(block) {
         let items = [];
 
-        for (let item of this.adminNavigation[block]) {
+        for (let item of this.administration.adminNavigation[block]) {
             if ( item.componentconfig.onlyForDevs && !this.session.isDev ) continue;
             item.name = item.adminaction;
             if (item.admin_label) {
@@ -123,51 +84,17 @@ export class AdministrationMenu implements OnDestroy {
             }
 
         }
+
         return items;
     }
 
-    private openContent(block: string, item) {
-        this.hidden = true;
-        window.console.log(this.hidden);
-        // already loaded?
-        if (this.opened_itemid == item.id) {
-            return true;
-        }
-
-        this.opened_itemid = item.id;
-        if (this.admincontentObject) {
-            this.admincontentObject.destroy();
-        }
-
-        let adminItem: any = {};
-
-        if (!this.adminNavigation[block]) {
-            return false;
-        }
-
-        this.adminNavigation[block].some(blockAction => {
-                if (blockAction.id == item.id) {
-                    adminItem = blockAction;
-                    return true;
-                }
-            }
-        );
-
-        if (adminItem.component) {
-            // this.router.navigate(['admin/'+block+'/'+item.adminaction]);
-            this.metadata.addComponent(adminItem.component, this.admincontentcontainer).subscribe(admObject => {
-                admObject.instance.componentconfig = adminItem.componentconfig;
-                this.admincontentObject = admObject;
-            });
-        }
-    }
-
-    private loadHome() {
-        this.metadata.addComponent('AdminHomeScreen', this.adminhomecontainer);
-    }
-
-    public ngAfterViewInit() {
-        this.loadHome();
+    /**
+     * emit nav changes to main screen
+     *
+     * @param item
+     */
+    private openContent(block, item) {
+        this.administration.navigateto(block, item);
     }
 
 }
