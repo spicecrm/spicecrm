38d7804b831ba7b4bb7a46fca274795dfff6b6f8
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2019-12-20 18:47:29 +0100

    admin components to do adminhomescreencarditem render items of card block

---
+++ src/admincomponents/components/administrationhomescreen.ts
@@ -0,0 +1,35 @@
+/**
+ * @module AdminComponentsModule
+ */
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {metadata} from '../../services/metadata.service';
+import {language} from '../../services/language.service';
+import {administration} from "../services/administration.service";
+
+@Component({
+
+    templateUrl: './src/admincomponents/templates/administrationhomescreen.html'
+})
+
+export class AdministrationHomeScreen {
+
+    constructor(
+        private router: Router,
+        private metadata: metadata,
+        private language: language,
+        private administration: administration
+    ) {
+    }
+
+
+    /**
+     * emit nav changes to main screen
+     *
+     * @param item
+     */
+    private openContent(block, item) {
+        this.administration.navigateto(block, item);
+    }
+
+}
