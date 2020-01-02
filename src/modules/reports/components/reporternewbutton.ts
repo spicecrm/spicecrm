5502f75201aa541922194874963747a3bdd7673d
Mehyar <mehyar.sawas@twentyreasons.com>
2020-01-02 10:36:04 +0100

    Reports: new button: router navigate used.

--- src/modules/reports/components/reporternewbutton.ts
+++ src/modules/reports/components/reporternewbutton.ts
@@ -1,9 +1,6 @@
 import {Component} from "@angular/core";
 import {language} from "../../../services/language.service";
-import {modal} from "../../../services/modal.service";
-import {metadata} from "../../../services/metadata.service";
-import {model} from "../../../services/model.service";
-import {modellist} from "../../../services/modellist.service";
+import {Router} from "@angular/router";
 
 @Component({
     templateUrl: './src/modules/reports/templates/reporternewbutton.html'
@@ -12,26 +9,12 @@ import {modellist} from "../../../services/modellist.service";
 export class ReporterNewButton {
 
     constructor(private language: language,
-                private modal: modal,
-                private metadata: metadata,
-                private modelList: modellist,
-                private model: model) {
+                private router: Router) {
     }
-
+    /*
+    *navigate to designer
+    */
     public execute() {
-        let modules = this.metadata.getModules();
-        if (!modules) return;
-
-        modules.sort();
-        this.modal
-            .prompt('input', this.language.getLabel('LBL_SELECT_A_MODULE'), this.language.getLabel('LBL_MODULE'), null, null, modules)
-            .subscribe(index => {
-                if (!modules[index]) return;
-
-                this.model.addModel('', '', {report_module: modules[index]})
-                    .subscribe(res => {
-                        if (res) this.modelList.reLoadList();
-                    });
-            });
+        this.router.navigate(['/module/KReports/designer/new']);
     }
 }
