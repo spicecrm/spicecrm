d3e10907c86bf8f12b9b98d9df45e5fb1517f829
Mehyar <mehyar.sawas@twentyreasons.com>
2020-01-02 12:28:57 +0100

    CRN: 1000332: System Input Base64: built.

--- src/systemcomponents/systemcomponents.ts
+++ src/systemcomponents/systemcomponents.ts
@@ -117,6 +117,7 @@ import /*embed*/ {SystemFilterBuilderFilterExpressionGroup} from "./components/s
 import /*embed*/ {SystemSelectUOM} from "./components/systemselectuom";
 import /*embed*/ {SystemInputMedia} from './components/systeminputmedia';
 import /*embed*/ {SystemInputTimezone} from './components/systeminputtimezone';
+import /*embed*/ {SystemInputBase64} from './components/systeminputbase64';
 
 import /*embed*/ {SystemModuleTree} from "./components/systemmoduletree";
 import /*embed*/ {SystemModuleTreeItem} from "./components/systemmoduletreeitem";
@@ -186,6 +187,7 @@ import /*embed*/ {SystemModuleTreeItem} from "./components/systemmoduletreeitem"
         SystemInputTime,
         SystemInputCompanycodes,
         SystemInputBackendMethod,
+        SystemInputBase64,
         SystemLink,
         SystemLoaderProgress,
         SystemLoadingModal,
@@ -282,6 +284,7 @@ import /*embed*/ {SystemModuleTreeItem} from "./components/systemmoduletreeitem"
         SystemInputTime,
         SystemInputCompanycodes,
         SystemInputBackendMethod,
+        SystemInputBase64,
         SystemLink,
         SystemLoaderProgress,
         SystemModal,
