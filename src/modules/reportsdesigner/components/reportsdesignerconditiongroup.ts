f24485a2e8cb6b56f548dfbfe6c47de7fccc9e0e
Mehyar <mehyar.sawas@twentyreasons.com>
2020-01-03 15:09:47 +0100

    Reports Designer Filter: group actions handler moved from group component to filter.

--- src/modules/reportsdesigner/components/reportsdesignerconditiongroup.ts
+++ src/modules/reportsdesigner/components/reportsdesignerconditiongroup.ts
@@ -1,79 +1,40 @@
 /**
  * @module ModuleReportsDesigner
  */
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {language} from '../../../services/language.service';
 import {model} from "../../../services/model.service";
 import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
 import {ReportsDesignerService} from "../services/reportsdesigner.service";
 
-declare var _;
-
 @Component({
     selector: 'reports-designer-condition-group',
     templateUrl: './src/modules/reportsdesigner/templates/reportsdesignerconditiongroup.html',
     styles: [`
-        .cdk-drop-list:hover {background: #fff}
+        .cdk-drop-list:hover {
+            background: #fff
+        }
     `]
 })
-export class ReportsDesignerConditionGroup implements OnInit, OnDestroy {
-
-    public expanded: number;
+export class ReportsDesignerConditionGroup {
 
     /*
      * @input group: object
      */
     @Input() private group: any;
-
     /*
      * @input canDelete: boolean
      */
     @Input() private canDelete: boolean = false;
-
     /*
      * @output groupDeleted: EventEmitter<string> = groupId
      */
-    @Output() private groupDeleted: EventEmitter<string> = new EventEmitter<string>();
+    @Output() private treeChange: EventEmitter<any> = new EventEmitter<any>();
+    private expanded: number;
 
     constructor(private language: language,
                 private reportsDesignerService: ReportsDesignerService,
                 private model: model) {
-    }
-
-    /*
-     * @return group.type: string
-     */
-    get groupType() {
-        return this.group.type;
-    }
-
-    /*
-     * @param value: string
-     * @set group.type
-     * @set whereGroups
-     */
-    set groupType(value) {
-        this.group.type = value;
-        let whereGroups = this.whereGroups;
-        whereGroups.some(group => {
-            if (group.id == this.group.id) group.type = value;
-        });
-        this.whereGroups = whereGroups;
-    }
-
-    /*
-     * @return wheregroups: object[]
-     */
-    get whereGroups() {
-        return this.model.getField('wheregroups');
-    }
-
-    /*
-     * @param groups: object[]
-     * @set wheregroups
-     */
-    set whereGroups(groups) {
-        this.model.setField('wheregroups', groups);
     }
 
     /*
@@ -92,58 +53,6 @@ export class ReportsDesignerConditionGroup implements OnInit, OnDestroy {
     }
 
     /*
-     * @loadWhereGroups
-     * @setDropLists
-     */
-    public ngOnInit() {
-        if (!this.group) this.loadWhereGroups();
-        this.setDropLists();
-    }
-
-    /*
-     * @reset dropLists
-     */
-    public ngOnDestroy() {
-        this.reportsDesignerService.dropLists = [];
-    }
-
-    /*
-     * @set dropLists
-     */
-    private setDropLists() {
-        this.reportsDesignerService.dropLists = this.whereGroups.map(group => group.id).reverse();
-    }
-
-    /*
-     * @set group
-     * @set group.children
-     * @set group.conditions
-     * @set whereGroups
-     */
-    private loadWhereGroups() {
-        if (this.whereGroups && this.whereGroups.length > 0) {
-            this.group = this.whereGroups.find(group => group.id == 'root');
-            if (this.group) {
-                this.group.children = [];
-                this.group.conditions = this.whereConditions.filter(condition => condition.groupid == this.group.id);
-                this.addGroupChildren(this.group);
-            }
-        } else {
-            this.group = {
-                id: 'root',
-                groupid: 'root',
-                unionid: 'root',
-                type: 'AND',
-                parent: '-',
-                notexists: '',
-                conditions: [],
-                children: []
-            };
-            this.whereGroups = [this.group];
-        }
-    }
-
-    /*
      * @removePlaceHolderElement
      * @moveItemInArray? item in group.conditions
      * @addCondition?
@@ -159,19 +68,24 @@ export class ReportsDesignerConditionGroup implements OnInit, OnDestroy {
     }
 
     /*
-    * recursive method to push a group to the parent.children array and retrieve the group conditions
-    * @param parent: object
-    * @push group Item to parent.children array
-    * @call self and pass the group as parent
-    */
-    private addGroupChildren(parent) {
-        for (let group of this.whereGroups) {
-            if (group.parent == parent.id) {
-                group.conditions = this.whereConditions.filter(condition => condition.groupid == group.id);
-                parent.children.push(group);
-                this.addGroupChildren(parent.children[parent.children.length -1]);
-            }
-        }
+     * @emit object = {action: string, id: string} by treeChange
+     */
+    private addGroup() {
+        this.treeChange.emit({action: 'addGroup', id: this.group.id});
+    }
+
+    /*
+     * @emit object = {action: string, id: string} by treeChange
+     */
+    private deleteGroup() {
+        this.treeChange.emit({action: 'deleteGroup', id: this.group.id});
+    }
+
+    /*
+     * @emit obj: object by treeChange
+     */
+    private handleTreeChange(obj) {
+        this.treeChange.emit(obj);
     }
 
     /*
@@ -180,61 +94,6 @@ export class ReportsDesignerConditionGroup implements OnInit, OnDestroy {
     private toggleExpand(conditionId) {
         if (!this.reportsDesignerService.expertMode) return;
         this.expanded = conditionId == this.expanded ? null : conditionId;
-    }
-
-    /*
-     * @emit group.id by groupDeleted
-     */
-    private emitGroupSelfDeletion() {
-        this.groupDeleted.emit(this.group.id);
-    }
-
-    /*
-     * @define group
-     * @push group to group.children
-     * @set whereGroups
-     * @setDropLists
-     */
-    private addGroup() {
-        let guid = this.model.generateGuid();
-        let group = {
-            id: guid,
-            groupid: guid,
-            unionid: 'root',
-            group: '',
-            type: 'AND',
-            parent: this.group.id,
-            conditions: [],
-            children: []
-        };
-        this.group.children.push(group);
-        this.whereGroups = [...this.whereGroups, group];
-        this.setDropLists();
-    }
-
-    /*
-     * @param groupId: string
-     * @set group.children
-     * @set whereGroups
-     * @cleanWhereConditions
-     */
-    private deleteGroup(groupId) {
-        this.group.children = this.group.children.filter(group => group.id != groupId);
-        this.whereGroups = this.whereGroups.filter(group => group.id != groupId);
-        this.whereGroups = this.whereGroups.filter(group => group.parent == '-' || this.whereGroups.some(g => group.parent == g.id));
-        this.cleanWhereConditions();
-    }
-
-    /*
-     * @forEach whereConditions
-     * @deleteCondition if it does not have a parent
-     */
-    private cleanWhereConditions() {
-        this.whereConditions.forEach(condition => {
-            if (!this.whereGroups.some(group => group.id == condition.groupid)) {
-                this.deleteCondition(condition.id);
-            }
-        });
     }
 
     /*
@@ -266,17 +125,18 @@ export class ReportsDesignerConditionGroup implements OnInit, OnDestroy {
             valuetokey: '',
         };
         this.group.conditions.push(condition);
-        this.whereConditions = [...this.whereConditions, condition];
+        this.whereConditions = this.whereConditions ? [...this.whereConditions, condition] : [condition];
     }
 
     /*
      * @param id: string
-     * @filter whereConditions from id
+     * @filter whereConditions from deleted
      * @set group.conditions
-     * @set whereConditions
      */
     private deleteCondition(id) {
+        this.group.conditions = this.group.conditions
+            .filter(condition => condition.id != id)
+            .filter(condition => condition.groupid == id);
         this.whereConditions = this.whereConditions.filter(condition => condition.id != id);
-        this.group.conditions = this.whereConditions.filter(condition => condition.groupid == this.group.id);
     }
 }
