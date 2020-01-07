9a9418d50d494345a662fe65ae3728ded1d51b6e
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-01-07 10:05:56 +0100

    custom icons rendering

--- src/admincomponents/components/administrationmain.ts
+++ src/admincomponents/components/administrationmain.ts
@@ -46,11 +46,11 @@ export class AdministrationMain implements AfterViewInit {
      * render the default home screen after view init
      */
     public ngAfterViewInit(): void {
-        setTimeout(() => {
+
         this.administration.admincomponent$.subscribe(admincomponent => {
             this.openContent(admincomponent);
         });
-        });
+
     }
 
     /**
