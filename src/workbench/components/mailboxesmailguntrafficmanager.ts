6c78690cc1cbed31c0d4293551deb3d5418e0424
Christian Knoll <christian.knoll@twentyreasons.com>
2020-01-11 17:55:22 +0100

    update email transports and mailbox manager

--- src/workbench/components/mailboxesmailguntrafficmanager.ts
+++ src/workbench/components/mailboxesmailguntrafficmanager.ts
@@ -1,38 +1,30 @@
 /**
  * @module WorkbenchModule
  */
-import {Component, OnInit, ViewContainerRef} from "@angular/core";
-import {backend} from "../../services/backend.service";
+import {Component, Injector} from "@angular/core";
 import {language} from "../../services/language.service";
-import {metadata} from "../../services/metadata.service";
 import {modal} from "../../services/modal.service";
 import {model} from "../../services/model.service";
-import {toast} from "../../services/toast.service";
 import {view} from "../../services/view.service";
 
+/**
+ * renders the config component for the mailgun transport handler
+ */
 @Component({
     selector: "mailboxes-mailgun-traffic-manager",
     templateUrl: "./src/workbench/templates/mailboxesmailguntrafficmanager.html",
 })
-export class MailboxesMailgunTrafficManager implements OnInit {
+export class MailboxesMailgunTrafficManager {
 
     constructor(
-        private metadata: metadata,
         private language: language,
-        private backend: backend,
+        private injector: Injector,
         private model: model,
         private modal: modal,
-        private toast: toast,
-        private view: view,
-        private ViewContainerRef: ViewContainerRef
+        private view: view
     ) {
-        this.model.module = "Mailboxes";
-        this.view.isEditable = true;
-        this.view.setEditMode();
-    }
-
-    public ngOnInit() {
-        if (this.model.data.settings.length === 0) {
+        let settings = this.model.getField('settings')
+        if (!settings || (settings && settings.length == 0)) {
             this.model.data.settings = {
                 api_key: "",
                 domain: "",
@@ -44,13 +36,10 @@ export class MailboxesMailgunTrafficManager implements OnInit {
         }
     }
 
+    /**
+     * open the test modal and send a test message
+     */
     public testConnection() {
-
-        this.modal.openModal("MailboxesmanagerTestModal", true, this.ViewContainerRef.injector )
-            .subscribe((modalRef) => {
-                console.log("test happened");
-            }
-        );
-
+        this.modal.openModal("MailboxesmanagerTestModal", true, this.injector);
     }
 }
