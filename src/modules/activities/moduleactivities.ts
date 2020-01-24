a32ae6a64a023e914af5d39a33a80b894155387c
Christian Knoll <christian.knoll@twentyreasons.com>
2020-01-24 17:27:13 +0100

    current user participationstatus fields

--- src/modules/activities/moduleactivities.ts
+++ src/modules/activities/moduleactivities.ts
@@ -37,7 +37,7 @@ import /*embed*/ {ActivityParticipationPanelHeader} from "./components/activityp
 import /*embed*/ {ActivityParticipationPanelParticipant} from "./components/activityparticipationpanelparticipant";
 import /*embed*/ {ActivityParticipationStatus} from "./components/activityparticipationstatus";
 import /*embed*/ {fieldActivityParticipationStatus} from "./fields/fieldactivityparticipationstatus";
-import /*embed*/ {fieldActivityCurrentUserParticipationStatus} from "./fields/fieldActivityCurrentUserParticipationStatus";
+import /*embed*/ {fieldActivityCurrentUserParticipationStatus} from "./fields/fieldactivitycurrentuserparticipationstatus";
 
 import /*embed*/ {ActivityTimeline} from './components/activitytimeline';
 import /*embed*/ {ActivityTimelineFilter} from "./components/activitytimelinefilter";
