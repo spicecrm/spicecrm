51da593df321c35947cd88d867bc924565d46a2f
MehyarSawas <mehyar.sawas@outlook.com>
2020-01-29 13:10:58 +0100

    Reports Designer: Presentation components added.

---
+++ src/modules/reportsdesigner/components/reportsdesignerpresentitem.ts
@@ -0,0 +1,48 @@
+/**
+ * @module ModuleReportsDesigner
+ */
+import {AfterViewInit, Component, Input, ViewChild, ViewContainerRef} from '@angular/core';
+import {language} from "../../../services/language.service";
+import {ReportsDesignerService} from "../services/reportsdesigner.service";
+import {metadata} from "../../../services/metadata.service";
+import {model} from "../../../services/model.service";
+
+@Component({
+    selector: 'reports-designer-present-item',
+    templateUrl: './src/modules/reportsdesigner/templates/reportsdesignerpresentitem.html'
+})
+export class ReportsDesignerPresentItem implements AfterViewInit {
+
+    /*
+    * @input component: string
+    */
+    @Input() private component: string = '';
+    @ViewChild('itemContainer', {static: true, read: ViewContainerRef}) private itemContainer: ViewContainerRef;
+    private componentRef: any;
+
+    constructor(private language: language, private metadata: metadata, private model: model) {
+    }
+
+    /*
+    * @render view
+    */
+    public ngAfterViewInit() {
+        this.render();
+    }
+
+    /*
+    * @addComponent
+    * @set componentRef
+    */
+    private render() {
+        this.metadata.addComponent(this.component, this.itemContainer)
+            .subscribe(componentRef => this.componentRef = componentRef);
+    }
+
+    /*
+    * @destroy componentRef
+    */
+    public ngOnDestroy() {
+        if (this.componentRef) this.componentRef.destroy();
+    }
+}
---
+++ src/modules/reportsdesigner/components/reportsdesignerpresentitemstandard.ts
@@ -0,0 +1,86 @@
+/**
+ * @module ModuleReportsDesigner
+ */
+import {Component, OnInit} from '@angular/core';
+import {language} from "../../../services/language.service";
+import {ReportsDesignerService} from "../services/reportsdesigner.service";
+import {model} from "../../../services/model.service";
+import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
+
+@Component({
+    templateUrl: './src/modules/reportsdesigner/templates/reportsdesignerpresentitemstandard.html'
+})
+export class ReportsDesignerPresentItemStandard implements OnInit {
+
+    constructor(private language: language, private model: model) {
+    }
+
+    /*
+     * @return listfields: object[]
+     */
+    get listFields() {
+        return this.model.getField('listfields')
+            .sort((a, b) => !!a.sortpriority && !!b.sortpriority ? +a.sortpriority < +b.sortpriority : +a.sequence < +b.sequence ? -1 : 1);
+    }
+
+    /*
+     * @return standardViewProperties: object
+     */
+    get standardViewProperties() {
+        return this.model.getField('presentation_params').pluginData.standardViewProperties;
+    }
+
+    /*
+     * @initializePluginData
+     */
+    public ngOnInit() {
+        this.initializePluginData();
+    }
+
+    /*
+    * @set standardViewProperties
+    * @setField presentation_params
+    */
+    private initializePluginData() {
+        const presentationParams = this.model.getField('presentation_params');
+        if (!presentationParams.pluginData.standardViewProperties) {
+            presentationParams.pluginData.standardViewProperties = {
+                processCount: 'Synchronous',
+                listEntries: 25
+            };
+            this.model.setField('presentation_params', presentationParams);
+        }
+    }
+
+    /*
+     * @set field.link: string
+     */
+    protected setFieldLink(field, value) {
+        field.link = value ? 'yes' : 'no';
+    }
+
+    /*
+     * @moveItemInArray item in group.conditions
+     * @set listfield.sortpriority
+     * @set listfields
+     */
+    private onDrop(dragEvent: CdkDragDrop<any>) {
+        moveItemInArray(dragEvent.container.data, dragEvent.previousIndex, dragEvent.currentIndex);
+        dragEvent.container.data.map((item, index) => {
+            item.sortpriority = index;
+            return item;
+        });
+        this.model.setField('listfields', dragEvent.container.data.slice());
+    }
+
+    /*
+    * A function that defines how to track changes for items in the iterable (ngForOf).
+    * https://angular.io/api/common/NgForOf#properties
+    * @param index
+    * @param item
+    * @return index
+    */
+    private trackByFn(index, item) {
+        return item.fieldid;
+    }
+}
