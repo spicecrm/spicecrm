503a7819e8100f4f03be21d1b5b6bf3bdc7c5bda
MehyarSawas <mehyar.sawas@outlook.com>
2020-01-29 13:10:58 +0100

    Reports Designer: Presentation components added.

--- src/modules/reportsdesigner/components/reportsdesignerintegrate.ts
+++ src/modules/reportsdesigner/components/reportsdesignerintegrate.ts
@@ -4,6 +4,7 @@
 import {Component} from '@angular/core';
 import {language} from "../../../services/language.service";
 import {ReportsDesignerService} from "../services/reportsdesigner.service";
+import {metadata} from "../../../services/metadata.service";
 
 @Component({
     selector: 'reports-designer-integrate',
@@ -11,4 +12,38 @@ import {ReportsDesignerService} from "../services/reportsdesigner.service";
 })
 export class ReportsDesignerIntegrate {
 
+    protected items: any[] = [];
+    private selectedItemId: string = '';
+
+    constructor(private language: language, private metadata: metadata) {
+    }
+
+    /*
+    * @loadItems
+    */
+    public ngOnInit() {
+        this.loadItems();
+    }
+
+    /*
+    * @set items from metadata.getComponentSetObjects
+    */
+    private loadItems() {
+        const conf = this.metadata.getComponentConfig('ReportsDesignerPresent', 'KReports');
+        if (conf.componentset && conf.componentset.length > 0) {
+            const items = this.metadata.getComponentSetObjects(conf.componentset);
+            this.items = items
+                .filter(item => !!item.componentconfig)
+                .map(item => ({...item.componentconfig, id: item.id, sequence: item.sequence}))
+                .sort((a, b) => +a.sequence > +b.sequence ? 1 : -1);
+        }
+    }
+
+    /*
+    * @param itemId: string
+    * @set selectedItemId
+    */
+    private setSelectedItemId(itemId) {
+        this.selectedItemId = itemId;
+    }
 }
--- src/modules/reportsdesigner/components/reportsdesignerpresent.ts
+++ src/modules/reportsdesigner/components/reportsdesignerpresent.ts
@@ -3,7 +3,8 @@
  */
 import {Component} from '@angular/core';
 import {language} from "../../../services/language.service";
-import {ReportsDesignerService} from "../services/reportsdesigner.service";
+import {metadata} from "../../../services/metadata.service";
+import {model} from "../../../services/model.service";
 
 @Component({
     selector: 'reports-designer-present',
@@ -11,4 +12,74 @@ import {ReportsDesignerService} from "../services/reportsdesigner.service";
 })
 export class ReportsDesignerPresent {
 
+    protected items: any[] = [];
+
+    constructor(private language: language, private metadata: metadata, private model: model) {
+    }
+
+    /*
+    * @return listtype: string
+    */
+    get selectedItemId() {
+        return this.model.getField('listtype');
+    }
+
+    /*
+    * @param value: string
+    * @setField listtype
+    */
+    set selectedItemId(value) {
+        this.model.setField('listtype', value);
+        this.initializePresentationParams(value);
+
+    }
+
+    /*
+    * @loadItems
+    */
+    public ngOnInit() {
+        this.initializePresentationParams();
+        this.loadItems();
+    }
+
+    /*
+    * @set items from metadata.getComponentSetObjects
+    */
+    private loadItems() {
+        const conf = this.metadata.getComponentConfig('ReportsDesignerPresent', 'KReports');
+        if (conf.componentset && conf.componentset.length > 0) {
+            const items = this.metadata.getComponentSetObjects(conf.componentset);
+            if (!items || items.length == 0) return;
+            this.items = items
+                .filter(item => !!item.componentconfig)
+                .map(item => ({
+                    name: this.language.getLabel(item.componentconfig.name),
+                    id: item.componentconfig.plugin,
+                    component: item.componentconfig.component,
+                    sequence: item.sequence
+                }))
+                .sort((a, b) => +a.sequence > +b.sequence ? 1 : -1);
+        }
+    }
+
+    /*
+    * @define presentationParams
+    * @set presentationParams.plugin
+    * @setField presentation_params
+    */
+    private initializePresentationParams(plugin?) {
+        let presentationParams = this.model.getField('presentation_params');
+        if (!presentationParams || !presentationParams.plugin) {
+            presentationParams = {
+                plugin: 'standard',
+                pluginData: {}
+            };
+        } else if (presentationParams.plugin != plugin) {
+            presentationParams = {
+                plugin: plugin,
+                pluginData: {}
+            };
+        }
+        this.model.setField('presentation_params', presentationParams);
+    }
 }
