64670cb580b180e3ae0d6fc2320dab3a823f8826
MehyarSawas <mehyar.sawas@outlook.com>
2020-02-04 11:37:52 +0100

    ToBottomNoScrollDirective: boolean input added.

--- src/directives/directives/tobottomnoscroll.ts
+++ src/directives/directives/tobottomnoscroll.ts
@@ -6,17 +6,20 @@ import {
     HostBinding,
     ElementRef,
     Renderer2,
-    DoCheck, AfterViewInit
+    DoCheck, AfterViewInit, Input
 } from '@angular/core';
 @Directive({
     selector: '[tobottomnoscroll]',
 })
 export class ToBottomNoScrollDirective implements DoCheck {
 
+    @Input('tobottomnoscroll') private toBottomNoScroll: boolean = true;
+
     constructor(private element: ElementRef, private renderer: Renderer2) {
     }
 
     public ngDoCheck() {
+        if (!this.toBottomNoScroll) return;
         let rect = this.element.nativeElement.getBoundingClientRect();
         this.renderer.setStyle(this.element.nativeElement, 'height', window.innerHeight - rect.top - parseInt( getComputedStyle(this.element.nativeElement).marginBottom, 10 ) - parseInt( getComputedStyle(this.element.nativeElement).paddingBottom, 10 ) + 'px');
     }
