fe8023adf80d4dfdf807acafb6a1db547511a3c7
MehyarSawas <mehyar.sawas@outlook.com>
2020-02-10 12:38:42 +0100

    Reports Designer: Details: load categories added.

--- src/modules/reportsdesigner/components/reportsdesignerdetails.ts
+++ src/modules/reportsdesigner/components/reportsdesignerdetails.ts
@@ -5,6 +5,7 @@ import {Component, OnInit} from '@angular/core';
 import {language} from "../../../services/language.service";
 import {ReportsDesignerService} from "../services/reportsdesigner.service";
 import {model} from "../../../services/model.service";
+import {backend} from "../../../services/backend.service";
 
 @Component({
     selector: 'reports-designer-details',
@@ -12,11 +13,25 @@ import {model} from "../../../services/model.service";
 })
 export class ReportsDesignerDetails implements OnInit {
 
-    constructor(private language: language, private model: model) {
+    protected categories: any[] = [];
+
+    constructor(private language: language, private model: model, private backend: backend) {
     }
 
     public ngOnInit() {
-        if (!this.model.getField('reportoptions')) this.model.setField('reportoptions', {});
+        if (!this.model.getField('reportoptions')) {
+            this.model.setField('reportoptions', {});
+        }
+        this.loadCategories();
+    }
+
+    /**
+     * load reports categories from backend
+     */
+    private loadCategories() {
+        this.backend.getRequest('KReporter/categoriesmanager/categories').subscribe(categories => {
+            if (!!categories) this.categories = categories;
+        });
     }
 
     /**
