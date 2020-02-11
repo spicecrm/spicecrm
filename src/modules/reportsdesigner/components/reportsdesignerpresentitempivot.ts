6f0a47800f310885798e05a5efe13bba87ba3fe6
MehyarSawas <mehyar.sawas@outlook.com>
2020-02-11 16:16:57 +0100

    Reports Designer: Pivot component built.

--- src/modules/reportsdesigner/components/reportsdesignerpresentitempivot.ts
+++ src/modules/reportsdesigner/components/reportsdesignerpresentitempivot.ts
@@ -1,15 +1,155 @@
 /**
  * @module ModuleReportsDesigner
  */
-import {Component} from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
 import {language} from "../../../services/language.service";
 import {model} from "../../../services/model.service";
+import {CdkDrag, CdkDragDrop, CdkDragEnter, CdkDragExit, moveItemInArray} from "@angular/cdk/drag-drop";
+import {ReportsDesignerService} from "../services/reportsdesigner.service";
 
 @Component({
-    templateUrl: './src/modules/reportsdesigner/templates/reportsdesignerpresentitempivot.html'
+    selector: 'reports-designer-present-item-pivot',
+    templateUrl: './src/modules/reportsdesigner/templates/reportsdesignerpresentitempivot.html',
+    styles: ['.reports-designer-table-adjust-drag-placeholder .cdk-drag-placeholder {width: max-content; display: block;}']
 })
 export class ReportsDesignerPresentItemPivot {
 
-    constructor(public language: language, public model: model) {
+    constructor(public language: language,
+                public model: model,
+                public renderer: Renderer2,
+                public reportsDesignerService: ReportsDesignerService) {
+    }
+
+    /**
+     * @return pivot: object
+     */
+    get advancedOptions(): any {
+        return this.model.getField('presentation_params').pluginData.advancedOptions;
+    }
+
+    /**
+     * @return pivot: object
+     */
+    get pivotRows() {
+        return this.model.getField('presentation_params').pluginData.rowData;
+    }
+
+    /**
+     * @return pivot: object
+     */
+    get pivotColumns() {
+        return this.model.getField('presentation_params').pluginData.columnData;
+    }
+
+    /**
+     * @return pivot: object
+     */
+    get pivotValues() {
+        return this.model.getField('presentation_params').pluginData.valueData;
+    }
+
+    /**
+     * @return listfields: object[]
+     */
+    get listFields() {
+        return this.model.getField('listfields')
+            .sort((a, b) => {
+                if (!isNaN(parseInt(a.sortpriority, 10)) && !isNaN(parseInt(b.sortpriority, 10))) {
+                    return +a.sortpriority > +b.sortpriority ? 1 : -1;
+                } else {
+                    return +a.sequence > +b.sequence ? 1 : -1;
+                }
+            });
+    }
+
+    public ngOnInit() {
+        this.initializePluginData();
+    }
+
+    /**
+     * initialize the plugin properties
+     */
+    public initializePluginData() {
+        const presentationParams = this.model.getField('presentation_params');
+        if (!presentationParams.pluginData.advancedOptions) {
+            presentationParams.pluginData.advancedOptions = {};
+        }
+        if (!presentationParams.pluginData.columnData) {
+            presentationParams.pluginData.columnData = [];
+        }
+        if (!presentationParams.pluginData.valueData) {
+            presentationParams.pluginData.valueData = [];
+        }
+        if (!presentationParams.pluginData.rowData) {
+            presentationParams.pluginData.rowData = [];
+        } else if (typeof presentationParams.pluginData.rowData == 'string') {
+            presentationParams.pluginData.rowData = [];
+        }
+
+        this.model.setField('presentation_params', presentationParams);
+    }
+
+    /**
+     * reset listFields sort priority as in the list
+     * @param dragEvent: CdkDragDrop<any>
+     */
+    private onDrop(dragEvent: CdkDragDrop<any>) {
+        this.reportsDesignerService.removePlaceHolderElement(dragEvent.previousContainer.element.nativeElement);
+
+        if (dragEvent.previousContainer === dragEvent.container) {
+            moveItemInArray(dragEvent.container.data, dragEvent.previousIndex, dragEvent.currentIndex);
+        } else {
+            dragEvent.container.data.push({
+                id: dragEvent.item.data.id,
+                fieldid: dragEvent.item.data.fieldid,
+                name: dragEvent.item.data.name,
+            });
+        }
+    }
+
+    /**
+     * append a placeholder to the dom keep space reserved for the dragged element in its origin
+     * @param e: CdkDragExit
+     */
+    private dropExited(e: CdkDragExit) {
+        this.reportsDesignerService.dragPlaceHolderNode = e.item.getRootElement().cloneNode(true);
+        this.renderer.setStyle(this.reportsDesignerService.dragPlaceHolderNode, 'display', 'table-row');
+        let index = e.container.data.findIndex(item => item.id == e.item.data.id);
+        if (index > -1) {
+            e.container.element.nativeElement.insertBefore(
+                this.reportsDesignerService.dragPlaceHolderNode,
+                e.container.element.nativeElement.children[index]
+            );
+        }
+    }
+
+    /**
+     * remove PlaceHolder Element
+     * @param e: CdkDragEnter
+     */
+    private dropEnteredDragList(e) {
+        this.reportsDesignerService.removePlaceHolderElement(e.container.element.nativeElement);
+    }
+
+    /**
+     * A function that defines how to track changes for items in the iterable (ngForOf).
+     * https://angular.io/api/common/NgForOf#properties
+     * @param index
+     * @param item
+     * @return index
+     */
+    private trackByFn(index, item) {
+        return item.fieldid;
+    }
+
+    /**
+     * delete selected item from list
+     * @param arrayName: string
+     * @param id: string
+     */
+    private deleteItem(arrayName, id) {
+        const presentationParams = this.model.getField('presentation_params');
+        presentationParams.pluginData[arrayName] = presentationParams.pluginData[arrayName].filter(item => item.id != id);
+        this.model.setField('presentationParams', presentationParams);
     }
 }
