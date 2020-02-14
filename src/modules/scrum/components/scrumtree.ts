2c96c79259df859876a7555c5076359ad8004bd3
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-02-14 15:19:30 +0100

    scrum config

---
+++ src/modules/scrum/components/scrumtree.ts
@@ -0,0 +1,17 @@
+/**
+ * @module ModuleScrum
+ */
+import {
+    Component,
+    ViewChild,
+    ViewContainerRef,
+    ElementRef, OnDestroy, AfterViewInit
+} from '@angular/core';
+
+@Component({
+    selector: 'scrumtree',
+    templateUrl: './src/modules/scrum/templates/scrumtree.html',
+})
+export class ScrumTree {
+
+}
---
+++ src/modules/scrum/components/scrumtreenode.ts
@@ -0,0 +1,16 @@
+/**
+ * @module ModuleScrum
+ */
+import {
+    Component,
+    ViewChild,
+    ViewContainerRef,
+    ElementRef, OnDestroy, AfterViewInit
+} from '@angular/core';
+
+@Component({
+    templateUrl: './src/modules/scrum/templates/scrumtreenode.html',
+})
+export class ScrumTreeNode {
+
+}
