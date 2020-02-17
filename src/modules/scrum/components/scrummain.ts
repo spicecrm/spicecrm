ac5da598d8dbe36a5c634b2b632c9a42395ee5fb
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-02-17 17:41:12 +0100

    scrum load themes and epics renders theme component on click

--- src/modules/scrum/components/scrummain.ts
+++ src/modules/scrum/components/scrummain.ts
@@ -1,12 +1,10 @@
 /**
  * @module ModuleScrum
  */
-import {Component, ViewChild, ViewContainerRef, OnDestroy, ViewChildren, QueryList} from '@angular/core';
-import {Router} from '@angular/router';
+import {Component, OnDestroy} from '@angular/core';
 import {metadata} from '../../../services/metadata.service';
-import {language} from '../../../services/language.service';
-import {layout} from '../../../services/layout.service';
 import {modellist} from '../../../services/modellist.service';
+import {model} from "../../../services/model.service";
 
 @Component({
     selector: 'scrum-main',
@@ -17,44 +15,28 @@ export class ScrumMain implements OnDestroy {
     /**
      * the element reference for the content of the view
      */
-    @ViewChild('tablecontent', {read: ViewContainerRef, static: true}) private tablecontent: ViewContainerRef;
 
-    /**
-     * the subscription to the modellist
-     */
-    private modellistsubscribe: any = undefined;
+    private modellistsubscribe: any = {};
 
-    /**
-     * the componentconfig
-     */
-    public componentconfig: any = {};
+    private focus: string = null;
 
+    constructor(private metadata: metadata, private modellist: modellist, private model: model) {
 
-    /**
-     * returns if the listservic eis loading
-     */
-    get isloading() {
-        return this.modellist.isLoading;
+        this.modellistsubscribe = this.modellist.listtype$.subscribe(newType => this.loadList());
+
+        this.loadList();
+
     }
 
-    constructor(private router: Router, private metadata: metadata, private modellist: modellist, private language: language, private layout: layout) {
-
-        // get the confih
-        this.componentconfig = this.metadata.getComponentConfig('ObjectList', this.modellist.module);
-
-        // set the limit for the loading
-        this.modellist.loadlimit = 50;
-
-        // load the list and initialize from sesson data if this is set
-        // this.loadList(true);
-
-        // subscribe to changes of the listtype
-        // this.modellistsubscribe = this.modellist.listtype$.subscribe(newType => this.switchListtype());
+    private loadList() {
+        this.focus = null;
+        // this.modellist.setSortField('sequence', 'ASC', false);
+        this.modellist.getListData();
     }
 
-    /**
-     * unsubscribe from the modellist subscription
-     */
+    private selectComponent(id) {
+        this.focus = id;
+    }
     public ngOnDestroy() {
         this.modellistsubscribe.unsubscribe();
     }
--- src/modules/scrum/components/scrumtree.ts
+++ src/modules/scrum/components/scrumtree.ts
@@ -1,17 +1,30 @@
 /**
  * @module ModuleScrum
  */
-import {
-    Component,
-    ViewChild,
-    ViewContainerRef,
-    ElementRef, OnDestroy, AfterViewInit
-} from '@angular/core';
+import {Component, Output, EventEmitter} from '@angular/core';
+import {metadata} from '../../../services/metadata.service';
+import {modellist} from '../../../services/modellist.service';
+import {model} from '../../../services/model.service';
 
 @Component({
     selector: 'scrumtree',
     templateUrl: './src/modules/scrum/templates/scrumtree.html',
 })
 export class ScrumTree {
+    @Output() private selectedobject: EventEmitter<string> = new EventEmitter<string>();
 
+    private focus: string = '';
+
+    constructor(private model: model, private metadata: metadata, private modellist: modellist) {
+
+    }
+
+    protected trackbyfn(index, item) {
+        return item.id;
+    }
+
+    private selectComponent(id) {
+        this.focus = id;
+        this.selectedobject.emit(id);
+    }
 }
