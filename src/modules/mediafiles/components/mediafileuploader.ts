9f0bdbc9567bf4b91e4d631e991fa6900ed44387
strombo <andreas@gloeckl.name>
2020-02-19 14:53:36 +0100

    refactoring SystemInputMedia (part 2)

--- src/modules/mediafiles/components/mediafileuploader.ts
+++ src/modules/mediafiles/components/mediafileuploader.ts
@@ -15,31 +15,58 @@ import {SystemInputMedia} from '../../../systemcomponents/components/systeminput
 @Component({
     selector: 'media-file-uploader',
     templateUrl: './src/modules/mediafiles/templates/mediafileuploader.html',
-    providers: [mediafiles, model, view],
-    styles: [
-        ':host {height: 100%;}',
-        ':host >>> div.uploadbar {margin-left:-16px;margin-right:-16px;margin-top:16px;margin-bottom:-16px;width:calc(100% + 32px);height:8px;}',
-        ':host >>> div.uploadprogress {width: 90%;height: 100%;background-color: red;}',
-        'field-container ::ng-deep button.slds-button.slds-button--icon { display: none; }'
-    ]
+    providers: [mediafiles, model, view]
 })
 export class MediaFileUploader {
+// private i7: any = {};
+    /**
+     * The upload progress in percent.
+     */
+    private theProgress = 0;
 
-    private theProgress: number = 0;
-    private noMetaData: boolean = false;
+    /**
+     * Should the meta data be queried? Name, Copyright, ... Otherwise only the image input field will be displayed.
+     */
+    private noMetaData = false;
 
+    /**
+     * Observable for submitting the ID of the new media file.
+     */
     private answer: Observable<boolean | string> = null;
+
+    /**
+     * Subject for submitting the ID of the new media file.
+     */
     private answerSubject: Subject<boolean | string> = null;
 
+    /**
+     * Reference for the modal.
+     */
     private self: any;
 
+    /**
+     * Indicates whether saving is in progress.
+     */
     private isSaving = false;
+
+    /**
+     * Indicates whether the user is currently editing.
+     */
     private isEditing = true;
-    private tagsEditing = true;
+
+    /**
+     * Reference to the input component. Is used to access public properties of the component.
+     */
     @ViewChild(SystemInputMedia, {static: false}) public inputMedia;
 
+    /**
+     * Reference to the meta data of the media, delivered from SystemInputMedia.
+     */
     private mediaMetaData;
 
+    /**
+     * The ID of the fieldset.
+     */
     private fieldsetId: string;
 
     constructor(private mediafiles: mediafiles, private metadata: metadata, private backend: backend, private lang: language, private toast: toast, public model: model, public view: view) {
@@ -61,6 +88,9 @@ export class MediaFileUploader {
 
     }
 
+    /**
+     * Handler when the cancel button has been clicked.
+     */
     private cancel(): void {
         this.model.cancelEdit();
         this.answerSubject.next(false);
@@ -68,6 +98,9 @@ export class MediaFileUploader {
         this.self.destroy();
     }
 
+    /**
+     * Is everything ready to save?
+     */
     private get canSave(): boolean {
         return this.mediaMetaData && !this.isSaving;
     }
@@ -76,26 +109,29 @@ export class MediaFileUploader {
      * simple getter to get the image
      */
     get image() {
-        return this.model.getField('file');
+        let file = this.model.getField('file');
+        return file ? this.model.getField('filetype')+'|'+file : undefined;
     }
 
     /**
      * setter to set the image data (base64) and the metadata
      * @param imageData
      */
-    set image(imageData) {
-        this.model.setField('file', imageData);
+    set image( imageData: string ) {
+        // The image comes in the format 'filetype|base64filedata', like 'jpeg|/9j/4AAQSkZJRgABA...'
+        let positionDelimiter = imageData.indexOf('|');
+        this.model.setField('file', imageData.substring( positionDelimiter + 1 ));
         this.mediaMetaData = this.inputMedia.mediaMetaData;
-
         if (!this.model.getField('name')) {
             this.model.setField('name', this.mediaMetaData.filename.replace(/\.[^\.]+$/, '').replace(/_/, ' '));
         }
-
-        this.model.setField('mediatype', this.mediaMetaData.mediatype);
-        this.model.setField('filetype', this.mediaMetaData.fileformat);
+        this.model.setField('mediatype', 1 ); // 1 ... image
+        this.model.setField('filetype', imageData.substring( 0, positionDelimiter ));
     }
 
-
+    /**
+     * After the user finished his input operations: Save the media files model to the backend.
+     */
     private save(): void {
         if (!this.canSave) return;
         this.isSaving = true;
@@ -107,19 +143,14 @@ export class MediaFileUploader {
             this.model.save().subscribe(() => {
                 this.answerSubject.next(this.model.id);
                 this.answerSubject.complete();
-                window.setTimeout(() => this.self.destroy(), 2000);
+                window.setTimeout(() => this.self.destroy(), 2000); // After the upload is complete: Let the user time to recognize this before destroying the component.
             });
         } else this.isSaving = false;
     }
 
-    public set tagsAsString(value) {
-        this.model.setField('tags', value);
-    }
-
-    public get tagsAsString() {
-        return this.model.getField('tags');
-    }
-
+    /**
+     * Handler when the x-button has been clicked.
+     */
     public onModalEscX() {
         if (!this.isSaving) this.cancel();
         return false;
--- src/objectfields/components/fieldmediafileimage.ts
+++ src/objectfields/components/fieldmediafileimage.ts
@@ -7,8 +7,7 @@ import {
     OnInit,
     ViewChild,
     AfterViewInit,
-    ChangeDetectorRef,
-    Renderer2
+    Renderer2, OnDestroy
 } from '@angular/core';
 import { model } from '../../services/model.service';
 import { view } from '../../services/view.service';
@@ -23,53 +22,60 @@ import { backend } from '../../services/backend.service';
     selector: 'field-media-file-image',
     templateUrl: './src/objectfields/templates/fieldmediafileimage.html',
     providers: [ mediafiles ],
-    styles: [
-        'div.field-media-file-image-popover { position: absolute; width: 160px; height: 160px; left: calc(50% - 80px); top: calc(50% - 80px); z-index: 1; background-color: #ccc; border-radius: 0.25rem; background-color: #fdfdfd; box-shadow: 0 2px 3px 0 rgba(0,0,0,.16); border: 1px solid #d8dde6 }',
-        'div.field-media-file-image-popover img { border: 1px solid #999; }',
-        'div.left div.field-media-file-image-popover { left: calc(100% - 160px); }',
-        'div.bottom div.field-media-file-image-popover { top: 0; }',
-
-        'button.close { position: absolute; top: 0.25rem; right: 0.25rem; cursor: pointer; margin: 0; }',
-        'button.close svg { filter: drop-shadow( 0px 0px 3px #fff) }',
-        'div.format-button button.delete { position: absolute; bottom: 0.25rem; left: 0.25rem; margin: 0; }',
-        'button.toLightbox { position: absolute; bottom: 0.25rem; right: 0.25rem; margin: 0; }',
-
-        'div.formatButton { display: inline-block; }',
-        'div.format-image img { max-height:100%; display:block; margin:auto;padding:.25rem;top:0;bottom:0;left:0;right:0; }',
-        'div.format-image button.delete { position: absolute; bottom:0.25rem; right:0.25rem; margin:0; }',
-        'button.withThumb { width: 4rem; }',
-        'input { background-color: #54698d; }',
-        'div.format-button img.imgInButton { border-radius: 0.25rem 0 0 0.25rem; padding: 1px 0 1px 1px; height: calc(2rem - 2px); width: calc(2rem - 1px); }'
-    ]
 })
-export class fieldMediaFileImage extends fieldGeneric implements OnInit, AfterViewInit {
+export class fieldMediaFileImage extends fieldGeneric implements OnInit, AfterViewInit, OnDestroy {
 
-    // @Input() private size: number = 30;
-
+    /**
+     * Pixel size of the thumbnail.
+     */
     private thumbSize: number;
 
-    private relateIdField: string = '';
-    private relateNameField: string = '';
-    private relateType: string = '';
+    /**
+     * Holz function for unsubscribing from click listener.
+     */
+    private unsubscribeClickListener: any;
 
-    private clickListener: any;
-
+    /**
+     * The base64 encoded url of the image.
+     */
     private imageUrl: string;
+
+    /**
+     * The base64 encoded url of the enlarged image.
+     */
     private imageUrlEnlarged: string;
 
+    /**
+     * Is the image currently enlarged? (PopOver)
+     */
     private enlarged = false;
-    private lightboxOpen = false;
 
-    private fieldIsEmpty: boolean = true;
+    /**
+     * Is the field empty?
+     */
+    private fieldIsEmpty = true;
 
-    private lastValue: string = '';
+    /**
+     * Holds the last value of the field, to detect change.
+     */
+    private lastValue = '';
 
-    private height: string = '';
+    /**
+     * Pixel height of the field.
+     */
+    private height = '';
 
     @ViewChild('buttonToEnlargement', {static: true}) private buttonToEnlargement: ElementRef;
     @ViewChild('buttonToPicker', {static: true}) private buttonToPicker: ElementRef;
 
+    /**
+     * The pixel size of 1 rem.
+     */
     private size1rem: number;
+
+    /**
+     * The pixel width of the parent element.
+     */
     private widthOfParent: number;
 
     constructor(
@@ -82,8 +88,7 @@ export class fieldMediaFileImage extends fieldGeneric implements OnInit, AfterVi
         private renderer: Renderer2,
         private mediafiles: mediafiles,
         private backend: backend ,
-        private elRef: ElementRef,
-        private changeDetRef: ChangeDetectorRef
+        private elRef: ElementRef
     ) {
         super( model, view, language, metadata, router );
     }
@@ -93,18 +98,18 @@ export class fieldMediaFileImage extends fieldGeneric implements OnInit, AfterVi
     }
 
     public ngAfterViewInit() {
-
         this.size1rem = this.getSize1rem();
         this.thumbSize = this.size1rem * 2 - 2;
         this.widthOfParent = this.getWidthOfParent();
 
         if ( this.fieldconfig.height ) this.height = 'calc(' + this.fieldconfig.height + ' - 2px - 0.5rem )';
 
-        if (this.model.isLoading) this.model.data$.subscribe( () => { this.afterLoadingModel(); } );
-        else this.afterLoadingModel();
-
+        this.model.data$.subscribe( () => { this.afterLoadingModel(); } );
     }
 
+    /**
+     * Stuff to do, when the model has been loaded.
+     */
     private afterLoadingModel(): void {
         this.lastValue = this.model.data[this.fieldname];
         this.loadImages();
@@ -116,25 +121,35 @@ export class fieldMediaFileImage extends fieldGeneric implements OnInit, AfterVi
         });
     }
 
+    /**
+     * Handler for a mouse click.
+     * @param event Mouse Event
+     */
     public onClick( event: MouseEvent ): void {
         // clicked inside?
         if ( !this.elementRef.nativeElement.contains( event.target )) this.enlarged= false;
     }
 
-    private closePopups(): void {
-        this.clickListener();
-        this.enlarged = false;
-    }
-
+    /**
+     * Show enlarged variant of the image.
+     */
     private openEnlarged(): void {
-        this.clickListener = this.renderer.listen( 'document', 'click', ( event ) => this.onClick( event ) );
+        this.unsubscribeClickListener = this.renderer.listen( 'document', 'click', ( event ) => this.onClick( event ) );
         this.enlarged = true;
     }
+
+    /**
+     * Close enlarged variant of the image.
+     */
     private closeEnlarged(): void {
         this.enlarged = false;
         if ( this.buttonToEnlargement ) this.buttonToEnlargement.nativeElement.focus();
+        this.unsubscribeClickListener();
     }
 
+    /**
+     * Load the image/thumbnails from the backend with the proper size.
+     */
     private loadImages(): void {
         if ( this.model.data[this.fieldname] ) {
             this.fieldIsEmpty = false;
@@ -153,12 +168,9 @@ export class fieldMediaFileImage extends fieldGeneric implements OnInit, AfterVi
         } else this.fieldIsEmpty = true;
     }
 
-    private openLightbox() {
-        this.enlarged = false;
-        alert('Lightbox (große Anzeige des Bildes) ist noch nicht implementiert.');
-    }
-    private closeLightbox() { 1; }
-
+    /**
+     * Removes the image in case it is a button image.
+     */
     private clearField4button(): void {
         this.imageUrl = this.imageUrlEnlarged = this.value = '';
         this.enlarged = false;
@@ -166,11 +178,17 @@ export class fieldMediaFileImage extends fieldGeneric implements OnInit, AfterVi
         this.fieldIsEmpty = true;
     }
 
+    /**
+     * Removes the image in case it is a real image (not a button image).
+     */
     private clearField4image(): void {
         this.imageUrl = this.model.data[this.fieldname] = '';
         this.fieldIsEmpty = true;
     }
 
+    /**
+     * Get the image from the user.
+     */
     private getImage(): void {
         this.mediafiles.getMediaFile( 1, '', this.fieldconfig.noImagePicker || false, this.fieldconfig.noMetaData || false, this.fieldconfig.category ).subscribe( (answer) => {
             if ( answer ) {
@@ -180,22 +198,60 @@ export class fieldMediaFileImage extends fieldGeneric implements OnInit, AfterVi
         });
     }
 
+    /**
+     * Edit the existing image. ToDo!
+     */
+    private editImage(): void {
+        1;
+    }
+
+    /**
+     * Determine the pixel size of 1 rem.
+     */
     private getSize1rem(): number {
         return Math.ceil( Number( getComputedStyle( document.documentElement,null ).fontSize.replace( /px$/, '' )));
     }
 
+    /**
+     * Determine the pixel width of the parent element (less any padding).
+     */
     private getWidthOfParent(): number {
         return Number( getComputedStyle( this.elRef.nativeElement.parentElement.parentElement, null ).width.replace( /px$/, '' ))
             - Number( getComputedStyle( this.elRef.nativeElement.parentElement.parentElement, null ).paddingLeft.replace( /px$/, '' ))
             - Number( getComputedStyle( this.elRef.nativeElement.parentElement.parentElement, null ).paddingRight.replace( /px$/, '' ));
     }
 
+    /**
+     * Determine the maximal pixel width of the image, using the width of the parent element.
+     */
     private determineWidthOfImage(): number {
         return Math.round( this.widthOfParent );
     }
 
     private get imageStyle() {
         return { height: this.height };
+    }
+
+    /**
+     * Set model and view to edit mode.
+     */
+    public setEditMode() {
+        if ( this.isEditable() ) {
+            this.model.startEdit();
+            this.view.setEditMode( this.fieldid );
+        }
+    }
+
+    /**
+     * Is the field editable?
+     */
+    public isEditable(): boolean {
+        return this.getStati( this.fieldname ).editable && !this.getStati( this.fieldname ).readonly && !this.getStati( this.fieldname ).disabled && !this.getStati( this.fieldname ).hidden;
+    }
+
+    public ngOnDestroy() {
+        if ( this.unsubscribeClickListener ) this.unsubscribeClickListener();
+        super.ngOnDestroy();
     }
 
 }
