a7a99d77735ab671553f4eeef799a0655009463c
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-02-19 12:45:29 +0100

    scrum tree clean service id and type obj interface added

--- src/modules/scrum/components/scrumtreedetail.ts
+++ src/modules/scrum/components/scrumtreedetail.ts
@@ -1,22 +1,12 @@
 /**
  * @module ModuleScrum
  */
-import {
-    Component,
-    ElementRef,
-    ViewChild,
-    ViewContainerRef,
-    Input,
-    OnChanges,
-    OnDestroy
-} from '@angular/core';
+import {Component, ViewChild, ViewContainerRef, Input, OnChanges, OnDestroy} from '@angular/core';
 import {model} from '../../../services/model.service';
 import {view} from '../../../services/view.service';
 import {metadata} from '../../../services/metadata.service';
-import {modellist} from '../../../services/modellist.service';
-import {language} from '../../../services/language.service';
-import {broadcast} from '../../../services/broadcast.service';
-import {scrum} from '../services/scrum.service';
+import {scrumtree} from '../services/scrum.service';
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'scrum-tree-detail',
@@ -24,60 +14,85 @@ import {scrum} from '../services/scrum.service';
     providers: [model, view]
 })
 export class ScrumTreeDetail implements OnChanges, OnDestroy {
+    /**
+     * container for the rendering the details of the selected object
+     */
+    @ViewChild('scrumdetailcontainer', {
+        read: ViewContainerRef,
+        static: true
+    }) private scrumdetailcontainer: ViewContainerRef;
 
-    @ViewChild('scrumdetailcontainer', {read: ViewContainerRef, static: true}) private scrumdetailcontainer: ViewContainerRef;
-
+    /**
+     * inputs of the id and the type of the focused object
+     */
     @Input() private focusid: string = '';
+    @Input() private focustype: string = '';
 
     private viewComponent: any = null;
-    private modelSubscription: any = null;
 
-    constructor(private scrum: scrum, private modellist: modellist, private view: view, private language: language, private elementRef: ElementRef, private metadata: metadata, private model: model, private broadcast: broadcast) {
-        this.model.module = this.modellist.module;
-        this.modelSubscription = this.broadcast.message$.subscribe(message => {
-            this.handleMessage(message);
-        });
+    /**
+     * the componentset to be rendered
+     */
+    private componentset: string;
+
+    /**
+     * holds all subscriptions for the componentn to be unsubscribed in onDestroy
+     */
+    private subscriptions: Subscription = new Subscription();
+
+    constructor(private scrum: scrumtree, private metadata: metadata, private model: model) {
+
     }
 
-    private handleMessage(message: any) {
-        if (message.messagedata.module !== this.model.module)  return;
-
-        switch (message.messagetype) {
-            case 'model.delete':
-                break;
-            case 'model.save':
-                if (this.model.id === message.messagedata.id) {
-                    this.model.data = message.messagedata.data;
-                }
-                break;
-        }
-    }
-
+    /**
+     * reacts on ch<nges and if required destroy the view, reloads it and also load the model
+     */
     public ngOnChanges() {
-        if (this.focusid) {
-            if (!this.viewComponent) {
-                this.metadata.addComponent('ObjectRecordDetails', this.scrumdetailcontainer).subscribe(component => {
-                    this.viewComponent = component;
-                });
-            }
-        } else {
-            if (this.viewComponent) {
-                this.viewComponent.destroy();
-                this.viewComponent = null;
-            }
-        }
 
         if (this.focusid && this.focusid != this.model.id) {
             this.model.id = this.focusid;
+            this.model.module = this.focustype;
             this.model.getData();
+
+            let config = this.metadata.getComponentConfig('ScrumTreeDetail', this.model.module);
+            this.componentset = config.componentset;
+            /*
+            this.destroyContainer();
+
+            if (this.focusid) {
+                if (!this.viewComponent) {
+                    this.metadata.addComponent('ObjectPageHeader', this.scrumdetailcontainer)
+                    this.metadata.addComponent('ObjectRecordDetails', this.scrumdetailcontainer).subscribe(component => {
+                        this.viewComponent = component;
+                    });
+                }
+            }
+
+             */
+        } else if(!this.focusid) {
+            // this.destroyContainer();
+            this.componentset = undefined;
+        }
+
+    }
+
+    private destroyContainer(){
+        if (this.viewComponent) {
+            this.viewComponent.destroy();
+            this.viewComponent = null;
         }
     }
 
+    /**
+     * unsubscribe from the subscriptions
+     */
     public ngOnDestroy() {
-        this.modelSubscription.unsubscribe();
+        this.subscriptions.unsubscribe();
     }
 
-
+    /**
+     * permission to edit
+     */
     get canEdit() {
         try {
             return this.model.checkAccess('edit');
