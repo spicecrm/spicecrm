9c7852e446df9f5d8c9650ae129bccd1552502e6
MehyarSawas <mehyar.sawas@outlook.com>
2020-02-27 16:14:59 +0100

    Reports Designer: Filter: Reference operator added.

---
+++ src/modules/reports/components/reporterfilteritemreference.ts
@@ -0,0 +1,38 @@
+/**
+ * @module ModuleReports
+ */
+import {Component, Input, OnInit} from '@angular/core';
+import {model} from "../../../services/model.service";
+import {language} from "../../../services/language.service";
+
+@Component({
+    selector: 'reporter-filter-item-reference',
+    templateUrl: './src/modules/reports/templates/reporterfilteritemreference.html'
+})
+export class ReporterFilterItemReference implements OnInit {
+    /**
+     * whereCondition: object
+     */
+    @Input() private whereCondition: any = {};
+
+    private fieldName: string;
+
+    constructor(private language: language, private model: model) {
+    }
+
+    get referenceFields() {
+        const whereConditions = this.model.getField('whereconditions');
+        return whereConditions.filter(condition => condition.type == this.whereCondition.type && !!condition.reference && this.whereCondition.fieldid != condition.fieldid);
+    }
+
+    /**
+     * set the fieldName from path and load the referenceFields
+     */
+    public ngOnInit() {
+        const pathArray = this.whereCondition.path.split('::');
+
+        // the last entry has to be the field
+        let fieldArray = pathArray[pathArray.length - 1].split(':');
+        this.fieldName = fieldArray[1];
+    }
+}
