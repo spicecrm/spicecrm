0598296ee0644e4b88970586ed1a6bacb61e6799
MehyarSawas <mehyar.sawas@outlook.com>
2020-02-27 14:18:53 +0100

    Reports Designer: Filter: Disable context field if it is in use: added.

--- src/modules/reportsdesigner/components/reportsdesignerconditiongroupexpansion.ts
+++ src/modules/reportsdesigner/components/reportsdesignerconditiongroupexpansion.ts
@@ -1,8 +1,9 @@
 /**
  * @module ModuleReportsDesigner
  */
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {language} from '../../../services/language.service';
+import {model} from "../../../services/model.service";
 
 declare var _;
 
@@ -10,14 +11,37 @@ declare var _;
     selector: 'reports-designer-condition-group-expansion',
     templateUrl: './src/modules/reportsdesigner/templates/reportsdesignerconditiongroupexpansion.html'
 })
-export class ReportsDesignerConditionGroupExpansion {
+export class ReportsDesignerConditionGroupExpansion implements OnInit {
 
     /**
     * @input whereCondition: object
      */
     @Input() private whereCondition: any;
 
-    constructor(private language: language) {
+    private contextUsed: boolean = false;
+
+    constructor(private language: language, private model: model) {
+    }
+
+    public ngOnInit() {
+        this.setContextUsed();
+    }
+
+    /**
+     * set the contextUsed to true if any of visualizationParams plugins has used it
+     */
+    private setContextUsed() {
+        const visualizationParams = this.model.getField('visualization_params');
+        if (!visualizationParams) return;
+        for (let key in visualizationParams) {
+            if (visualizationParams.hasOwnProperty(key)) {
+                if (!!visualizationParams[key].googlecharts && !!visualizationParams[key].googlecharts.context &&
+                    !!this.whereCondition.context && this.whereCondition.context == visualizationParams[key].googlecharts.context) {
+                    this.contextUsed = true;
+                    break;
+                }
+            }
+        }
     }
 
     /**
