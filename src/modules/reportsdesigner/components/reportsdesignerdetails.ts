8dfb408428bc529264933dd4077bc46d39005e3b
Christian Knoll <christian.knoll@twentyreasons.com>
2020-03-01 11:54:36 +0100

    reporter designer update

--- src/modules/reportsdesigner/components/reportsdesignerdetails.ts
+++ src/modules/reportsdesigner/components/reportsdesignerdetails.ts
@@ -3,22 +3,41 @@
  */
 import {Component, OnInit} from '@angular/core';
 import {language} from "../../../services/language.service";
-import {ReportsDesignerService} from "../services/reportsdesigner.service";
+
 import {model} from "../../../services/model.service";
+import {metadata} from "../../../services/metadata.service";
 import {backend} from "../../../services/backend.service";
 
+
+/**
+ * renders teh details panel in teh report designer
+ */
 @Component({
     selector: 'reports-designer-details',
     templateUrl: './src/modules/reportsdesigner/templates/reportsdesignerdetails.html'
 })
 export class ReportsDesignerDetails implements OnInit {
 
+    /**
+     * the categories
+     */
     protected categories: any[] = [];
 
-    constructor(private language: language, private model: model, private backend: backend) {
+    /**
+     * the fieldset
+     */
+    private fieldset: string;
+
+    constructor(private language: language, private model: model, private backend: backend, private metadata: metadata) {
+
     }
 
     public ngOnInit() {
+
+        // load the config for the fieldset
+        let componentconfig = this.metadata.getComponentConfig('ReportsDesignerDetails', this.model.module);
+        this.fieldset = componentconfig.fieldset;
+
         if (!this.model.getField('reportoptions')) {
             this.model.setField('reportoptions', {});
         }
