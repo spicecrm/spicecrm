7c05df4fd720f9111e3b1f6932f4faca6d8ceca9
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-03-03 14:10:29 +0100

    emailschedule renders campaigntaskemailpanel component, typo

--- src/modules/emails/components/emailschedulesmodal.ts
+++ src/modules/emails/components/emailschedulesmodal.ts
@@ -8,23 +8,34 @@ import {modellist} from '../../../services/modellist.service';
 import {language} from '../../../services/language.service';
 import {view} from "../../../services/view.service";
 import {backend} from "../../../services/backend.service";
+import {metadata} from "../../../services/metadata.service";
+import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({
     selector: "email-schedules-modal",
     templateUrl: "./src/modules/emails/templates/emailschedulesmodal.html",
+    providers: [model, view],
 })
 export class EmailSchedulesModal {
     private self: any = {};
     private componentconfig: any = {};
-
     constructor(private language: language,
                 private model: model,
                 private injector: Injector,
                 private view: view,
                 private modal: modal,
+                private metadata: metadata,
                 private modellist: modellist,
                 private backend: backend) {
 
+        this.view.isEditable = true;
+        this.view.setEditMode();
+
+    }
+
+    public ngOnInit() {
+        this.model.module = 'EmailSchedules';
+        this.model.initialize();
     }
 
     get itemcount() {
@@ -35,43 +46,16 @@ export class EmailSchedulesModal {
         this.self.destroy();
     }
 
-    /**
-     * @openModal ObjectModalModuleLookup
-     * @pass module
-     * @pass multiselect
-     * @setField email_subject
-     * @setField email_body
-     * @setField email_stylesheet_id
-     */
-    private copyFromTemplate() {
-        this.modal.openModal('ObjectModalModuleLookup', true, this.injector)
-            .subscribe(selectModal => {
-                selectModal.instance.module = 'EmailTemplates';
-                selectModal.instance.multiselect = false;
-                selectModal.instance.selectedItems.subscribe(items => {
-                    if (items.length) {
-                        this.model.setField('email_subject', items[0].subject);
-                        this.model.setField('email_body', items[0].body_html);
-                        this.model.setField('email_stylesheet_id', items[0].style);
-                    }
-                });
-            });
-    }
 
-    private send() {
+    private saveSchedule() {
         this.modal.openModal('SystemLoadingModal').subscribe(loadingRef => {
             loadingRef.instance.messagelabel = 'LBL_LOADING';
-
             let selectedIds = this.modellist.getSelectedIDs();
             let params = {
                 module: this.modellist.module,
-                // subject:,
-                //                 // body:,
-                //                 // stylesheetid:,
-                //                 // mailbox:,
                 ids: selectedIds
             };
-            window.console.log(params);
+
         });
     }
 }
