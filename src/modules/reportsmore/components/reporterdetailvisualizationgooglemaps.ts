4ef31a8bb32fbd9e2329f784ba762aa6d75e7447
MehyarSawas <mehyar.sawas@outlook.com>
2020-03-05 14:43:59 +0100

    Reporter More: Google Maps: Cluster, Spiderfy and address handling added.

--- src/modules/reportsmore/components/reporterdetailvisualizationgooglemaps.ts
+++ src/modules/reportsmore/components/reporterdetailvisualizationgooglemaps.ts
@@ -6,13 +6,13 @@ import {model} from '../../../services/model.service';
 import {language} from '../../../services/language.service';
 import {libloader} from '../../../services/libloader.service';
 
-/**
- * @ignore
- */
+/** @ignore */
 declare let google: any;
-/**
- * @ignore
- */
+/** @ignore */
+declare let MarkerClusterer: any;
+/** @ignore */
+declare let OverlappingMarkerSpiderfier: any;
+/** @ignore */
 declare let _: any;
 
 @Component({
@@ -24,7 +24,22 @@ export class ReporterDetailVisualizationGoogleMaps implements AfterViewInit {
     public vizdata: any;
     @ViewChild('mapContainer', {read: ViewContainerRef, static: false}) private mapContainer: ViewContainerRef;
     @ViewChild('legendContainer') private legendContainer: TemplateRef<any>;
+    /**
+     * google maps instance
+     */
     private map: any = {};
+    /**
+     * OverlappingMarkerSpiderfier
+     */
+    private oms: any;
+    /**
+     * popup window instance for markers
+     */
+    private infoWindow: any = {};
+    /**
+     * google maps pin markers arry
+     */
+    protected markers: any[] =  [];
 
     constructor(
         private language: language,
@@ -50,22 +65,35 @@ export class ReporterDetailVisualizationGoogleMaps implements AfterViewInit {
 
     /**
      * load google maps library
+     * load cluster library if active
+     * load spiderfy library if active
      */
     public ngAfterViewInit() {
-        this.libLoader.loadLib('maps.googleapis').subscribe(() => this.renderMap());
+        this.libLoader.loadLib('maps.googleapis').subscribe(() => {
+            this.renderMap();
+            if (!!this.vizdata.data.data.mapaddins.cluster) {
+                this.libLoader.loadLib('MarkerClustererPlus').subscribe(() => this.setMarkerCluster());
+            } else if (!!this.vizdata.data.data.mapaddins.spiderfy) {
+                this.libLoader.loadLib('OverlappingMarkerSpiderfier').subscribe(() => this.setMarkerSpiderfier());
+            }
+        });
     }
 
     /**
-     * render google map
+     * render google map and set the markers from report results
+     * set legend if active
      */
     private renderMap() {
         this.zone.runOutsideAngular(() => {
-            this.map = new google.maps.Map(this.mapContainer.element.nativeElement, {
-                center: {lat: 48.183607, lng: 16.321538},
-                zoom: 11
-            });
+            this.map = new google.maps.Map(this.mapContainer.element.nativeElement);
+            this.infoWindow = new google.maps.InfoWindow();
+
+            // close popup window on map click
+            google.maps.event.addListener(this.map, 'click', () => this.infoWindow.close());
+
             if (!!this.vizdata) {
                 this.setMarkers();
+
                 if (!!this.vizdata.data.data.mapaddins.legend.display) {
                     this.setLegendFromTemplate();
                 }
@@ -74,7 +102,40 @@ export class ReporterDetailVisualizationGoogleMaps implements AfterViewInit {
     }
 
     /**
-     * define the legend for the map
+     * set maker cluster
+     */
+    private setMarkerCluster() {
+        const markerCluster = new MarkerClusterer(this.map, this.markers,
+            {imagePath: 'vendor/google-maps/MarkerClustererPlus/images/m'});
+    }
+
+    /**
+     * set maker spiderfier
+     */
+    private setMarkerSpiderfier() {
+        this.oms = new OverlappingMarkerSpiderfier(this.map,
+            {
+                markersWontMove: true,
+                markersWontHide: true,
+                keepSpiderfied: true
+            });
+        this.markers.forEach(marker => this.addMarkerToOMS(marker));
+    }
+
+    /**
+     * add marker to OverlappingMarkerSpiderfier and set listener for popup
+     * @param marker
+     */
+    private addMarkerToOMS(marker) {
+        if (!this.oms) return;
+        this.oms.addMarker(marker, () => {
+            this.infoWindow.setContent(marker.info);
+            this.infoWindow.open(this.map, marker);
+        });
+    }
+
+    /**
+     * set legend for the map
      */
     private setLegendFromTemplate() {
         const templateRef = this.legendContainer.createEmbeddedView(null);
@@ -87,26 +148,73 @@ export class ReporterDetailVisualizationGoogleMaps implements AfterViewInit {
      * set markers from report result records
      */
     private setMarkers() {
+        this.markers = [];
+        const mapBounds = new google.maps.LatLngBounds();
+        const mapGeoCoder = new google.maps.Geocoder();
+        const iconData = {
+            path: `M7.8,1.3L7.8,1.3C6-0.4,3.1-0.4,1.3,1.3c-1.8,1.7-1.8,4.6-0.1,6.3c0,0,0,0,0.1,0.1l3.2,3.2l3.2-3.2C9.6,6,9.6,3.2,7.8,1.3`,
+            strokeColor: '#fff',
+            fillOpacity: 1,
+            fillColor: '',
+            scale: 2,
+            anchor: {x: 4.5,y: 5}
+        };
+
         this.vizdata.data.data.pinpoints.forEach(item => {
-            if (!(!!item.latitude) || !(!!item.longitude)) return;
-            const marker = new google.maps.Marker({
-                position: {lat: +item.latitude, lng: +item.longitude},
+
+            iconData.fillColor = '#' + item.colorLabel;
+            const markerData: any = {
+                id: this.model.generateGuid(),
                 map: this.map,
-                animation: google.maps.Animation.DROP,
-                icon: {
-                    path: `M7.8,1.3L7.8,1.3C6-0.4,3.1-0.4,1.3,1.3c-1.8,1.7-1.8,4.6-0.1,6.3c0,0,0,0,0.1,0.1l3.2,3.2l3.2-3.2C9.6,6,9.6,3.2,7.8,1.3`,
-                    strokeColor: '#fff',
-                    fillColor: '#' + item.colorLabel,
-                    fillOpacity: 1,
-                    scale: 2
-                }
-            });
-            const infoWindow = new google.maps.InfoWindow({
-                content: item.info
-            });
-            marker.addListener('click', () => {
-                infoWindow.open(this.map, marker);
-            });
+                position: null,
+                title: !!item.title ? item.title : '',
+                icon: iconData,
+                info: !!item.info ? item.info : '',
+                animation: google.maps.Animation.DROP
+            };
+            // set popup window content
+            this.infoWindow.setContent(markerData.info);
+
+            // set the marker position either from the geo data or from address by mapGeoCoder
+            if (!!item.latitude && !!item.longitude) {
+                markerData.position = {lat: +item.latitude, lng: +item.longitude};
+                this.createMarker(markerData, mapBounds);
+            } else {
+                const address = `${item.street_address} ${item.postal_code} ${item.locality} ${item.country}`;
+                markerData.routeLabel = !!item.routeLabel ? item.routeLabel : '';
+                markerData.routeAddress = !!item.routeAddress ? item.routeAddress : '';
+
+                mapGeoCoder.geocode( {address: address}, (results, status) => {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        markerData.position = results[0].geometry.location;
+                        this.createMarker(markerData, mapBounds);
+                    }
+                });
+            }
         });
+    }
+
+    /**
+     * create a marker from the given markerData and push it to markers array
+     * add info window to the marker and fit the map bounds
+     * @param markerData: object
+     * @param mapBounds: google.maps.LatLngBounds
+     */
+    private createMarker(markerData, mapBounds) {
+        const marker = new google.maps.Marker(markerData);
+        marker.addListener('click', () => {
+            this.infoWindow.close();
+            // if spiderfy is active it will add its own listener
+            if (!this.oms) {
+                this.infoWindow.open(this.map, marker);
+            }
+        });
+        this.markers.push(marker);
+        mapBounds.extend(marker.position);
+        this.map.fitBounds(mapBounds);
+
+        if (!!this.oms) {
+            this.addMarkerToOMS(marker);
+        }
     }
 }
