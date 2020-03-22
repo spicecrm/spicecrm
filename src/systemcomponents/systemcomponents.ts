a69e78f9de63a8ab12800d24e3a789cd394aae57
Christian Knoll <christian.knoll@twentyreasons.com>
2020-03-22 16:40:30 +0100

    refactor syste-language-label to system-label

--- src/systemcomponents/systemcomponents.ts
+++ src/systemcomponents/systemcomponents.ts
@@ -75,7 +75,7 @@ import /*embed*/ {SystemInputTags} from "./components/systeminputtags";
 import /*embed*/ {SystemInputTime} from "./components/systeminputtime";
 import /*embed*/ {SystemInputCompanycodes} from "./components/systeminputcompanycodes";
 import /*embed*/ {SystemInputBackendMethod} from "./components/systeminputbackendmethod";
-import /*embed*/ {SystemLanguageLabel} from "./components/systemlanguagelabel";
+import /*embed*/ {SystemLabel} from "./components/systemlabel";
 import /*embed*/ {SystemLink} from "./components/systemlink";
 import /*embed*/ {SystemLoaderProgress} from "./components/systemloaderprogress";
 import /*embed*/ {SystemLoadingModal} from "./components/systemloadingmodal";
@@ -210,7 +210,7 @@ import /*embed*/ {SystemNavigationManagerRouteContainer} from "./components/syst
         SystemInputCompanycodes,
         SystemInputBackendMethod,
         SystemInputBase64,
-        SystemLanguageLabel,
+        SystemLabel,
         SystemLink,
         SystemLoaderProgress,
         SystemLoadingModal,
@@ -328,7 +328,7 @@ import /*embed*/ {SystemNavigationManagerRouteContainer} from "./components/syst
         SystemInputCompanycodes,
         SystemInputBackendMethod,
         SystemInputBase64,
-        SystemLanguageLabel,
+        SystemLabel,
         SystemLink,
         SystemLoaderProgress,
         SystemModal,
