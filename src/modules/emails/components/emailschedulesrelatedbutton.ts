edff38bf4b51703b58e4e1af4ce1fbea20a44917
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-03-23 15:42:38 +0100

    chekemailslink returns an observable + locked isloading

--- src/modules/emails/components/emailschedulesrelatedbutton.ts
+++ src/modules/emails/components/emailschedulesrelatedbutton.ts
@@ -32,26 +32,22 @@ export class EmailSchedulesRelatedButton {
     }
 
 
-
     /**
      *  execute checkemailslink and await the response
      *  subscribe and save the instances of linkedbeans, modelid and currentmodule to use them in the modal that will open
      */
     public execute() {
         let await = this.modal.await('SystemLoadingModal');
-        this.checkEmailsLink().subscribe( response => {
+        this.checkEmailsLink().subscribe(response => {
             await.emit(true);
-            if(response) {
-                this.modal.openModal('EmailSchedulesRelatedModal', true, this.injector).subscribe( modal => {
+            if (response) {
+                this.modal.openModal('EmailSchedulesRelatedModal', true, this.injector).subscribe(modal => {
                     modal.instance.linkedBeans = this.linkedBeans;
                     modal.instance.modelId = this.modelId;
                     modal.instance.currentModule = this.model.module;
-
                 });
             }
-        }
-        );
-
+        });
     }
 
     /**
@@ -63,50 +59,49 @@ export class EmailSchedulesRelatedButton {
     private checkEmailsLink(): Observable<any> {
         let responseSubject = new Subject<any>();
         let arrayOfModules = [];
-            // search for fields of type link to get the related modules, proof that each of these is actually a module by looping through the fielddefs of metadata where all the object
-            // properties are the names of the modules, if this is true then, push this property(key) to the arrayOfModules
+        // search for fields of type link to get the related modules, proof that each of these is actually a module by looping through the fielddefs of metadata where all the object
+        // properties are the names of the modules, if this is true then, push this property(key) to the arrayOfModules
         Object.keys(this.model.fields).forEach(item => {
-                if (this.model.fields[item].type == 'link' && this.model.fields[item].hasOwnProperty('vname') && !this.model.fields[item].hasOwnProperty('link_type')) {
-                    let module = this.model.fields[item].name;
-                    for(let key in this.metadata.fieldDefs) {
-                        if(key.toLowerCase() == module) {
-                            arrayOfModules.push(key);
-                        }
+            if (this.model.fields[item].type == 'link' && this.model.fields[item].hasOwnProperty('vname') && !this.model.fields[item].hasOwnProperty('link_type')) {
+                let module = this.model.fields[item].name;
+                for (let key in this.metadata.fieldDefs) {
+                    if (key.toLowerCase() == module) {
+                        arrayOfModules.push(key);
+                    }
+                }
+            }
+        });
+        // filter the arrayOfModules by looping first through the array and saving the name of the module at each position (pos)
+        // then loop through the fielddefs of metadata and verify if email and email1 are properties of each module in arrayOfModule, if both check true than push
+        // the module into filteredModules
+        let filteredModules = [];
+        for (let pos in arrayOfModules) {
+            let module = arrayOfModules[pos];
+            Object.keys(this.metadata.fieldDefs).forEach(item => {
+                if (this.metadata.fieldDefs[item] != null && item == module) {
+                    if (this.metadata.fieldDefs[item].hasOwnProperty('email') && this.metadata.fieldDefs[item].hasOwnProperty('email1')) {
+                        filteredModules.push(module);
                     }
                 }
             });
-            // filter the arrayOfModules by looping first through the array and saving the name of the module at each position (pos)
-            // then loop through the fielddefs of metadata and verify if email and email1 are properties of each module in arrayOfModule, if both check true than push
-            // the module into filteredModules
-        let filteredModules = [];
-        for(let pos in arrayOfModules) {
-                let module = arrayOfModules[pos];
-                Object.keys(this.metadata.fieldDefs).forEach(item => {
-                    if(this.metadata.fieldDefs[item] != null && item == module) {
-                        if(this.metadata.fieldDefs[item].hasOwnProperty('email') && this.metadata.fieldDefs[item].hasOwnProperty('email1')) {
-                            filteredModules.push(module);
-                        }
-                    }
-                });
-            }
+        }
         let params = {modules: filteredModules};
         this.backend.getRequest(`/module/EmailSchedules/checkRelated/${this.model.module}/${this.model.id}`, params).subscribe(result => {
-                if (result.status) {
-                    this.linkedBeans = result.linkedBeans;
-                    this.modelId = result.beanId;
-                    responseSubject.next(result.status);
-                } else {
-                    responseSubject.next(false);
-                    this.toast.sendToast(this.language.getLabel('LBL_ERROR'), 'error');
-                }
-                // add the corresponding labels
-                for(let bean in this.linkedBeans) {
-                    let link = this.linkedBeans[bean];
-                    link.vname = this.model.fields[link.link].vname;
-                }
-                responseSubject.complete();
-            });
+            if (result.status) {
+                this.linkedBeans = result.linkedBeans;
+                this.modelId = result.beanId;
+                responseSubject.next(result.status);
+            } else {
+                responseSubject.next(false);
+                this.toast.sendToast(this.language.getLabel('LBL_ERROR'), 'error');
+            }
+            // add the corresponding labels
+            for (let bean in this.linkedBeans) {
+                let link = this.linkedBeans[bean];
+                link.vname = this.model.fields[link.link].vname;
+            }
+            responseSubject.complete();
+        });
         return responseSubject.asObservable();
     }
-
 }
