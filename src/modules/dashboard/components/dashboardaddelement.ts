b75cf1a27cb1e00f7370e65d90e85046b911fe8b
MehyarSawas <mehyar.sawas@outlook.com>
2020-03-24 16:52:51 +0100

    Dashboard Add Element: Modules list sorted.

--- src/modules/dashboard/components/dashboardaddelement.ts
+++ src/modules/dashboard/components/dashboardaddelement.ts
@@ -24,6 +24,7 @@ export class DashboardAddElement {
     private loadLimit: number = 40;
     private addDashlet: EventEmitter<any> = new EventEmitter<any>();
     private searchTimeout: any;
+    private modules = [];
 
     constructor(private language: language, private metadata: metadata, private backend: backend) {
     }
@@ -53,12 +54,9 @@ export class DashboardAddElement {
         }
     }
 
-    get modules() {
-        return this.metadata.getModules();
-    }
-
     public ngOnInit() {
         this.getDashlets();
+        this.modules = this.metadata.getModules().sort();
     }
 
     private resetValues() {
