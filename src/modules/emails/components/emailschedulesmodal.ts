c66f0076ee2c38037bbd1626a8d88e35d2b086a6
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-03-24 09:46:44 +0100

    messages

--- src/modules/emails/components/emailschedulesmodal.ts
+++ src/modules/emails/components/emailschedulesmodal.ts
@@ -67,10 +67,10 @@ export class EmailSchedulesModal {
             this.backend.postRequest('/modules/EmailSchedules/saveSchedule', {}, body).subscribe(result => {
                 loadingRef.instance.self.destroy();
                 if (result.status) {
-                    this.toast.sendToast(result.status, 'success');
+                    this.toast.sendToast(this.language.getLabel('MSG_SUCCESSFULLY_EXECUTED'), 'success');
                     this.close();
                 } else {
-                    this.toast.sendToast(result.status, 'error');
+                    this.toast.sendToast(this.language.getLabel('LBL_ERROR'), 'error');
                 }
             });
         });
