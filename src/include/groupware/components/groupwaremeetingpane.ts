9f3d1bc3555b9851363d8afed3ce5e5ddf835368
Dawid Bestrzynski <dawid.bestrzynski@twentyreasons.com>
2020-03-25 14:53:42 +0100

    Groupware Meeting Pane

--- src/include/groupware/components/groupwaremeetingpane.ts
+++ src/include/groupware/components/groupwaremeetingpane.ts
@@ -20,16 +20,43 @@ import {Component} from "@angular/core";
 // import {model} from "../../../services/model.service";
 // import {view} from "../../../services/view.service";
 
+import {GroupwareService} from '../../../include/groupware/services/groupware.service';
+import {backend} from "../../../services/backend.service";
+
+
 @Component({
     selector: 'groupware-meeting-pane',
     templateUrl: './src/include/groupware/templates/groupwaremeetingpane.html'
 })
 export class GroupwareMeetingPane {
+    /**
+     * Currently active tab.
+     */
+    private activetab: 'beans' | 'search' | 'attachments' | 'linked' = 'beans';
 
     constructor(
-        /*private model: model,
-        private view: view*/
+        private backend: backend,
+        private groupware: GroupwareService,
     ) {
-        console.log('meeting pane');
+        this.groupware.getEmailFromSpice();
+    }
+
+    /**
+     * Sets a tab as open and displays its content.
+     * @param tab
+     */
+    private open(tab) {
+        this.activetab = tab;
+    }
+
+    /**
+     * Checks if the current email has already been archived in SpiceCRM.
+     */
+    get isArchived() {
+        if (this.groupware.emailId.length === 0) {
+            return false;
+        }
+
+        return true;
     }
 }
