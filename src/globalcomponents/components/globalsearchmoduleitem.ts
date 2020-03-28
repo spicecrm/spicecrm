6125e4b1207ff36396c871d99dc7b326ee16d31e
Christian Knoll <christian.knoll@twentyreasons.com>
2020-03-28 17:23:07 +0100

    Changes from _type to _module

--- src/globalcomponents/components/globalsearchmoduleitem.ts
+++ src/globalcomponents/components/globalsearchmoduleitem.ts
@@ -26,7 +26,7 @@ export class GlobalSearchModuleItem implements OnInit {
     }
 
     public ngOnInit() {
-        this.model.module = this.listitem._type;
+        this.model.module = this.listitem._source._module;
         this.model.id = this.listitem._id;
         this.model.data = this.model.utils.backendModel2spice(this.model.module, this.listitem._source);
         this.model.data.acl = this.listitem.acl;
