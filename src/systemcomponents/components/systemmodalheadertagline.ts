7f3260b7d3373ad6955aadbd748eed35792d095b
strombo <andreas@gloeckl.name>
2020-04-01 09:31:34 +0200

    SystemModalHeaderTagline

---
+++ src/systemcomponents/components/systemmodalheadertagline.ts
@@ -0,0 +1,13 @@
+/**
+ * @module SystemComponents
+ */
+import {Component} from '@angular/core';
+
+/**
+ * a component that is simply a container as part of the system-modal-header and can hold a tagline of the header.
+ */
+@Component({
+    selector: 'system-modal-header-tagline',
+    templateUrl: './src/systemcomponents/templates/systemmodalheadertagline.html',
+})
+export class SystemModalHeaderTagline {}
