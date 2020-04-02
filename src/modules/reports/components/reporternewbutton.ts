d26562a62a5c68d0f56f66d38cc38775dc622b91
MehyarSawas <mehyar.sawas@outlook.com>
2020-04-02 10:52:26 +0200

    Ticket SPICEUI-335: Reporter Buttons: acl check added.

--- src/modules/reports/components/reporternewbutton.ts
+++ src/modules/reports/components/reporternewbutton.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {language} from "../../../services/language.service";
 import {Router} from "@angular/router";
+import {model} from "../../../services/model.service";
 
 @Component({
     templateUrl: './src/modules/reports/templates/reporternewbutton.html'
@@ -9,12 +10,21 @@ import {Router} from "@angular/router";
 export class ReporterNewButton {
 
     constructor(private language: language,
+                private model: model,
                 private router: Router) {
     }
     /*
     *navigate to designer
     */
     public execute() {
+        if (this.hidden) return;
         this.router.navigate(['/module/KReports/designer/new']);
+    }
+
+    /**
+     * hide the button while the model is editing
+     */
+    get hidden() {
+        return this.model.data.acl && !this.model.checkAccess('create');
     }
 }
