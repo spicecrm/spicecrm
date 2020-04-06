e74509f8450da53ef00ced945347288d9deac73d
MehyarSawas <mehyar.sawas@outlook.com>
2020-04-06 09:54:06 +0200

    System Input Radio Button Group: adjustment. clean up.

--- src/systemcomponents/components/systeminputradiobuttongroup.ts
+++ src/systemcomponents/components/systeminputradiobuttongroup.ts
@@ -41,6 +41,10 @@ export class SystemInputRadioButtonGroup implements ControlValueAccessor, AfterV
      */
     @Input() protected readonly disabled: boolean = false;
     /**
+     * the value to be set
+     */
+    protected groupName: string;
+    /**
      * save on change function for ControlValueAccessor
      */
     private onChange: (value: string) => void;
@@ -49,8 +53,11 @@ export class SystemInputRadioButtonGroup implements ControlValueAccessor, AfterV
      */
     private onTouched: () => void;
 
-
+    /**
+     * set group name for dom
+     */
     constructor(private cdRef: ChangeDetectorRef) {
+        this.groupName = _.uniqueId('group-name-');
     }
 
     /**
@@ -120,11 +127,7 @@ export class SystemInputRadioButtonGroup implements ControlValueAccessor, AfterV
      */
     private setItemsInitialValues() {
 
-        const inputName = _.uniqueId('input-name-');
-
         this.inputOptions.forEach(inputOption => {
-
-            inputOption.name = inputName;
 
             if (!inputOption.id) {
                 inputOption.id = _.uniqueId('input-id-');
