60e22b62e1c23ff01a106134a3217e83e1eff224
Christian Knoll <christian.knoll@twentyreasons.com>
2020-04-14 23:58:04 +0200

    sanitize and cleanup outlook addin

--- src/include/groupware/components/groupwaredetailpaneview.ts
+++ src/include/groupware/components/groupwaredetailpaneview.ts
@@ -1,68 +1,100 @@
 /**
  * @module ModuleGroupware
  */
-import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
-import {GroupwareService} from '../../../include/groupware/services/groupware.service';
+import {AfterViewInit, Component, OnDestroy, ViewChild, ViewContainerRef} from '@angular/core';
+import {Subscription} from "rxjs";
 import {model} from "../../../services/model.service";
 import {metadata} from "../../../services/metadata.service";
 import {navigation} from "../../../services/navigation.service";
 import {navigationtab} from "../../../services/navigationtab.service";
-import {broadcast} from "../../../services/broadcast.service";
-import {Subscription} from "rxjs";
-import {ActivatedRoute} from "@angular/router";
+
 
 /**
- * Outlook add-in detail pane showing a list of beans that use the email addresses found in the email.
- * In case there is just one such bean, the details of it will be shown.
+ * The pane shows the details customized for the oputlook add in
  */
 @Component({
     selector: 'groupware-detail-pane-view',
     templateUrl: './src/include/groupware/templates/groupwaredetailpaneview.html',
     providers: [model]
 })
-export class GroupwareDetailPaneView implements AfterViewInit, OnDestroy{
+export class GroupwareDetailPaneView implements AfterViewInit, OnDestroy {
 
+    /**
+     * reference to the header container
+     */
     @ViewChild('header', {read: ViewContainerRef, static: true}) private header: ViewContainerRef;
+
+    /**
+     * reference to the main container
+     */
     @ViewChild('main', {read: ViewContainerRef, static: true}) private main: ViewContainerRef;
 
+    /**
+     * indicates that we have passed the view initialization and can render the view
+     */
     private initialized: boolean = false;
+
+    /**
+     * the rendered componentes that need to be destroyed if we rerender
+     */
     private componentRefs: any[] = [];
 
+    /**
+     * subscriptions that need to be unsubscribed in the on Destroy lifecycle hook
+     */
     private componentSubscriptions: Subscription = new Subscription();
 
     constructor(private navigation: navigation,
                 private navigationtab: navigationtab,
-                private activatedRoute: ActivatedRoute,
                 private metadata: metadata,
-                private model: model,
-                private broadcast: broadcast,
-                private elementref: ElementRef) {
+                private model: model) {
 
+        // subsribe to the route
         this.componentSubscriptions.add(
             this.navigationtab.activeRoute$.subscribe(route => {
-                if (this.model.module != route.params.module || this.model.id != route.params.id) {
-                    // load the model
-                    // get the bean details
-                    this.model.module = route.params.module;
-                    this.model.id = route.params.id;
-                    this.model.getData(true, 'detailview', true, true).subscribe(data => {
-                        // this.navigation.setActiveModule(this.moduleName, this.model.id, data.summary_text);
-                        this.navigationtab.setTabInfo({displayname: data.summary_text, displaymodule: this.model.module});
-                    });
-
-                    if (this.initialized) {
-                        this.buildContainer();
-                    }
-                }
+                this.setRouteData(route);
             })
         );
+
+        // subscribe to the broacast message
+        // ToDo: once we make the add in pinnable
+        /*
         this.componentSubscriptions.add(
             this.broadcast.message$.subscribe(message => {
                 this.handleMessage(message);
             })
         );
+        */
     }
 
+    /**
+     * initialize and load based on the route data
+     *
+     * @param route
+     */
+    private setRouteData(route) {
+        if (this.model.module != route.params.module || this.model.id != route.params.id) {
+            // load the model
+            // get the bean details
+            this.model.module = route.params.module;
+            this.model.id = route.params.id;
+            this.model.getData(true, 'detailview', true, true).subscribe(data => {
+                // this.navigation.setActiveModule(this.moduleName, this.model.id, data.summary_text);
+                this.navigationtab.setTabInfo({displayname: data.summary_text, displaymodule: this.model.module});
+            });
+
+            if (this.initialized) {
+                this.buildContainer();
+            }
+        }
+    }
+
+    /**
+     * handles th ebroadcast message
+     * ToDo: add the pinnable support here
+     *
+     * @param message
+     */
     private handleMessage(message) {
         switch (message.messagetype) {
             case 'applauncher.setrole':
@@ -71,17 +103,26 @@ export class GroupwareDetailPaneView implements AfterViewInit, OnDestroy{
         }
     }
 
+    /**
+     * initialize the view and render it
+     */
     public ngAfterViewInit(): void {
-        if(this.model.module && this.model.id){
+        if (this.model.module && this.model.id) {
             this.initialized = true;
             this.buildContainer();
         }
     }
 
+    /**
+     * unsubscribe from all subscriptions
+     */
     public ngOnDestroy() {
         this.componentSubscriptions.unsubscribe();
     }
 
+    /**
+     * load the config and build the container
+     */
     private buildContainer() {
         for (let component of this.componentRefs) {
             component.destroy();
