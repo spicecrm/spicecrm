1bc897c49488ff1e7f9097bcac6451274802d0ef
Christian Knoll <christian.knoll@twentyreasons.com>
2020-04-19 23:07:54 +0200

    outputemplates module

---
+++ src/modules/outputtemplates/moduleoutputtemplates.ts
@@ -0,0 +1,30 @@
+/**
+ * @module ModuleOutputTemplates
+ */
+import {CommonModule} from "@angular/common";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+
+import {DirectivesModule} from "../../directives/directives";
+import {ObjectFields} from "../../objectfields/objectfields";
+import {GlobalComponents} from "../../globalcomponents/globalcomponents";
+import {ObjectComponents} from "../../objectcomponents/objectcomponents";
+import {SystemComponents} from "../../systemcomponents/systemcomponents";
+
+import /*embed*/ {OutputTemplatesEditor} from "./components/outputtemplateseditor";
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ObjectFields,
+        GlobalComponents,
+        ObjectComponents,
+        SystemComponents,
+        DirectivesModule,
+    ],
+    declarations: [
+        OutputTemplatesEditor
+    ]
+})
+export class ModuleOutputTemplates {}
