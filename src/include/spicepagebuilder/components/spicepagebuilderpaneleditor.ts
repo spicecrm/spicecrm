6bffef186dfc930c8e6228f38b51d6ffb6aca550
MehyarSawas <mehyar.sawas@outlook.com>
2020-04-27 19:40:48 +0200

    Spice Page Builder: New elements added.

--- src/include/spicepagebuilder/components/spicepagebuilderpaneleditor.ts
+++ src/include/spicepagebuilder/components/spicepagebuilderpaneleditor.ts
@@ -1,12 +1,15 @@
 /**
  * @module ModuleSpicePageBuilder
  */
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, Renderer2, ViewChild, ViewContainerRef} from '@angular/core';
 import {BehaviorSubject} from "rxjs";
 import {SpicePageBuilderService} from "../services/spicepagebuilder.service";
+import {libloader} from "../../../services/libloader.service";
 
 /** @ignore */
 declare var _;
+/** @ignore */
+declare var html_beautify: any;
 
 /**
  * render a set of tools and configurations to be used for building pages
@@ -16,7 +19,11 @@ declare var _;
     templateUrl: './src/include/spicepagebuilder/templates/spicepagebuilderpaneleditor.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SpicePageBuilderPanelEditor {
+export class SpicePageBuilderPanelEditor implements OnInit {
+    /**
+     * code container reference to beatify
+     */
+    @ViewChild('codeContainer', {read: ViewContainerRef, static: false}) private codeContainer: ViewContainerRef;
     /**
      * hold the element to be edited
      */
@@ -26,11 +33,56 @@ export class SpicePageBuilderPanelEditor {
      */
     public response: BehaviorSubject<boolean> = new BehaviorSubject(false);
     /**
+     * is true when the beatify library is loaded
+     */
+    public beautifyLoaded: boolean = false;
+    /**
      * holds a reference to the component for destroy
      */
     public self: any = {};
+    /**
+     * align radio options
+     */
+    public alignArray: any[] = [
 
-    constructor(private spicePageBuilderService: SpicePageBuilderService) {
+    ];
+
+    constructor(private spicePageBuilderService: SpicePageBuilderService,
+                private cdRef: ChangeDetectorRef,
+                private libloader: libloader) {
+    }
+
+    /**
+     * set divider spacing
+     * @param value
+     */
+    set dividerSpacing(value) {
+        this.element.style['margin-top'] = value;
+        this.element.style['padding-top'] = value;
+    }
+
+    /**
+     * @return divider margin top
+     */
+    get dividerSpacing() {
+        return this.element.style['margin-top'];
+    }
+
+    /**
+     * call to load beatify library
+     */
+    public ngOnInit(): void {
+        this.loadBeatifyLibrary();
+    }
+
+    /**
+     * load beatify library
+     */
+    private loadBeatifyLibrary() {
+        this.libloader.loadLib('jsbeautify').subscribe(loaded => {
+            this.beautifyLoaded = true;
+            this.cdRef.detectChanges();
+        });
     }
 
     /**
@@ -57,7 +109,7 @@ export class SpicePageBuilderPanelEditor {
      * close the modal and emit response true for the element
      */
     private confirm() {
-        this.response.next(true);
+        this.response.next(JSON.parse(JSON.stringify(this.element)));
         this.response.complete();
         this.self.destroy();
     }
@@ -68,5 +120,33 @@ export class SpicePageBuilderPanelEditor {
                 this.element.src = src;
             }
         });
+    }
+
+    /**
+     * beatify html code
+     */
+    private beautify() {
+        this.element.content = html_beautify(this.element.content, {
+            indent_size: 4,
+            indent_char:  " ",
+            indent_with_tabs: false,
+            end_with_newline: false,
+            indent_level: 0,
+            preserve_newlines: true,
+            max_preserve_newlines: 10,
+            space_in_paren: false,
+            space_in_empty_paren: false,
+            unindent_chained_methods: false,
+            break_chained_methods: false,
+            keep_array_indentation: false,
+            unescape_strings: false,
+            wrap_line_length: 100,
+            e4x: false,
+            comma_first: false,
+            operator_position: "before-newline",
+            indent_empty_lines: false,
+            templating: ["auto"]
+        });
+        this.cdRef.detectChanges();
     }
 }
