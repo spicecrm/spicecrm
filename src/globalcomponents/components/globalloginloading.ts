1009e79c58b64012986dd4834ee8704168a10d5b
Christian Knoll <christian.knoll@twentyreasons.com>
2020-04-28 22:05:23 +0200

    global loading image and loader screen changes

---
+++ src/globalcomponents/components/globalloginloading.ts
@@ -0,0 +1,18 @@
+/**
+ * @module GlobalComponents
+ */
+import {
+    Component
+} from '@angular/core';
+
+
+/**
+ * the login component that is rendered on the login screen and prompts for usrname and password resp offers alternative login methods
+ */
+@Component({
+    selector: 'global-login-loading',
+    templateUrl: './src/globalcomponents/templates/globalloginloading.html',
+})
+export class GlobalLoginLoading {
+
+}
