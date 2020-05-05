ed4c689df0d20d9a755eda68e35387c8334073dd
strombo <andreas@gloeckl.name>
2020-05-05 15:59:49 +0200

    theming: set meta tag theme-color

--- src/services/configuration.service.ts
+++ src/services/configuration.service.ts
@@ -345,6 +345,10 @@ export class configurationService {
         for ( let colorname of allColors ) {
             document.documentElement.style.setProperty( '--'+colorname, colorsOfTheme[colorname] ? colorsOfTheme[colorname] : null );
         }
+
+        // color brand-primary may be set also by css file. we need it now to set the theme color in meta tag:
+        let colorBrandPrimary = document.documentElement.style.getPropertyValue( '--brand-primary' );
+        if ( colorBrandPrimary ) document.querySelector('meta[name="theme-color"]').setAttribute('content', colorBrandPrimary );
     }
 
     /**
