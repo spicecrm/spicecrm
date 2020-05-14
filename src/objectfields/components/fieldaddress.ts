0c3f11708c8fe3e6be7442a43a943270cefa5765
Christian Knoll <christian.knoll@twentyreasons.com>
2020-05-14 13:55:16 +0200

    fix address field

--- src/objectfields/components/fieldaddress.ts
+++ src/objectfields/components/fieldaddress.ts
@@ -64,8 +64,8 @@ export class fieldAddress extends fieldGeneric {
         this.city = address.city;
         this.district = address.district;
         this.postalcode = address.postalcode;
-        this.state = address.state;
         this.country = address.country;
+        this.state = address.state;
         this.latitude = address.latitude;
         this.longitude = address.longitude;
     }
