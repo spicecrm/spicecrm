62385cc974a04127fbfea0d18e4396e8cc5a8eea
Christian Knoll <christian.knoll@twentyreasons.com>
2020-05-17 06:59:53 +0200

    country and statehandling in address fields

---
+++ src/systemcomponents/components/systeminputstate.ts
@@ -0,0 +1,119 @@
+/**
+ * @module WorkbenchModule
+ */
+import {
+    Component, EventEmitter, forwardRef, Input, OnChanges, OnInit, Output
+} from '@angular/core';
+import {configurationService} from '../../services/configuration.service';
+import {metadata} from '../../services/metadata.service';
+import {language} from '../../services/language.service';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+
+@Component({
+    selector: 'system-input-state',
+    templateUrl: './src/systemcomponents/templates/systeminputstate.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => SystemInputState),
+            multi: true
+        }
+    ]
+})
+export class SystemInputState implements OnChanges, ControlValueAccessor {
+
+    /**
+     * the country for the state
+     */
+    @Input() private country: string;
+
+    /**
+     * the options from the enum
+     */
+    private states: any[] = [];
+
+    /**
+     * for the value accessor
+     */
+    private onChange: (value: string) => void;
+    private onTouched: () => void;
+
+    /**
+     * the value
+     */
+    private _value: string;
+
+    /**
+     * set to true if the address inpout shoudl be strict according to the dropdown values
+     */
+    private strict: boolean = false;
+
+    constructor(private metadata: metadata, private language: language, private configuration: configurationService) {
+        let addressmode = this.configuration.getCapabilityConfig('spiceui').addressmode;
+        if(addressmode == 'strict') this.strict = true;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(value) {
+        if (value != this._value) {
+            this._value = value;
+            this.onChange(value);
+        }
+    }
+
+    /**
+     * Set the function to be called
+     * when the control receives a change event.
+     *
+     * @param fn a function
+     */
+    public registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+
+    /**
+     * Set the function to be called
+     * when the control receives a touch event.
+     *
+     * @param fn a function
+     */
+    public registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
+
+    /**
+     * Write a new value to the element.
+     *
+     * @param value value to be executed when there is a change in contenteditable
+     */
+    public writeValue(value: any): void {
+        this._value = value;
+    }
+
+    /**
+     * on changes load the states for the country
+     */
+    public ngOnChanges(): void {
+        if(this.strict) {
+            this.getStates();
+        }
+    }
+
+    /**
+     * get teh states filtered by the country
+     */
+    public getStates() {
+        this.states = [];
+        if (this.country) {
+            let states = this.configuration.getData('countries');
+            if (states.states) {
+                this.states = states.states.filter(s => s.cc == this.country);
+                this.states.sort((a, b) => this.language.getLabel(a.label).localeCompare(this.language.getLabel(b.label)));
+            }
+        }
+    }
+
+}
