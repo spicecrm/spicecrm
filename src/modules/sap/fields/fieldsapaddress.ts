54198331d5cdf13e732627f3c015b2f08ce43d76
Christian Knoll <christian.knoll@twentyreasons.com>
2020-05-17 06:59:53 +0200

    country and statehandling in address fields

--- src/modules/sap/fields/fieldsapaddress.ts
+++ src/modules/sap/fields/fieldsapaddress.ts
@@ -28,67 +28,11 @@ export class fieldSAPAddress extends fieldAddress {
         public language: language,
         public metadata: metadata,
         public router: Router,
-        private configuration: configurationService
+        public configuration: configurationService
     ) {
-        super(model, view, language, metadata, router);
+        super(model, view, language, metadata, router, configuration);
 
-        this.loadCountries();
 
-    }
-
-    private loadCountries() {
-        let countries = this.configuration.getData('countries');
-        this.countries = countries?.countries ? countries.countries : [];
-
-        this.countries.sort((a, b) => this.language.getLabel(a.label) > this.language.getLabel(b.label) ? 1 : -1);
-    }
-
-    get states() {
-        let ret = [];
-        if (this.country) {
-            let states = this.configuration.getData('countries');
-            if (states.states) {
-                ret = states.states.filter(s => s.cc == this.country);
-                ret.sort((a, b) => this.language.getLabel(a.label) > this.language.getLabel(b.label) ? 1 : -1);
-            }
-        }
-
-        return ret;
-    }
-
-    /**
-     * a getter for the country
-     */
-    get country() {
-        return this.model.data[this.addresskey + 'address_country'];
-    }
-
-    /**
-     * a setter for the country
-     *
-     * @param value
-     */
-    set country(value) {
-        if(value != this.model.data[this.addresskey + 'address_country']) {
-            this.model.setField(this.addresskey + 'address_country', value);
-            this.state = '';
-        }
-    }
-
-    /**
-     * a getter for the street
-     */
-    get street_number() {
-        return this.model.data[this.addresskey + 'address_street_number'];
-    }
-
-    /**
-     * a setter for the street
-     *
-     * @param value
-     */
-    set street_number(value) {
-        this.model.setField(this.addresskey + 'address_street_number', value);
     }
 
     /**
@@ -138,35 +82,6 @@ export class fieldSAPAddress extends fieldAddress {
         this.state = address.state;
         this.latitude = address.latitude;
         this.longitude = address.longitude;
-    }
-
-    /**
-     * builds a formatted address form all elements and renders it on the screen
-     */
-    public buildAddress() {
-        let address = '';
-        let address_arr = [];
-        if (this.model.data[this.addresskey + 'address_attn']) {
-            address_arr.push(this.model.data[this.addresskey + 'address_attn']);
-        }
-        if (this.model.data[this.addresskey + 'address_street']) {
-            address_arr.push(this.model.data[this.addresskey + 'address_street']);
-        }
-        if (this.model.data[this.addresskey + 'address_postalcode']) {
-            address_arr.push(this.model.data[this.addresskey + 'address_postalcode'] + ' ' + this.model.data[this.addresskey + 'address_city']);
-        }
-        if (this.model.data[this.addresskey + 'address_state']) {
-            let state = this.model.getField(this.addresskey + 'address_state');
-            let stateItem = this.states.find(s => s.sc == state);
-            address_arr.push(stateItem ? this.language.getLabel(stateItem.label) : state);
-        }
-        if (this.model.data[this.addresskey + 'address_country']) {
-            let country = this.model.getField(this.addresskey + 'address_country');
-            let countryItem = this.countries.find(c => c.cc == country);
-            address_arr.push(countryItem ? this.language.getLabel(countryItem.label) : country);
-        }
-        address = address_arr.join(', ');
-        return address;
     }
 
 }
