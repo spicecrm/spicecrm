c39593813b49c8dfc7803d9b84e978b2d12b57d9
Christian Knoll <christian.knoll@twentyreasons.com>
2020-05-20 06:50:41 +0200

    Price Conditions

--- src/modules/priceconditions/components/priceconditionsbydetermination.ts
+++ src/modules/priceconditions/components/priceconditionsbydetermination.ts
@@ -1,9 +1,8 @@
 /**
  * @module ModulePriceConditions
  */
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {Router} from '@angular/router';
-import {animate, state, style, transition, trigger} from "@angular/animations";
 import {metadata} from '../../../services/metadata.service';
 import {model} from '../../../services/model.service';
 import {backend} from '../../../services/backend.service';
@@ -15,43 +14,15 @@ import {priceconditonsconfiguration} from '../services/priceconditonsconfigurati
 declare var _: any;
 
 @Component({
+    selector: 'price-conditions-by-determination',
     templateUrl: './src/modules/priceconditions/templates/priceconditionsbydetermination.html',
-    providers: [priceconditonsconfiguration],
-    animations: [
-        trigger('conditionscard', [
-            transition(':enter', [
-                style({opacity: 0, height: '0px', overflow: 'hidden'}),
-                animate('.5s', style({height: '*', opacity: 1})),
-                style({overflow: 'unset'})
-            ]),
-            transition(':leave', [
-                style({overflow: 'hidden'}),
-                animate('.5s', style({height: '0px', opacity: 0}))
-            ])
-        ]),
-        trigger('animateicon', [
-            state('open', style({transform: 'scale(1, 1)'})),
-            state('closed', style({transform: 'scale(1, -1)'})),
-            transition('open => closed', [
-                animate('.5s'),
-            ]),
-            transition('closed => open', [
-                animate('.5s'),
-            ])
-        ])
-    ]
 })
-export class PriceConditionsByDetermination {
-
-    /**
-     * inidcates that the panel is loading
-     */
-    private loading: boolean = true;
+export class PriceConditionsByDetermination implements OnInit {
 
     /**
      * all loaded conditions
      */
-    private conditions: any[] = [];
+    @Input() private conditions: any[] = [];
 
     /**
      * the list of determinationtypes
@@ -63,61 +34,31 @@ export class PriceConditionsByDetermination {
      */
     private activedeterminationtype: string;
 
-    /**
-     * for the collapsible panel if the panel is open
-     */
-    private _isopen: boolean = true;
 
     constructor(private language: language, private metadata: metadata, private model: model, private router: Router, private backend: backend, private configuration: configurationService, private priceconditonsconfiguration: priceconditonsconfiguration) {
-        this.priceconditonsconfiguration.loaded$.subscribe(loaded => {
-            if (loaded) {
-                this.loadConditions();
-            }
-        });
     }
 
-
-    /**
-     * toggle Open or Close the panel
-     */
-    private toggleOpen(e: MouseEvent) {
-        e.stopPropagation();
-        this._isopen = !this._isopen;
+    public ngOnInit(): void {
+        this.analyzeConditions();
     }
 
 
     /**
      * loads the conditions for the accounnt on the backend
      */
-    private loadConditions() {
-        this.backend.getRequest(`module/PriceConditions/list/${this.model.module}/${this.model.id}`).subscribe(conditions => {
-            this.conditions = conditions;
-
-            // determine the conditiontypes we have
-            /**
-             for (let condition of conditions) {
-                if (this.conditiontypes.indexOf(condition.priceconditiontype_id) < 0) {
-                    this.conditiontypes.push(condition.priceconditiontype_id);
-                }
-            }*/
+    private analyzeConditions() {
+        this.determinationtypes = [];
+        let determinationtypes = _.uniq(this.conditions.map(d => d.priceconditiontypedetermination_id));
+        for (let determinationtype of determinationtypes) {
+            this.determinationtypes.push(this.priceconditonsconfiguration.config.determinations.find(d => d.id == determinationtype));
+        }
+        this.determinationtypes.sort((a, b) => a.sortindex > b.sortindex ? 1 : -1);
 
 
-            this.determinationtypes = [];
-            let determinationtypes = _.uniq(this.conditions.map(d => d.priceconditiontypedetermination_id));
-            for(let determinationtype of determinationtypes){
-                this.determinationtypes.push(this.priceconditonsconfiguration.config.determinations.find(d => d.id == determinationtype));
-            }
-            this.determinationtypes.sort((a, b) => a.sortindex > b.sortindex ? 1 : -1);
-
-
-            // set the first one to active
-            if (this.determinationtypes.length > 0) {
-                this.setDeterminationType(this.determinationtypes[0].id);
-            }
-
-            // set loading to false
-            this.loading = false;
-        });
+        // set the first one to active
+        if (this.determinationtypes.length > 0) {
+            this.setDeterminationType(this.determinationtypes[0].id);
+        }
     }
 
     /**
@@ -127,13 +68,6 @@ export class PriceConditionsByDetermination {
      */
     private setDeterminationType(determinationtypeid) {
         this.activedeterminationtype = determinationtypeid;
-    }
-
-    /**
-     * a helper to get if we have related models and the state is open
-     */
-    get isopen() {
-        return this._isopen;
     }
 
     get activedeterminations() {
--- src/modules/priceconditions/modulepriceconditions.ts
+++ src/modules/priceconditions/modulepriceconditions.ts
@@ -14,6 +14,7 @@ import {DirectivesModule} from '../../directives/directives';
 import /*embed*/ {priceconditonsconfiguration} from './services/priceconditonsconfiguration.service';
 
 import /*embed*/ {PriceConditionsAccountsPanel} from './components/priceconditionsaccountspanel';
+import /*embed*/ {PriceConditionsByCondition} from './components/priceconditionsbycondition';
 import /*embed*/ {PriceConditionsConditionsList} from './components/priceconditionsconditionslist';
 import /*embed*/ {PriceConditionsByDetermination} from './components/priceconditionsbydetermination';
 import /*embed*/ {PriceConditionsByDeterminationList} from './components/priceconditionsbydeterminationlist';
@@ -30,6 +31,7 @@ import /*embed*/ {PriceConditionsByDeterminationList} from './components/priceco
     ],
     declarations: [
         PriceConditionsAccountsPanel,
+        PriceConditionsByCondition,
         PriceConditionsConditionsList,
         PriceConditionsByDetermination,
         PriceConditionsByDeterminationList
