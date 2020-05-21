d5430966d4c794c9610063965c42618e9f32ee1d
Christian Knoll <christian.knoll@twentyreasons.com>
2020-05-21 11:33:42 +0200

    google login .. removed sync

--- src/globalcomponents/components/globallogingoogle.ts
+++ src/globalcomponents/components/globallogingoogle.ts
@@ -104,28 +104,10 @@ export class GlobalLoginGoogle {
                 this.loginService.authData.userName = "";
                 this.loginService.authData.password = "";
                 // this.session.authData.sessionId = user_token;
-                this.loginService.login().subscribe(
-                    (res) => {
-                        this.synchronize();
-                    },
-                    (err) => {
-                        console.log(err);
-                    },
-                );
+                this.loginService.login();
             })
             .catch((error: { error: string }) => {
                 console.log(JSON.stringify(error, undefined, 2));
             });
-    }
-
-    public synchronize() {
-        this.backend.getRequest('/google/calendar/sync').subscribe(
-            (res) => {
-                console.log('Successfully Synchronized');
-            },
-            (err) => {
-                console.log('Synchronization Error');
-            },
-        );
     }
 }
