3cf9e11c58012e0ab89a20e250187f9a4fdbbe97
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-06-08 16:16:25 +0200

    currency fix

--- src/modules/currencies/components/currencymanager.ts
+++ src/modules/currencies/components/currencymanager.ts
@@ -21,6 +21,7 @@ export class CurrencyManager implements OnInit {
     private currencies: any = [];
     private loading: boolean = true;
 
+
     constructor(
         private metadata: metadata,
         private language: language,
@@ -60,12 +61,17 @@ export class CurrencyManager implements OnInit {
         });
     }
 
+    /**
+     * reload the currencies when the event emitter has been emitted
+     * @param event
+     */
     private reload(event) {
         if(event) {
             this.modal.openModal('SystemLoadingModal').subscribe(modalRef => {
                 this.backend.getRequest('currencies').subscribe(data => {
                     if (data) {
                         this.currencies = data;
+                        this.currencies.shift();
                     } else {
                         this.toast.sendToast(this.language.getLabel('LBL_ERROR'), 'error');
                     }
