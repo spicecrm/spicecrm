1fe86d193f4d63f62a5c1923722dbe4a9b966317
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-06-10 09:52:23 +0200

    repair and labeling

--- src/admincomponents/components/administrationdictrepair.ts
+++ src/admincomponents/components/administrationdictrepair.ts
@@ -19,7 +19,7 @@ declare var moment: any;
 export class AdministrationDictRepair {
 
     private loading: boolean = false;
-
+    private sqlresult: string = '';
     private dbErrors: any = [];
 
     constructor(private backend: backend, private toast: toast, private language: language) {
@@ -29,9 +29,12 @@ export class AdministrationDictRepair {
     private doRepair() {
         this.loading = true;
         this.backend.postRequest('dictionary/repair').subscribe((result: any) => {
-            if(!result.response) {
+            if (!result.response) {
                 this.dbErrors = result.errors;
+            } else if (result.synced) {
+                this.toast.sendToast(this.language.getLabel('LBL_REPAIR_DATABASE_ALREADY_SYNCED'), 'success');
             } else {
+                this.sqlresult = result.sql;
                 this.toast.sendToast(this.language.getLabel('LBL_REPAIR_DATABASE_SYNCED'), 'success');
             }
             this.loading = false;
