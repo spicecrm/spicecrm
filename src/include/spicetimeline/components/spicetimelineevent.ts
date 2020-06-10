8195ba72ba1bf746a0e5506d25fd0d75603bf779
MehyarSawas <mehyar.sawas@outlook.com>
2020-06-10 11:18:47 +0200

    Module SpiceTimeline built. Component Service Planner added.

---
+++ src/include/spicetimeline/components/spicetimelineevent.ts
@@ -0,0 +1,62 @@
+/**
+ * @module ModuleSpiceTimeline
+ */
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {metadata} from "../../../services/metadata.service";
+import {model} from "../../../services/model.service";
+
+@Component({
+    selector: 'spice-timeline-event',
+    templateUrl: './src/include/spicetimeline/templates/spicetimelineevent.html',
+    providers: [model]
+})
+export class SpiceTimelineEvent implements OnChanges, OnInit {
+    /**
+     * holds the records main module
+     */
+    @Input() protected event: any;
+    /**
+     * a fieldset id for loading a body fieldset in the event
+     */
+    private fieldset: string;
+
+    constructor(private metadata: metadata,
+                private model: model) {
+    }
+
+    /**
+     * call to set the model data
+     */
+    public ngOnInit() {
+        this.loadFieldset();
+    }
+
+    /**
+     * call to set the model data
+     */
+    public ngOnChanges() {
+        this.setModelData();
+    }
+
+    /**
+     * load event fieldsets
+     */
+    private loadFieldset() {
+        let config = this.metadata.getComponentConfig('SpiceTimelineEvent', this.model.module);
+        if (config && config.fieldset) {
+            this.fieldset = config.fieldset;
+        }
+    }
+
+    /**
+     * set model data from event
+     */
+    private setModelData() {
+        this.model.id = this.event.id;
+        this.model.module = this.event.module;
+        this.model.data = this.event.data;
+    }
+}
+
+
+
