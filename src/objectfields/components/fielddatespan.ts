f264f5c8b6d6b32cf02defc48c2de6b7087c1b56
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-06-25 08:36:56 +0200

    startdate, enddate moment obj

--- src/objectfields/components/fielddatespan.ts
+++ src/objectfields/components/fielddatespan.ts
@@ -45,11 +45,11 @@ export class fieldDateSpan extends fieldGeneric implements OnInit {
 
 
     get formattedStartDate() {
-        return this.startDate ? this.startDate.format(this.userpreferences.getDateFormat()) : '';
+        return this.startDate ? new moment(this.startDate).format(this.userpreferences.getDateFormat()) : '';
     }
 
     get formattedEndDate() {
-        return this.endDate ? this.endDate.format(this.userpreferences.getDateFormat()) : '';
+        return this.endDate ? new moment(this.endDate).format(this.userpreferences.getDateFormat()) : '';
     }
 
     get startDate() {
