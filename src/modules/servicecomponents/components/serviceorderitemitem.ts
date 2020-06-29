b656961aa5e1dfd278cb9d231365821190e6061b
Christian Knoll <christian.knoll@twentyreasons.com>
2020-06-29 08:26:23 +0200

    Service order Management

--- src/modules/servicecomponents/components/serviceorderitemitem.ts
+++ src/modules/servicecomponents/components/serviceorderitemitem.ts
@@ -35,13 +35,13 @@ export class ServiceOrderItemItem implements OnInit  {
     /**
      * the columns to be displayed
      */
-    private fieldsetItems: any[] = [];
+    public fieldsetItems: any[] = [];
 
     constructor(
-        private metadata: metadata,
-        private language: language,
-        private model: model,
-        private view: view
+        public metadata: metadata,
+        public language: language,
+        public model: model,
+        public view: view
     ) {
     }
 
@@ -97,7 +97,7 @@ export class ServiceOrderItemItem implements OnInit  {
     /**
      * set the configuration
      */
-    private setConfig() {
+    public setConfig() {
         let config = this.metadata.getComponentConfig('ServiceOrderItemPanel', this.serviceorder.module);
         if (config.fieldset) {
             this.fieldsetItems = this.metadata.getFieldSetItems(config.fieldset);
