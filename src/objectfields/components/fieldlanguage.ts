5eb47316905c7220943c1c6ed1de3955549153c4
MehyarSawas <mehyar.sawas@outlook.com>
2020-06-29 08:58:07 +0200

    Field Language: empty option added.

--- src/objectfields/components/fieldlanguage.ts
+++ src/objectfields/components/fieldlanguage.ts
@@ -7,44 +7,75 @@ import {view} from '../../services/view.service';
 import {language} from '../../services/language.service';
 import {metadata} from '../../services/metadata.service';
 import {fieldGeneric} from './fieldgeneric';
-import {Router}   from '@angular/router';
+import {Router} from '@angular/router';
 
+/**
+ * Display the available system languages in a select list and handle setting the language field value.
+ */
 @Component({
     selector: 'field-enum',
     templateUrl: './src/objectfields/templates/fieldlanguage.html'
 })
-export class fieldLanguage extends fieldGeneric implements OnInit
-{
-
-    options: Array<any> = [];
+export class fieldLanguage extends fieldGeneric implements OnInit {
+    /**
+     * display value for the language
+     */
+    protected displayValue: string = '';
+    /**
+     * system language possible options
+     */
+    protected options: any[] = [];
 
     constructor(public model: model, public view: view, public language: language, public metadata: metadata, public router: Router) {
         super(model, view, language, metadata, router);
-    }
-
-    getValue(): String{
-        for(let language of this.options){
-            if(language.value == this.value){
-                return language.display;
-            }
-        };
-
-        return this.value;
 
     }
 
-    ngOnInit(){
-         this.getOptions();
+    /**
+     * get language options and subscribe to data changes
+     */
+    public ngOnInit() {
+        super.ngOnInit();
+        this.getOptions();
+        this.subscribeToDataChanges();
     }
 
-    getOptions()
-    {
-        for(let language of this.language.getAvialableLanguages()){
-            this.options.push({
-                value: language.language,
-                display: language.text
-            });
+    /**
+     * get language options
+     */
+    private getOptions() {
+        this.options = this.language.getAvialableLanguages().map(language => ({
+            value: language.language,
+            display: language.text
+        }));
+        if (!!this.fieldconfig.canBeEmpty) {
+            this.options.unshift({value: '', display: ''});
         }
     }
 
+    /**
+     * subscribe to model data change and set the value and the display value
+     */
+    private subscribeToDataChanges() {
+
+        this.subscriptions.add(
+            this.model.data$.subscribe(data => {
+
+                if (!!data[this.fieldname] && this.value != data[this.fieldname]) {
+                    this.value = data[this.fieldname];
+                }
+                if (this.displayValue != this.value) {
+                    this.setDisplayValue();
+                }
+            })
+        );
+    }
+
+    /**
+     * set the display value
+     */
+    private setDisplayValue() {
+        const lang = this.options.find(lang => lang.value == this.model.data[this.fieldname]);
+        this.displayValue = lang.display || '';
+    }
 }
