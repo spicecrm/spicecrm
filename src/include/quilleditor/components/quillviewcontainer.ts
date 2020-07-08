f8eacf712c89af4aa306a4a38b203a954e2f8919
MehyarSawas <mehyar.sawas@outlook.com>
2020-07-08 09:36:01 +0200

    SPICEUI-364: Quill View: set content value corrected.

--- src/include/quilleditor/components/quillviewcontainer.ts
+++ src/include/quilleditor/components/quillviewcontainer.ts
@@ -106,7 +106,7 @@ export class QuillViewContainer implements AfterViewInit, OnChanges {
      */
     private setEditorContent(): any {
 
-        if (!this.quillEditor || !this.content) return;
+        if (!this.quillEditor) return;
         this.quillEditor.setContents(
             this.quillEditor.clipboard.convert(this.getCleanHtml())
         );
@@ -117,6 +117,7 @@ export class QuillViewContainer implements AfterViewInit, OnChanges {
      * get clean html value by ensuring the encode/decode the code snippets
      */
     private getCleanHtml() {
+        if (!this.content) return '';
         const regexp = /(?<=<pre class="ql-syntax" spellcheck="false">)[\s\S]*?(?=<\/pre>)/g;
         const match = regexp.exec(
             this.decodeHTMLEntities(this.content)
