54e2f4e889f1f52d75ecfbeeb1aaaff5f6e98c2a
Christian Knoll <christian.knoll@twentyreasons.com>
2020-07-11 17:29:57 +0200

    fix spice text add button

--- src/include/spicetexts/components/spicetextsaddbutton.ts
+++ src/include/spicetexts/components/spicetextsaddbutton.ts
@@ -3,6 +3,7 @@
  */
 import {Component, Input} from '@angular/core';
 import {model} from '../../../services/model.service';
+import {metadata} from '../../../services/metadata.service';
 import {language} from '../../../services/language.service';
 import {modal} from "../../../services/modal.service";
 import {relatedmodels} from "../../../services/relatedmodels.service";
@@ -25,6 +26,7 @@ export class SpiceTextsAddButton {
                 private modal: modal,
                 private configurationService: configurationService,
                 private language: language,
+                private metadata: metadata,
                 private relatedModels: relatedmodels) {
         this.model.module = 'SpiceTexts';
     }
@@ -32,6 +34,10 @@ export class SpiceTextsAddButton {
     get sysTextIds() {
         let sysTextIds = this.configurationService.getData('systextids');
         return sysTextIds ? _.values(sysTextIds).filter(text => text.module == this.parent.module) : [];
+    }
+
+    get canAdd(){
+        return this.metadata.checkModuleAcl('SpiceTexts', 'create') && !this.allTranslated;
     }
 
     get allTranslated() {
