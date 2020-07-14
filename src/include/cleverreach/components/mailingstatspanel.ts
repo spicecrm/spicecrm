599e763724877fe2ee8ce1fd7d2cb0124726c34b
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-07-14 12:42:03 +0200

    cleanup mailingstats

--- src/include/cleverreach/components/mailingstatspanel.ts
+++ src/include/cleverreach/components/mailingstatspanel.ts
@@ -7,9 +7,6 @@ import {metadata} from '../../../services/metadata.service';
 import {model} from '../../../services/model.service';
 import {language} from '../../../services/language.service';
 import {backend} from "../../../services/backend.service";
-import {libloader} from '../../../services/libloader.service';
-
-declare var google: any;
 
 /**
  * gets the statistics of a mailing / campaigntask
@@ -20,10 +17,7 @@ declare var google: any;
 export class MailingStatsPanel implements OnInit {
     public mailingStats: any = {};
     private isLoading: boolean = false;
-    private chart: any;
-    private options: {};
     public data: any;
-    private res: any[] = [];
 
 
     constructor(
@@ -31,7 +25,6 @@ export class MailingStatsPanel implements OnInit {
         private metadata: metadata,
         private model: model,
         private backend: backend,
-        private libloader: libloader
     ) {
 
     }
@@ -49,28 +42,6 @@ export class MailingStatsPanel implements OnInit {
             });
     }
 
-    // public drawChart() {
-    //     this.isLoading = true;
-    //     this.backend.getRequest(`/CleverReach/CampaignTasks/${this.model.id}/stats`)
-    //         .subscribe(response => {
-    //             this.mailingStats = response.basic;
-    //             for (let i in this.mailingStats) {
-    //                 this.res.push([i, this.mailingStats [i]]);
-    //             }
-    //             this.data = new google.visualization.DataTable();
-    //             this.data.addColumn('string', 'Metric');
-    //             this.data.addColumn('number', 'Value');
-    //             this.data.addRows(this.res);
-    //             window.console.log(this.data);
-    //             this.chart = new google.visualization.PieChart(document.getElementById('piechart'));
-    //             this.options = {
-    //                 pieHole: 0.4,
-    //             };
-    //             this.chart.draw(this.data, this.options);
-    //             this.isLoading = false;
-    //         });
-    // }
-
     /**
      * loads mailing activity from backend when the application first starts
      */
@@ -81,12 +52,6 @@ export class MailingStatsPanel implements OnInit {
             this.mailingStats = response;
             this.isLoading = false;
         });
-        // this.libloader.loadLib('googlecharts').subscribe((next) => {
-        //     google.charts.load('current', {packages: ['corechart']});
-        //     google.charts.setOnLoadCallback(() => {
-        //         this.drawChart();
-        //     });
-        // });
 
     }
 
