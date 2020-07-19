cd4a265827ac869ea3e8ab529aa186e208ee5265
Christian Knoll <christian.knoll@twentyreasons.com>
2020-07-19 12:45:59 +0200

    minor ACL Fixes

--- src/objectfields/components/fieldgenericdisplay.ts
+++ src/objectfields/components/fieldgenericdisplay.ts
@@ -60,7 +60,7 @@ export class fieldGenericDisplay {
      */
     get link() {
         try {
-            return this.view.displayLinks && this.fieldconfig.link && this.model.data.acl.detail;
+            return this.view.displayLinks && this.fieldconfig.link && this.model.checkAccess('detail');
         } catch (e) {
             return false;
         }
