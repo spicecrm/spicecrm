1d34d081e62b2f10d084af19a9ab8d6fea86b8d8
MehyarSawas <mehyar.sawas@outlook.com>
2020-09-11 10:25:04 +0200

    Language Service: Set flag "setPreferences" to save the current language to user preferences.

--- src/services/language.service.ts
+++ src/services/language.service.ts
@@ -110,7 +110,7 @@ export class language {
 
         this.http.get(
             this.configurationService.getBackendUrl() + '/language/'+this.currentlanguage,
-            {headers: this.session.getSessionHeader(), observe: "response"}
+            {headers: this.session.getSessionHeader(), observe: "response", params: {setPreferences: '1'}}
         ).subscribe(
             (res: any) => {
                 let response = res.body;
