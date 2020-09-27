7cfd19288ee6fc0633f039279773ff59010eb673
Christian Knoll <christian.knoll@twentyreasons.com>
2020-09-27 15:40:15 +0200

    Domain Manager

---
+++ src/workbench/components/domainmanageraddfieldmodal.ts
@@ -0,0 +1,65 @@
+/**
+ * @module WorkbenchModule
+ */
+import {
+    Component
+} from '@angular/core';
+import {metadata} from '../../services/metadata.service';
+import {modelutilities} from '../../services/modelutilities.service';
+import {domainmanager} from '../services/domainmanager.service';
+
+@Component({
+    templateUrl: './src/workbench/templates/domainmanageraddfieldmodal.html',
+})
+export class DomainManagerAddFieldModal {
+
+    /**
+     * reference to the modal self
+     */
+    private self: any;
+
+    /**
+     * the domain definition
+     */
+    private domainfield: any = {
+        name: '',
+        fieldtype: '',
+        scope: 'g',
+        required: false
+    };
+
+    constructor(private domainmanager: domainmanager, private metadata: metadata, private modelutilities: modelutilities) {
+
+    }
+
+    /**
+     * close the modal
+     */
+    private close() {
+        this.self.destroy();
+    }
+
+    /**
+     * check if we can save
+     *
+     * name and fieldtype are defined and name does not exists yet
+     *
+     */
+    get canSave() {
+        return this.domainfield.name && this.domainfield.dbtype && !this.domainmanager.domainfields.find(f => f.name == this.domainfield.name && f.sysdomaindefinition_id == this.domainmanager.currentDomainDefinition);
+    }
+
+    /**
+     * saves the modal
+     */
+    private save() {
+        if(this.canSave) {
+            this.domainfield.id = this.modelutilities.generateGuid();
+            this.domainfield.sysdomaindefinition_id = this.domainmanager.currentDomainDefinition;
+            this.domainmanager.domainfields.push(this.domainfield);
+            this.close();
+        }
+    }
+
+
+}
