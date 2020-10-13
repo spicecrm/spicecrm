4a91918ba5d2757e7308f47a42f7795e0aa2d9dd
maretval <valerie.maret@twentyreasons.com>
2020-10-13 08:56:46 +0200

    CR1000467: regex capability for validation rule

--- src/services/model.service.ts
+++ src/services/model.service.ts
@@ -643,7 +643,12 @@ export class model implements OnDestroy {
         }
 
         let val_left = this.data[condition.fieldname];
-        let val_right = this.evaluateValidationParams(condition.valuations);
+        let val_right = null;
+        if(condition.comparator.match(/regex/g)) {
+            val_right = condition.valuations;
+        } else {
+            val_right = this.evaluateValidationParams(condition.valuations);
+        }
 
         check = modelutilities.compare(val_left, condition.comparator, val_right);
 
