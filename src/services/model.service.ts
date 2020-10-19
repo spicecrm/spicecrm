5006f9b4979f7ddd0435717a1fb56811a2881459
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-10-19 11:24:43 +0200

    bugfix duplicate contacts in copyrules for calls/meetings: dont generate new ids automatically

--- src/services/model.service.ts
+++ src/services/model.service.ts
@@ -1183,10 +1183,11 @@ export class model implements OnDestroy {
         if (!parent.data.id) parent.data.id = parent.id;
         let copyrules = this.metadata.getCopyRules(parent.module, this.module);
         for (let copyrule of copyrules) {
-            if (copyrule.fromfield && copyrule.tofield) {
+            if (copyrule.fromfield && copyrule.tofield ) {
                 // this.setFieldValue(copyrule.tofield, parent.getFieldValue(copyrule.fromfield));
                 // this.setFieldValue(copyrule.tofield, parent.data[copyrule.fromfield]);
-                this.copyValue(copyrule.tofield, parent.data[copyrule.fromfield]);
+                console.log(copyrule.params);
+                this.copyValue(copyrule.tofield, parent.data[copyrule.fromfield], copyrule.params);
             } else if (copyrule.tofield && copyrule.fixedvalue) {
                 this.setFieldValue(copyrule.tofield, copyrule.fixedvalue);
             }
@@ -1199,25 +1200,28 @@ export class model implements OnDestroy {
      * @param toField
      * @param value
      */
-    private copyValue(toField, value) {
+    private copyValue(toField, value, params) {
         let fieldDef = this.metadata.getFieldDefs(this.module, toField);
-
+        console.log(params);
         // if not found just set the field attribute
         if (!fieldDef) this.setField(toField, value);
 
         // handle links
+
         switch (fieldDef.type) {
             case 'link':
-                if (_.isObject(value) && value.beans) {
-                    const newLink = {beans: {}};
-                    for (let relId in value.beans) {
-                        if (!value.beans.hasOwnProperty(relId)) continue;
+                if(params.generatenewid) {
+                    if (_.isObject(value) && value.beans) {
+                        const newLink = {beans: {}};
+                        for (let relId in value.beans) {
+                            if (!value.beans.hasOwnProperty(relId)) continue;
 
-                        const newId = this.utils.generateGuid();
-                        newLink.beans[newId] = {...value.beans[relId]};
-                        newLink.beans[newId].id = newId;
+                            const newId = this.utils.generateGuid();
+                            newLink.beans[newId] = {...value.beans[relId]};
+                            newLink.beans[newId].id = newId;
+                        }
+                        this.setField(toField, newLink);
                     }
-                    this.setField(toField, newLink);
                 }
                 break;
             default:
