f5543bde34541ec4d1ba3ced512a393a81e21212
Mehyar Sawas-Donauer <mehyar.sawas@outlook.com>
2020-11-09 12:12:13 +0100

    CRNR: 1000478: System Label inline edit on right click added.

--- src/systemcomponents/components/systemlabel.ts
+++ src/systemcomponents/components/systemlabel.ts
@@ -9,12 +9,13 @@ import {
     HostListener,
     Input,
     OnChanges,
-    OnDestroy,
+    OnDestroy, Renderer2,
     SimpleChanges
 } from '@angular/core';
 import {Subscription} from "rxjs";
 import {language} from '../../services/language.service';
 import {modal} from "../../services/modal.service";
+import {footer} from "../../services/footer.service";
 
 @Component({
     selector: 'system-label',
@@ -40,6 +41,8 @@ export class SystemLabel implements OnChanges, AfterViewInit, OnDestroy {
 
     constructor(private language: language,
                 private modal: modal,
+                private footer: footer,
+                private renderer: Renderer2,
                 private cdRef: ChangeDetectorRef) {
         this.subsciptions.add(
             this.language.currentlanguage$.subscribe(() => this.detectChanges())
@@ -88,5 +91,67 @@ export class SystemLabel implements OnChanges, AfterViewInit, OnDestroy {
         this.modal.openModal('SystemLabelEditorModal', true).subscribe(modalRef => {
             modalRef.instance.labelData = {name: this.label, global_translations: [], custom_translations: []};
         });
+    }
+
+    /**
+     * open label editor modal
+     * @private
+     */
+    public openModal() {
+        this.modal.openModal('SystemLabelEditorModal', true).subscribe(modalRef => {
+            modalRef.instance.labelData = {name: this.label, global_translations: [], custom_translations: []};
+        });
+    }
+
+    /**
+     * handle right click to edit translations
+     * @private
+     */
+    @HostListener('contextmenu', ['$event'])
+    private onRightClick(event) {
+        if (!this.language.inlineEditEnabled) return;
+        event.preventDefault();
+        const dropdown = this.createDropdown(event);
+        this.renderer.appendChild(this.footer.footercontainer.element.nativeElement, dropdown);
+        const docClickListener = this.renderer.listen('document', 'click', event => {
+            this.closeDropdown(event, dropdown);
+            docClickListener();
+        });
+    }
+
+    /**
+     * remove the dropdown from the footer
+      * @param event
+     * @param dropdown
+     * @private
+     */
+    private closeDropdown(event: MouseEvent, dropdown: HTMLElement) {
+        if (this.footer.footercontainer.element.nativeElement.contains(dropdown)) {
+            this.renderer.removeChild(this.footer.footercontainer.element.nativeElement, dropdown);
+        }
+    }
+
+    /**
+     * create dropdown
+     * @param event
+     * @private
+     */
+    private createDropdown(event: MouseEvent): HTMLElement {
+        const dropdown = this.renderer.createElement('div');
+        const addClasses = (item, classes) => classes.forEach(itemClass => this.renderer.addClass(item, itemClass));
+        this.renderer.setStyle(dropdown, 'top', event.pageY + 'px');
+        this.renderer.setStyle(dropdown, 'left', event.pageX + 'px');
+        addClasses(dropdown, ['slds-dropdown--inverse', 'slds-dropdown', 'slds-theme--inverse']);
+        const ul = this.renderer.createElement('ul');
+        this.renderer.addClass(ul,'slds-dropdown__list');
+        const li = this.renderer.createElement('li');
+        addClasses(li, ['slds-slds-dropdown__item', 'slds-p-around--xx-small']);
+
+        this.renderer.setProperty(li, 'onclick', () => this.openModal());
+        this.renderer.setProperty(li, 'innerHTML', `<a>${this.language.getLabel('LBL_EDIT_LABEL')}</a>`);
+
+        this.renderer.appendChild(ul, li);
+        this.renderer.appendChild(dropdown, ul);
+        return dropdown;
     }
 }
