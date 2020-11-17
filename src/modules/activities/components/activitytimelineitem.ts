277cec7f5dbe1fab3bc75353ac42a9f8c0a9fb36
Christian Knoll <christian.knoll@twentyreasons.com>
2020-11-17 08:53:49 +0100

    documentation

--- src/modules/activities/components/activitytimelineitem.ts
+++ src/modules/activities/components/activitytimelineitem.ts
@@ -23,6 +23,9 @@ import {Subscription} from "rxjs";
 declare var moment: any;
 declare var _: any;
 
+/**
+ * a generic component to render an item in teh activity timeline
+ */
 @Component({
     selector: 'activitytimeline-item',
     templateUrl: './src/modules/activities/templates/activitytimelineitem.html',
