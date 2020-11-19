58a328658209e88604915d5bf2bdc936b450a2d3
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2020-11-19 14:21:17 +0100

    repair language reloads language from frontend as well

--- src/admincomponents/components/administrationdictrepairitem.ts
+++ src/admincomponents/components/administrationdictrepairitem.ts
@@ -5,7 +5,9 @@ import {Component, Injector, OnInit} from '@angular/core';
 import {backend} from '../../services/backend.service';
 import {toast} from "../../services/toast.service";
 import {language} from "../../services/language.service";
+import {loader} from "../../services/loader.service";
 import {modal} from "../../services/modal.service";
+import {Subject} from "rxjs";
 
 
 @Component({
@@ -15,8 +17,8 @@ import {modal} from "../../services/modal.service";
 export class AdministrationDictRepairItem {
 
     private sql: string = '';
-
-    constructor(private backend: backend, private toast: toast, private language: language, private modal: modal, private injector: Injector) {
+    private loaderHandler: Subject<string> = new Subject<string>();
+    constructor(private backend: backend, private toast: toast, private language: language, private modal: modal, private injector: Injector, private loader: loader) {
     }
 
     public executeDB() {
@@ -35,6 +37,9 @@ export class AdministrationDictRepairItem {
     public executeLG() {
         let await = this.modal.await(this.language.getLabel('LBL_LOADING'));
         this.backend.getRequest('/repair/language').subscribe(result => {
+            if(result.response) {
+                this.language.getLanguage(this.loaderHandler);
+            }
             await.emit(true);
             this.toast.sendToast('LBL_LANGUAGES_REPAIRED', 'success');
         });
