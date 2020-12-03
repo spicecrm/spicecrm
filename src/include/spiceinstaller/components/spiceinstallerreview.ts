8b6f0391fb036282cc9fef6e3dc33976ea274ae5
Dawid Bestrzynski <dawid.bestrzynski@twentyreasons.com>
2020-12-03 16:03:14 +0100

    Singleton installer update

--- src/include/spiceinstaller/components/spiceinstallerreview.ts
+++ src/include/spiceinstaller/components/spiceinstallerreview.ts
@@ -38,7 +38,7 @@ export class SpiceInstallerReview implements AfterViewInit {
      */
     private install() {
         this.loading = true;
-        this.http.post(`${this.spiceinstaller.configObject.backendconfig.backendUrl}/KREST/spiceinstaller/install`, this.spiceinstaller.configObject).subscribe(
+        this.http.post(`${this.spiceinstaller.configObject.backendconfig.backendUrl}/spiceinstaller/install`, this.spiceinstaller.configObject).subscribe(
             (response: any) => {
                 let res = response;
                 this.loading = false;
