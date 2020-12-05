98f4bb615714404a5ae91506d7b8f9dac16c88b0
Christian Knoll <christian.knoll@twentyreasons.com>
2020-12-05 16:46:51 +0100

    CR1000494 ne field fieldModelAttachment has been added replacing the fieldFile that will be dpürecated in one of the next releases.

--- src/systemcomponents/components/systemimagepreviewmodal.ts
+++ src/systemcomponents/components/systemimagepreviewmodal.ts
@@ -9,18 +9,54 @@ import {language} from '../../services/language.service';
 })
 export class SystemImagePreviewModal {
 
+    /**
+     * reference to the modal itself
+     * @private
+     */
     private self: any = {};
+
+    /**
+     * the soruce of the image
+     * @private
+     */
     @Input() private imgsrc: string = '';
+
+    /**
+     * type of the image
+     * @private
+     */
     @Input() private imgtype: string = '';
+
+    /**
+     * the name of the image
+     * @private
+     */
     @Input() private imgname: string = '';
+
+    /**
+     * can be set to true to display a page not available error
+     * 
+     * @private
+     */
+    @Input() private loadingerror: boolean = false;
 
     constructor(private language: language) {
     }
 
+    /**
+     * cloes the modal
+     *
+     * @private
+     */
     private closeModal() {
         this.self.destroy();
     }
 
+    /**
+     * triggered form the download button
+     *
+     * @private
+     */
     private download() {
         let blob = this.b64toBlob(this.imgsrc.replace('data:' + this.imgtype + ';base64,', ''), this.imgtype);
         let blobUrl = URL.createObjectURL(blob);
@@ -32,6 +68,14 @@ export class SystemImagePreviewModal {
         a.remove();
     }
 
+    /**
+     * converts the base 64 stroing to a blbo and adds it as url so the file can be displayed
+     *
+     * @param b64Data
+     * @param contentType
+     * @param sliceSize
+     * @private
+     */
     private b64toBlob(b64Data, contentType = '', sliceSize = 512) {
 
         let byteCharacters = atob(b64Data);
