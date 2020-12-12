703a66b8dbc6763e9be780b2d705e7f97aae5602
Christian Knoll <christian.knoll@twentyreasons.com>
2020-12-12 14:07:30 +0100

    Cleanup VersionManager Service

--- src/include/spicetimeline/spicetimeline.ts
+++ src/include/spicetimeline/spicetimeline.ts
@@ -5,7 +5,6 @@ import {CommonModule} from "@angular/common";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 
-import {VersionManagerService} from "../../services/versionmanager.service";
 import {DirectivesModule} from "../../directives/directives";
 
 import {ObjectFields} from "../../objectfields/objectfields";
@@ -37,13 +36,4 @@ import /*embed*/ {SpiceTimelineEvent} from './components/spicetimelineevent';
         SpiceTimeline,
     ]
 })
-export class ModuleSpiceTimeline {
-    public readonly version = "1.0";
-    public readonly build_date = "/*build_date*/";
-
-    constructor(
-        private vms: VersionManagerService,
-    ) {
-        this.vms.registerModule(this);
-    }
-}
+export class ModuleSpiceTimeline {}
