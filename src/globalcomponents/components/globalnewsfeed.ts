081549829b1e0fb2675d34730d63035bb97cc903
Christian Knoll <christian.knoll@twentyreasons.com>
2020-12-20 08:23:56 +0100

    cleanup global newsfeed

--- src/globalcomponents/components/globalnewsfeed.ts
+++ src/globalcomponents/components/globalnewsfeed.ts
@@ -4,16 +4,39 @@
 import {Component} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpResponse} from "@angular/common/http";
 
+/**
+ * renders a bar on the login screen with a newsfeed from a wordpress site
+ */
 @Component({
     selector: 'global-newsfeed',
     templateUrl: './src/globalcomponents/templates/globalnewsfeed.html',
 })
 export class GlobalNewsFeed {
 
+    /**
+     * the news items to be displayed
+     * @private
+     */
     private news: Array<any> = [];
+
+    /**
+     * an indicator while the news feed is being retrieved
+     * @private
+     */
     private isLoading: boolean = true;
 
     constructor(private http: HttpClient) {
+
+    }
+
+    /**
+     * loads the newsfeedb
+     *
+     * ToDo: add capability to configure newsfeed url
+     *
+     * @private
+     */
+    private loadNewsFeed(){
         let posturl = btoa('https://www.spicecrm.io/wp-json/wp/v2/posts');
         this.http.get('proxy?useurl=' + posturl)
             .subscribe((res: any) => {
@@ -22,11 +45,4 @@ export class GlobalNewsFeed {
             });
     }
 
-    private getContainerClass() {
-        if (this.isLoading) {
-            return 'slds-align--absolute-center';
-        } else {
-            return '';
-        }
-    }
 }
