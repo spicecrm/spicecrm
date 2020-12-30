6b34ec6afe77a6775d93aeadd6facadf6524022c
Christian Knoll <christian.knoll@twentyreasons.com>
2020-12-30 12:27:43 +0100

    domain manager changes

--- src/workbench/services/domainmanager.service.ts
+++ src/workbench/services/domainmanager.service.ts
@@ -98,8 +98,8 @@ export class domainmanager {
     public getValdiationValuesdById(validationid) {
         let validationValues = this.domainfieldvalidationvalues.filter(v => v.sysdomainfieldvalidation_id == validationid && v.scope == 'c');
         let globalValidationValues = this.domainfieldvalidationvalues.filter(v => v.sysdomainfieldvalidation_id == validationid && v.scope != 'c');
-        for(let globalValidationValue of globalValidationValues){
-            if(validationValues.findIndex(v => v.minvalue == globalValidationValue.minvalue) == -1){
+        for (let globalValidationValue of globalValidationValues) {
+            if (validationValues.findIndex(v => v.enumvalue == globalValidationValue.enumvalue) == -1) {
                 validationValues.push(globalValidationValue);
             }
         }
@@ -226,7 +226,7 @@ export class domainmanager {
 
                             if (option && !this.language.languagedata.applang[('VAL_' + table[field].options + '_' + option).toUpperCase()]) {
                                 let labelid = this.modelutilities.generateGuid();
-                                if(scope == 'g') {
+                                if (scope == 'g') {
                                     this.languagelabels.push({
                                         id: labelid,
                                         name: ('VAL_' + table[field].options + '_' + option).toUpperCase()
