d64afa3fd27e49dc5042a2356dfc7a7ade8e24e2
Christian Knoll <christian.knoll@twentyreasons.com>
2021-01-09 16:17:12 +0100

    changes in user add modal to allow parent and copyrules

--- src/modules/users/components/useraddbutton.ts
+++ src/modules/users/components/useraddbutton.ts
@@ -1,7 +1,7 @@
 /**
  * @module ModuleUsers
  */
-import {Component} from "@angular/core";
+import {Component, Injector} from "@angular/core";
 import {modal} from "../../../services/modal.service";
 import {session} from "../../../services/session.service";
 import {model} from "../../../services/model.service";
@@ -9,15 +9,22 @@ import {language} from "../../../services/language.service";
 import {metadata} from "../../../services/metadata.service";
 import {configurationService} from "../../../services/configuration.service";
 
+/**
+ * a special button to enable adding a user
+ */
 @Component({
+    selector: 'user-add-button',
     templateUrl: "./src/modules/users/templates/useraddbutton.html"
 })
 
 export class UserAddButton {
 
+    /**
+     * set the button to disabled
+     */
     public disabled: boolean = true;
 
-    constructor(private modal: modal, private language: language, private model: model, private session: session, private metadata: metadata, private configurationService: configurationService) {
+    constructor(private modal: modal, private language: language, private model: model, private session: session, private metadata: metadata, private injector: Injector, private configurationService: configurationService) {
         // CR1000463: use spiceacl to enable listing and access foreign user records
         // keep BWC for old modules/ACL/ACLController.php
         let _aclcontroller = this.configurationService.getSystemParamater('aclcontroller');
@@ -30,7 +37,12 @@ export class UserAddButton {
         }
     }
 
+    /**
+     * execute the click on the button
+     *
+     * @private
+     */
     private execute() {
-        this.modal.openModal("UserAddModal");
+        this.modal.openModal("UserAddModal", true, this.injector);
     }
 }
