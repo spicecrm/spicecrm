6d6e457cddafd297cd75ff5875f0620788268ff7
Dawid Bestrzynski <dawid.bestrzynski@twentyreasons.com>
2021-01-13 13:59:42 +0100

    Added parent details field

---
+++ src/objectfields/components/fieldparentdetails.ts
@@ -0,0 +1,63 @@
+/**
+ * @module ObjectFields
+ */
+import {Component, OnInit} from '@angular/core';
+import {model} from '../../services/model.service';
+import {view} from '../../services/view.service';
+import {language} from '../../services/language.service';
+import {metadata} from '../../services/metadata.service';
+import {fieldGeneric} from './fieldgeneric';
+import {Router} from '@angular/router';
+
+@Component({
+    templateUrl: './src/objectfields/templates/fieldparentdetails.html'
+})
+export class fieldParentDetails extends fieldGeneric implements OnInit {
+    private parentDefs: any = {};
+
+    constructor(public model: model, public view: view, public language: language, public metadata: metadata, public router: Router) {
+        super(model, view, language, metadata, router);
+    }
+
+    public ngOnInit() {
+        super.ngOnInit();
+        this.getParentDefinition();
+    }
+
+    get module() {
+        return this.fieldconfig.module;
+    }
+
+    get componentset() {
+        return this.fieldconfig.componentset;
+    }
+
+    /**
+     * Display only if model module equals config module.
+     */
+    get display() {
+        return this.parentId && this.module == this.parentModule;
+    }
+
+    /**
+     * Parent module getter.
+     */
+    get parentModule() {
+        return this.parentDefs?.type_name ? this.model.getField(this.parentDefs.type_name) : undefined;
+    }
+
+    /**
+     * Parent ID getter.
+     */
+    get parentId() {
+        return this.parentDefs?.id_name ? this.model.getField(this.parentDefs.id_name) : undefined;
+    }
+
+    /**
+     * Initializes parent object definitions.
+     * @private
+     */
+    private getParentDefinition() {
+        this.parentDefs = this.metadata.getFieldDefs(this.model.module, this.fieldname);
+    }
+}
