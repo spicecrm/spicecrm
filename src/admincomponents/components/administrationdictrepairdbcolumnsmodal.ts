eaf34fee7457019dbfb94ed0fcae2d922e4e4313
Rainer Grohmann <rainer.grohmann@twentyreasons.com>
2021-01-14 12:37:17 +0100

    db-column cleanup for admins in workbench

---
+++ src/admincomponents/components/administrationdictrepairdbcolumnsmodal.ts
@@ -0,0 +1,163 @@
+/**
+ * @module AdminComponentsModule
+ */
+import {Component} from '@angular/core';
+import {Md5} from "ts-md5";
+import {backend} from '../../services/backend.service';
+import {toast} from "../../services/toast.service";
+import {language} from "../../services/language.service";
+import {modal} from "../../services/modal.service";
+import {metadata} from "../../services/metadata.service";
+import {helper} from "../../services/helper.service";
+
+
+@Component({
+    selector: 'administration-dict-repair-db-columns-modal',
+    templateUrl: './src/admincomponents/templates/administrationdictrepairdbcolumnsmodal.html'
+})
+export class AdministrationDictRepairDbColumnsModal {
+
+    private self: any = {};
+
+    private _module: string = '';
+    public modules: string[];
+
+    private allFields: any = [];
+    private selectedFields: any = [];
+
+
+    constructor(
+        private backend: backend,
+        private toast: toast,
+        private language: language,
+        private modal: modal,
+        private metadata: metadata,
+        private helper: helper
+    ) {
+        this.modules = this.metadata.getModules();
+        this.modules.sort();
+    }
+
+    get module() {
+        return this._module;
+    }
+
+    set module(module) {
+        if (module != this._module) {
+            this._module = module;
+            this.mergeColumns();
+        }
+    }
+
+    /**
+     * get all the db columns for one module
+     */
+    private mergeColumns() {
+        this.allFields = [];
+        // get all db columns from the backend
+        this.backend.getRequest('dictionary/browser/' + this.module + '/dbcolumns').subscribe((result: any) => {
+
+            // get all module fields (vardefs)
+            const moduleFields = this.metadata.getModuleFields(this.module);
+
+            // Step 1: Set all the vardef fields into a new array
+            for (let field in moduleFields) {
+                if(moduleFields[field].source != 'non-db') {
+                    this.allFields.unshift({
+                        name: moduleFields[field].name,
+                        vardef_available: true
+                    });
+                }
+            }
+
+            // Step 2: Check if the db field is already in the list
+            // yes -> set db_available = true
+            // not -> add new object with vardef_available = false
+            for (let dbfield in result) {
+                let dbname = result[dbfield].name;
+                let withvardef = false;
+
+                for (let allfield in this.allFields) {
+                    if(this.allFields[allfield].name == dbname) {
+
+                        this.allFields[allfield].db_available = true;
+
+                        // Fields with both should be moved to the end of the list (these are not interesting)
+                        this.allFields.push(this.allFields[allfield]);
+                        this.allFields.splice (allfield, 1);
+
+                        withvardef = true;
+                    }
+                }
+
+                // if there is a db field without vardef
+                if(!withvardef) {
+                    this.allFields.unshift({
+                        name: dbname,
+                        db_available: true
+                    });
+                }
+
+            }
+        });
+    }
+
+    /**
+     * changed selection
+     */
+    private selectColumn(field, keyvalue) {
+        if(field.todelete) {
+            this.selectedFields[keyvalue] = field;
+        } else {
+            this.selectedFields.splice(keyvalue);
+        }
+    }
+
+    /**
+     * close the modal
+     */
+    private close() {
+        this.self.destroy();
+    }
+
+    /**
+     * execute db repair
+     */
+    private doRepair() {
+
+        // build the list of selected columns and set it into the helper message
+        let todeleteString = '';
+        for (let selfield of this.selectedFields) {
+            todeleteString += selfield.name + ', ';
+        }
+        this.helper.confirm(this.language.getLabel('LBL_CLEAN_DB_COLUMNS'), this.language.getLabel('MSG_DELETE_COLUMNS', 'long') + ' ' +  todeleteString)
+            .subscribe(answer => {
+                if (answer) {
+                    this.delete();
+                }
+            });
+    }
+
+    /**
+     * after the answer the delete is executed
+     */
+    private delete() {
+        this.modal.openModal('SystemLoadingModal').subscribe(loadingRef => {
+            let postData = {
+                dbcolumns: this.selectedFields,
+                module: this.module
+            };
+            this.backend.postRequest('repair/dbcolumns', {}, postData).subscribe((result: any) => {
+                if (result) {
+                    this.toast.sendToast(this.language.getLabel('MSG_SUCCESSFULLY_EXECUTED'), 'success');
+                    this.close();
+                } else {
+                    this.toast.sendToast(this.language.getLabel('LBL_ERROR'), 'error');
+                    this.close();
+                }
+                loadingRef.instance.self.destroy();
+            });
+        });
+    }
+
+}
