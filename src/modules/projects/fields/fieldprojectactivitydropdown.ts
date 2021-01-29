c02b7b868cf5a643bbe07d1c88bf4c7a6a11c1c0
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2021-01-29 10:59:32 +0100

    dropdown translate value

--- src/modules/projects/fields/fieldprojectactivitydropdown.ts
+++ src/modules/projects/fields/fieldprojectactivitydropdown.ts
@@ -29,6 +29,15 @@ export class fieldProjectActivityDropdown extends fieldGeneric implements OnInit
         this.getActivityTypes();
     }
 
+    /**
+     *
+     * @param value
+     * @private
+     */
+    private translateValue(value) {
+      return this.options.find(val => val.id == value)?.name;
+    }
+
 
     /**
      * gets the ProjectActivityTypes options for a ProjectPlannedActivity
