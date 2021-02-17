418adb9f419492a9f4b9e5410547b25d404f3e9f
sebastian hautz <76937890+sebastianhautz@users.noreply.github.com>
2021-02-17 17:03:15 +0100

    fixed the routes // bug

--- src/workbench/components/mailboxesmanager.ts
+++ src/workbench/components/mailboxesmanager.ts
@@ -64,7 +64,7 @@ export class MailboxesManager {
         // get the transports
         if (!this.configuration.getData('mailboxtransports')) {
             this.configuration.setData('mailboxtransports', []);
-            this.backend.getRequest('/mailboxes/transports').subscribe(transports => {
+            this.backend.getRequest('mailboxes/transports').subscribe(transports => {
                 this.configuration.setData('mailboxtransports', transports);
             });
         }
