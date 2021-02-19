32485071d531b3d50781bba9e0d969b84485f72d
sebastian hautz <76937890+sebastianhautz@users.noreply.github.com>
2021-02-19 12:01:07 +0100

    fixed {} to [] in isset()

--- src/objectcomponents/components/objectlistheaderactionsexporttargetlistmodal.ts
+++ src/objectcomponents/components/objectlistheaderactionsexporttargetlistmodal.ts
@@ -56,7 +56,7 @@ export class ObjectListHeaderActionsExportTargetlistModal {
                 listid: this.modellist.currentList.id,
                 ids: selectedIds
             };
-            this.backend.postRequest('/modules/ProspectLists/exportFromList', {}, params).subscribe(result => {
+            this.backend.postRequest('modules/ProspectLists/exportFromList', {}, params).subscribe(result => {
                 loadingRef.instance.self.destroy();
                 if (result.status == 'success') {
                     this.router.navigate(['/module/ProspectLists/' + result.id]);
