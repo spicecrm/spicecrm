9f37174d81d9a893ea51e66487cb913e9fd3b475
Christian Knoll <christian.knoll@twentyreasons.com>
2021-02-21 21:49:50 +0100

    proper icon split in buttonicon

--- src/systemcomponents/components/systembuttonicon.ts
+++ src/systemcomponents/components/systembuttonicon.ts
@@ -79,7 +79,7 @@ export class SystemButtonIcon implements OnChanges {
      */
     private getIcon() {
         if (this.icon) {
-            return this.icon;
+            return this.icon.indexOf(":") > 0 ? this.icon.split(":")[1] : this.icon;
         }
 
         if (this.module && this.metadata.getModuleIcon(this.module)) {
@@ -95,7 +95,9 @@ export class SystemButtonIcon implements OnChanges {
      * returns the sprite
      */
     private getSprite() {
-        if (this.module && this.metadata.getModuleIcon(this.module) && this.metadata.getModuleIcon(this.module).indexOf(":") > 0) {
+        if(this.icon && this.icon.indexOf(":") > 0) {
+            return this.icon.split(":")[0];
+        } else if (this.module && this.metadata.getModuleIcon(this.module) && this.metadata.getModuleIcon(this.module).indexOf(":") > 0) {
             return this.metadata.getModuleIcon(this.module).split(":")[0];
         } else if (this.module) {
             return 'standard';
