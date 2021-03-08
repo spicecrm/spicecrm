173ad282a48eb07ff4620b6f88f506ed2cd05b02
Christian Knoll <christian.knoll@twentyreasons.com>
2021-03-08 07:56:26 +0100

    field messages cleanup

--- src/objectfields/components/fieldmessages.ts
+++ src/objectfields/components/fieldmessages.ts
@@ -1,7 +1,7 @@
 /**
  * @module ObjectFields
  */
-import { Component, Input, OnChanges, OnInit, Optional } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, Optional} from '@angular/core';
 import {model} from '../../services/model.service';
 import {view} from '../../services/view.service';
 import {language} from '../../services/language.service';
@@ -11,45 +11,74 @@ import {fielderrorgrouping} from '../../services/fielderrorgrouping.service';
     selector: 'field-messages',
     templateUrl: './src/objectfields/templates/fieldmessages.html'
 })
-export class FieldMessagesComponent implements OnInit, OnChanges
-{
-    @Input() fieldname:string = '';
-    @Input('messages') _messages = [];
-    errors = [];
-    warnings = [];
-    notices = [];
+export class FieldMessagesComponent implements OnInit, OnChanges {
+    /**
+     * the fieldname
+     *
+     * @private
+     */
+    @Input() private fieldname: string = '';
 
-    constructor ( private model: model, private view: view, private language: language, @Optional() private fielderrorgroup: fielderrorgrouping ) { }
+    /**
+     * the messages collected
+     * @private
+     */
+    @Input('messages') private _messages = [];
 
-    ngOnInit() {
-        this.model.messageChange$.subscribe( () => {
+    /**
+     * the errors
+     *
+     * @private
+     */
+    private errors = [];
+
+    /**
+     * the warnings
+     *
+     * @private
+     */
+    private warnings = [];
+
+    /**
+     * notices
+     *
+     * @private
+     */
+    private notices = [];
+
+    constructor(private model: model, private view: view, private language: language, @Optional() private fielderrorgroup: fielderrorgrouping) {
+    }
+
+    public ngOnInit() {
+        this.model.messageChange$.subscribe(() => {
             this.updateMessages();
         });
     }
 
-    ngOnChanges() {
+    public ngOnChanges() {
         this.updateMessages();
     }
 
-    updateMessages() {
-        let messages: Array<any>;
-        if( this._messages.length == 0 && this.fieldname ) {
-            messages = this.model.getFieldMessages( this.fieldname ) || [];
+    public ngOnDestroy() {
+        if (this.fielderrorgroup) this.fielderrorgroup.setError(this.fieldname, false);
+    }
+
+    private updateMessages() {
+        let messages: any[];
+        if (this._messages.length == 0 && this.fieldname) {
+            messages = this.model.getFieldMessages(this.fieldname) || [];
         } else {
             messages = this._messages;
         }
-        this.errors = this.filterMessages( messages, 'error');
-        this.warnings = this.filterMessages( messages, 'warning');
-        this.notices = this.filterMessages( messages, 'notice');
-        if ( this.fielderrorgroup ) this.fielderrorgroup.setError( this.fieldname,this.errors.length !== 0 );
+        this.errors = this.filterMessages(messages, 'error');
+        this.warnings = this.filterMessages(messages, 'warning');
+        this.notices = this.filterMessages(messages, 'notice');
+        if (this.fielderrorgroup) this.fielderrorgroup.setError(this.fieldname, this.errors.length !== 0);
     }
 
-    filterMessages( messages: Array<any>, type?: string) {
-        return messages.filter((e) => {return (!type || e.type == type)});
+    private filterMessages(messages: any[], type?: string) {
+        return messages.filter((e) => {
+            return (!type || e.type == type);
+        });
     }
-
-    ngOnDestroy(){
-        if ( this.fielderrorgroup ) this.fielderrorgroup.setError( this.fieldname, false );
-    }
-
 }
