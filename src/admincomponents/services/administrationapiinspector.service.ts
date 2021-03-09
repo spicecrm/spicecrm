b8e0d842dbbac9c8fa4d4c109b89ccc893b50dbb
Christian Knoll <christian.knoll@twentyreasons.com>
2021-03-09 13:54:03 +0100

    API Manager Cleanup & FE Design

--- src/admincomponents/services/administrationapiinspector.service.ts
+++ src/admincomponents/services/administrationapiinspector.service.ts
@@ -64,6 +64,9 @@ export class administrationapiinspectorService {
      */
     public apiMethods: any[] = [];
 
+    /**
+     * a filter string to search by
+     */
     public _apiFilter: string;
 
     /**
@@ -209,105 +212,6 @@ export class administrationapiinspectorService {
     }
 
     /**
-     * recursively goes through all endpoint entrys and nests them
-     * @param segments
-     * @param startArray
-     * @param routeInfo
-     * @param level
-     * @param parentId
-     */
-    public populateArray(segments, startArray, routeInfo, level, parentId) {
-        segments.forEach(segment => {
-            if (startArray.hasOwnProperty(segment) === false) {
-                startArray[segment] = {
-                    id: this.modelutilities.generateGuid(),
-                    parentId: parentId,
-                    level: level,
-                    methods: [],
-                    subroutes: {},
-                };
-            }
-            if (segments.length === 1) {
-                // routeInfo = this.parseRoute(routeInfo);
-                startArray[segment].methods.push(routeInfo);
-            }
-            segments.shift();
-            this.populateArray(segments, startArray[segment].subroutes, routeInfo, level + 1, startArray[segment].id);
-
-        });
-    }
-
-    /**
-     * routeinfo überschreiben
-     * flattens the methods for the systemtree
-     * aufteilen der subobjects als eigenen eintrag im array
-     */
-
-    public parseRoute(routeInfo) {
-        let subRoutes = [];
-        for (const key in routeInfo) {
-
-            subRoutes.push({
-                class: routeInfo[key].class,
-                description: routeInfo[key].description,
-                extension: routeInfo[key].extension,
-                function: routeInfo[key].function,
-                method: routeInfo[key].method,
-                options: routeInfo[key].options,
-                parameters: routeInfo[key].parameters,
-                requestBody: routeInfo[key].requestBody,
-                responses: routeInfo[key].responses,
-                route: routeInfo[key].route,
-                summary: routeInfo[key].summary
-            });
-
-        }
-        return subRoutes;
-    }
-
-    /**
-     * apiEndrooints is a nested object first is the place in the object then comes the original object
-     * edits the array for the tree view
-     * @public
-     */
-
-    public parse() {
-        this.apiEndpoints.forEach(routeInfo => {
-            const segments = routeInfo.route.split("/");
-            this.populateArray(segments.slice(1), this.finalArray, routeInfo, 0, null);
-        });
-        this.flattendArray = this.flatten(this.finalArray);
-    }
-
-
-    /**
-     * flattens the object and converts it to an array for the sytemtree
-     * @param data
-     */
-
-    public flatten(data, arr = []) {
-        let i = 0;
-        for (const key in data) {
-            arr.push({
-                id: data[key].id,
-                parent_id: data[key].parentId,
-                parent_sequence: i,
-                name: key,
-                clickable: true,
-                methods: data[key].methods,
-                extension: data[key].extension,
-                function: data[key].function
-            });
-
-            if (Object.keys(data[key]).length > 0) {
-                this.flatten(data[key].subroutes, arr);
-            }
-            i++;
-        }
-        return arr;
-    }
-
-    /**
      * select the active API .. fired when the secltion in the tree changes
      *
      * @param selectedId
@@ -318,7 +222,7 @@ export class administrationapiinspectorService {
     }
 
     /**
-     * returns teh methods for a route
+     * returns the methods for a route
      *
      * @param route
      * @param method
