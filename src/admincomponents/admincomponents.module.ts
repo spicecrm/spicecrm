045e28b14147bc141cb9bff769c708ac16bef374
sebastian hautz <76937890+sebastianhautz@users.noreply.github.com>
2021-03-09 16:03:20 +0100

    added response component for the systemtree

--- src/admincomponents/admincomponents.module.ts
+++ src/admincomponents/admincomponents.module.ts
@@ -46,6 +46,8 @@ import /*embed*/ { AdministrationAPIInspector } from './components/administratio
 import /*embed*/ { AdministrationAPIInspectorDetails } from './components/administrationapiinspectordetails';
 import /*embed*/ { AdministrationApiInspectorMethodParameters } from "./components/administrationapiinspectormethodparameters";
 import /*embed*/ { AdministrationApiInspectorMethods } from "./components/administrationapiinspectormethods";
+import /*embed*/ { AdministrationapiinspectorMethodResponses } from "./components/administrationapiinspectormethodresponses";
+import /*embed*/ { AdministrationapiinspectorMethodRequests } from "./components/administrationapiinspectormethodrequests";
 
 
 import /*embed*/ { AdministrationSysTrashcanManager } from './components/administrationsystrashcanmanager';
@@ -87,6 +89,8 @@ import /*embed*/ {AdministrationDefaultPreferences} from './components/administr
         AdministrationApiInspectorMethods,
         AdministrationApiInspectorMethodParameters,
         AdministrationAPIInspectorDetails,
+        AdministrationapiinspectorMethodResponses,
+        AdministrationapiinspectorMethodRequests,
         AdministrationMain,
         AdministrationMenu,
         AdministrationMenuRouteItem,
