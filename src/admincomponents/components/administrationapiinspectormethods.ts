f0b73db40126f8b898c1841f7211411e10e920e3
Christian Knoll <christian.knoll@twentyreasons.com>
2021-03-10 09:20:40 +0100

    API Manager frontend changes

--- src/admincomponents/components/administrationapiinspectormethods.ts
+++ src/admincomponents/components/administrationapiinspectormethods.ts
@@ -16,38 +16,6 @@ export class AdministrationApiInspectorMethods {
     ) {
     }
 
-    /**
-     * sets a fixed with for the method badge and a color for the type of method
-     *
-     * @param method
-     * @private
-     */
-    private getMethodStyle(method) {
-
-        let color = 'inherit';
-
-        switch (method) {
-            case 'get':
-                color = '#05628a';
-                break;
-            case 'post':
-                color = '#f38303';
-                break;
-            case 'put':
-                color = '#41b658';
-                break;
-            case 'delete':
-                color = '#d83a00';
-                break;
-        }
-
-        return {
-            width: '100px',
-            color: '#ffffff',
-            background: color
-        };
-    }
-
     private test(apiMethod: any, e: MouseEvent){
         e.preventDefault();
         e.stopPropagation();
