ed3d4e19e97a6c9f819edd900e95614c8c8ce0f7
maretval <valerie.maret@twentyreasons.com>
2021-04-01 15:36:36 +0200

    spicecrm all-in-one fix php8

--- api/include/SpiceFTSManager/SpiceFTSHandler.php
+++ api/include/SpiceFTSManager/SpiceFTSHandler.php
@@ -1,31 +1,31 @@
 <?php
 /*********************************************************************************
-* This file is part of SpiceCRM. SpiceCRM is an enhancement of SugarCRM Community Edition
-* and is developed by aac services k.s.. All rights are (c) 2016 by aac services k.s.
-* You can contact us at info@spicecrm.io
-* 
-* SpiceCRM is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version
-* 
-* The interactive user interfaces in modified source and object code versions
-* of this program must display Appropriate Legal Notices, as required under
-* Section 5 of the GNU Affero General Public License version 3.
-* 
-* In accordance with Section 7(b) of the GNU Affero General Public License version 3,
-* these Appropriate Legal Notices must retain the display of the "Powered by
-* SugarCRM" logo. If the display of the logo is not reasonably feasible for
-* technical reasons, the Appropriate Legal Notices must display the words
-* "Powered by SugarCRM".
-* 
-* SpiceCRM is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-********************************************************************************/
+ * This file is part of SpiceCRM. SpiceCRM is an enhancement of SugarCRM Community Edition
+ * and is developed by aac services k.s.. All rights are (c) 2016 by aac services k.s.
+ * You can contact us at info@spicecrm.io
+ *
+ * SpiceCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo. If the display of the logo is not reasonably feasible for
+ * technical reasons, the Appropriate Legal Notices must display the words
+ * "Powered by SugarCRM".
+ *
+ * SpiceCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ********************************************************************************/
 namespace SpiceCRM\includes\SpiceFTSManager;
 
 use SpiceCRM\data\BeanFactory;
@@ -336,7 +336,7 @@ class SpiceFTSHandler
     function getGlobalSearchModules()
     {
         global $current_language;
-$db = DBManagerFactory::getInstance();
+        $db = DBManagerFactory::getInstance();
 
         // so we have the variable -> will then be filled once the metadata is included
         $listViewDefs = [];
@@ -987,10 +987,10 @@ $db = DBManagerFactory::getInstance();
                     $queryParam['query']['bool']['filter']['bool']['should'] = $aclFilters['should'];
                     $queryParam['query']['bool']['filter']['bool']['minimum_should_match'] = 1;
                 }
-                if (isset($aclFilters['should']) && count($aclFilters['must_not']) > 1) {
+                if (isset($aclFilters['must_not']) && count($aclFilters['must_not']) > 1) {
                     $queryParam['query']['bool']['filter']['bool']['must_not'] = $aclFilters['must_not'];
                 }
-                if (isset($aclFilters['should']) && count($aclFilters['must']) > 1) {
+                if (isset($aclFilters['must']) && count($aclFilters['must']) > 1) {
                     $queryParam['query']['bool']['filter']['bool']['must'] = $aclFilters['must'];
                 }
             }
@@ -1559,7 +1559,7 @@ $db = DBManagerFactory::getInstance();
     function bulkIndexBeans($packagesize, $module = null, $toConsole = false)
     {
         global $timedate;
-$db = DBManagerFactory::getInstance();
+        $db = DBManagerFactory::getInstance();
 
         $beanCounter = 0;
         // BEGIN CR1000257
