754636c013c31f7149b12786b136e2b5b353be5a
Christian Knoll <christian.knoll@twentyreasons.com>
2021-04-03 12:46:33 +0200

    emit default error on failed login

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -148,6 +148,7 @@ export class loginService {
                             break;
                         default:
                             this.toast.sendToast('Application Error', 'error', 'Error Authenticating');
+                            loginSuccess.error(err.statusText);
                             break;
                     }
                     loginSuccess.complete();
