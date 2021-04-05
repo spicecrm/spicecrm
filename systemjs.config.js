40f9f6ff7f9aa07d8f952bc11014a7e14daefb7e
maretval <valerie.maret@twentyreasons.com>
2021-04-05 20:19:44 +0200

    spicecrm all-in-one release 2021.01.001

--- systemjs.config.js
+++ systemjs.config.js
@@ -35,7 +35,7 @@
         packages: {
             'app': {
                 main: './spiceui',
-                defaultExtension: 'js?v=2021.01.001.1617646303785'
+                defaultExtension: 'js?v=2021.01.001.1617646648210'
             },
             rxjs: {
                 main: "index",
