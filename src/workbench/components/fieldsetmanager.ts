aefec7a59c174a5eba9fae554351f122807abc6b
maretval <valerie.maret@twentyreasons.com>
2021-04-06 12:15:00 +0200

    refactored routes systemdeploymentcrs/*

--- src/workbench/components/fieldsetmanager.ts
+++ src/workbench/components/fieldsetmanager.ts
@@ -121,7 +121,7 @@ export class FieldsetManager {
         }
 
         if (this.change_request_required) {
-            this.backend.getRequest('systemdeploymentcrs/active').subscribe(crresponse => {
+            this.backend.getRequest('module/SystemDeploymentCRs/active').subscribe(crresponse => {
                 if (crresponse.id == "") {
                     this.setNoneMode();
                     this.crNoneActive = true;
