f6719a001f7afaad507e29375c9ac2560ae84577
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2021-04-12 11:47:26 +0200

    CRNR: 1000553: Subscriptions: created service and button.

--- src/objectcomponents/objectcomponents.ts
+++ src/objectcomponents/objectcomponents.ts
@@ -63,6 +63,7 @@ import /*embed*/ {ObjectActionSaveRelatedButton} from './components/objectaction
 import /*embed*/ {ObjectActionNewrelatedButton} from './components/objectactionnewrelatedbutton';
 import /*embed*/ {ObjectActionNewCopyRuleBeanButton, ObjectActionNewCopyRuleBeanButtonModelHelper} from './components/objectactionnewcopyrulebeanbutton';
 import /*embed*/ {ObjectReminderButton} from './components/objectreminderbutton';
+import /*embed*/ {ObjectSubscriptionButton} from './components/objectsubscriptionbutton';
 import /*embed*/ {ObjectActionSelectButton} from './components/objectactionselectbutton';
 import /*embed*/ {ObjectActionBeanToMailButton} from './components/objectactionbeantomailbutton';
 import /*embed*/ {ObjectActionMailModal} from './components/objectactionmailmodal';
@@ -312,6 +313,7 @@ import /*embed*/ {ObjectActionDeactivateBeansModal} from "./components/objectact
         ObjectModalModuleLookupAggregates,
         ObjectSelectButton,
         ObjectReminderButton,
+        ObjectSubscriptionButton,
         ObjectActionBeanToMailButton,
         ObjectActionMailModal,
         ObjectMergeButton,
