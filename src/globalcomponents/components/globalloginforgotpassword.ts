7f2ad7cf52fa7bf859ed2c740e22bbe16037bd1d
Markus Übeleis <office@cope-it.at>
2021-04-14 16:37:45 +0200

    refactored route /passwordtoken/token/{token}

--- src/globalcomponents/components/globalloginforgotpassword.ts
+++ src/globalcomponents/components/globalloginforgotpassword.ts
@@ -187,7 +187,7 @@ export class GlobalLoginForgotPassword {
             // submit the request
             this.sending = true;
             this.http.post(
-                this.configuration.getBackendUrl() + `/passwordtoken/token/${this.token}`,
+                this.configuration.getBackendUrl() + `/authentication/passwordtoken/token/${this.token}`,
                 {
                     newPassword: this.password
                 }
