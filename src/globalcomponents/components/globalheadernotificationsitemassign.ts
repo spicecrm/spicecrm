71d4ebd010ba6b756af5643e79ffd609ba1b5ac9
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2021-04-20 11:24:45 +0200

    Notifications: display new notifications temporary in a box

--- src/globalcomponents/components/globalheadernotificationsitemassign.ts
+++ src/globalcomponents/components/globalheadernotificationsitemassign.ts
@@ -3,6 +3,7 @@
  */
 import {Component, Input} from '@angular/core';
 import {NotificationI} from "../interfaces/globalcomponents.interfaces";
+import {NotificationService} from "../../services/notification.service";
 
 /**
  * display a notification item for assignment type
@@ -16,4 +17,11 @@ export class GlobalHeaderNotificationsItemAssign {
      * holds the notification data
      */
     @Input() public notification: NotificationI;
+    /**
+     * if true render the box template for new pushed notifications
+     */
+    @Input() public asBox: boolean = false;
+
+    constructor(public notificationService: NotificationService) {
+    }
 }
