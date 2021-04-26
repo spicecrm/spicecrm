ecca25bb09b729f69aa5f378a3a270538787e4f8
Markus Übeleis <office@cope-it.at>
2021-04-26 12:23:30 +0200

    bugfix authentication login uris

--- src/include/spiceinstaller/components/spiceinstallerreview.ts
+++ src/include/spiceinstaller/components/spiceinstallerreview.ts
@@ -52,7 +52,7 @@ export class SpiceInstallerReview implements AfterViewInit {
                             let response = res;
                             if (response.success == true) {
                                 this.configurationService.setSiteData(response.site);
-                                this.router.navigate(['/authentication/login']);
+                                this.router.navigate(['/login']);
                             }
                         },
                         (err: any) => {
