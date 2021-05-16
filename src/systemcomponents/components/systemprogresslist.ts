7bf771e7c3c4dc6e2b0b4b66e177d23677ffc34f
Christian Knoll <christian.knoll@twentyreasons.com>
2021-05-16 08:47:33 +0200

    System Progress list component

---
+++ src/systemcomponents/components/systemprogresslist.ts
@@ -0,0 +1,88 @@
+/**
+ * @module SystemComponents
+ */
+import {Component, Input} from "@angular/core";
+
+/**
+ * Displays a progress list indicator
+ */
+@Component({
+    selector: "system-progress-list",
+    templateUrl: "./src/systemcomponents/templates/systemprogresslist.html"
+})
+export class SystemProgressList {
+
+    /**
+     * The progress of completion.
+     */
+    @Input() private step: number = 0;
+
+    /**
+     * the steps to be displayed as labels
+     *
+     * @private
+     */
+    @Input() private steps: string[] = [];
+
+    /**
+     * internal var to keep the value if se shoudl display shaded
+     * @private
+     */
+    private _shade: boolean = false;
+
+    /**
+     * inpout to set the shade param with a setter so we can simply checkl the existence of the attribute
+     *
+     * @param val
+     */
+    @Input('system-progress-list-shade') set shade(val: boolean) {
+        if (val === false) {
+            this._shade = false;
+        } else {
+            this._shade = true;
+        }
+    }
+
+    constructor() {
+    }
+
+    /**
+     * returns the class for the step indicator
+     *
+     * @param step
+     * @private
+     */
+    private getStepClass(step: string) {
+        let thisIndex = this.steps.indexOf(step);
+        if (thisIndex == this.step) {
+            return 'slds-is-active';
+        }
+        if (thisIndex < this.step) {
+            return 'slds-is-completed';
+        }
+    }
+
+
+    /**
+     * return if the current step is complete
+     * @param step
+     * @private
+     */
+    private getStepComplete(step: string) {
+        let thisIndex = this.steps.indexOf(step);
+        if (thisIndex < this.step) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * the width of the progressbar
+     */
+    get progressBarWidth() {
+        return {
+            width: (this.step / (this.steps.length - 1) * 100) + '%'
+        };
+    }
+
+}
