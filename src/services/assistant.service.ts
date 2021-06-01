8c3bbe803391089cdfcad360ea56985a98e8d6a7
Christian Knoll <christian.knoll@twentyreasons.com>
2021-06-01 09:33:00 +0200

    reset intitialized status on assistant service after logout

--- src/services/assistant.service.ts
+++ src/services/assistant.service.ts
@@ -73,6 +73,9 @@ export class assistant {
                 // clear the items
                 this.assitantItems = [];
 
+                // reset the iniailtized status so we will load again on the next login
+                this.initialized = false;
+
                 // cancel the reminder interval if we have any
                 if (this.reminder) {
                     clearInterval(this.reminder);
