af697b2aea6c278007a06ddf4cc17031c6d0df81
Christian Knoll <christian.knoll@twentyreasons.com>
2021-06-10 09:59:13 +0200

    prevent error when link is not defined for subpanel but endpoint is

--- src/objectcomponents/components/objectrelatedlisttable.ts
+++ src/objectcomponents/components/objectrelatedlisttable.ts
@@ -77,7 +77,7 @@ export class ObjectRelatedlistTable implements OnInit {
             return;
         }
 
-        if (!this.metadata.fieldDefs[this.model.module][this.relatedmodels._linkName]) {
+        if (!this.metadata.fieldDefs[this.model.module][this.relatedmodels._linkName] && ! this.relatedmodels.linkEndPoint) {
             this.logger.error('Missing link or wrong link name ("' + this.relatedmodels._linkName + '")!');
         } else {
             if (!this.sequencefield && this.metadata.fieldDefs[this.model.module][this.relatedmodels._linkName].sequence_field) {
