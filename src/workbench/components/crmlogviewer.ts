61a9c4ec75d1765502734afdf66de261926ff0b1
strombo <andreas@gloeckl.name>
2021-06-14 16:14:39 +0200

    crm log viewer changes (4)

--- src/workbench/components/crmlogviewer.ts
+++ src/workbench/components/crmlogviewer.ts
@@ -1,7 +1,7 @@
 /**
  * @module WorkbenchModule
  */
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, ViewChild } from '@angular/core';
 import { language } from '../../services/language.service';
 import { backend } from '../../services/backend.service';
 import { modal } from '../../services/modal.service';
@@ -19,23 +19,52 @@ declare var moment: any;
 })
 export class CRMLogViewer {
 
-    // Configuration:
-    private log_levels = [ 'debug', 'info', 'warn', 'deprecated', 'login', 'error', 'fatal', 'security' ];
+    /**
+     * Reference to the log level checkbox group.
+     * @private
+     */
+    @ViewChild('loglevelsCheckboxes') private loglevelsCheckboxes;
 
-    // Various:
-    private filter = { log_level: '', pid: '', user_id: '', text: '', transaction_id: '', end: undefined };
+    /**
+     * the possible log levels
+     * @private
+     */
+    private loglevels = [ 'debug', 'info', 'warn', 'deprecated', 'login', 'error', 'fatal', 'security' ];
+
+    /**
+     * filter
+     * @private
+     */
+    private filter = { loglevels: [], pid: '', user_id: '', text: '', transaction_id: '', end: undefined };
     private filterUserName: string;
 
+    /**
+     * Limit: maximal entries from the backend.
+     * @private
+     */
     private limit = '250';
 
     private load$ = new EventEmitter();
 
+    /**
+     * The number of entries got from the backend.
+     * @private
+     */
     private countEntries: number;
 
+    /**
+     * Set the filter field "end" to now.
+     * @private
+     */
     private setNow() {
         this.filter.end = new moment();
     }
 
+    /**
+     * Set the filter field "user".
+     * @param idAndName
+     * @private
+     */
     private set filterUser( idAndName: string ) {
         if (!idAndName) {
             this.filter.user_id = '';
@@ -47,6 +76,10 @@ export class CRMLogViewer {
         this.filterUserName = valueArray[1];
     }
 
+    /**
+     * Get the value from filter field "user".
+     * @private
+     */
     private get filterUser(): string {
         if ( !this.filter.user_id ) return undefined;
         return this.filter.user_id+'::'+this.filterUserName;
@@ -54,18 +87,28 @@ export class CRMLogViewer {
 
     constructor( private lang: language, private backend: backend, private modal: modal, private toast: toast ) { }
 
-    // Are all the inputs correct and ready for the backend request?
+    /**
+     * Are all the inputs correct and ready for the backend request?
+     * @private
+     */
     private canLoad() {
         if ( this.filter.pid && !this.filter.pid.match(/\d$/) ) return false;
         return true;
     }
 
-    // Load button was pressed.
+    /**
+     * Load button was pressed.
+     */
     private buttonLoad() {
         this.load$.emit();
     }
 
-    // The values in the list can be clicked to be transfered to the corresponding filter input field.
+    /**
+     * The values in the list can be clicked to be transfered to the corresponding filter input field.
+     * @param type Kind of value.
+     * @param value The value.
+     * @private
+     */
     private valueClicked( type: string, value: any ) {
         switch ( type ) {
             case 'date_entered':
@@ -76,7 +119,11 @@ export class CRMLogViewer {
                 this.filterUserName = value.user_name;
                 break;
             }
-            case 'log_level': this.filter.log_level = value; break;
+            case 'log_level':
+                let i = this.filter.loglevels.findIndex( element => element === value);
+                if ( i === -1 ) this.filter.loglevels.push( value );
+                this.loglevelsCheckboxes.writeValue( this.filter.loglevels );
+                break;
             case 'pid': this.filter.pid = value.toString(); break;
         }
     }
@@ -89,15 +136,12 @@ export class CRMLogViewer {
         this.modal.prompt('confirm', 'Truncate the API log and delete all entries?', 'Empty the API Log?').subscribe(
             res => {
                 if (res) {
-                    // this.isLoading = true;
                     this.backend.deleteRequest('admin/crmlog').subscribe(
                         () => {
-                            // this.isLoading = false;
                             this.load$.emit();
                         },
                         () => {
                             this.toast.sendToast('Error truncating log', 'error');
-                            // this.isLoading = false;
                         }
                     );
                 }
