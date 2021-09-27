0549b689a0091c845d9708bf47c868d118d9d88e
strombo <andreas@gloeckl.name>
2021-09-27 15:02:09 +0200

    Login Management (backend)

--- api/include/ErrorHandlers/UnauthorizedException.php
+++ api/include/ErrorHandlers/UnauthorizedException.php
@@ -5,6 +5,7 @@ class UnauthorizedException extends Exception {
 
     protected $isFatal = false;
     protected $httpCode = 401;
+    protected $loginBlocked = false;
 
     /**
      * UnauthorizedException constructor.
@@ -22,10 +23,20 @@ class UnauthorizedException extends Exception {
      * 8=user in ad found, no local user found
      * 9=required ldap group membership is missing
      * 10=invalid username/password combination on ldap bind
+     * 11=client ip address not allowed (black listed)
      */
     function __construct( $message = null, $errorCode = null ) {
         if ( !isset( $message )) $this->lbl = 'ERR_HTTP_NO_AUTHORIZATION';
         parent::__construct( isset( $message ) ? $message : 'No Authorization', $errorCode );
+    }
+
+    public function setLoginBlocked( $bool ) {
+        $this->loginBlocked = $bool;
+        return $this;
+    }
+
+    public function isLoginBlocked() {
+        return $this->loginBlocked;
     }
 
 }
