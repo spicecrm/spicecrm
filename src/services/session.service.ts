e919cc7b170fed30cca9e5ad23c98389b109cd7d
strombo <andreas@gloeckl.name>
2021-09-27 13:42:56 +0200

    password renewal after login

--- src/services/session.service.ts
+++ src/services/session.service.ts
@@ -32,6 +32,7 @@ interface authDataIf {
     obtainGDPRconsent: boolean;
     canchangepassword: boolean;
     address_country?: string;
+    passwordExpiresInDays: boolean | number;
 }
 
 /**
@@ -59,7 +60,8 @@ export class session {
         tenant_id: '',
         tenant_name: '',
         obtainGDPRconsent: false,
-        canchangepassword: false
+        canchangepassword: false,
+        passwordExpiresInDays: false
     };
 
     /**
