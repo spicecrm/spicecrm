b3f4fa9b9b32224df7ca6c666ddf58c490d863c3
strombo <andreas@gloeckl.name>
2021-09-28 12:21:55 +0200

    invented error code 12 for exception, better handling of validity

--- api/include/ErrorHandlers/UnauthorizedException.php
+++ api/include/ErrorHandlers/UnauthorizedException.php
@@ -24,6 +24,7 @@ class UnauthorizedException extends Exception {
      * 9=required ldap group membership is missing
      * 10=invalid username/password combination on ldap bind
      * 11=client ip address not allowed (black listed)
+     * 12= password is system generated, has to get renewed
      */
     function __construct( $message = null, $errorCode = null ) {
         if ( !isset( $message )) $this->lbl = 'ERR_HTTP_NO_AUTHORIZATION';
