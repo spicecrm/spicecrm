64951020a43ff001c37eeba825d2c0c6d569b6eb
Christian Knoll <christian.knoll@twentyreasons.com>
2021-10-03 10:17:23 +0200

    CORE: fix parameters in list types post

--- api/include/SpiceUI/api/extensions/systemui.php
+++ api/include/SpiceUI/api/extensions/systemui.php
@@ -135,10 +135,15 @@ $routes = [
                 'type' => ValidationMiddleware::TYPE_BASE64
             ],
             'listcomponent' => [
-                'in' => 'path',
+                'in' => 'body',
                 'description' => 'the used component',
                 'type' => ValidationMiddleware::TYPE_STRING,
                 'example' => 'ObjectList',
+            ],
+            'name' => [
+                'in' => 'body',
+                'description' => 'the name of the list',
+                'type' => ValidationMiddleware::TYPE_STRING
             ]
         ]
     ],
