a5794ac6ea355f2475cb950d1cf9262865074730
Valentina Panetta <32577345+ValentinaPanetta@users.noreply.github.com>
2021-10-05 09:42:36 +0200

    sets change detection strategy on push to prevent infinite loop in children

--- src/systemcomponents/components/systeminputdatepicker.ts
+++ src/systemcomponents/components/systeminputdatepicker.ts
@@ -1,7 +1,7 @@
 /**
  * @module SystemComponents
  */
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 import {language} from '../../services/language.service';
 import {userpreferences} from "../../services/userpreferences.service";
 
@@ -11,6 +11,7 @@ declare var moment: any;
 @Component({
     selector: 'system-input-date-picker',
     templateUrl: './src/systemcomponents/templates/systeminputdatepicker.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
     host: {
         class: 'slds-datepicker'
     }
