eabe92f71d9f5a3eff8ecbfd56bae880116664b5
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2021-10-08 11:09:25 +0200

    CR1000660: SystemPrompt: Added Input Date.

--- src/systemcomponents/components/systemprompt.ts
+++ src/systemcomponents/components/systemprompt.ts
@@ -20,40 +20,40 @@ export class SystemPrompt implements OnInit, AfterViewInit {
     /**
      * the type of prompt
      */
-    @Input() private type: 'info'|'input'|'confirm';
+    @Input() public type: 'info'|'input'|'input_date'|'confirm';
 
     /**
      * the text that is rendered in the popup
      */
-    @Input() private text: string;
+    @Input() public text: string;
 
     /**
      * the header for the popup in the modal
      */
-    @Input() private headertext: string;
+    @Input() public headertext: string;
 
     /**
      * theme according to lightning design -> https://www.lightningdesignsystem.com/utilities/themes/
      */
-    @Input() private theme: string;
+    @Input() public theme: string;
     /**
      * the value to be set
      */
-    protected radioGroupName: string;
+    public radioGroupName: string;
     /**
      * ???
      */
-    @Input() private value: string|number = null;
+    @Input() public value: string|number = null;
 
     /**
      * an array of options .. if sent rather than an input in the type input a select option is rendered
      */
-    @Input() private options: Array<{value: string, display: string}>;
+    @Input() public options: Array<{value: string, display: string}>;
 
     /**
      * if true display the input options as radio group
      */
-    @Input() private optionsAsRadio: boolean = false;
+    @Input() public optionsAsRadio: boolean = false;
 
     /**
      * the observabkle for the answer
@@ -115,7 +115,7 @@ export class SystemPrompt implements OnInit, AfterViewInit {
     /**
      * when ok is clicked
      */
-    private clickOK() {
+    public clickOK() {
         if ( this.type === 'input' ) this.answerSubject.next( this.value );
         else this.answerSubject.next( true );
         this.answerSubject.complete();
@@ -125,7 +125,7 @@ export class SystemPrompt implements OnInit, AfterViewInit {
     /**
      * issue cancel and close the modal
      */
-    private clickCancel() {
+    public clickCancel() {
         this.answerSubject.next( false );
         this.answerSubject.complete();
         this.self.destroy();
