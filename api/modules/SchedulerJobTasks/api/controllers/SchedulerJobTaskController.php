f22e99c7d9c21da699862ac76576b4529edaf4d8
Dawid Bestrzynski <dawid.bestrzynski@twentyreasons.com>
2021-10-25 14:31:14 +0200

    Changed extensions includes path to extensions include

--- api/modules/SchedulerJobTasks/api/controllers/SchedulerJobTaskController.php
+++ api/modules/SchedulerJobTasks/api/controllers/SchedulerJobTaskController.php
@@ -60,7 +60,7 @@ class SchedulerJobTaskController
      */
     public function getSchedulerJobTaskClasses(Request $req, Response $res, array $args): Response {
         $classList = [];
-        $checkRootPaths = ['include', 'modules', 'extensions/includes', 'extensions/modules', 'custom/modules', 'custom/include'];
+        $checkRootPaths = ['include', 'modules', 'extensions/include', 'extensions/modules', 'custom/modules', 'custom/include'];
 
         foreach ($checkRootPaths as $checkRootPath) {
             $dirHandle = opendir("./$checkRootPath");
