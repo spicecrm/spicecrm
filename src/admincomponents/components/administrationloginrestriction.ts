4bf9fab3b992f80bb6ed07e8af3c3a4bda7c96c9
strombo <andreas@gloeckl.name>
2021-11-08 12:32:19 +0100

    CR1000666: Security Settings Improvements (small changes and bugfixes)

--- src/admincomponents/components/administrationloginrestriction.ts
+++ src/admincomponents/components/administrationloginrestriction.ts
@@ -31,7 +31,8 @@ export class AdministrationLoginRestriction implements OnInit {
             enabled: false,
             number_attempts: 0,
             monitored_period: 0,
-            blocking_duration: 0
+            blocking_duration: 0,
+            blocking_type: 'permanent'
         },
         ip: {
             enabled: false,
@@ -63,7 +64,7 @@ export class AdministrationLoginRestriction implements OnInit {
             user_enabled: this.restrictions.user.enabled ? '1':'0',
             user_number_attempts: this.restrictions.user.number_attempts,
             user_monitored_period: this.restrictions.user.monitored_period,
-            user_blocking_duration: this.restrictions.user.blocking_duration
+            user_blocking_duration: this.restrictions.user.blocking_type === 'time' ? this.restrictions.user.blocking_duration : '0',
         };
         this.isLoading = true;
         this.backend.postRequest('configuration/configurator/editor/login_attempt_restriction', null, { config: config })
@@ -86,6 +87,7 @@ export class AdministrationLoginRestriction implements OnInit {
                 this.restrictions.user.number_attempts = response.user_number_attempts || 0,
                 this.restrictions.user.monitored_period = response.user_monitored_period || 0,
                 this.restrictions.user.blocking_duration = response.user_blocking_duration || 0;
+                this.restrictions.user.blocking_type = this.restrictions.user.blocking_duration ? 'time':'permanent';
                 this.restrictionsBackup = JSON.parse( JSON.stringify( this.restrictions ) );
                 this.isLoading = false;
             },
