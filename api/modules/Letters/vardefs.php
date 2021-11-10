12a5700ba90d0f434a9ebd7b823a56e1340729c8
natalia <natalia.bochenek@twentyreasons.com>
2021-11-10 12:16:37 +0100

    Module Letters: add link between Letters and Consumers

--- api/modules/Letters/vardefs.php
+++ api/modules/Letters/vardefs.php
@@ -119,12 +119,21 @@ $dictionary['Letter'] = [
             'bean_name' => 'Account',
             'source' => 'non-db',
             'vname' => 'LBL_ACCOUNT',
-            'comment'  => 'The link to the contact',
+            'comment'  => 'The link to the account',
+        ],
+        'consumers' => [
+            'name' => 'consumers',
+            'type' => 'link',
+            'relationship' => 'consumer_letters',
+            'module' => 'Consumers',
+            'bean_name' => 'Consumer',
+            'source' => 'non-db',
+            'vname' => 'LBL_CONSUMER',
+            'comment'  => 'The link to the consumer',
         ],
 
     ],
     'relationships' => [
-
         'outputtemplate_letters' => [
             'lhs_module' => 'OutputTemplates',
             'lhs_table' => 'outputtemplates',
@@ -134,12 +143,9 @@ $dictionary['Letter'] = [
             'rhs_key' => 'outputtemplate_id',
             'relationship_type' => 'one-to-many'
         ],
-
-
     ],
     'indices' => [],
     'comment' => 'Contains a record of letters sent to and from the application',
-
 ];
 
 VardefManager::createVardef('Letters', 'Letter', ['default', 'assignable']);
