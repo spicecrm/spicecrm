02bdc76de5d2fadaac90f6e1eb21581851ce47d0
Christian Knoll <christian.knoll@twentyreasons.com>
2021-11-13 11:33:19 +0100

    cleanup Activities, History, Calls, Meetings, Tasks removing unneeded functions and DB activity Stream Support

--- api/modules/Meetings/Meeting.php
+++ api/modules/Meetings/Meeting.php
@@ -35,14 +35,8 @@
 ********************************************************************************/
 namespace SpiceCRM\modules\Meetings;
 
-use SpiceCRM\data\BeanFactory;
 use SpiceCRM\data\SugarBean;
 use DateTime;
-use SpiceCRM\includes\database\DBManagerFactory;
-use SpiceCRM\includes\authentication\AuthenticationController;
-use SpiceCRM\includes\utils\SpiceUtils;
-use SpiceCRM\modules\Contacts\Contact;
-use SpiceCRM\modules\SpiceACL\SpiceACL;
 use SpiceCRM\includes\TimeDate;
 
 class Meeting extends SugarBean
@@ -94,10 +88,8 @@ class Meeting extends SugarBean
             $this->contacts->add($this->contact_id);
         }
 
-
         return $return_id;
     }
-
 
     function retrieve($id = -1, $encode = false, $deleted = true, $relationships = true)
     {
@@ -112,113 +104,6 @@ class Meeting extends SugarBean
         }
 
         return $ret;
-    }
-
-
-    function get_user_meetings($user, $timespan = 'today')
-    {
-
-        $timedate = TimeDate::getInstance();
-
-        $template = $this;
-
-        // get the own meetings
-        $myquery = "SELECT id FROM meetings WHERE deleted = 0 AND assigned_user_id = '$user->id' AND status = 'planned'";
-
-        // First, get the list of IDs.
-        $invitedquery = "SELECT meetings.id FROM meetings, meetings_users WHERE meetings.id = meetings_users.meeting_id AND meetings_users.user_id='$user->id' AND meetings_users.deleted=0 AND meetings.deleted = 0 AND meetings.status = 'planned'";
-
-        // add the timespan
-        switch ($timespan) {
-            case 'all':
-                $end = new DateTime();
-                $end->setTime(23, 59, 59);
-                $invitedquery .= " AND meetings.date_start <= '" . $timedate->asDb($end) . "'";
-                $myquery .= " AND meetings.date_start <= '" . $timedate->asDb($end) . "'";
-                break;
-            case 'today':
-                $start = new DateTime();
-                $start->setTime(0, 0, 0);
-                $end = new DateTime();
-                $end->setTime(23, 59, 59);
-                $invitedquery .= " AND meetings.date_start >= '" . $timedate->asDb($start) . "' AND meetings.date_start <= '" . $timedate->asDb($end) . "'";
-                $myquery .= " AND meetings.date_start >= '" . $timedate->asDb($start) . "' AND meetings.date_start <= '" . $timedate->asDb($end) . "'";
-                break;
-            case 'overdue':
-                $end = new DateTime();
-                $end->setTime(0, 0, 0);
-                $invitedquery .= " AND meetings.date_start < '" . $timedate->asDb($end) . "'";
-                $myquery .= " AND meetings.date_start < '" . $timedate->asDb($end) . "'";
-                break;
-            case 'future':
-                $start = new DateTime();
-                $start->setTime(0, 0, 0);
-                $invitedquery .= " AND meetings.date_start > '" . $timedate->asDb($start) . "''";
-                $myquery .= " AND meetings.date_start > '" . $timedate->asDb($start) . "''";
-                break;
-        }
-
-        $result = $this->db->query($invitedquery . ' UNION ' . $myquery, true);
-
-        $list = [];
-
-        while ($row = $this->db->fetchByAssoc($result)) {
-            $record = BeanFactory::getBean('Meetings', $row['id']);
-
-            if ($record != null) {
-                // this copies the object into the array
-                $list[] = $record;
-            }
-        }
-        return $list;
-
-    }
-
-    /*
-     * function to retrieve a query string for the activity stream
-     */
-    function get_activities_query($parentModule, $parentId, $own = false)
-    {
-        $current_user = AuthenticationController::getInstance()->getCurrentUser();
-        $query = "SELECT DISTINCT(meetings.id), date_start sortdate, 'Meetings' module FROM meetings LEFT JOIN meetings_contacts on meetings.id = meetings_contacts.meeting_id where ((parent_type = '$parentModule' and parent_id = '$parentId') OR meetings_contacts.contact_id='$parentId') and meetings.deleted = 0 and status in ('Planned')";
-
-        switch ($own) {
-            case 'assigned':
-                $query .= " AND meetings.assigned_user_id='$current_user->id'";
-                break;
-            case 'created':
-                $query .= " AND meetings.created_by='$current_user->id'";
-                break;
-        }
-
-        return $query;
-    }
-
-    function get_history_query($parentModule, $parentId, $own = false)
-    {
-        $current_user = AuthenticationController::getInstance()->getCurrentUser();
-
-        $queryArray = [
-            'select' => "SELECT meetings.id, date_start sortdate, 'Meetings' module",
-            'from' => "FROM meetings LEFT JOIN meetings_contacts on meetings.id = meetings_contacts.meeting_id",
-            'where' => "WHERE ((parent_type = '$parentModule' AND parent_id = '$parentId') OR meetings_contacts.contact_id='$parentId') AND meetings.deleted = 0 AND status NOT IN ('Planned')",
-            'order_by' => ""
-        ];
-
-        switch ($own) {
-            case 'assigned':
-                $queryArray['where'] .= " AND meetings.assigned_user_id='$current_user->id'";
-                break;
-            case 'created':
-                $queryArray['where'] .= " AND meetings.created_by='$current_user->id'";
-                break;
-        }
-
-        if (SpiceACL::getInstance() && method_exists(SpiceACL::getInstance(), 'addACLAccessToListArray')) {
-            SpiceACL::getInstance()->addACLAccessToListArray($queryArray, $this);
-        }
-
-        return $queryArray['select'] . ' ' . $queryArray['from'] . ' ' . $queryArray['where'] . ' ' . $queryArray['order_by'];
     }
 
 }
