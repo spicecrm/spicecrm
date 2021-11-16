551bbef331b33f204941e03225473121fab66d7f
Christian Knoll <christian.knoll@twentyreasons.com>
2021-11-16 15:56:41 +0100

    handle readonly flag in record details properly

--- src/objectcomponents/components/objectrecorddetails.ts
+++ src/objectcomponents/components/objectrecorddetails.ts
@@ -44,6 +44,10 @@ export class ObjectRecordDetails implements OnInit, OnDestroy {
     }
 
     public ngOnInit() {
+
+        // build the container
+        this.buildContainer();
+
         // check if we are in readonly mode or if the view shpoudl be set as editable
         if (this.readonly === true || this.componentconfig.readonly) {
             this.view.isEditable = false;
@@ -61,7 +65,6 @@ export class ObjectRecordDetails implements OnInit, OnDestroy {
             }));
         }
 
-        this.buildContainer();
     }
 
     /**
@@ -78,8 +81,8 @@ export class ObjectRecordDetails implements OnInit, OnDestroy {
         if (!this.componentSet) {
             // if we do not have a coimponentset from external check the default config
             if (!this.componentconfig.componentset) {
-                let componentconfig = this.metadata.getComponentConfig('ObjectRecordDetails', this.model.module);
-                this.componentSet = componentconfig.componentset;
+                this.componentconfig = this.metadata.getComponentConfig('ObjectRecordDetails', this.model.module);
+                this.componentSet = this.componentconfig.componentset;
             } else {
                 this.componentSet = this.componentconfig.componentset;
             }
