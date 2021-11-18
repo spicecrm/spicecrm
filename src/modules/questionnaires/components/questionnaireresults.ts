05c9c3348bd6da7777b180754c662512e82784d2
strombo <andreas@gloeckl.name>
2021-11-18 18:40:50 +0100

    Important bugfixes QuestionnaireParticipations/Evaluations (2)

--- src/modules/questionnaires/components/questionnaireresults.ts
+++ src/modules/questionnaires/components/questionnaireresults.ts
@@ -9,22 +9,41 @@ import { language } from '../../../services/language.service';
 import { modal } from '../../../services/modal.service';
 import { questionnaireParticipationService } from '../services/questionnaireparticipation.service';
 import { broadcast } from '../../../services/broadcast.service';
+import { take } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
-    templateUrl: './src/modules/questionnaires/templates/questionnaireresults.html',
-    providers: [questionnaireParticipationService]
+    templateUrl: './src/modules/questionnaires/templates/questionnaireresults.html'
 })
 export class QuestionnaireResults implements OnInit {
 
+    /**
+     * Is this component right now used for a QuestionnaireParticipation (or for a ServiceFeedback))?
+     */
+    private isForParticipation: boolean;
+
     private questionnaireParticipation: questionnaireParticipationService;
+
+    private subscription: Subscription;
 
     constructor( private model: model, private metadata: metadata, private language: language, private backend: backend, private modal: modal, private injector: Injector, private broadcast: broadcast ) { }
 
     public ngOnInit() {
-        this.broadcast.message$.subscribe(msg => {
-            // Reload the questionnaire in case the participation/answers had been (re-)saved elsewhere:
-            if ( msg.messagetype == 'questionnaireParticipation.saved' && msg.messagedata.parentType === this.model.module && msg.messagedata.parentId === this.model.id ) {
-                this.questionnaireParticipation.reload();
+        this.isForParticipation = ( this.model.module === 'QuestionnaireParticipations' );
+        this.subscription = this.broadcast.message$.subscribe(msg => {
+            // In case the participation/answers had been (re-)saved elsewhere
+            // • Reload the questionnaire.
+            // • Reload the detail view in case the status/completed field has changed.
+            if ( msg.messagetype == 'questionnaireParticipation.filledIn' ) {
+                if ( this.isForParticipation && msg.messagedata.id === this.questionnaireParticipation.participationId ) {
+                    this.questionnaireParticipation.isLoaded$.subscribe( () => {
+                        if ( this.questionnaireParticipation.isCompleted && this.model.getField('completed') !== true && !this.model.isEditing ) this.model.getData( true, null, true );
+                    });
+                    this.questionnaireParticipation?.reload();
+                } else if ( msg.messagedata.parentType === 'ServiceFeedbacks' && msg.messagedata.parentId === this.model.id ) {
+                    if ( this.model.getField('servicefeedback_status') !== 'completed' && !this.model.isEditing ) this.model.getData( true, null, true );
+                    this.questionnaireParticipation?.reload();
+                }
             }
         });
     }
@@ -33,8 +52,11 @@ export class QuestionnaireResults implements OnInit {
      * Is there a participation?
      * @private
      */
-    private get noParticipation() {
-        return this.questionnaireParticipation && this.questionnaireParticipation.isLoaded && !this.questionnaireParticipation.participationId;
+    private get participationExists() {
+        if ( this.questionnaireParticipation?.isLoaded ) {
+            if ( this.questionnaireParticipation.participationId ) return true;
+            else return false;
+        } else return undefined;
     }
 
     /**
@@ -50,18 +72,20 @@ export class QuestionnaireResults implements OnInit {
      * @private
      */
     private fillOut(): void {
-        this.modal.openModal('QuestionnaireFillOutModal').subscribe(modal => {
-            modal.instance.parentId = this.model.id;
-            modal.instance.parentType = this.model.module;
-        });
+        this.modal.openModal('QuestionnaireFillOutModal')
+            .pipe(take(1))
+            .subscribe(modal => {
+                if ( this.isForParticipation ) {
+                    modal.instance.participationId = this.model.id;
+                } else {
+                    modal.instance.parentId = this.model.id;
+                    modal.instance.parentType = this.model.module;
+                }
+            });
     }
 
-    /**
-     * In case we know it (parent ist ServiceFeedbacks): Is the Questionnaire completed (filled out)?
-     * @private
-     */
-    private get questionnaireIsCompleted(): boolean {
-        return this.model.module === 'ServiceFeedbacks' && this.model.getField('servicefeedback_status') === 'completed';
+    public ngOnDestroy(): void {
+        this.subscription.unsubscribe();
     }
 
 }
