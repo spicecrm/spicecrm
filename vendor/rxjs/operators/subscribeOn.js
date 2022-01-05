adc169fca8cee3a3de7c24be97b165060ecf3485
Christian Knoll <christian.g.knoll@gmail.com>
2022-01-05 15:43:33 +0100

    cleanup vendor dir

--- vendor/rxjs/operators/subscribeOn.js
+++
@@ -1,1 +0,0 @@
-"use strict";function __export(e){for(var r in e)exports.hasOwnProperty(r)||(exports[r]=e[r])}Object.defineProperty(exports,"__esModule",{value:!0}),__export(require("rxjs-compat/operators/subscribeOn"));
