98b990f2f812fc07e4955cd85d9539ad6707e60f
Christian Knoll <christian.g.knoll@gmail.com>
2022-01-24 08:59:44 +0100

    CORE category tree extensions

--- src/workbench/components/categorytreemanagernode.ts
+++ src/workbench/components/categorytreemanagernode.ts
@@ -23,7 +23,7 @@ declare var moment: any;
     selector: 'categgory-tree-manager-node',
     templateUrl: '../templates/categorytreemanagernode.html',
 })
-export class CategoryTreeManagerNode implements OnInit{
+export class CategoryTreeManagerNode implements OnInit {
 
 
     /**
@@ -70,14 +70,17 @@ export class CategoryTreeManagerNode implements OnInit{
     /**
      * checks that we can save
      */
-    get canSave(){
-        return this.node.node_name && this.node.node_key && this.keyUnique;
+    get canSave() {
+        // regex for numeric check
+        let lkey = new RegExp(/([1-9][0-9]*)|0/);
+
+        return this.node.node_name && this.node.node_key && lkey.test(this.node.node_key) && this.keyUnique;
     }
 
     /**
      * checks that the key is unique
      */
-    get keyUnique(){
+    get keyUnique() {
         return this.node.node_key && this.nodes.filter(n => n.node_key == this.node.node_key && n.id != this.node.id).length == 0;
     }
 
@@ -95,7 +98,7 @@ export class CategoryTreeManagerNode implements OnInit{
      *
      * @private
      */
-    public save(){
+    public save() {
         this.node.add_params = this.addParams;
 
         // format the dates back
@@ -110,7 +113,7 @@ export class CategoryTreeManagerNode implements OnInit{
      *
      * @private
      */
-    public close(){
+    public close() {
         this.action.emit(false);
         this.self.destroy();
     }
