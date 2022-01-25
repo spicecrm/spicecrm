0723df7ab50adb6e819b1e04b995099cf0644684
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2022-01-25 09:45:37 +0100

    1000337: Workflows: adjustments

--- api/include/language/ar_sy.lang.php
+++ api/include/language/ar_sy.lang.php
@@ -2,31 +2,31 @@
 /*********************************************************************************
 * SugarCRM Community Edition is a customer relationship management program developed by
 * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
-* 
+*
 * This program is free software; you can redistribute it and/or modify it under
 * the terms of the GNU Affero General Public License version 3 as published by the
 * Free Software Foundation with the addition of the following permission added
 * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
 * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
 * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
-* 
+*
 * This program is distributed in the hope that it will be useful, but WITHOUT
 * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
 * details.
-* 
+*
 * You should have received a copy of the GNU Affero General Public License along with
 * this program; if not, see http://www.gnu.org/licenses or write to the Free
 * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 * 02110-1301 USA.
-* 
+*
 * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
 * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
-* 
+*
 * The interactive user interfaces in modified source and object code versions
 * of this program must display Appropriate Legal Notices, as required under
 * Section 5 of the GNU Affero General Public License version 3.
-* 
+*
 * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
 * these Appropriate Legal Notices must retain the display of the "Powered by
 * SugarCRM" logo. If the display of the logo is not reasonably feasible for
@@ -1250,12 +1250,11 @@ $app_list_strings = [
         'email' => 'أيميل',
         'system' => 'نظام',
     ],
-    'workflowdefinition_status' => [
-        'active' => 'نشط',
-        'active_once' => 'نشط (تشغيل مرة واحدة]',
-        'active_scheduled' => 'نشط مقرر',
-        'active_scheduled_once' => 'نشط مقرر (تشغيل مرة واحدة]',
-        'inactive' => 'غير نشط'
+    'workflowdefinition_frequency' => [
+        'always' => 'دائماً',
+        'run_once' => 'مرة واحدة',
+        'scheduled_always' => 'موقوت دائما',
+        'scheduled_once' => 'موقوت مرة واحدة'
     ],
     'workflowdefinition_precondition' => [
         'a' => 'دائماً',
