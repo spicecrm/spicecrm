21fe549930c770ce493c6ee466bf0b1580c0367c
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2022-02-09 09:39:41 +0100

    OAuth2: built frontend oauth2 service. Adjusted token/fetchProfile api.

--- api/include/authentication/api/extensions/oauth2.php
+++ api/include/authentication/api/extensions/oauth2.php
@@ -3,7 +3,7 @@
 
 use SpiceCRM\includes\authentication\AuthenticationController;
 use SpiceCRM\includes\RESTManager;
-use SpiceCRM\includes\authentication\api\controllers\OAuthController;
+use SpiceCRM\includes\authentication\api\controllers\OAuth2Controller;
 use SpiceCRM\includes\Middleware\ValidationMiddleware;
 use SpiceCRM\includes\SugarObjects\SpiceConfig;
 
@@ -16,9 +16,9 @@ $config      = SpiceConfig::getInstance()->config;
 $routes = [
     [
         'method'      => 'get',
-        'route'       => '/authentication/oauth',
-        'class'       => OAuthController::class,
-        'function'    => 'handleOAuthRedirect',
+        'route'       => '/authentication/oauth2/profile',
+        'class'       => OAuth2Controller::class,
+        'function'    => 'getUserProfile',
         'description' => 'This is the route used as the redirect URL for OAuth',
         'options'     => ['noAuth' => true, 'adminOnly' => false],
         'parameters'  => [
@@ -30,6 +30,28 @@ $routes = [
             ],
             'code'  => [
                 'in'          => 'query',
+                'description' => 'Authorization code that needs to be exchanged for the access token.',
+                'type'        => ValidationMiddleware::TYPE_STRING,
+                'required'    => true,
+            ],
+        ]
+    ],
+    [
+        'method'      => 'post',
+        'route'       => '/authentication/oauth2/accessToken',
+        'class'       => OAuth2Controller::class,
+        'function'    => 'getAccessToken',
+        'description' => 'request access token based on auth code',
+        'options'     => ['noAuth' => true, 'adminOnly' => false],
+        'parameters'  => [
+            'issuer' => [
+                'in'          => 'body',
+                'description' => '',
+                'type'        => ValidationMiddleware::TYPE_STRING,
+                'required'    => false,
+            ],
+            'code'  => [
+                'in'          => 'body',
                 'description' => 'Authorization code that needs to be exchanged for the access token.',
                 'type'        => ValidationMiddleware::TYPE_STRING,
                 'required'    => true,
