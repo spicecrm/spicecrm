af998e6839caf1933f74440531553b256fcadb99
Christian Knoll <christian.g.knoll@gmail.com>
2022-02-16 06:13:38 +0100

    CORE move fts initialize to fts status

--- src/admincomponents/components/administrationftsstats.ts
+++ src/admincomponents/components/administrationftsstats.ts
@@ -4,10 +4,7 @@
 import {Component} from '@angular/core';
 import {metadata} from '../../services/metadata.service';
 import {language} from '../../services/language.service';
-import {ftsconfiguration} from '../services/ftsconfiguration.service';
 import {backend} from '../../services/backend.service';
-
-
 
 @Component({
     templateUrl: '../templates/administrationftsstats.html'
@@ -22,12 +19,11 @@ export class AdministrationFTSStats {
         public metadata: metadata,
         public language: language,
         public backend: backend
-
     ) {
         this.backend.getRequest('admin/elastic/stats').subscribe(stats => {
             this.stats = stats;
 
-            for(let index in stats.indices) {
+            for (let index in stats.indices) {
                 this.indices.push({
                     name: index,
                     size: stats.indices[index].total.store.size_in_bytes,
@@ -42,4 +38,5 @@ export class AdministrationFTSStats {
 
         });
     }
+
 }
