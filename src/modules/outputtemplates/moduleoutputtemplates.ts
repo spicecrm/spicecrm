3ea5b6f57114ee29178ba9c303b64ce25083baed
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2022-02-23 10:14:17 +0100

    Revert "PDF-JS: installed."
    This reverts commit bad01a95954e8275071d47ad836a88815e63c508.

--- src/modules/outputtemplates/moduleoutputtemplates.ts
+++ src/modules/outputtemplates/moduleoutputtemplates.ts
@@ -23,6 +23,7 @@ import /*embed*/ {ObjectActionOutputBeanModal} from "./components/objectactionou
 import /*embed*/ {fieldOutputTemplates} from "./fields/fieldoutputtemplates";
 import /*embed*/ {OutputTemplatesVariableHelper} from './components/outputtemplatesvariablehelper';
 import /*embed*/ {ObjectActionMarkSentBeanButton} from "./components/objectactionmarksentbeanbutton";
+import {PdfViewerModule} from "ng2-pdf-viewer";
 
 @NgModule({
     imports: [
@@ -33,6 +34,7 @@ import /*embed*/ {ObjectActionMarkSentBeanButton} from "./components/objectactio
         ObjectComponents,
         SystemComponents,
         DirectivesModule,
+        PdfViewerModule
     ],
     declarations: [
         OutputTemplatesEditor,
