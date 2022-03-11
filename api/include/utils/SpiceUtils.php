215767c7add0a969f2c9ca6d5155eea1c6930c23
Christian Knoll <christian.g.knoll@gmail.com>
2022-03-11 16:09:02 +0100

    CORE at br2nl to SpiceUTILS

--- api/include/utils/SpiceUtils.php
+++ api/include/utils/SpiceUtils.php
@@ -1287,4 +1287,22 @@ class SpiceUtils
         }
         return $string;
     }
+
+
+    public static function br2nl($str) {
+        $regex = "#<[^>]+br.+?>#i";
+        preg_match_all($regex, $str, $matches);
+
+        foreach ($matches[0] as $match) {
+            $str = str_replace($match, "<br>", $str);
+        }
+
+        $brs = array('<br>', '<br/>', '<br />');
+        $str = str_replace("\r\n", "\n", $str); // make from windows-returns, *nix-returns
+        $str = str_replace("\n\r", "\n", $str); // make from windows-returns, *nix-returns
+        $str = str_replace("\r", "\n", $str); // make from windows-returns, *nix-returns
+        $str = str_ireplace($brs, "\n", $str); // to retrieve it
+
+        return $str;
+    }
 }
