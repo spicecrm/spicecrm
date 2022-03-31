fffd87e6a769394cf3059e296bb7062d12ddbc20
strombo <andreas@gloeckl.name>
2022-03-31 17:14:32 +0200

    packaging

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2022.01.001.1648561058354",
+    buildNumber: "2022.01.001.1648739391715",
     copyright: "© 2015 -  2022 aac services k.s. All rights reserved."
     }
