90088982418c00fe4dec0f0c7ea1fcd78967d5a0
strombo <andreas@gloeckl.name>
2022-04-04 17:56:02 +0200

    packaging

--- environments/environment.prod.js
+++ environments/environment.prod.js
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    buildNumber: "2022.01.001.1648739391715",
+    buildNumber: "2022.01.001.1649087382320",
     copyright: "© 2015 -  2022 aac services k.s. All rights reserved."
 };
 //# sourceMappingURL=environment.prod.js.map
