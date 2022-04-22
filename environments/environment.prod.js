953912e279004ec4f4d5dd6f72ca15c4347cb131
Christian Knoll <christian.g.knoll@gmail.com>
2022-04-22 10:38:13 +0200

    packaging

--- environments/environment.prod.js
+++ environments/environment.prod.js
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    buildNumber: "2022.01.001.1650467678829",
+    buildNumber: "2022.01.001.1650616551676",
     copyright: "© 2015 -  2022 aac services k.s. All rights reserved."
 };
 //# sourceMappingURL=environment.prod.js.map
--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2022.01.001.1650561760998",
+    buildNumber: "2022.01.001.1650616551676",
     copyright: "© 2015 -  2022 aac services k.s. All rights reserved."
     }
