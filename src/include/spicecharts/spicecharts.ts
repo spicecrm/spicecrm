ba00759e03e453d116951026d290cffe11f2572d
Christian Knoll <christian.g.knoll@gmail.com>
2022-04-24 08:06:07 +0200

    new module SpiceCharts to add ChartJS as library

---
+++ src/include/spicecharts/spicecharts.ts
@@ -0,0 +1,33 @@
+/**
+ * @module ModuleSpiceCharts
+ */
+import {CommonModule} from "@angular/common";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+
+import {DirectivesModule} from "../../directives/directives";
+
+import {ObjectFields} from "../../objectfields/objectfields";
+import {GlobalComponents} from "../../globalcomponents/globalcomponents";
+import {ObjectComponents} from "../../objectcomponents/objectcomponents";
+import {SystemComponents} from "../../systemcomponents/systemcomponents";
+
+// import interfaces
+import {SpiceChart} from "./components/spicechart"
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ObjectFields,
+        GlobalComponents,
+        ObjectComponents,
+        SystemComponents,
+        DirectivesModule
+    ],
+    declarations: [
+        SpiceChart
+    ]
+})
+export class SpiceChartsModule {
+}
