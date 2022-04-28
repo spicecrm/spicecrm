bbf877b877f92e3a5cd9f78adcc79cc4598bd2ea
natoku <NatGit4856!>
2022-04-28 13:27:16 +0200

    core: CR1000739: CRMDB-679: enhancing campaigntaskmailmergepanel with subject line and texteditor, rendering mailmerge fields in the pdf template (CampaignTask, CampaignTasksController, campaigntaskmailmergemodal: campaigntaskmailmergepanel)

--- api/include/SugarObjects/templates/company/vardefs.php
+++ api/include/SugarObjects/templates/company/vardefs.php
@@ -437,7 +437,21 @@ $vardefs = [
             'dbType' => 'longtext',
             'maxWidth' => 300,
             'maxHeight' => 300
-        ]
+        ],
+        'mailmerge_subject' => [
+            'name' => 'mailmerge_subject',
+            'type' => 'varchar',
+            'source' => 'non-db',
+            'vname' => 'LBL_MAILMERGE_SUBJECT',
+            'comnment' => 'set the mailmerge subject for rendering pdf template'
+        ],
+        'mailmerge_body' => [
+            'name' => 'mailmerge_body',
+            'type' => 'varchar',
+            'source' => 'non-db',
+            'vname' => 'LBL_MAILMERGE_BODY',
+            'comnment' => 'set the mailmerge subject for rendering pdf template'
+        ],
     ],
     'relationships' => [
         strtolower($object_name) . '_email_addresses' =>
