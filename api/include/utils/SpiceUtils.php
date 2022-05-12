4588f693a16dcfca6d5b5036602418617473de5e
maretval <valerie.maret@twentyreasons.com>
2022-05-12 23:27:08 +0200

    CR1000747: clean up Opportunity; replace n-2-n accounts_opportunities relationship by a 1-2-n

--- api/include/utils/SpiceUtils.php
+++ api/include/utils/SpiceUtils.php
@@ -923,7 +923,7 @@ class SpiceUtils
             }
         }
         if (!empty($params['convert']) && $params['convert']) {
-            $amount = $currency->convertFromDollar($amount, 6);
+            $amount = $currency->convertFromBase($amount, 6);
         }
 
         if (!empty($params['currency_symbol']) && $params['currency_symbol']) {
