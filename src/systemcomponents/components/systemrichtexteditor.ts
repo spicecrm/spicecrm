827814cbd21b09318c17339a61bb7ad7a3821139
strombo <andreas@gloeckl.name>
2022-05-20 11:23:45 +0200

    additional tags and attributs for ckeditor (3)

--- src/systemcomponents/components/systemrichtexteditor.ts
+++ src/systemcomponents/components/systemrichtexteditor.ts
@@ -150,7 +150,8 @@ export class SystemRichTextEditor implements OnInit, OnDestroy, ControlValueAcce
                 CKSource.Editor.create(this.ckEditor.element.nativeElement, {
                     toolbar: [],
                     htmlSupport: {
-                        allow: this.generateHtmlTagsAllowAttributes(['div', 'span', 'table', 'p', 'h1', 'h2', 'h3', 'h4', 'input', 'fieldset', 'button', 'label', 'textarea'])
+                        allow: this.generateHtmlTagsAllowAttributes(['div', 'span', 'table', 'p', 'h1', 'h2', 'h3', 'h4', 'input', 'fieldset', 'button', 'label', 'textarea', 'select', 'option', 'optgroup'])
+                        // hr
                     },
                     autosave: {
                         save: ( editor ) => {
@@ -197,8 +198,25 @@ export class SystemRichTextEditor implements OnInit, OnDestroy, ControlValueAcce
                     'name': true,
                     'for': true,
                     'checked': true,
+                    'selected': true,
                     'size': true,
-                    'placeholder': true
+                    'autocomplete': true,
+                    'autofocus': true,
+                    'placeholder': true,
+                    'max': true,
+                    'maxlength': true,
+                    'min': true,
+                    'minlength': true,
+                    'multiple': true,
+                    'required': true,
+                    'disabled': true,
+                    'form': true,
+                    'step': true,
+                    'readonly': true,
+                    'cols': true,
+                    'rows': true,
+                    'wrap': true,
+                    'label': true
                 }
             }
         });
