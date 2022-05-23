6101ad5f1e9bc0b4224c8a61d78ba57ca75c5cbe
Christian Knoll <christian.g.knoll@gmail.com>
2022-05-23 12:53:53 +0200

    CORE fix System Tenant

--- api/modules/SystemTenants/api/controllers/SystemTenantsController.php
+++ api/modules/SystemTenants/api/controllers/SystemTenantsController.php
@@ -92,7 +92,7 @@ class SystemTenantsController
         }
 
         $dbName = SpiceConfig::getInstance()->config['dbconfig']['db_name'];
-        DBManagerFactory::switchDatabase($dbName, SpiceConfig::getInstance()->config);
+        DBManagerFactory::switchToMasterDatabase();
 
         /* @var SystemTenant */
         $tenant = BeanFactory::getBean('SystemTenants', $authController->systemtenantid);
