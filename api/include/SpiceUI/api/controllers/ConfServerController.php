cf8d7eb3a578f8b43196bdedd651d784a2279bcc
maretval <valerie.maret@twentyreasons.com>
2022-05-24 17:56:27 +0200

    Package > LanguageLoader: use original id from reference for syslanguagetranslations records

--- api/include/SpiceUI/api/controllers/ConfServerController.php
+++ api/include/SpiceUI/api/controllers/ConfServerController.php
@@ -196,7 +196,7 @@ class ConfServerController
         $langs = explode(',', $args['language']);
 
         //$records = $db->query("SELECT * FROM $table WHERE version='$version'");
-        $records = $db->query("SELECT ll.id, ll.name, lt.syslanguage, lt.translation_default, lt.translation_short, lt.translation_long FROM syslanguagelabels ll, syslanguagetranslations lt WHERE ll.id = lt.syslanguagelabel_id AND lt.syslanguage in ('".implode("','", $langs)."')");
+        $records = $db->query("SELECT ll.id, ll.name, lt.syslanguage, lt.translation_default, lt.translation_short, lt.translation_long, lt.id translation_id FROM syslanguagelabels ll, syslanguagetranslations lt WHERE ll.id = lt.syslanguagelabel_id AND lt.syslanguage in ('".implode("','", $langs)."')");
         if (!$records) {
             return $langArray;
         }
