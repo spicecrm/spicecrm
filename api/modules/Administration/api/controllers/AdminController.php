a1d29d4730e7a9bde63ce147ec2d31853eb5f4db
maretval <valerie.maret@twentyreasons.com>
2022-06-19 20:24:11 +0200

    DictionaryManager: fixes load relationships

--- api/modules/Administration/api/controllers/AdminController.php
+++ api/modules/Administration/api/controllers/AdminController.php
@@ -19,8 +19,6 @@ use SpiceCRM\includes\utils\SpiceFileUtils;
 use SpiceCRM\includes\utils\SpiceUtils;
 use SpiceCRM\data\BeanFactory;
 use SpiceCRM\includes\UploadStream;
-use SpiceCRM\modules\ACLActions\ACLAction;
-use SpiceCRM\modules\Relationships\Relationship;
 use SpiceCRM\includes\SpiceFTSManager\SpiceFTSHandler;
 use SpiceCRM\modules\Configurator\Configurator;
 use SpiceCRM\includes\SpiceDictionary\SpiceDictionaryVardefs;
@@ -201,12 +199,14 @@ class AdminController
      */
     public function buildSQLforRepair()
     {
+
         $db = DBManagerFactory::getInstance();
         $execute = false;
         VardefManager::clearVardef();
+
         if (SpiceDictionaryVardefs::isDbManaged()) {
             $vardefs = SpiceDictionaryVardefs::loadVardefs();
-
+//echo print_r($vardefs, true);die('as');
             $db->transactionStart();
             $db->truncateQuery('sysdictionaryfields', true);
 
@@ -219,6 +219,7 @@ class AdminController
 
         $repairedTables = [];
         $sql = '';
+
         foreach (SpiceModules::getInstance()->getModuleList() as $module) {
             $focus = BeanFactory::getBean($module);
             if (($focus instanceof SpiceBean) && !isset($repairedTables[$focus->_tablename])) {
@@ -279,6 +280,8 @@ class AdminController
         $sql = '';
 
         foreach (SpiceModules::getInstance()->getModuleList() as $module) {
+            SpiceDictionaryHandler::loadModuleFiles($module);
+
             $focus = BeanFactory::getBean($module);
             if (($focus instanceof SpiceBean) && !isset($repairedTables[$focus->_tablename])) {
                 $sql .= $db->repairTable($focus, $execute);
@@ -451,7 +454,8 @@ class AdminController
     {
         unset($_SESSION['relationships']);
         // rebuild relationship cache
-        $rel = new Relationship();
+        SpiceDictionaryHandler::loadModuleFiles('Relationships');
+        $rel = BeanFactory::getBean('Relationships');
         $rel->build_relationship_cache();
     }
 
@@ -653,10 +657,6 @@ class AdminController
             foreach ($extensions as $extDir => $extFile) {
                 $this->merge_files("Ext/Vardefs", 'vardefs.ext.php');
             }
-        }
-
-        if (is_dir('custom/Extension/modules/Jobs/Ext/ScheduledTasks')) {
-            $this->merge_files("Ext/ScheduledTasks", 'scheduledtasks.ext.php');
         }
     }
 
