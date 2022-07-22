9664b35d1e1f889469beaf484ff755f1adf036b8
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2022-07-22 11:08:03 +0200

    GeneralSettings: bug fix. initial values for logger

--- environments/environment.prod.js
+++ environments/environment.prod.js
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    buildNumber: "2022.02.001.1658132898501",
+    buildNumber: "2022.02.001.1658158792572",
     copyright: "© 2015 -  2022 aac services k.s. All rights reserved."
 };
 //# sourceMappingURL=environment.prod.js.map
