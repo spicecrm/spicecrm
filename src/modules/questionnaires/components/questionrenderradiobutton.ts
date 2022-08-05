da7871f6d54cf221be0d9f367fa989e5e311573f
strombo <andreas@gloeckl.name>
2022-08-05 14:11:34 +0200

    bufix: error when missing question option

--- src/modules/questionnaires/components/questionrenderradiobutton.ts
+++ src/modules/questionnaires/components/questionrenderradiobutton.ts
@@ -13,6 +13,8 @@ declare var _;
 })
 export class QuestionRenderRadioButton implements OnInit {
 
+    public _ = _; // Workaround to use _ (underscore.js) inside the html template.
+
     /**
      * The question option for which to render the radio button.
      */
