eb17ff7f144897d5b070bbd2f7444ff5da510c80
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2022-08-09 17:10:34 +0200

    DictionaryController: Added Repair custom legacy dom language

--- api/modules/Administration/api/extensions/dictionary.php
+++ api/modules/Administration/api/extensions/dictionary.php
@@ -15,9 +15,17 @@ $routes = [
         'description' => 'save all vardefs to sysdictionaryfields table and relationships to relationships table',
         'options' => ['noAuth' => false, 'adminOnly' => true, 'validate' => true],
     ],
+    [
+        'method' => 'post',
+        'route' => '/admin/repair/custom/enum',
+        'class' => DictionaryController::class,
+        'function' => 'repairCustomEnum',
+        'description' => 'save all vardefs to sysdictionaryfields table and relationships to relationships table',
+        'options' => ['noAuth' => false, 'adminOnly' => true, 'validate' => true],
+    ],
 ];
 
 /**
  * get a Rest Manager Instance
  */
-$RESTManager = RESTManager::getInstance()->registerExtension('dictionary', '2.0', [], $routes);
+RESTManager::getInstance()->registerExtension('dictionary', '2.0', [], $routes);
