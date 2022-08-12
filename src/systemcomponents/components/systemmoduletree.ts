af3304ad0405e4ccd8b7d682ffa0e04e7e56205c
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2022-08-12 11:03:47 +0200

    CR1000774: Report Designer: added audit fields

--- src/systemcomponents/components/systemmoduletree.ts
+++ src/systemcomponents/components/systemmoduletree.ts
@@ -18,6 +18,11 @@ declare var _: any;
 export class SystemModuleTree implements AfterViewInit {
 
     /**
+     * enable displaying the audit fields
+     */
+    @Input() public displayAuditFields: boolean = false;
+
+    /**
      * enable displaying the relationship fields
      */
     @Input() public displayRelationshipFields: boolean = false;
@@ -33,6 +38,10 @@ export class SystemModuleTree implements AfterViewInit {
     @Output() public itemSelected: EventEmitter<any> = new EventEmitter<any>();
 
     public selectedNodeId: string = '';
+    /**
+     * if nodes are loading
+     */
+    public isLoading = false;
 
     constructor(public language: language) {
 
