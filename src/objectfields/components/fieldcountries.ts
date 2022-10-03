e34420625b7d15cccc3e7bf977a6ee4c2b817136
maretval <valerie.maret@twentyreasons.com>
2022-10-03 15:36:27 +0200

    CR1000797: fix empty value  fieldCountries

--- src/objectfields/components/fieldcountries.ts
+++ src/objectfields/components/fieldcountries.ts
@@ -37,6 +37,7 @@ export class fieldCountries extends fieldGeneric {
      * return the translated value for the selected country
      */
     public  getSelectedCountry(): string {
+        if(!this.value) return '';
         let selectedCountry = this.countries.filter(item => item.cc == this.value);
         return selectedCountry[0].name;
     }
