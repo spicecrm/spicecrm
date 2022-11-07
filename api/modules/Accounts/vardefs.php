037f4600fcccdc8a8234fcd855d8ac1f642c013d
Christian Knoll <christian.g.knoll@gmail.com>
2022-11-07 15:02:22 +0100

    add salesdocitems link to contacts and consumers

--- api/modules/Accounts/vardefs.php
+++ api/modules/Accounts/vardefs.php
@@ -704,6 +704,7 @@ if (file_exists("extensions/modules/SalesDocs")) {
         'relationship' => 'salesdocitems_accountsop',
         'module' => 'SalesDocItems',
         'source' => 'non-db',
+        'comment' => 'allows to display items on salesdocs via a n:m relationship using the salesdocs table'
     ];
 }
 if (file_exists("modules/Addresses")) {
