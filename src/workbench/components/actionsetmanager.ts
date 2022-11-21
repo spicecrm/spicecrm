4a5df9de907af17d459d5d690d71d9f708c78d43
maretval <valerie.maret@twentyreasons.com>
2022-11-21 18:11:53 +0100

    CR1000835 Actionset Manager: fix edit mode after adding new actionset

--- src/workbench/components/actionsetmanager.ts
+++ src/workbench/components/actionsetmanager.ts
@@ -148,6 +148,7 @@ export class ActionsetManager {
 
     public checkForChangesFunction() {
         if(this.currentActionSet.id != "") {
+            this.checkMode();
             return JSON.stringify(this.currentActionSet) == this.actionSetBackup ? false: true;
         } else {
             return false;
