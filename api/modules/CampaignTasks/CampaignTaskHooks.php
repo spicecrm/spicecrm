728cedc8d6f281b56e9e917b3194101bdf197927
natoku <natalia.okutansoy@twentyreasons.com>
2022-11-23 09:26:00 +0100

    CampaignTaskHooks: complete comment

--- api/modules/CampaignTasks/CampaignTaskHooks.php
+++ api/modules/CampaignTasks/CampaignTaskHooks.php
@@ -3,16 +3,20 @@
 namespace SpiceCRM\modules\CampaignTasks;
 
 use SpiceCRM\data\BeanFactory;
+use SpiceCRM\data\SpiceBean;
 
 
 class CampaignTaskHooks
 {
     /**
      * change activity_type if bean is inactive
-     * @param $bean
+     * @param $bean SpiceBean
      * @param $event
      * @param $args
+     * @return void
+     * @throws \Exception
      */
+
     public function changeActivityType(&$bean, $event, $args)
     {
         foreach ($bean->getProspectBeans() as $prospectBean){
