aa821d550be21dbdf193988da16110cc8d281a4c
maretval <valerie.maret@twentyreasons.com>
2022-11-23 01:08:03 +0100

    CR1000781: allocate additional values dynamically

--- api/modules/Events/api/controllers/EventController.php
+++ api/modules/Events/api/controllers/EventController.php
@@ -19,18 +19,18 @@ use SpiceCRM\includes\TimeDate;
 class EventController
 {
     /**
-     * returns registartions in the linked event
+     * returns registrations in the linked event
      * @param Request $req
      * @param Response $res
      * @param array $args
      * @return Response
      */
-    public function getEventRegistrations(Request $req, Response $res, array $args): Response
+    public function createEventRegistrations(Request $req, Response $res, array $args): Response
     {
         $body = $req->getParsedBody();
         $listDataIds = $body['targetListIds'];
         $registrationData = $body['registrationData'];
-        $event =  BeanFactory::getBean('Events', $body['eventId']);
+        $event = BeanFactory::getBean('Events', $body['eventId']);
         $existingEventRegistrations = $event->get_linked_beans('eventregistrations');
         $participants = [];
         foreach ($existingEventRegistrations as $existingEventRegistration){
@@ -44,23 +44,30 @@ class EventController
             $prospects = array_merge($prospects, $allProspectList->get_linked_beans('users'));
             $prospects = array_merge($prospects, $allProspectList->get_linked_beans('accounts'));
 
-
             foreach ($prospects as $prospect) {
                 $eventRegistration = BeanFactory::getBean('EventRegistrations');
-                    if (!in_array($prospect->id, $participants)) {
-                        $eventRegistration->salutation = $prospect->salutation;
-                        $eventRegistration->first_name = $prospect->first_name;
-                        $eventRegistration->last_name = $prospect->last_name;
-                        $eventRegistration->parent_id = $prospect->id;
-                        $eventRegistration->parent_type = $prospect->_module;
-                        $eventRegistration->event_id = $body['eventId'];
-                        $eventRegistration->assigned_user_id = AuthenticationController::getInstance()->getCurrentUser()->id;
-                        $eventRegistration->registration_status = $registrationData['registration_status'];
-                        $eventRegistration->description = $registrationData['description'];
-                        $eventRegistration->save();
+                if (!in_array($prospect->id, $participants)) {
+                    // map personal data
+                    $eventRegistration->salutation = $prospect->salutation;
+                    $eventRegistration->first_name = $prospect->first_name;
+                    $eventRegistration->last_name = $prospect->last_name;
+                    $eventRegistration->parent_id = $prospect->id;
+                    $eventRegistration->parent_type = $prospect->_module;
+                    $eventRegistration->event_id = $body['eventId'];
+                    $eventRegistration->assigned_user_id = AuthenticationController::getInstance()->getCurrentUser()->id;
+                    // set additional values common to all registrations
+                    if(is_array($registrationData)){
+                        foreach($registrationData as $property => $value){
+                            if(!in_array($eventRegistration->field_defs[$property]['type'], ['link', 'linked', 'relate'])){
+                                $eventRegistration->$property = $value;
+                            }
+                        }
                     }
+                    // save
+                    $eventRegistration->save();
+                }
             }
         }
-        return $res->withJson('succes');
+        return $res->withJson(['success' => true]);
     }
 }
