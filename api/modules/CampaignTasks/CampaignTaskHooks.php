b0ddb7f2b94887c507f7d9f587d3e40c7d5d761c
natoku <natalia.okutansoy@twentyreasons.com>
2022-11-23 09:23:41 +0100

    CampaignTaskHooks: delete obsolete instance loading

--- api/modules/CampaignTasks/CampaignTaskHooks.php
+++ api/modules/CampaignTasks/CampaignTaskHooks.php
@@ -15,15 +15,12 @@ class CampaignTaskHooks
      */
     public function changeActivityType(&$bean, $event, $args)
     {
-        // load the campaign task
-        $campaignTask = BeanFactory::getBean('CampaignTasks', $bean->id);
-
-        foreach ($campaignTask->getProspectBeans() as $prospectBean){
+        foreach ($bean->getProspectBeans() as $prospectBean){
             // check whether bean is inactive if true
             if ($prospectBean->is_inactive == 1) {
                 $campaignLog = BeanFactory::getBean('CampaignLog');
                 // get specific fields related to the campaign log
-                $campaignLog->retrieve_by_string_fields(['target_id' => $prospectBean->id, 'campaigntask_id' => $campaignTask->id]);
+                $campaignLog->retrieve_by_string_fields(['target_id' => $prospectBean->id, 'campaigntask_id' => $bean->id]);
 
                 // change status to inactive
                 $campaignLog->activity_type = 'inactive';
