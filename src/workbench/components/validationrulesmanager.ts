ec7d45a7f048f50850816a17e4eff0c51808804d
maretval <valerie.maret@twentyreasons.com>
2022-11-27 21:39:39 +0100

    CR1000840: fix display rules list in alphabetical order

--- src/workbench/components/validationrulesmanager.ts
+++ src/workbench/components/validationrulesmanager.ts
@@ -41,6 +41,7 @@ export class ValidationRulesManager {
     set current_module(val: string) {
         this._current_module = val;
         this.rules = this.metadata.getModuleValidations(val);
+        this.rules.sort((a, b) => a.name.localeCompare(b.name))
         this.copyRulesToBackup();
         this.current_rule = null;
     }
