d4d13762d4f18ccf91490b0780a6c9f671c3d9f6
Christian Knoll <christian.g.knoll@gmail.com>
2022-11-29 14:04:52 +0100

    add img tag to enable tags

--- src/systemcomponents/components/systemrichtexteditor.ts
+++ src/systemcomponents/components/systemrichtexteditor.ts
@@ -197,7 +197,7 @@ export class SystemRichTextEditor implements OnInit, OnDestroy, ControlValueAcce
                     },
                     toolbar: [],
                     htmlSupport: {
-                        allow: this.generateHtmlTagsAllowAttributes(['div', 'span', 'table', 'p', 'h1', 'h2', 'h3', 'h4', 'input', 'fieldset', 'button', 'label', 'textarea', 'select', 'option', 'optgroup'])
+                        allow: this.generateHtmlTagsAllowAttributes(['div', 'img', 'span', 'table', 'p', 'h1', 'h2', 'h3', 'h4', 'input', 'fieldset', 'button', 'label', 'textarea', 'select', 'option', 'optgroup'])
                         // hr
                     },
                     autosave: {
