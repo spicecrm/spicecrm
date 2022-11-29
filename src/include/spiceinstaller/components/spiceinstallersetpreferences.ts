e126b08f65a3d7d7617ba0b5e19840fb319f4c3a
strombo <andreas@gloeckl.name>
2022-11-29 11:39:52 +0100

    CR1000791, part 2: installer now with preferences (2)

--- src/include/spiceinstaller/components/spiceinstallersetpreferences.ts
+++ src/include/spiceinstaller/components/spiceinstallersetpreferences.ts
@@ -8,7 +8,7 @@ import {Router} from '@angular/router';
 import {configurationService} from '../../../services/configuration.service';
 import {toast} from '../../../services/toast.service';
 import {backend} from "../../../services/backend.service";
-import {spiceinstaller} from "../services/spiceinstaller.service";
+import { spiceinstaller, stepObject } from "../services/spiceinstaller.service";
 
 /** @ignore */
 declare var moment: any;
@@ -17,8 +17,9 @@ declare var moment: any;
     selector: 'spice-installer-set-preferences',
     templateUrl: '../templates/spiceinstallersetpreferences.html'
 })
-
 export class SpiceInstallerSetPreferences {
+
+    @Input() public selfStep: stepObject;
 
     public languages: any = [];
     public loading = false;
@@ -131,7 +132,7 @@ export class SpiceInstallerSetPreferences {
 
         // pre-set language by browser/system language
         let browserLanguage = navigator.language;
-        if ( this.browserLanguages[browserLanguage] ) this.spiceinstaller.configObject.preferences.language = this.browserLanguages[browserLanguage];
+        if ( this.browserLanguages[browserLanguage] ) this.spiceinstaller.configObject.language = this.browserLanguages[browserLanguage];
 
         // pre-set timezone by guessing it with moment
         this.spiceinstaller.configObject.preferences.timezone = moment.tz.guess();
@@ -146,6 +147,12 @@ export class SpiceInstallerSetPreferences {
             this.languages = result.languages;
         });
 
+        this.spiceinstaller.jumpSubject.subscribe( fromTo => {
+            if ( fromTo.from === this.selfStep ) {
+                if ( this.selfStep.completed || fromTo.to?.pos < this.selfStep.pos ) this.spiceinstaller.jump( fromTo.to );
+                else this.setPreferences();
+            }
+        });
     }
 
 
@@ -168,8 +175,7 @@ export class SpiceInstallerSetPreferences {
      */
     public setPreferences() {
         this.spiceinstaller.selectedStep.completed = true;
-        this.spiceinstaller.steps[6] = this.spiceinstaller.selectedStep;
-        this.spiceinstaller.next(this.spiceinstaller.steps[6]);
+        this.spiceinstaller.jumpSubject.next({ from: this.selfStep, to: this.selfStep.next })
     }
 
     public preSetPreferences() {
