70f5a5a1b473dab27b469a9946a74705f5658ea8
Christian Knoll <christian.g.knoll@gmail.com>
2022-12-05 11:35:10 +0100

    ck5 autosave option readded cleanup tags

--- src/modules/emails/components/emailreplymodal.ts
+++ src/modules/emails/components/emailreplymodal.ts
@@ -123,7 +123,7 @@ export class EmailReplyModal implements OnInit {
 
         let historytext = "";
         historytext += "<br><br>";
-        historytext += "<div spicecrm_reply_quote='' class='spicecrm_reply_quote'>";
+        historytext += "<div data-spice-reply-quote='' class='spicecrm_reply_quote'>";
         historytext += "<div dir='ltr' class='crm_attr'>";
         historytext += "<b>" + this.language.getLabel('LBL_FROM') + ":</b> <a href='mailto:" + this.parent.getField('from_addr') + "'>" + this.parent.getField('from_addr') + "</a>";
         historytext += "<br>";
@@ -137,7 +137,7 @@ export class EmailReplyModal implements OnInit {
 
         historytext += '<blockquote class="crm_quote" style="margin:0px 0px 0px 0.8ex;border-left:1px solid rgb(204,204,204);padding-left:1ex">';
 
-        const body: string = this.parent.getField('body').replace('data-signature=""', '').replace('spicecrm_temp_quote=""', '');
+        const body: string = this.parent.getField('body').replace('data-signature=""', '').replace('data-spice-temp-quote=""', '');
 
         if (body.startsWith('<html>')) {
             const containerDiv = document.createElement('html');
