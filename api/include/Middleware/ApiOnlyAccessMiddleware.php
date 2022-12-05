c30efea1595742ab43e6d2ea58342d596b8bbcf1
Christian Knoll <christian.g.knoll@gmail.com>
2022-12-05 09:33:03 +0100

    CR1000849 - apiOnly validation in REST Endpoints

---
+++ api/include/Middleware/ApiOnlyAccessMiddleware.php
@@ -0,0 +1,17 @@
+<?php
+namespace SpiceCRM\includes\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use SpiceCRM\includes\authentication\AuthenticationController;
+use SpiceCRM\includes\ErrorHandlers\ForbiddenException;
+
+class APIOnlyAccessMiddleware
+{
+    public function __invoke(Request $request, RequestHandler $handler) {
+        if (!AuthenticationController::getInstance()->is_api_user) {
+            throw new ForbiddenException('API access only.');
+        }
+        return $handler->handle($request);
+    }
+}
