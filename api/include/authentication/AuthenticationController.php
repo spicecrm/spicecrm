b1ec7abd2dce771490ae1817639f3fc372a5ca2e
maretval <valerie.maret@twentyreasons.com>
2022-12-12 12:49:32 +0100

    AuthenticationController: do not checkPasswordExpire when external_auth_only is set

--- api/include/authentication/AuthenticationController.php
+++ api/include/authentication/AuthenticationController.php
@@ -402,7 +402,7 @@ class AuthenticationController
      */
     private function checkPasswordExpire(User $userObj)
     {
-        if (( $userObj->system_generated_password or $userObj->hasExpiredPassword() ) and !$userObj->is_api_user ) {
+        if (( $userObj->system_generated_password || $userObj->hasExpiredPassword() ) && !$userObj->is_api_user && !$userObj->external_auth_only) {
             $necessaryLabels = LanguageManager::getSpecificLabels( SpiceConfig::getInstance()->config['default_language'] ?: 'en_us', [
                 'LBL_CANCEL','LBL_CHANGE_PASSWORD', 'LBL_NEW_PWD', 'LBL_NEW_PWD_REPEATED', 'LBL_PWD_GUIDELINE', 'LBL_SET_PASSWORD',
                 'LBL_ONE_LOWERCASE', 'LBL_ONE_UPPERCASE', 'LBL_ONE_SPECIALCHAR', 'LBL_ONE_DIGIT', 'LBL_MIN_LENGTH', 'MSG_PWD_NOT_LEGAL',
