c4dc367ee59faa84c88b7e644d98e70a2d7c61a0
Christian Knoll <christian.g.knoll@gmail.com>
2022-12-27 09:52:50 +0100

    cleanup removal of localization

--- api/include/SugarObjects/templates/person/Person.php
+++ api/include/SugarObjects/templates/person/Person.php
@@ -77,54 +77,8 @@ class Person extends Basic
      */
     public function _create_proper_name_field()
     {
-        global $locale, $app_list_strings;
-        if (empty($locale)) {
-            $locale = new Localization();
-        }
-
-        // Bug# 46125 - make first name, last name, salutation and title of Contacts respect field level ACLs
-        $first_name = "";
-        $last_name = "";
-        $salutation = "";
-        $title = "";
-
-        // first name has at least read access
-        $first_name = $this->first_name;
-
-        // last name has at least read access
-        $last_name = $this->last_name;
-
-
-        // salutation has at least read access
-        if (isset($this->field_defs['salutation']['options'])
-            && isset($app_list_strings[$this->field_defs['salutation']['options']])
-            && isset($app_list_strings[$this->field_defs['salutation']['options']][$this->salutation])) {
-
-            $salutation = $app_list_strings[$this->field_defs['salutation']['options']][$this->salutation];
-        } // if
-
-        // last name has at least read access
-        $title = $this->title;
-
-        // Corner Case:
-        // Both first name and last name cannot be empty, at least one must be shown
-        // In that case, we can ignore field level ACL and just display last name...
-        // In the ACL field level access settings, last_name cannot be set to "none"
-        if (empty($first_name) && empty($last_name)) {
-            $full_name = $locale->getLocaleFormattedName("", $last_name, $salutation, $title);
-        } else {
-            if ($this->createLocaleFormattedName) {
-                $full_name = $locale->getLocaleFormattedName($first_name, $last_name, $salutation, $title);
-            } else {
-                $full_name = $locale->getLocaleFormattedName($first_name, $last_name);
-            }
-        }
-
-        $this->name = $full_name;
-        $this->full_name = $full_name; //used by campaigns
+        return $this->first_name ? "{$this->first_name} {$this->last_name}" : $this->last_name;
     }
-
-
 
     /**
      * handle saving/adding the primary email address
