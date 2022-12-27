51a8847c2d7a9524f32346a2e85d966874ad99bc
Christian Knoll <christian.g.knoll@gmail.com>
2022-12-27 09:47:12 +0100

    minor cleanup

--- api/data/SpiceBean.php
+++ api/data/SpiceBean.php
@@ -455,57 +455,6 @@ class SpiceBean
     }
 
 
-    /**
-     * This function is designed to cache references to field arrays that were previously stored in the
-     * bean files and have since been moved to separate files. Was previously in include/CacheHandler.php
-     *
-     * @param $module_dir string the module directory
-     * @param $module string the name of the module
-     * @param $key string the type of field array we are referencing, i.e. list_fields, column_fields, required_fields
-     * *@deprecated
-     */
-    private function _loadCachedArray(
-        $module_dir, $module, $key
-    )
-    {
-        static $moduleDefs = [];
-
-        $fileName = 'field_arrays.php';
-
-        $cache_key = "load_cached_array.$module_dir.$module.$key";
-        $result = SugarCache::sugar_cache_retrieve($cache_key);
-        if (!empty($result)) {
-            // Use SugarCache::EXTERNAL_CACHE_NULL_VALUE to store null values in the cache.
-            if ($result == SugarCache::EXTERNAL_CACHE_NULL_VALUE) {
-                return null;
-            }
-
-            return $result;
-        }
-
-        if (file_exists('modules/' . $module_dir . '/' . $fileName)) {
-            // If the data was not loaded, try loading again....
-            if (!isset($moduleDefs[$module])) {
-                include('modules/' . $module_dir . '/' . $fileName);
-                $moduleDefs[$module] = $fields_array;
-            }
-            // Now that we have tried loading, make sure it was loaded
-            if (empty($moduleDefs[$module]) || empty($moduleDefs[$module][$module][$key])) {
-                // It was not loaded....  Fail.  Cache null to prevent future repeats of this calculation
-                SugarCache::sugar_cache_put($cache_key, SugarCache::EXTERNAL_CACHE_NULL_VALUE);
-                return null;
-            }
-
-            // It has been loaded, cache the result.
-            SugarCache::sugar_cache_put($cache_key, $moduleDefs[$module][$module][$key]);
-            return $moduleDefs[$module][$module][$key];
-        }
-
-        // It was not loaded....  Fail.  Cache null to prevent future repeats of this calculation
-        SugarCache::sugar_cache_put($cache_key, SugarCache::EXTERNAL_CACHE_NULL_VALUE);
-        return null;
-    }
-
     function bean_implements($interface)
     {
         // by default return ACL true
