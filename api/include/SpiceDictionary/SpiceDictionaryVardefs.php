4dbe003196956f9e9349c27521fef9de0d2a494c
Christian Knoll <christian.g.knoll@gmail.com>
2023-01-05 18:54:19 +0100

    dictionary Manager OCI8 fix

--- api/include/SpiceDictionary/SpiceDictionaryVardefs.php
+++ api/include/SpiceDictionary/SpiceDictionaryVardefs.php
@@ -1681,7 +1681,10 @@ AND sysdi.deleted = 0 AND sysdi.status = 'a'
         }
 
         if(is_array($sqlInserts) && count($sqlInserts) > 0){
-            $sqls[] = "INSERT INTO sysdictionaryfields (" . implode(', ', $insertColumns) . ") VALUES ".implode(', ', $sqlInserts).";";
+            foreach ($sqlInserts as $sqlInsert){
+                $sqls[] = "INSERT INTO sysdictionaryfields (" . implode(', ', $insertColumns) . ") VALUES $sqlInsert";
+            }
+            //$sqls[] = "INSERT INTO sysdictionaryfields (" . implode(', ', $insertColumns) . ") VALUES ".implode(', ', $sqlInserts).";";
         }
 
         // reset temp arrays
