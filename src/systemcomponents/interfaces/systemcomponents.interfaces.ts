eada5f71edb562c069e35279394edefbc95ae361
Maria Zakharova <maria.zakharova@twentyreasons.com>
2023-01-10 16:26:44 +0100

    CR1000872: SystemTree: @input customButtonsContainer added to accept item custom actions template. Code cleanup.

--- src/systemcomponents/interfaces/systemcomponents.interfaces.ts
+++ src/systemcomponents/interfaces/systemcomponents.interfaces.ts
@@ -19,3 +19,27 @@ export interface InputRadioOptionI {
     /** disabled value for the input */
     disabled?: boolean;
 }
+
+export interface SystemTreeItemI {
+    id: string;
+    name: string;
+    parent_id?: string;
+    parent_sequence?: string;
+    clickable?: boolean;
+    icon?: string;
+    systemTreeDefs?: {
+        icon?: string,
+        expanded?: boolean,
+        clickable?: boolean,
+        level?: number,
+        isSelected?: boolean,
+        hasChildren?: boolean,
+    };
+}
+
+export interface SystemTreeConfigI {
+    draggable?: boolean;
+    canadd?: boolean;
+    expandall?: boolean;
+    collapsible?: boolean;
+}
