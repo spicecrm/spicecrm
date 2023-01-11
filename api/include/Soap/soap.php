a3a73cc3fb0792867814320e5d79521065e013aa
Christian Knoll <christian.g.knoll@gmail.com>
2023-01-11 16:28:14 +0100

    add WSDL generation documentation re style and literal

--- api/include/Soap/soap.php
+++ api/include/Soap/soap.php
@@ -7,6 +7,10 @@ use SpiceCRM\includes\UploadStream;
 use SpiceCRM\includes\SugarObjects\SpiceModules;
 use SpiceCRM\includes\SpiceDictionary\SpiceDictionaryHandler;
 
+/**
+ * generate wssdl with <url>/apoi/soap?wsdl?style=rpc|document&use=encoded|literal
+ */
+
 // ensure error reporting and display_errors is set properly
 error_reporting(E_ERROR);
 ini_set('display_errors', 0);
