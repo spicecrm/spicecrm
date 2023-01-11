4a5f460ff167f901b00ebfc8c0f9b0f2e7f76201
Maria Zakharova <maria.zakharova@twentyreasons.com>
2023-01-11 10:50:07 +0100

    CR1000871: Fix null value after installation

--- src/modules/home/components/home.ts
+++ src/modules/home/components/home.ts
@@ -19,7 +19,7 @@ export class Home {
         // fallback to value set in active role
         if(!this.hasDashboardSet){
             let activeRole = this.metadata.getActiveRole();
-            if(activeRole.default_dashboardset.length > 0){
+            if(activeRole.default_dashboardset){
                 this.hasDashboardSet = true;
             }
         }
