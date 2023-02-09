d0dca42bc2de8680f8c03416724d77e8269fed9c
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2023-02-09 15:45:18 +0100

    CR1000883: Targets Manager: Labels adjustments. ACL edit on button added

--- src/modules/campaigns/components/campaigntasktargetsmanagerbutton.ts
+++ src/modules/campaigns/components/campaigntasktargetsmanagerbutton.ts
@@ -7,6 +7,8 @@ import {
 import {Params, Router} from '@angular/router';
 import {model} from '../../../services/model.service';
 import {navigationtab} from '../../../services/navigationtab.service';
+import {metadata} from "../../../services/metadata.service";
+import {layout} from "../../../services/layout.service";
 
 /**
  * @ignore
@@ -21,7 +23,18 @@ declare var moment: any;
     templateUrl: '../templates/campaigntasktargetsmanagerbutton.html',
 })
 export class CampaignTaskTargetsManagerButton {
-    constructor(public model: model, public router: Router, @Optional() public navigationtab: navigationtab) {
+    constructor(public model: model,
+                public router: Router,
+                public metadata: metadata,
+                public layout: layout,
+                @Optional() public navigationtab: navigationtab) {
+    }
+
+    /**
+     * disabled if the user does not have acl edit on target lists or on small screen
+     */
+    get disabled(): boolean {
+        return !this.metadata.checkModuleAcl('ProspectLists', 'edit') || this.layout.screenwidth == 'small';
     }
 
     /**
