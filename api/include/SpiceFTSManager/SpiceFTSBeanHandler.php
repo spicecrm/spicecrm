97243cd777c8bd41826f476b8863df5f8b62455b
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2023-02-14 13:42:12 +0100

    Revert "SpiceFTSBeanHandler: Index Feld '_phone' change type from keyword to text."
    This reverts commit d732ac94f0e02e36ed42afd92ffb6f4e3fa0ffd2.

--- api/include/SpiceFTSManager/SpiceFTSBeanHandler.php
+++ api/include/SpiceFTSManager/SpiceFTSBeanHandler.php
@@ -461,7 +461,7 @@ class SpiceFTSBeanHandler
         // if we have a phone searc enabled add a _phone field
         if ($this->indexSettings['phonesearch']) {
             $properties['_phone'] = [
-                'type' => 'text',
+                'type' => 'keyword',
                 'index' => true
             ];
         }
