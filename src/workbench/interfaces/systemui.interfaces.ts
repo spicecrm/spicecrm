24136392b100e4ab8d8d8b9b648c851a331ae951
Maria Zakharova <maria.zakharova@twentyreasons.com>
2023-03-09 16:22:43 +0100

    CR1000853: Workbench Interface to administrate hooks

--- src/workbench/interfaces/systemui.interfaces.ts
+++ src/workbench/interfaces/systemui.interfaces.ts
@@ -14,10 +14,10 @@ export interface RoleI {
     name: string;
     label: string;
     icon: string;
-    systemdefault: 1|0;
-    portaldefault: 1|0;
-    showsearch: 1|0;
-    showfavorites: 1|0;
+    systemdefault: 1 | 0;
+    portaldefault: 1 | 0;
+    showsearch: 1 | 0;
+    showfavorites: 1 | 0;
     description: string;
     default_dashboard: string;
     default_dashboardset: string,
@@ -26,4 +26,20 @@ export interface RoleI {
     scope: 'custom' | 'global';
     scope_icon: string;
     systemTreeDefs: object;
+}
+
+export interface LogicHookI {
+    id: string;
+    module: string;
+    // event: string;
+    event: 'select'|'before_relationship_add' | 'after_relationship_add' | 'before_relationship_delete' | 'after_relationship_delete' | 'before_save' | 'after_save' | 'before_retrieve' | 'after_retrieve' | 'before_delete' | 'after_delete' | 'before_restore' | 'after_restore' | 'after_save_completed' | 'before_logout';
+    hook_index: number;
+    hook_include: string;
+    hook_class: string;
+    hook_method: string;
+    hook_active: 1 | 0;
+    description: string;
+    version: string;
+    package: string;
+    type: string;
 }
