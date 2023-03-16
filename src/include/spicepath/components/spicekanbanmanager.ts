021a7f5a326b1fcce17c710a3d3aef7923a0846c
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2023-03-16 09:58:47 +0100

    CR1000915: SpiceKanban Manager initial commit

---
+++ src/include/spicepath/components/spicekanbanmanager.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'spice-kanban-manager',
+    templateUrl: '../templates/spicekanbanmanager.html'
+})
+
+export class SpiceKanbanManager {
+
+}
