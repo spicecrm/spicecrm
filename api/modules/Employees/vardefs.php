fd2d93a63727c1d5299791b5e53208ca08895493
Niklas Schmies <Nschmies@web.de>
2023-03-21 09:19:45 +0100

    CR1000914: Employees vardefs cleanup

--- api/modules/Employees/vardefs.php
+++ api/modules/Employees/vardefs.php
@@ -6,24 +6,6 @@ SpiceDictionaryHandler::getInstance()->dictionary['Employee'] = [
     'table' => 'employees',
     'audited' =>  true,
     'fields' => [
-        'user_id' => [
-            'name' => 'user_id',
-            'vname' => 'LBL_USER_ID',
-            'type' => 'id',
-        ],
-        'user_name' => [
-            'name' => 'user_name',
-            'rname' => 'name',
-            'id_name' => 'user_id',
-            'vname' => 'LBL_USER',
-            'type' => 'relate',
-            'table' => 'users',
-            'module' => 'Users',
-            'dbType' => 'varchar',
-            'link' => 'users',
-            'len' => 255,
-            'source' => 'non-db'
-        ],
         'users' => [
             'name' => 'users',
             'vname' => 'LBL_USERS',
