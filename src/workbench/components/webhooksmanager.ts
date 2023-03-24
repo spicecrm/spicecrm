ffec712aaa4dd5acdb78807cf11c82001d26d3cf
maretval <valerie.maret@twentyreasons.com>
2023-03-24 18:23:29 +0100

    CR1000853: fix allWebHooks()

--- src/workbench/components/webhooksmanager.ts
+++ src/workbench/components/webhooksmanager.ts
@@ -62,7 +62,7 @@ export class WebHooksManager {
         if (this.hooksManager._module == '*') {
             return this.webHooks;
         }
-        return this.webHooks.filter(webHook =>webHook.module == this.hooksManager.module );
+        return this.webHooks.filter(webHook =>webHook.module == this.hooksManager._module );
     }
 
     /**
