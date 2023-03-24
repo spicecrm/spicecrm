b6df57f5a758b9ecffcc090e63a0d3f7f157f7e5
maretval <valerie.maret@twentyreasons.com>
2023-03-24 18:26:50 +0100

    CR1000853: merged class HooksManager

--- src/workbench/components/hooksmanager.ts
+++ src/workbench/components/hooksmanager.ts
@@ -4,6 +4,8 @@
 import {
     Component
 } from '@angular/core';
+import {metadata} from "../../services/metadata.service";
+
 
 @Component({
     selector: 'hooks-manager',
@@ -12,8 +14,30 @@ import {
 export class HooksManager {
     public hook: any;
     public activeTab: string = 'logic';
+    public _module: string = '*';
+    public modules: string[];
 
     public hooksManager =
         [{label: 'Logic Hooks', value: 'logic'}, {label: 'Web Hooks', value: 'web'}];
 
+    constructor(public metadata: metadata,) {
+        this.modules = this.metadata.getModules();
+        this.modules.sort();
+    }
+    /**
+     * get module
+     */
+    get module() {
+        return this._module;
+    }
+
+    /**
+     * set module
+     * @param module
+     */
+    set module(module) {
+        if (module != this._module) {
+            this._module = module;
+        }
+    }
 }
