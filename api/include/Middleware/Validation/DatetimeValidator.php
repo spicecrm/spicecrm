09fd14d7370e6da29342c8a7e5d19e8fa69c9d12
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2023-03-24 12:28:07 +0100

    datetime validation: removed redundant check on errors since createFromFormat returns false on failure

--- api/include/Middleware/Validation/DatetimeValidator.php
+++ api/include/Middleware/Validation/DatetimeValidator.php
@@ -15,7 +15,7 @@ class DatetimeValidator extends Validator
 {
     public function validate(): bool {
         $dateTime = DateTime::createFromFormat("Y-m-d H:i:s", $this->paramValue);
-        if (parent::isRequired() && ($dateTime === false || $dateTime::getLastErrors() !== false)) {
+        if (parent::isRequired() && !$dateTime) {
             throw new ValidationException(
                 "Invalid date time format",
                 null,
