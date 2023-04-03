fe7837adad1486593f0939c7356ba59361e77b8a
natoku <natalia.okutansoy@twentyreasons.com>
2023-04-03 09:33:10 +0200

    CR1000915: added spicekanbanmanagerdetails and spicekanbanmanagerchecks components

---
+++ src/include/spicepath/components/spicekanbanmanagerchecks.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'spice-kanban-manager-checks',
+    templateUrl: '../templates/spicekanbanmanagerchecks.html'
+})
+
+export class SpiceKanbanManagerChecks {
+
+}
