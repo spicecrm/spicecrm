8f4b920f8245a6d1ae6bbb55685fb9bf7670233b
Maria Zakharova <maria.zakharova@twentyreasons.com>
2023-04-03 12:21:06 +0200

    CR1000915: Added drag and drop functionality for mock data

--- src/include/spicepath/components/spicekanbanmanagerlist.ts
+++ src/include/spicepath/components/spicekanbanmanagerlist.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
+import {backend} from "../../../services/backend.service";
 
 @Component({
     selector: 'spice-kanban-manager-list',
@@ -7,4 +9,30 @@ import {Component} from '@angular/core';
 
 export class SpiceKanbanManagerList {
     public enumValues = Array.from({length: 50}, (_,i) => 'Item ' + i);
+
+
+    constructor(public backend:backend) {
+    }
+    /**
+     * drag and drop for beanguidestages
+     */
+    public drop(event: CdkDragDrop<any[]>) {
+        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+
+        this.saveSequence();
+    }
+
+    /**
+     * saving the new sequence of beanguidestages
+     */
+    public saveSequence() {
+
+        this.enumValues.forEach((entry, index) => {
+            // entry.stage_sequence = index;
+        });
+
+        this.backend.postRequest(`configuration/configurator/spicebeanguidestages`, null);
+
+    }
+
 }
