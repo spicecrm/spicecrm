a76492e6e6a3af00b0c25a252cea22c453e65876
maretval <valerie.maret@twentyreasons.com>
2023-04-03 22:33:13 +0200

    packaging

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2023.01.001.1680601358858",
+    buildNumber: "2023.01.001.1680602236455",
     copyright: "© 2015 -  2023 aac services k.s. All rights reserved."
     }
