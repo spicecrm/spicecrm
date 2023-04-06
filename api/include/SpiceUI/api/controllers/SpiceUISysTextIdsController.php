f34b8e24bee17d9f9075d3e2e867e49701bc6137
natoku <natalia.okutansoy@twentyreasons.com>
2023-04-06 18:11:29 +0200

    CR1000740: added systemtextconfiguratormodal to project

--- api/include/SpiceUI/api/controllers/SpiceUISysTextIdsController.php
+++ api/include/SpiceUI/api/controllers/SpiceUISysTextIdsController.php
@@ -2,7 +2,10 @@
 
 namespace SpiceCRM\includes\SpiceUI\api\controllers;
 
+use Psr\Http\Message\ServerRequestInterface as Request;
 use SpiceCRM\includes\database\DBManagerFactory;
+use SpiceCRM\includes\SpiceSlim\SpiceResponse as Response;
+use SpiceCRM\includes\utils\SpiceUtils;
 
 class SpiceUISysTextIdsController {
 
@@ -18,5 +21,32 @@ class SpiceUISysTextIdsController {
         while ($row = $db->fetchByAssoc($queryRes)) $list[$row['text_id']] = $row;
 
         return $list;
+    }
+
+    /**
+     * @param Request $req
+     * @param Response $res
+     * @param array $args
+     * @return Response array with systext id and status
+     */
+    function addSysText(Request $req, Response $res, array $args): Response {
+        $body = $req->getParsedBody();
+        $db = DBManagerFactory::getInstance();
+
+        // id of the entry in the systextids table
+        $sysTextId =  SpiceUtils::createGuid();
+
+        // inserting data to systextids table
+        $sysTextIdsQuery = "INSERT into systextids (id, text_id, name, label) values ('$sysTextId','{$body['textId']}', '{$body['name']}', '{$body['label']}')";
+        $db->query($sysTextIdsQuery);
+
+        // if successful add the module and text_id
+        if($sysTextIdsQuery) {
+            $sysTextModulesId =  SpiceUtils::createGuid();
+            $sysTextModulesQuery = "INSERT into systextids_modules (id, text_id, module) values ('$sysTextModulesId','{$body['textId']}', '{$body['module']}')";
+            $db->query($sysTextModulesQuery);
+        }
+
+        return $res->withJson(['success' => true, 'textId' => $body['textId']]);
     }
 }
