d7bf86fec5ca69885dce7cf3a8e973cfd8c4291c
maretval <valerie.maret@twentyreasons.com>
2023-04-13 19:42:13 +0200

    KReporter link display: clean up labels

--- api/modules/Opportunities/vardefs.php
+++ api/modules/Opportunities/vardefs.php
@@ -432,27 +432,6 @@ SpiceDictionaryHandler::getInstance()->dictionary['Opportunity'] = [
             'source' => 'non-db',
             'vname' => 'LBL_LEADS',
         ],
-        'campaigns' => [
-            'name' => 'campaigns',
-            'type' => 'link',
-            'relationship' => 'opportunities_campaign',
-            'module' => 'CampaignLog',
-            'bean_name' => 'CampaignLog',
-            'source' => 'non-db',
-            'vname' => 'LBL_CAMPAIGNS',
-            'reportable' => false
-        ],
-        'campaign_link' => [
-            'name' => 'campaign_link',
-            'type' => 'link',
-            'relationship' => 'opportunities_campaign',
-            'vname' => 'LBL_CAMPAIGNS',
-            'link_type' => 'one',
-            'module' => 'Campaigns',
-            'bean_name' => 'Campaign',
-            'source' => 'non-db',
-            'reportable' => false
-        ],
         'currencies' => [
             'name' => 'currencies',
             'type' => 'link',
