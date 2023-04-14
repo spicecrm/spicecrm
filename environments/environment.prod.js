c3a51c130177d6dcd3fbfb92834aad3f15e11102
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2023-04-14 14:34:17 +0200

    packaging

--- environments/environment.prod.js
+++ environments/environment.prod.js
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    buildNumber: "2023.01.001.1679494743723",
+    buildNumber: "2023.01.001.1681475482073",
     copyright: "© 2015 -  2023 aac services k.s. All rights reserved."
 };
 //# sourceMappingURL=environment.prod.js.map
