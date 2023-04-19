1e915636bc3a97e1050f025dd651ecff7fd40220
maretval <valerie.maret@twentyreasons.com>
2023-04-19 18:15:46 +0200

    SystemDeploymentPackageSource to release

--- api/modules/SystemDeploymentPackages/SystemDeploymentPackageSource.php
+++ api/modules/SystemDeploymentPackages/SystemDeploymentPackageSource.php
@@ -34,7 +34,7 @@ namespace SpiceCRM\modules\SystemDeploymentPackages;
 class SystemDeploymentPackageSource
 {
     /** @var string used when no entry is set in sysuipackagerepositories table */
-    public static $public_source = 'https://spicecrmstage.spicecrm.io/api';
+    public static $public_source = 'https://spicecrmrelease.spicecrm.io/api';
 
 
     /**
