cd8d2c79d91a982aec8d6e48acb88143a54a89d2
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2023-04-20 10:48:14 +0200

    CR1000740: KanbankManagerService created.

--- src/include/spicepath/components/spicekanbanmanagerlist.ts
+++ src/include/spicepath/components/spicekanbanmanagerlist.ts
@@ -1,18 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
 import {backend} from "../../../services/backend.service";
+import {KanbanManagerService} from "../services/kanbanmanager.service";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'spice-kanban-manager-list',
     templateUrl: '../templates/spicekanbanmanagerlist.html'
 })
 
-export class SpiceKanbanManagerList {
-    public enumValues = Array.from({length: 50}, (_,i) => 'Item ' + i);
+export class SpiceKanbanManagerList implements OnDestroy {
+    public enumValues = Array.from({length: 50}, (_, i) => 'Item ' + i);
+    private subscription: Subscription = new Subscription();
 
-
-    constructor(public backend:backend) {
+    constructor(public backend: backend,
+                public kanbanManagerService: KanbanManagerService) {
+        this.loadItems();
+        this.subscribeToSelectionChange();
     }
+
     /**
      * drag and drop for beanguidestages
      */
@@ -35,4 +41,19 @@ export class SpiceKanbanManagerList {
 
     }
 
+    public ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
+    private loadItems() {
+        // todo: load beanguidestages from backend
+    }
+
+    private subscribeToSelectionChange() {
+        this.subscription = this.kanbanManagerService.selectedBeanGuide$.subscribe({
+            next: () => {
+                // todo: load beanguidestages from backend
+            }
+        });
+    }
 }
