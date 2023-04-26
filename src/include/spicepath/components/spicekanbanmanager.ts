cc60e44f6d144e27100cee5d76cdee17b78f9d9b
vragolancic <ivicanikolic.mail@gmail.com>
2023-04-26 15:16:13 +0200

    CR1000915: add new kan ban manager modal

--- src/include/spicepath/components/spicekanbanmanager.ts
+++ src/include/spicepath/components/spicekanbanmanager.ts
@@ -1,6 +1,8 @@
 import {Component, Injector} from '@angular/core';
 
 import {modal} from '../../../services/modal.service';
+import {metadata} from '../../../services/metadata.service';
+
 import {KanbanManagerService} from "../services/kanbanmanager.service";
 
 @Component({
@@ -12,17 +14,25 @@ import {KanbanManagerService} from "../services/kanbanmanager.service";
 export class SpiceKanbanManager {
 
 
+    /**
+     * module name
+     */
+    public moduleName: string = '';
+
     public preview: boolean = false;
 
     constructor(
         public modal: modal,
         public injector: Injector,
+        public metadata: metadata,
         public kanbanManagerService: KanbanManagerService
     ) {
     }
 
+
+
     openAddModal() {
-        this.modal.openModal('SpiceKanbanManagerAddModal', true, this.injector);
+        this.modal.openModal('SpiceKanbanManagerAddModal', true, this.injector).subscribe(modalRef  => modalRef.instance.moduleName = this.moduleName);
     }
 
     public togglePreview () {
--- src/include/spicepath/components/spicekanbanmanageraddmodal.ts
+++ src/include/spicepath/components/spicekanbanmanageraddmodal.ts
@@ -1,4 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, Injector} from '@angular/core';
+
+import {modal} from '../../../services/modal.service';
+import {metadata} from '../../../services/metadata.service';
+import {backend} from '../../../services/backend.service';
+import {toast} from "../../../services/toast.service";
+import {modelutilities} from '../../../services/modelutilities.service';
+
 
 @Component({
     selector: 'spice-kanban-manager-add-modal',
@@ -12,6 +19,83 @@ export class SpiceKanbanManagerAddModal {
      */
     public self: any;
 
+    /**
+     * module name
+     */
+    public moduleName: string = '';
+
+    /**
+     * module fields array
+     */
+    public moduleFields: any[] = [];
+
+    /**
+     *  selected field
+     */
+    public fieldName: string = '';
+
+    /**
+     * Kan Ban name field
+     */
+    public kanbanName: string = '';
+
+    constructor(
+        public modal: modal,
+        public metadata: metadata,
+        public backend: backend,
+        public toast: toast,
+        public modelutilities: modelutilities
+    ) {
+    }
+
+
+    public ngOnInit() {
+        if (this.moduleName && this.moduleName != '*') {
+            this.moduleFields = this.metadata.getModuleFields(this.moduleName);
+        }
+    }
+
+
+    /**
+     * Get fields from module
+     */
+    public getFieldNames() {
+        let fieldnames = [];
+
+        for (let fieldname in this.moduleFields) {
+            fieldnames.push(fieldname);
+        }
+
+        fieldnames.sort();
+        return fieldnames;
+    }
+
+    /**
+     * Add new Kan Ban in table spicebeanguides
+     */
+    public addKanban() {
+
+        let spinner = this.modal.await('LBL_SAVING');
+
+        let data = [{
+            id : this.modelutilities.generateGuid(),
+            module : this.moduleName,
+            status_field : this.fieldName,
+            name : this.kanbanName,
+        }];
+
+        this.backend.postRequest(`configuration/configurator/spicebeanguides`, null, {config: data}).subscribe({
+            next: () => {
+                spinner.emit(true);
+                this.toast.sendToast('LBL_SPICEBEANGUIDE_SAVED', "success");
+                this.close();
+            },
+            error: (err) => {
+                spinner.emit(true);
+                this.toast.sendToast('LBL_ERROR_SAVING_SPICEBEANGUIDE', "error", err);
+            }
+        });
+    }
 
     /**
      * closes the modal
@@ -19,5 +103,7 @@ export class SpiceKanbanManagerAddModal {
     public close() {
         this.self.destroy();
     }
+
+
 }
 
