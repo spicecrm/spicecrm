3029c93694610107fba23adb749aab7d4069ad6a
natoku <natalia.okutansoy@twentyreasons.com>
2023-05-03 12:37:09 +0200

    packaging

--- app/scripts.js
+++ app/scripts.js
@@ -4,9 +4,9 @@
  *
  *                     release: 2023.01.001
  *
- *                     date: 2023-04-19 17:23:57
+ *                     date: 2023-05-08 11:03:49
  *
- *                     build: 2023.01.001.1681917837156
+ *                     build: 2023.01.001.1683536629965
  *
  */
 "use strict";var __spreadArray=this&&this.__spreadArray||function(M,b,z){if(z||2===arguments.length)for(var e,p=0,o=b.length;p<o;p++)!e&&p in b||(e||(e=Array.prototype.slice.call(b,0,p)),e[p]=b[p]);return M.concat(e||Array.prototype.slice.call(b))};
--- app/vendor.js
+++ app/vendor.js
@@ -4,9 +4,9 @@
  *
  *                     release: 2023.01.001
  *
- *                     date: 2023-04-19 17:23:57
+ *                     date: 2023-05-08 11:03:49
  *
- *                     build: 2023.01.001.1681917837156
+ *                     build: 2023.01.001.1683536629965
  *
  */
 (self.webpackChunkcore=self.webpackChunkcore||[]).push([["vendor"],{5179:function(t){
