86834c0200de055348099ed50605ab34fb5b568c
Maria Zakharova <maria.zakharova@twentyreasons.com>
2023-05-09 11:50:07 +0200

    CR1000915: spice bean guide stages list

---
+++ src/include/spicepath/interfaces/kanbanmanager.interfaces.ts
@@ -0,0 +1,14 @@
+export interface SpiceBeanGuideStagesI {
+    id: string;
+    spicebeanguide_id: string;
+    stage: string;
+    secondary_stage: string;
+    stage_sequence: number;
+    stage_bucket: string;
+    stage_color: string;
+    stage_add_data: string;
+    stage_label: string;
+    stage_componentset: string;
+    not_in_kanban: number;
+    spicebeanguide_status: string;
+}
