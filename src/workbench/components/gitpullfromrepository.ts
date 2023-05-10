119b4940b82a0e092ed82a99410db397cb3d351e
Maria Zakharova <maria.zakharova@twentyreasons.com>
2023-05-10 10:27:14 +0200

    CR1000888: bugfix email login & token validation

--- src/workbench/components/gitpullfromrepository.ts
+++ src/workbench/components/gitpullfromrepository.ts
@@ -39,8 +39,7 @@ export class GitPullFromRepository {
      * Regex for password
      */
     public pwRegexp: RegExp = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})");
-   // public gitHubTokenRegexp: RegExp = new RegExp("^ghp_[a-zA-Z0-9]{36}$");
-   // public gitLabTokenRegexp: RegExp = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})");
+    public gitHubTokenRegexp: RegExp = new RegExp("^ghp_[a-zA-Z0-9]{36}$");
     public userRegexp: RegExp = new RegExp("^[a-zA-Z0-9-]+$");
     public emailRegexp: RegExp = new RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$");
 
@@ -53,7 +52,7 @@ export class GitPullFromRepository {
      */
     public loginCheck() {
         this.passwordCondition = this.loginDetails.password.length > 0 && this.pwRegexp.test(this.loginDetails.password);
-        this.tokenCondition = this.loginDetails.token.length > 0 // && this.gitHubTokenRegexp.test(this.loginDetails.token);
+        this.tokenCondition = this.loginDetails.token.length > 0  && (this.gitHubTokenRegexp.test(this.loginDetails.token)||this.pwRegexp.test(this.loginDetails.token));
         this.usernameCondition = this.userRegexp.test(this.loginDetails.username) || this.emailRegexp.test(this.loginDetails.username);
     }
 
