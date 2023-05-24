c0a1087dcfc816d4eb7944d58a3999db9488bf1b
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2023-05-24 10:02:14 +0200

    CR1000937: SpiceUIConfLoader: bug fix: fetch existing

--- api/include/SpiceUI/SpiceUIConfLoader.php
+++ api/include/SpiceUI/SpiceUIConfLoader.php
@@ -393,7 +393,7 @@ class SpiceUIConfLoader
                         }
                     } else{
                         // check if record is present
-                        if($dbResRow = $db->getOne("select * from $tb where id='{$decodeData['id']}'")){
+                        if($dbResRow = $db->getOne("select id from $tb where id='{$decodeData['id']}'")){
                             $skipInsert = true;
                         }
                     }
