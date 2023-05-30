3fc97bff3334764ff66e13d5106d9ed683c78232
Christian Knoll <christian.g.knoll@gmail.com>
2023-05-30 08:31:02 +0200

    CR1000995: render new salesdoc in new tab and not modal

--- src/modules/salesdocs/components/salesdocsaddbasics.ts
+++ src/modules/salesdocs/components/salesdocsaddbasics.ts
@@ -10,6 +10,7 @@ import {view} from "../../../services/view.service";
 import {modal} from "../../../services/modal.service";
 import {model} from "../../../services/model.service";
 import {configurationService} from '../../../services/configuration.service';
+import {navigation} from "../../../services/navigation.service";
 
 /**
  * renders a modal that allws picking the basic paramaters for the salesdoc when adding a new sales document
@@ -31,7 +32,7 @@ export class SalesDocsAddBasics {
      */
     public fieldset: string = '';
 
-    constructor(public metadata: metadata, public language: language, public view: view, public modal: modal, public injector: Injector, public model: model, public configuration: configurationService ) {
+    constructor(public metadata: metadata, public language: language, public view: view, public modal: modal, public injector: Injector, public model: model, public configuration: configurationService, public navigation: navigation ) {
         // set the basics for the view
         this.view.isEditable = true;
         this.view.setEditMode();
@@ -52,7 +53,25 @@ export class SalesDocsAddBasics {
      */
     public next() {
         this.self.destroy();
+        // open a new tab
+        this.navigation.addObjectTab({
+            path: 'module/:module/new/:id',
+            params: {module: this.model.module, id: this.model.id},
+            id: this.model.utils.generateGuid(),
+            active: true,
+            pinned: false,
+            enablesubtabs: false,
+            url: `module/${this.model.module}/new/${this.model.id}`,
+            tabdata: {
+                module: this.model.module,
+                id: this.model.id,
+                data: this.model.data
+            }
+        })
+
+
         // this.modal.openModal('SalesDocsAddMain', true, this.injector);
+        /*
         this.modal.openModal("ObjectEditModal", true, this.injector).subscribe(editModalRef => {
             editModalRef.instance.model.isNew = true;
             // Field "salesdocparty" in table "salesdoctypes" is either "B" (for Business) or "C" (for Customer).
@@ -61,6 +80,7 @@ export class SalesDocsAddBasics {
             let typeData = this.configuration.getData('salesdoctypes').find( typeRecord => typeRecord.name === salesdocType );
             editModalRef.instance.model.setField('salesdocparty', typeData.salesdocparty ? typeData.salesdocparty : 'C');
         });
+         */
     }
 
     /**
