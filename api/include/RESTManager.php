1e8bfe59ed7651aae7ab1b49f96106625b9dfaa8
strombo <andreas@gloeckl.name>
2023-05-31 10:16:31 +0200

    CRCR1000878, Part 1: Access to API via specific IP addresses

--- api/include/RESTManager.php
+++ api/include/RESTManager.php
@@ -61,6 +61,7 @@ use SpiceCRM\includes\utils\RESTRateLimiter;
 use SpiceCRM\includes\authentication\AuthenticationController;
 use SpiceCRM\modules\Contacts\Contact;
 use Throwable;
+use SpiceCRM\includes\Middleware\ipClientsMiddleware;
 
 class RESTManager
 {
@@ -518,6 +519,11 @@ class RESTManager
                 // add validation for API only
                 if (isset($route['options']['apiOnly']) && $route['options']['apiOnly'] === true) {
                     $routeObject->add(ApiOnlyAccessMiddleware::class);
+                }
+
+                // add validation for specific IP addresses only
+                if ( isset( $route['options']['ipClients'] ) and ( is_string( $route['options']['ipClients'] ) or is_array( $route['options']['ipClients'] ) or $route['options']['ipClients'] === true )) {
+                    $routeObject->add(ipClientsMiddleware::class);
                 }
 
                 if (isset($route['options']['moduleRoute']) && $route['options']['moduleRoute'] === true) {
