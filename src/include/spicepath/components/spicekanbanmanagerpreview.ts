7547faae1e89474d2a5ea205e89818073c387472
vragolancic <ivicanikolic.mail@gmail.com>
2023-06-16 15:39:38 +0200

    preview kanban fix

--- src/include/spicepath/components/spicekanbanmanagerpreview.ts
+++ src/include/spicepath/components/spicekanbanmanagerpreview.ts
@@ -1,39 +1,12 @@
-import {Component} from '@angular/core';
-
-import {modellist} from '../../../services/modellist.service';
-import {KanbanManagerService} from "../services/kanbanmanager.service";
+import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'spice-kanban-manager-preview',
     templateUrl: '../templates/spicekanbanmanagerpreview.html',
-    providers: [modellist]
 })
 
 export class SpiceKanbanManagerPreview{
 
-    public constructor(public modellist: modellist, public kanbanManagerService: KanbanManagerService) {
-    }
-    /**
-     * returns the name for the stage to be displayed
-     *
-     * @param stagedata
-     */
-    // public getStageLabel(stagedata) {
-    //     if (stagedata.stage_label) {
-    //         return stagedata.stage_label;
-    //     } else {
-    //         return stagedata.stage_name;
-    //     }
-    // }
-
-    /**
-     * Get active stages
-     */
-    get active(){
-        return this.kanbanManagerService.stages
-            .filter(dis=>dis.not_in_kanban == 0 && dis.spicebeanguide_id == this.kanbanManagerService.selectedBeanGuide.id);
-    }
-
-
+    @Input() active: any;
 
 }
