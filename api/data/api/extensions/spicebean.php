f752e82be7df62cac0d8b807c20269ca39297f45
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2023-06-22 16:21:29 +0200

    CR1001021: New API/module/{beanName}/find to find bean by string fields

--- api/data/api/extensions/spicebean.php
+++ api/data/api/extensions/spicebean.php
@@ -327,6 +327,29 @@ $routes = [
     ],
     [
         'method'      => 'get',
+        'route'       => '/module/{beanName}/find',
+        'class'       => SpiceBeanController::class,
+        'function'    => 'findBeanByStringFields',
+        'description' => 'Find a bean based on the provided retrieve fields',
+        'options'     => ['noAuth' => false, 'adminOnly' => false, 'moduleRoute' => true, 'validate' => true],
+        'parameters'  => [
+            'beanName'        => [
+                'in'          => 'path',
+                'type'        => ValidationMiddleware::TYPE_MODULE,
+                'required'    => true,
+                'description' => 'The name of the module',
+            ],
+            'retrieveFields' => [
+                'in' => 'query',
+                'type' => ValidationMiddleware::TYPE_OBJECT,
+                'description' => 'object with retrieve fields',
+                'example' => '{active: true}',
+                'required'    => true,
+            ]
+        ],
+    ],
+    [
+        'method'      => 'get',
         'route'       => '/module/{beanName}/{beanId}',
         'class'       => SpiceBeanController::class,
         'function'    => 'getBean',
