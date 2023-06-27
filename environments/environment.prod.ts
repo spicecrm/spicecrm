3e8f10d14cc9a21b6751d28b63d48f96890100dd
Maria Zakharova <maria.zakharova@twentyreasons.com>
2023-06-27 14:56:29 +0200

    packaging

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2023.01.001.1683792239418",
+    buildNumber: "2023.01.001.1687869638710",
     copyright: "© 2015 -  2023 aac services k.s. All rights reserved."
     }
