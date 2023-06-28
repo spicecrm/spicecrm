6cb9d9d1042c72a9f75d4e805ea624ef7a0ab816
vragolancic <ivicanikolic.mail@gmail.com>
2023-06-28 13:07:48 +0200

    CR1000915: save Kanban Stages in spicebeanguidestages

--- src/include/spicepath/components/spicekanbanmanageraddmodal.ts
+++ src/include/spicepath/components/spicekanbanmanageraddmodal.ts
@@ -3,8 +3,11 @@ import {Component, Output, EventEmitter} from '@angular/core';
 import {modal} from '../../../services/modal.service';
 import {metadata} from '../../../services/metadata.service';
 import {backend} from '../../../services/backend.service';
+import {language} from "../../../services/language.service";
 import {toast} from "../../../services/toast.service";
 import {SpiceBeanGuidesI} from "../interfaces/kanbanmanager.interfaces";
+import {KanbanManagerService} from "../services/kanbanmanager.service";
+import {modelutilities} from "../../../services/modelutilities.service";
 
 
 @Component({
@@ -35,12 +38,15 @@ export class SpiceKanbanManagerAddModal {
         public modal: modal,
         public metadata: metadata,
         public backend: backend,
+        public language: language,
         public toast: toast,
+        public modelUtilities: modelutilities,
+        public kanbanManagerService: KanbanManagerService
     ) {
     }
 
     /**
-     * Edit Kan Ban in table spicebeanguides
+     * Save Kan Ban in table spicebeanguides and Kan Ban Stages in spicebeanguidestages
      */
     public save() {
         let spinner = this.modal.await('LBL_SAVING');
@@ -60,6 +66,32 @@ export class SpiceKanbanManagerAddModal {
                 this.toast.sendToast('LBL_ERROR_SAVING_SPICEBEANGUIDE', "error", err);
             }
         });
+
+        if(!this.isEditing) {
+            const optionsKey = this.metadata.getFieldOptions(this.selectedBeanGuide.module, this.selectedBeanGuide.status_field);
+            const fieldValidation = this.kanbanManagerService.domainFieldValidations.find(val => val.name == optionsKey);
+            const fieldValidationValue = this.kanbanManagerService.domainFieldValidationsValues.filter(val => val.sysdomainfieldvalidation_id == fieldValidation.id).map(res => {
+                return {
+                    id: this.modelUtilities.generateGuid(),
+                    spicebeanguide_id: this.selectedBeanGuide.id,
+                    stage: res.enumvalue,
+                    stage_sequence: res.sequence,
+                    stage_label: res.label
+                }
+            });
+
+            this.backend.postRequest(`configuration/configurator/spicebeanguidestages`, null, {config: fieldValidationValue}).subscribe({
+                next: () => {
+                    spinner.emit(true);
+                    this.toast.sendToast('LBL_SPICEBEANGUIDESTAGES_SAVED', "success");
+                    this.close();
+                },
+                error: (err) => {
+                    spinner.emit(true);
+                    this.toast.sendToast('LBL_ERROR_SAVING_SPICEBEANGUIDESTAGES', "error", err);
+                }
+            });
+        }
     }
 
 
