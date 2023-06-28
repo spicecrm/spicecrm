f4cb9e14585ea2f2e50170acf339475a2ffaf997
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2023-06-28 14:42:30 +0200

    CR1001022: Adjusted Tracking Actions added link to campaign log. Adjusted Tracking links to contain parent type.

--- api/modules/EmailTrackingActions/api/controllers/EmailTrackingActionsController.php
+++ api/modules/EmailTrackingActions/api/controllers/EmailTrackingActionsController.php
@@ -251,8 +251,8 @@ class EmailTrackingActionsController
         $trackedAction = BeanFactory::getBean('EmailTrackingActions');
         if (!$trackedAction->retrieve_by_string_fields(['parent_type' => 'Emails', 'parent_id' => $data['Emails'], 'action' => $action], true, false)) {
             $trackedAction = BeanFactory::newBean('EmailTrackingActions');
-            $trackedAction->parent_type = 'Emails';
-            $trackedAction->parent_id = $data['Emails'];
+            $trackedAction->parent_type = $data['ParentType'];
+            $trackedAction->parent_id = $data['ParentId'];
             $trackedAction->action = $action;
             $trackedAction->user_agent = $_SERVER['HTTP_USER_AGENT'];
             $trackedAction->ip_address = SpiceUtils::getClientIP();
