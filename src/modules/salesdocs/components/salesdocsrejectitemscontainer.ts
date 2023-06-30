40643648230dfc1acae3ac3c031a77372c52fe64
maretval <valerie.maret@twentyreasons.com>
2023-06-30 18:24:20 +0200

    SalesDocsRejectItemsContainer: fix missing provider for salesdocrecord

--- src/modules/salesdocs/components/salesdocsrejectitemscontainer.ts
+++ src/modules/salesdocs/components/salesdocsrejectitemscontainer.ts
@@ -2,25 +2,16 @@
  * @module ModuleSalesDocs
  */
 import {
-    Component,
-    ElementRef,
-    Injector
+    Component
 } from '@angular/core';
-import {model} from '../../../services/model.service';
-import {metadata} from '../../../services/metadata.service';
-import {modal} from '../../../services/modal.service';
-import {view} from '../../../services/view.service';
-import {language} from '../../../services/language.service';
-import {backend} from '../../../services/backend.service';
-import {currency} from '../../../services/currency.service';
-import {configurationService} from '../../../services/configuration.service';
-import {userpreferences} from '../../../services/userpreferences.service';
 
 import {SalesDocsItemsContainer} from "./salesdocsitemscontainer";
+import {salesdocrecord} from "../services/salesdocrecord";
 
 @Component({
     selector: 'salesdocs-reject-items-container',
-    templateUrl: '../templates/salesdocsrejectitemscontainer.html'
+    templateUrl: '../templates/salesdocsrejectitemscontainer.html',
+    providers:[salesdocrecord]
 })
 export class SalesDocsRejectItemsContainer extends SalesDocsItemsContainer {
 
