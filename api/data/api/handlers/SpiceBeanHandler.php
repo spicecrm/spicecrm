841df993a90d280e6c19f138f3df51daf26a705e
maretval <valerie.maret@twentyreasons.com>
2023-07-22 21:00:58 +0200

    CR1001040: Dashlet configuration: added parameter resolvelinks to force the retrieve of related records

--- api/data/api/handlers/SpiceBeanHandler.php
+++ api/data/api/handlers/SpiceBeanHandler.php
@@ -190,7 +190,7 @@ class SpiceBeanHandler
                 $thisBean->retrieveListDetails();
 
                 // map and add to the array
-                $beanData[] = $this->mapBeanToArray($beanModule, $thisBean, false);
+                $beanData[] = $this->mapBeanToArray($beanModule, $thisBean, (isset($searchParams['resolvelinks']) ? $searchParams['resolvelinks'] : false ));
             }
             $retArray['aggregations'] = $result['aggregations'];
             $retArray['buckets'] = $result['buckets'];
@@ -2139,6 +2139,12 @@ class SpiceBeanHandler
                     }
                     break;
                 case 'link':
+                    // in case we already have the related beans. Could have been retrieved using retrieveListDetails for a dashlet
+                    if($thisBean->{$fieldId} && is_object($thisBean->{$fieldId}) && $thisBean->{$fieldId}->beans && is_array($thisBean->{$fieldId}->beans)){
+                        foreach ($thisBean->{$fieldId}->beans as $relatedBean) {
+                            $beanDataArray[$fieldId]['beans']->{$relatedBean->id} = $this->mapBeanToArray($relatedBean->_module, $relatedBean);
+                        }
+                    }
                     if (($resolvelinks && $fieldData['default'] === true && $fieldData['module']) || $fieldData['name'] == 'email_addresses') {
                         $beanDataArray[$fieldId]['beans'] = new stdClass();
                         $thisBean->load_relationship($fieldId);
