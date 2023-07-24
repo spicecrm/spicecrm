17092fc53be43d9f3303f7629825d62c4e9ed15a
danapatlatiuk <dana.patlatiuk@twentyreasons.com>
2023-07-24 11:55:55 +0200

    packaging

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2023.01.001.1690188256812",
+    buildNumber: "2023.01.001.1690191536981",
     copyright: "© 2015 -  2023 aac services k.s. All rights reserved."
     }
