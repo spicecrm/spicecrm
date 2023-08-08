44ed61534723f39b230eb523b38f2aa0d0f9abe6
Maria Zakharova <maria.zakharova@twentyreasons.com>
2023-08-08 08:56:44 +0200

    git pull test 6

--- src/workbench/components/gitpullfromrepository.ts
+++ src/workbench/components/gitpullfromrepository.ts
@@ -3,7 +3,7 @@ import {backend} from "../../services/backend.service";
 import {toast} from "../../services/toast.service";
 import {modal} from "../../services/modal.service";
 
-//test 5
+//test 6
 
 @Component({
     selector: 'git-pull-from-repository',
