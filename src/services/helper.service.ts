2604420ddbf8a5e9f69bee067af25bedd6db473e
natoku <natalia.okutansoy@twentyreasons.com>
2023-08-18 15:35:00 +0200

    CR1001055: systemobjectpreviewmodal: moved component content to systemobjectpreview and added system-object-preview in the html template

--- src/services/helper.service.ts
+++ src/services/helper.service.ts
@@ -4,6 +4,7 @@
 import {Injectable} from '@angular/core';
 import {Subject, Observable} from 'rxjs';
 import {modal} from './modal.service';
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Injectable({
     providedIn: 'root'
@@ -14,7 +15,12 @@ export class helper {
 
     public _base64_keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
-    constructor(public modalservice: modal) {
+    /**
+     * the blobURL. This is handled internally. When the data is sent this is created so the object can be rendered in the modal
+     */
+    public blobUrl: any;
+
+    constructor(public modalservice: modal, public sanitizer: DomSanitizer) {
     } // public metadata: metadata, public footer: footer
 
     /*
@@ -294,6 +300,33 @@ export class helper {
             case 'special': return specialChars.charAt(  Math.floor(Math.random() * specialChars.length ));
             case 'number':  return String.fromCharCode( Math.floor( Math.random() * 10 ) + 48 );
         }
+    }
+
+    /**
+     * internal function to translate the data to a BLOL URL
+     *
+     * @param byteCharacters the file data
+     * @param contentType the type
+     * @param sliceSize optional parameter to change performance
+     */
+    public datatoBlob(byteCharacters, contentType = '', sliceSize = 512) {
+        let byteArrays = [];
+
+        for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            let slice = byteCharacters.slice(offset, offset + sliceSize);
+
+            let byteNumbers = new Array(slice.length);
+            for (let i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+
+            let byteArray = new Uint8Array(byteNumbers);
+
+            byteArrays.push(byteArray);
+        }
+
+        let blob = new Blob(byteArrays, {type: contentType});
+        return blob;
     }
 
 }
