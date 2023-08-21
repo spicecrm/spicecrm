14f5fafe11435c97e0fbb1af63371a8fd9956cbe
natoku <natalia.okutansoy@twentyreasons.com>
2023-08-21 12:53:18 +0200

    CR1001055: systemimagepreview: new component, moved image preview from systemimagepreviewmodal to systemimagepreview, moved b64toBlob() function to helper service

--- src/services/helper.service.ts
+++ src/services/helper.service.ts
@@ -329,4 +329,33 @@ export class helper {
         return blob;
     }
 
+    /**
+     * converts the base 64 stroing to a blbo and adds it as url so the file can be displayed
+     *
+     * @param b64Data
+     * @param contentType
+     * @param sliceSize
+     * @private
+     */
+    public b64toBlob(b64Data, contentType = '', sliceSize = 512) {
+
+        let byteCharacters = atob(b64Data);
+        let byteArrays = [];
+
+        for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            let slice = byteCharacters.slice(offset, offset + sliceSize);
+
+            let byteNumbers = new Array(slice.length);
+            for (let i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+
+            let byteArray = new Uint8Array(byteNumbers);
+
+            byteArrays.push(byteArray);
+        }
+
+        let blob = new Blob(byteArrays, {type: contentType});
+        return blob;
+    }
 }
