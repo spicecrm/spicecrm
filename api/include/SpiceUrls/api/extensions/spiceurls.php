f3017e465ba2c5e25774532fc37b3c62805d6c8e
natoku <natalia.okutansoy@twentyreasons.com>
2023-08-23 15:48:38 +0200

    CR1001075: SpiceUrls: added backend functionalities for saving, retrieving and updating spiceurls

---
+++ api/include/SpiceUrls/api/extensions/spiceurls.php
@@ -0,0 +1,185 @@
+<?php
+/***** SPICE-HEADER-SPACEHOLDER *****/
+
+use SpiceCRM\includes\Middleware\ValidationMiddleware;
+use SpiceCRM\includes\RESTManager;
+use SpiceCRM\includes\SpiceUrls\api\controllers\SpiceUrlsController;
+
+
+/**
+ * get a Rest Manager Instance
+ */
+$RESTManager = RESTManager::getInstance();
+
+$routes = [
+    [
+        'method' => 'get',
+        'route' => '/common/spiceurls/module/{beanName}/{beanId}',
+        'class' => SpiceUrlsController::class,
+        'function' => 'getUrls',
+        'description' => 'retrieves an array of urls for a specific bean',
+        'options' => ['noAuth' => false, 'adminOnly' => false, 'validate' => true],
+        'parameters' => [
+            'beanName' => [
+                'in' => 'path',
+                'type' => ValidationMiddleware::TYPE_MODULE,
+                'description' => 'name of a module',
+                'example' => 'Accounts',
+                'required' => true
+            ],
+            'beanId' => [
+                'in' => 'path',
+                'type' => ValidationMiddleware::TYPE_GUID,
+                'description' => 'GUID of bean',
+                'required' => true
+            ]
+        ]
+    ],
+    [
+        'method' => 'get',
+        'route' => '/common/spiceurls/module/{beanName}/{beanId}/count',
+        'class' => SpiceUrlsController::class,
+        'function' => 'getUrlsCount',
+        'description' => 'get spice urls count. Define this route before this one /common/spiceurls/module/{beanName}/{beanId}/{urlId} to avoid conflict',
+        'options' => ['noAuth' => false, 'adminOnly' => false, 'validate' => true],
+        'parameters' => [
+            'beanName' => [
+                'in' => 'path',
+                'type' => ValidationMiddleware::TYPE_MODULE,
+                'description' => 'name of a module',
+                'example' => 'Accounts',
+                'required' => true
+            ],
+            'beanId' => [
+                'in' => 'path',
+                'type' => ValidationMiddleware::TYPE_GUID,
+                'description' => 'GUID of bean',
+                'required' => true
+            ]
+        ]
+    ],
+    [
+        'method' => 'get',
+        'route' => '/common/spiceurls/module/{beanName}/{beanId}/{urlId}',
+        'class' => SpiceUrlsController::class,
+        'function' => 'getUrl',
+        'description' => 'retrieves a single url by id',
+        'options' => ['noAuth' => false, 'adminOnly' => false, 'validate' => true],
+        'parameters' => [
+            'beanName' => [
+                'in' => 'path',
+                'type' => ValidationMiddleware::TYPE_MODULE,
+                'description' => 'name of a module',
+                'example' => 'Accounts',
+                'required' => true
+            ],
+            'beanId' => [
+                'in' => 'path',
+                'type' => ValidationMiddleware::TYPE_GUID,
+                'description' => 'GUID of bean',
+                'required' => true
+            ],
+            'urlId' => [
+                'in' => 'path',
+                'type' => ValidationMiddleware::TYPE_GUID,
+                'description' => 'GUID of the url',
+                'required' => true
+            ]
+        ]
+    ],
+    [
+        'method' => 'post',
+        'route' => '/common/spiceurls',
+        'class' => SpiceUrlsController::class,
+        'function' => 'saveUrl',
+        'description' => '',
+        'options' => ['noAuth' => false, 'adminOnly' => false, 'validate' => true],
+        'parameters' => [
+            'url' => [
+                'in' => 'body',
+                'type' => ValidationMiddleware::TYPE_STRING,
+                'required' => true,
+                'description' => 'url content',
+            ],
+            'url_name' => [
+                'in' => 'body',
+                'type' => ValidationMiddleware::TYPE_STRING,
+                'required' => false,
+                'description' => 'url name',
+            ],
+            'beanId' => [
+                'in' => 'body',
+                'type' => ValidationMiddleware::TYPE_GUID,
+                'description' => 'GUID of bean',
+                'required' => false
+            ],
+            'beanName' => [
+                'in' => 'body',
+                'type' => ValidationMiddleware::TYPE_MODULE,
+                'description' => 'name of a module',
+                'example' => 'Accounts',
+                'required' => false
+            ],
+        ]
+    ],
+    [
+        'method' => 'post',
+        'route' => '/common/spiceurls/{id}',
+        'class' => SpiceUrlsController::class,
+        'function' => 'updateUrlData',
+        'description' => 'save the url changes',
+        'options' => ['noAuth' => false, 'adminOnly' => false, 'validate' => true],
+        'parameters' => [
+            'id' => [
+                'in' => 'path',
+                'type' => ValidationMiddleware::TYPE_GUID,
+                'required' => true,
+                'description' => 'GUID of the Url',
+            ],
+            'description' => [
+                'in' => 'query',
+                'type' => ValidationMiddleware::TYPE_STRING,
+                'description' => 'text',
+            ],
+            'url_name' => [
+                'in' => 'query',
+                'type' => ValidationMiddleware::TYPE_STRING,
+                'description' => 'the name to set as display name for the url',
+            ],
+        ]
+    ],
+    [
+        'method' => 'delete',
+        'route' => '/common/spiceurls/module/{beanName}/{beanId}/{urlId}',
+        'class' => SpiceUrlsController::class,
+        'function' => 'deleteUrl',
+        'description' => '',
+        'options' => ['noAuth' => false, 'adminOnly' => false, 'validate' => true],
+        'parameters' => [
+            'beanName' => [
+                'in' => 'path',
+                'type' => ValidationMiddleware::TYPE_MODULE,
+                'description' => 'name of a module',
+                'example' => 'Accounts',
+                'required' => true
+            ],
+            'beanId' => [
+                'in' => 'path',
+                'type' => ValidationMiddleware::TYPE_GUID,
+                'description' => 'GUID of bean',
+                'required' => true
+            ],
+            'urlId' => [
+                'in' => 'path',
+                'type' => ValidationMiddleware::TYPE_GUID,
+                'description' => 'GUID of the url',
+                'required' => true
+            ]
+        ]
+    ]
+];
+
+/**
+ * register the Extension
+ */
+$RESTManager->registerExtension('spiceurls', '1.0', [], $routes);
