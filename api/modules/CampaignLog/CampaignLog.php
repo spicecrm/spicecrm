fb8304b6ca736d02c19a2a7287bbf30021a6bb61
Mehyar Sawas <mehyar.sawas@twentyreasons.com>
2023-09-21 15:52:45 +0200

    CR1001095: Email, CampaignLog: optOutParentEmailAddress: adjusted if statement

--- api/modules/CampaignLog/CampaignLog.php
+++ api/modules/CampaignLog/CampaignLog.php
@@ -70,7 +70,7 @@ class CampaignLog extends SpiceBean {
 
         foreach ($emailAddresses as $address) {
 
-            if ($address->primary_address != 1 || empty($address->opt_in_status) || $address->opt_in_status == 'opted_out') continue;
+            if ($address->primary_address != 1 || $address->opt_in_status == 'opted_out') continue;
 
             EmailAddress::setOptInStatus($parent, $address, 'opted_out');
 
