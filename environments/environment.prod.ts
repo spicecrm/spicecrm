cc379b9f3a73e4177dab96349af9fbfeaf10f44a
Christian Sigl <christiansigl@Christians-MacBook-Air.local>
2023-10-06 11:19:49 +0200

    wip FTS integration M2M relations

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2023.03.001.1696261739614",
+    buildNumber: "2023.03.001.1696423050524",
     copyright: "© 2015 -  2023 aac services k.s. All rights reserved."
     }
