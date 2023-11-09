c93cf2fcc0db0d3173cdb1046a0dde0808f51a05
natoku <natalia.okutansoy@twentyreasons.com>
2023-11-09 15:32:26 +0100

    Agreements: added one-2-m relationship link with Accounts to vardefs

--- api/modules/Agreements/vardefs.php
+++ api/modules/Agreements/vardefs.php
@@ -123,6 +123,16 @@ SpiceDictionaryHandler::getInstance()->dictionary['Agreement'] = [
             'table' => 'agreementconditions',
             'source' => 'non-db'
         ],
+        'accounts_oner' => [
+            'name' => 'accounts_oner',
+            'type' => 'link',
+            'relationship' => 'account_agreements',
+            'source' => 'non-db',
+            'vname' => 'LBL_ACCOUNTS_ONER',
+            'module' => 'Accounts',
+            'default' => false,
+            'comment' => 'One-2-many relationship link to Accounts'
+        ],
     ],
     'relationships' => [
         'account_agreements' => [
