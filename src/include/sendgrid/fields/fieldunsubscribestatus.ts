052390008d4edf8d6efc20a62b8d436753a67ecd
Maria Zakharova <maria.zakharova@twentyreasons.com>
2023-11-13 10:34:26 +0100

    CR1001113: global optout management

--- src/include/sendgrid/fields/fieldunsubscribestatus.ts
+++ src/include/sendgrid/fields/fieldunsubscribestatus.ts
@@ -40,19 +40,13 @@ export class fieldUnsubscribeStatus implements OnInit {
         else{
             this.subscribed = false;
         }
-       this.setOptedOut();
+        this.model.parentmodel = this.relatedmodels.model;
 
     }
-    public setOptedOut(){
-        this.model.parentmodel = this.relatedmodels.model;
-        //this can unsubscribe from all lists if opted out
-        // if(this.disabled()==true){
-        //     this.subscribed = false;
-        //     this.execute();
-        // }
-    }
+
     public disabled(){
         if(this.model.parentmodel.getField('optout_sendgrid')==1){
+            // for more than one account set disabled based on account(sendgrid_source) && this.model.getField('sendgrid_source') == 'sendgrid'
             return true;
         }
     }
