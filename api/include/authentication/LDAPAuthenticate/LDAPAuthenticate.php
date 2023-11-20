3ce4b14fd222bb54caccf8597a24656fb60e3adc
maretval <valerie.maret@twentyreasons.com>
2023-11-20 11:15:46 +0100

    LDAP ldapLogin: do not go further if ldap_search() returns false

--- api/include/authentication/LDAPAuthenticate/LDAPAuthenticate.php
+++ api/include/authentication/LDAPAuthenticate/LDAPAuthenticate.php
@@ -258,15 +258,14 @@ class LDAPAuthenticate implements AuthenticatorI
             return false;
         }
 
-        // lunch the search in Active Directory
-        try {
-            $loginFilter = new LDAPLoginFilter();
-            $filter = $loginFilter->buildLdapSearchFilter($this->loginAttr, $name, $this->loginFilter);
+        // launch the search in Active Directory
+        $loginFilter = new LDAPLoginFilter();
+        $filter = $loginFilter->buildLdapSearchFilter($this->loginAttr, $name, $this->loginFilter);
 //            $result = ldap_search($this->ldapConn, $this->baseDn, "(" . $this->loginAttr . "={$name})", array_merge(['dn'], [$this->bindAttr]));
-            $result = ldap_search($this->ldapConn, $this->baseDn, $filter, array_merge(['dn'], [$this->bindAttr]));
-        } catch (Exception $e) {
+        $result = ldap_search($this->ldapConn, $this->baseDn, $filter, array_merge(['dn'], [$this->bindAttr]));
+        if($result === false){
             $error = $this->logLdapError();
-            // throw new Exception("unable to query ldap: " . $error);
+            LoggerManager::getLogger()->error($error);
             return false;
         }
 
