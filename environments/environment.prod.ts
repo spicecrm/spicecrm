075426acf25a11d545a8e15e0ee31982e591996e
natoku <natalia.okutansoy@twentyreasons.com>
2023-11-21 08:39:42 +0100

    packaging

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2023.03.001.1699653493689",
+    buildNumber: "2023.03.001.1700552099747",
     copyright: "© 2015 -  2023 aac services k.s. All rights reserved."
     }
