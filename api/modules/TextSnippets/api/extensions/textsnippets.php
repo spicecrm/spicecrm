bad57904c015a22b32ae638040c209c378f64e18
Christian Sigl <christian.sigl@twentyreasons.com>
2023-11-27 15:43:37 +0100

    add general methods for opening template variable and text-snippet modals to helper.service, add route to return compiled templates as plain text

--- api/modules/TextSnippets/api/extensions/textsnippets.php
+++ api/modules/TextSnippets/api/extensions/textsnippets.php
@@ -46,7 +46,38 @@ $routes = [
                 'required' => true
             ]
         ]
-    ]
+    ],
+    [
+        'method' => 'post',
+        'route' => '/module/TextSnippets/{id}/liveCompilePlainText',
+        'class' => TextSnippetsController::class,
+        'function' => 'liveCompilePlainText',
+        'description' => 'live compile text snippet as plain text',
+        'options' => ['noAuth' => false, 'adminOnly' => false],
+        'parameters' => [
+            'model_data' => [
+                'in' => 'body',
+                'type' => ValidationMiddleware::TYPE_OBJECT,
+                'description' => 'model data to be parsed',
+                'example' => '{first_name: someone}',
+                'required' => true
+            ],
+            'module' => [
+                'in' => 'body',
+                'type' => ValidationMiddleware::TYPE_MODULE,
+                'description' => 'module name to be parsed',
+                'example' => 'Accounts',
+                'required' => true
+            ],
+            'id' => [
+                'in' => 'path',
+                'type' => ValidationMiddleware::TYPE_GUID,
+                'description' => 'if of parent bean',
+                'example' => '2816ba5c-97e7-11eb-8c42-00fffe0c4f07',
+                'required' => true
+            ]
+        ]
+    ],
 ];
 $RESTManager->registerRoutes($routes);
 
