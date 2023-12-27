034b5db0bfc694fd5fe5f073d900af1c25da3065
Christian Knoll <christian.g.knoll@gmail.com>
2023-12-27 17:21:02 +0100

    CORE add element_transformation to the price conditions

--- src/include/spicepricedetermination/interfaces/spicepricedeterminationconfigurator.interfaces.ts
+++ src/include/spicepricedetermination/interfaces/spicepricedeterminationconfigurator.interfaces.ts
@@ -8,7 +8,7 @@ export interface priceConditionElement {
     element_domain?: string;
     element_module?: string;
     element_module_field?: string;
-    element_method?: string;
+    elemewnt_transformation?: string;
     deleted?: boolean;
 }
 
