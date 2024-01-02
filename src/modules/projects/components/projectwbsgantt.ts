724f677d6c6ee2b91718cc8b642c083d2144c31c
maretval <valerie.maret@twentyreasons.com>
2024-01-02 15:15:23 +0100

    Revert "- remake of gantt chart wip"
    This reverts commit 79cc99a9

--- src/modules/projects/components/projectwbsgantt.ts
+++ src/modules/projects/components/projectwbsgantt.ts
@@ -1,56 +1,37 @@
 /**
  * @module ModuleProjects
  */
-import {Component, OnDestroy, OnInit} from "@angular/core";
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from "@angular/core";
 import {model} from "../../../services/model.service";
+import {libloader} from '../../../services/libloader.service';
 import {projectwbsHierarchy} from "../services/projectwbshierarchy.service";
+
+import moment from "moment";
+
+declare var gantt: any;
 
 @Component({
     selector: "projectwbs-gantt",
     templateUrl: "../templates/projectwbsgantt.html",
 })
-export class ProjectWBSGantt implements OnInit, OnDestroy {
-    public self: any
-    public items = [];
-    public milestones = []
-    private _zoomLevel: string = 'MONTH'
+export class ProjectWBSGantt implements  AfterViewInit, OnDestroy {
+    public self: any;
 
-    get zoomLevels() {
-        return [
-            {id: 'WEEK', label: 'Woche'},
-            {id: 'MONTH', label: 'Monat'},
-            {id: 'QUARTER', label: 'Quartal'},
-            {id: 'YEAR', label: 'Jahr'},
-        ]
-    }
+    @ViewChild("gantt_here") public ganttContainer;
 
-    get zoomLevel() {
-        return this._zoomLevel
-    }
-
-    set zoomLevel(level: string) {
-        this._zoomLevel = level
-    }
+    private g: any;
 
     constructor(
         public projectwbsHierarchy: projectwbsHierarchy,
-        public model: model
+        public model: model,
+        public libloader: libloader,
     ) {
+
+
     }
 
-    public ngOnInit() {
-        this.items = this.projectwbsHierarchy.members.map(t => ({
-            id: t.id,
-            name: t.data.name,
-            start: t.data.date_start.format("YYYY-MM-DD HH:mm:ss"),
-            end: t.data.date_end.format("YYYY-MM-DD HH:mm:ss"),
-            type: 'wbs',
-            // duration: t.data.date_end.diff(t.data.date_start, 'days'),
-            progress: t.data.level_of_completion ? t.data.level_of_completion / 100 : 0,
-            parent: t.data.parent_id,
-        }));
-
-        this.milestones = []
+    public ngAfterViewInit() {
+        this.loadNecessaryLibraries();
     }
 
     /**
@@ -58,7 +39,6 @@ export class ProjectWBSGantt implements OnInit, OnDestroy {
      * load cluster library and set the marker clusterer if the direction service is inactive
      */
     public loadNecessaryLibraries() {
-        /*
         this.libloader.loadLib('dhtmlx.gantt').subscribe(() => {
             gantt.config.xml_date = "%Y-%m-%d %H:%i";
             gantt.config.readonly = true;
@@ -149,14 +129,21 @@ export class ProjectWBSGantt implements OnInit, OnDestroy {
             gantt.parse({tasks, links});
             gantt.ext.zoom.setLevel("quarter");
         });
-
-        */
     }
 
-    public close() {
+    public zoomIn(){
+        gantt.ext.zoom.zoomIn();
+    }
+    public zoomOut(){
+        gantt.ext.zoom.zoomOut();
+    }
+
+    public close(){
         this.self.destroy();
     }
 
     public ngOnDestroy() {
+        gantt.clearAll();
     }
+
 }
