48f496c53c30ed531c2a0500fab0b1b6b4695615
natoku <natalia.okutansoy@twentyreasons.com>
2024-01-02 09:57:47 +0100

    CR1000740: SpiceTextController: retrieve custom spicebeanguides and texts

--- api/modules/SpiceTexts/api/controllers/SpiceTextController.php
+++ api/modules/SpiceTexts/api/controllers/SpiceTextController.php
@@ -21,14 +21,27 @@ class SpiceTextController
     public function getSpiceTexts(Request $req, Response $res, array $args): Response {
         $db = DBManagerFactory::getInstance();
 
-        //
+        $spiceTexts = [];
+
+        // check custom first
+        if ($db->tableExists('spicebeancustomguides')) {
+
+            $customQ = $db->query("
+            SELECT spicetexts.*, syscustomtextids.label, spicebeancustomguides.id as spiceBeanGuideId, 'custom' as scope FROM spicetexts 
+            INNER JOIN syscustomtextids ON spicetexts.text_id = syscustomtextids.text_id 
+            INNER JOIN spicebeancustomguides ON spicebeancustomguides.systextid = spicetexts.text_id 
+            WHERE parent_type = '{$args['parentType']}' and deleted = 0");
+
+            while ($custom = $db->fetchByAssoc($customQ)) {
+                $spiceTexts[] = $custom;
+            }
+        }
+
         $query = $db->query("
-            SELECT spicetexts.*, systextids.label, spicebeanguides.id as spiceBeanGuideId FROM spicetexts 
+            SELECT spicetexts.*, systextids.label, spicebeanguides.id as spiceBeanGuideId, 'global' as scope FROM spicetexts 
             INNER JOIN systextids ON spicetexts.text_id = systextids.text_id 
             INNER JOIN spicebeanguides ON spicebeanguides.systextid = spicetexts.text_id 
             WHERE parent_type = '{$args['parentType']}' and deleted = 0");
-
-        $spiceTexts = [];
 
         while ($row = $db->fetchByAssoc($query)) {
             $spiceTexts[] = $row;
