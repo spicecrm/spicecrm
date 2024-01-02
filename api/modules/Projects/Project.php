5f4e009a09f5b6b13f0f9dd6962213f82f4ac747
maretval <valerie.maret@twentyreasons.com>
2024-01-02 15:15:23 +0100

    Revert "- remake of gantt chart wip"
    This reverts commit 4d0984af

--- api/modules/Projects/Project.php
+++ api/modules/Projects/Project.php
@@ -1,14 +1,12 @@
 <?php
 /***** SPICE-SUGAR-HEADER-SPACEHOLDER *****/
-
 namespace SpiceCRM\modules\Projects;
 
 use SpiceCRM\data\BeanFactory;
 use SpiceCRM\data\SpiceBean;
 use SpiceCRM\includes\utils\SpiceUtils;
 
-class Project extends SpiceBean
-{
+class Project extends SpiceBean {
     // calculated information
     public $total_estimated_effort;
     public $total_actual_effort;
@@ -26,33 +24,34 @@ class Project extends SpiceBean
      */
     public function retrieve($id = -1, $encode = false, $deleted = true, $relationships = true)
     {
-        $bean = parent::retrieve($id, $encode, $deleted, $relationships);
+        $bean =  parent::retrieve($id, $encode, $deleted, $relationships);
 
         // calculations
-        if ($bean) {
+        if($bean){
             // calculate planned & actual efforts using wbs elements
             $this->total_estimated_effort = 0;
             $this->total_actual_effort = 0;
             $wbsElements = $this->get_linked_beans('projectwbss');
-
-            foreach ($wbsElements as $wbs) {
+            foreach($wbsElements as $wbs){
                 // handle planned efforts
-                if (is_string($wbs->planned_effort)) $wbs->planned_effort = intval($wbs->planned_effort);
+                if(is_string($wbs->planned_effort)) $wbs->planned_effort = intval($wbs->planned_effort);
                 $this->total_estimated_effort += $wbs->planned_effort;
                 // handle actual efforts
-                if (is_string($wbs->consumed_effort)) $wbs->consumed_effort = intval($wbs->consumed_effort);
+                if(is_string($wbs->consumed_effort)) $wbs->consumed_effort = intval($wbs->consumed_effort);
                 $this->total_actual_effort += $wbs->consumed_effort;
             }
 
             // calculate start and end dates according to earliest WBS element start date and lastest WBS end date
-            if (count($wbsElements) > 0) {
+            if(count($wbsElements) > 0) {
                 $this->estimated_start_date = SpiceUtils::getMinDate($wbsElements, 'date_start');
                 $this->estimated_end_date = SpiceUtils::getMaxDate($wbsElements, 'date_end');
             }
-        }
+                    }
 
         return $bean;
     }
+
+
 
 
     /**
@@ -64,14 +63,14 @@ class Project extends SpiceBean
     }
 
 
-    function bean_implements($interface)
-    {
-        switch ($interface) {
-            case 'ACL':
-                return true;
+
+    function bean_implements($interface){
+        switch($interface){
+            case 'ACL':return true;
         }
         return false;
     }
+
 
 
 }
