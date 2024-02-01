b82230f7c66672e1e6719a421e98dfcbaf009581
RebeccaTodor <rebeccatodor@yahoo.de>
2024-02-01 16:13:02 +0100

    CR1001193: added drag & drop to navigation tabs & tabs browser modal restricted dotted outline for drag & drop to kanbancontainer only

--- src/globalcomponents/components/globalnavigationtabbed.ts
+++ src/globalcomponents/components/globalnavigationtabbed.ts
@@ -3,11 +3,12 @@
  */
 import {ChangeDetectorRef, Component, Input, OnDestroy, ViewChild} from '@angular/core';
 import {metadata} from '../../services/metadata.service';
-import {navigation} from '../../services/navigation.service';
+import {navigation, objectTab} from '../../services/navigation.service';
 import {Subscription} from "rxjs";
 import {animate, state, style, transition, trigger} from "@angular/animations";
 import {GlobalNavigationTabbedMenu} from "./globalnavigationtabbedmenu";
 import {userpreferences} from "../../services/userpreferences.service";
+import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
 
 /** @ignore */
 const ANIMATIONS = [
@@ -130,5 +131,14 @@ export class GlobalNavigationTabbed implements OnDestroy {
         this.userPreferences.toUse.globalHeaderCollapsed = !this.userPreferences.toUse.globalHeaderCollapsed;
         this.userPreferences.setPreference('globalHeaderCollapsed', this.userPreferences.toUse.globalHeaderCollapsed);
         setTimeout(() => this.menuContainer.handleResize(), 200);
+    }
+
+    /**
+     * handles the drop event and resets the sequence fields
+     * @param event
+     */
+    public drop(event: CdkDragDrop<objectTab[]>){
+        moveItemInArray(this.navigation.objectTabs, event.previousIndex, event.currentIndex);
+        this.navigation.setSessionData();
     }
 }
