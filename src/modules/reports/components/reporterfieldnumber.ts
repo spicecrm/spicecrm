c02fd2d0532ea4219d0b8b0a14feb7cbe76681b4
Christian Knoll <christian.g.knoll@gmail.com>
2024-02-01 10:37:44 +0100

    CR1001224 new Fields for Reporter

--- src/modules/reports/components/reporterfieldnumber.ts
+++ src/modules/reports/components/reporterfieldnumber.ts
@@ -5,13 +5,12 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {userpreferences} from '../../../services/userpreferences.service';
 
 /**
- * display formatted report record value with percentage
+ * display formatted report record value
  */
 @Component({
-    templateUrl: '../templates/reporterfieldpercentage.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    templateUrl: '../templates/reporterfieldnumber.html'
 })
-export class ReporterFieldPercentage {
+export class ReporterFieldNumber {
     /**
      * report full record
      */
@@ -20,5 +19,5 @@ export class ReporterFieldPercentage {
      * report field
      */
     public field: any = {};
-
 }
+
