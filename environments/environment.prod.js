2bd4370376a1b94e7230b0cade67bd7e28c16a55
Christian Knoll <christian.g.knoll@gmail.com>
2024-02-13 07:57:59 +0100

    packaging

--- environments/environment.prod.js
+++ environments/environment.prod.js
@@ -1,6 +1,8 @@
-export const environment = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.environment = void 0;
+exports.environment = {
     production: true,
-    buildNumber: "2023.03.001.1705932637213",
+    buildNumber: "2024.01.001.1707807364649",
     copyright: "© 2015 -  2024 aac services k.s. All rights reserved."
 };
-//# sourceMappingURL=environment.prod.js.map
--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2023.03.001.1707146494805",
+    buildNumber: "2024.01.001.1707807364649",
     copyright: "© 2015 -  2024 aac services k.s. All rights reserved."
     }
