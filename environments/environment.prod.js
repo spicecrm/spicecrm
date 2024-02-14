c70d39a2f8faa686ff929935f60394add948558d
Christian Knoll <christian.g.knoll@gmail.com>
2024-02-14 15:53:05 +0100

    packaging

--- environments/environment.prod.js
+++ environments/environment.prod.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.environment = void 0;
 exports.environment = {
     production: true,
-    buildNumber: "2024.01.001.1707921924011",
+    buildNumber: "2024.01.001.1707922250869",
     copyright: "© 2015 -  2024 aac services k.s. All rights reserved."
 };
