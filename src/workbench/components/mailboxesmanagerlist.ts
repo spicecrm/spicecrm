01ab7b0ca651ba50bc4df27174d1d40bf767bb2e
RebeccaTodor <rebeccatodor@yahoo.de>
2024-02-15 16:31:24 +0100

    CR1001227 - Made own component for mailboxmanager list, ajusted its logic and ajusted the mailboxsetdefaultbutton so that the list refreshes after button click

--- src/workbench/components/mailboxesmanagerlist.ts
+++ src/workbench/components/mailboxesmanagerlist.ts
@@ -1,32 +1,34 @@
 /**
  * @module WorkbenchModule
  */
-import {Component, ViewChild, ViewContainerRef} from "@angular/core";
+import {Component, EventEmitter, Output} from "@angular/core";
 import {backend} from "../../services/backend.service";
+import {configurationService} from "../../services/configuration.service";
 import {footer} from "../../services/footer.service";
 import {language} from "../../services/language.service";
 import {metadata} from "../../services/metadata.service";
 import {modellist} from "../../services/modellist.service";
-import {model} from "../../services/model.service";
 import {modelutilities} from "../../services/modelutilities.service";
 import {toast} from "../../services/toast.service";
 import {view} from "../../services/view.service";
-import {configurationService} from "../../services/configuration.service";
 
 /**
  * part of the workbench to manage the mailboxes
  */
 @Component({
-    providers: [model, view, modellist],
-    selector: "mailboxes-manager",
-    templateUrl: "../templates/mailboxesmanager.html",
+    providers: [view],
+    selector: "mailboxes-manager-list",
+    templateUrl: "../templates/mailboxesmanagerlist.html",
 })
-export class MailboxesManager {
+
+export class MailboxesManagerList {
 
     /**
-     * the container with the view .. gets rendered daynamically
+     * the currently selected mailbox id
      */
-    @ViewChild("viewcontainer", {read: ViewContainerRef, static: true}) public viewcontainer: ViewContainerRef;
+    public selectedMailboxId: string;
+
+    @Output() public selectedMailboxChange = new EventEmitter<any>();
 
     /**
      * any component reference that is rendered
@@ -49,20 +51,12 @@ export class MailboxesManager {
         public footer: footer,
         public language: language,
         public metadata: metadata,
-        public model: model,
         public modelutils: modelutilities,
         public modelutilities: modelutilities,
         public toast: toast,
         public view: view,
         public configuration: configurationService
     ) {
-        // initialize the modellist service
-        this.modellist.initialize('Mailboxes');
-
-        // force the data load
-        this.modellist.getListData();
-
-        this.model.module = "Mailboxes";
         this.view.isEditable = true;
 
         // get the transports
@@ -79,31 +73,20 @@ export class MailboxesManager {
     }
 
     /**
+     * getter for the mailboxes returning them sorted
+     */
+    get mailboxes() {
+        return this.modellist.listData.list.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    /**
      * setter for the current mailbox that also triggert the rerendering and the load of the model
      *
      * @param mailbox
      */
-    public handleSelectionChange(mailbox: any) {
-
-        if (mailbox) {
-            // set the current mailbox and go load the model
-            this.model.id = mailbox.id;
-            this.model.setData(mailbox, false);
-
-            // render new
-            this.cleanView();
-            this.renderedview = [];
-            this.metadata.addComponent('ObjectRecordDetails', this.viewcontainer).subscribe(component => {
-                this.renderedview.push(component);
-            });
-            this.metadata.addComponent('ObjectRelateContainer', this.viewcontainer).subscribe(component => {
-                this.renderedview.push(component);
-            });
-        } else {
-            this.cleanView();
-            this.model.id = "";
-            this.model.initialize();
-        }
+    public setSelectedMailbox(mailbox) {
+        this.selectedMailboxId = mailbox.id;
+        this.selectedMailboxChange.emit(mailbox);
     }
 
     /**
@@ -114,6 +97,7 @@ export class MailboxesManager {
         for (let thisview of this.renderedview) {
             thisview.destroy();
         }
+
     }
 
     /**
@@ -121,8 +105,6 @@ export class MailboxesManager {
      */
     public reset() {
         this.view.setViewMode();
-        this.model.reset();
-        this.model.module = "Mailboxes";
     }
 
 }
