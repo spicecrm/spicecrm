7b83ae1bb0ac1ee3b290b20f415e6926b60f495f
RebeccaTodor <rebeccatodor@yahoo.de>
2024-02-15 16:31:24 +0100

    CR1001227 - Made own component for mailboxmanager list, ajusted its logic and ajusted the mailboxsetdefaultbutton so that the list refreshes after button click

--- src/workbench/workbench.module.ts
+++ src/workbench/workbench.module.ts
@@ -77,6 +77,7 @@ import {LanguageTranslationsManager} from "./components/languagetranslationsmana
 import {LanguageLabelManagerComponent,SortPipe} from "./components/languagelabelmanager";
 import {LanguageLabelModal} from "./components/languagelabelmodal";
 import {MailboxesManager} from "./components/mailboxesmanager";
+import {MailboxesManagerList} from "./components/mailboxesmanagerlist"
 import {MailboxesTransportsEnum} from "./components/mailboxestransportsenum";
 import {MailboxesmanagerTestModal} from "./components/mailboxesmanagertestmodal";
 import {MailboxesmanagerTestIMAPModal} from "./components/mailboxesmanagertestimapmodal";
@@ -237,6 +238,7 @@ import {WebHooksManagerEditModal} from "./components/webhooksmanagereditmodal";
         LanguageLabelManagerComponent,
         LanguageLabelModal,
         MailboxesManager,
+        MailboxesManagerList,
         MailboxesTransportsEnum,
         LanguageLabelModal,
         MailboxManagerAddDialog,
