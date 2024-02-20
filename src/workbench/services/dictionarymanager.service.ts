92f3d9105f6dc7d1dea0b54d0b2d14180948d324
Christian Knoll <christian.g.knoll@gmail.com>
2024-02-20 07:59:01 +0100

    fix dictionary manager service to prevent errors when loading

--- src/workbench/services/dictionarymanager.service.ts
+++ src/workbench/services/dictionarymanager.service.ts
@@ -194,6 +194,8 @@ export class dictionarymanager implements OnDestroy {
 
         let changed = false;
 
+        if(!this.loaded) return changed;
+
         const loaded = JSON.parse(this.loaded);
 
         const keys = [
