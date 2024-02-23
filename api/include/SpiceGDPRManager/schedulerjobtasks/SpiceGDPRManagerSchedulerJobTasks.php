f96a63fe9677082738df212cbf9fa5da568463b0
natoku <natalia.okutansoy@twentyreasons.com>
2024-02-23 13:50:51 +0100

    CR1001208: SpiceGDPRManagerSchedulerJobTasks: delete related beans depending on link type "many" or "one"

--- api/include/SpiceGDPRManager/schedulerjobtasks/SpiceGDPRManagerSchedulerJobTasks.php
+++ api/include/SpiceGDPRManager/schedulerjobtasks/SpiceGDPRManagerSchedulerJobTasks.php
@@ -2,35 +2,38 @@
 
 namespace SpiceCRM\includes\SpiceGDPRManager\schedulerjobtasks;
 
+use Exception;
 use SpiceCRM\data\BeanFactory;
 use SpiceCRM\data\SpiceBean;
+use SpiceCRM\includes\authentication\AuthenticationController;
 use SpiceCRM\includes\database\DBManagerFactory;
-use SpiceCRM\includes\SpiceFTSManager\SpiceFTSHandler;
+use SpiceCRM\includes\Logger\LoggerManager;
 use SpiceCRM\includes\SysModuleFilters\SysModuleFilters;
-use SpiceCRM\data\api\handlers\SpiceBeanHandler;
+use SpiceCRM\includes\TimeDate;
 
 class SpiceGDPRManagerSchedulerJobTasks
 {
-    public function processRetentions(){
+    public function processRetentions()
+    {
         $db = DBManagerFactory::getInstance();
         $retentions = $db->query("SELECT * FROM sysgdprretentions WHERE deleted = 0 AND active = 1");
-        while($retention = $db->fetchByAssoc($retentions)){
+        while ($retention = $db->fetchByAssoc($retentions)) {
             $moduleFilter = new SysModuleFilters();
             $filterWhere = $moduleFilter->generateWhereClauseForFilterId($retention['sysmodulefilter_id']);
 
             $seed = BeanFactory::getBean($moduleFilter->filtermodule);
 
             $query = "SELECT id, deleted FROM {$seed->_tablename} WHERE {$filterWhere}";
-            if($retention['retention_type'] != 'P'){
-                $query.= " AND {$seed->_tablename}.deleted = 0";
+            if ($retention['retention_type'] != 'P') {
+                $query .= " AND {$seed->_tablename}.deleted = 0";
             }
 
             $relatedModules = explode(',', $retention['delete_related']);
 
             // query the ids
             $ids = $db->query($query);
-            while($id = $db->fetchByAssoc($ids)){
-                switch($retention['retention_type']){
+            while ($id = $db->fetchByAssoc($ids)) {
+                switch ($retention['retention_type']) {
                     case 'I';
                         $seed = BeanFactory::getBean($moduleFilter->filtermodule, $id['id']);
                         $seed->is_inactive = 1;
@@ -48,7 +51,7 @@ class SpiceGDPRManagerSchedulerJobTasks
                     case 'P':
                         $seed = BeanFactory::getBean($moduleFilter->filtermodule, $id['id'], [], false);
 
-                        if($id['deleted'] == 0){
+                        if ($id['deleted'] == 0) {
 
                             // delete the bean itself
                             $seed->mark_deleted($id['id']);
@@ -72,20 +75,25 @@ class SpiceGDPRManagerSchedulerJobTasks
      *
      * @param SpiceBean $bean
      */
-    public function deleteRelated(SpiceBean $bean, $relatedModules, $purge = false){
-        if(count($relatedModules) > 0) {
+    public function deleteRelated(SpiceBean $bean, $relatedModules, $purge = false)
+    {
+        if (count($relatedModules) > 0) {
 
             foreach ($bean->field_defs as $fieldname => $fielddata) {
                 if ($fielddata['type'] == 'link') {
-                    if($bean->load_relationship($fieldname)) {
+                    if ($bean->load_relationship($fieldname)) {
                         $module = $bean->{$fieldname}->getRelatedModuleName();
                         if (in_array($module, $relatedModules)) {
                             $relatedBeans = $bean->get_linked_beans($fieldname);
+
                             foreach ($relatedBeans as $relatedBean) {
-                                $relatedBean->mark_deleted($relatedBean->id);
+                                $relationshipDef = $bean->{$fieldname}->relationship;
+
+                                // delete related data
+                                $this->deleteRelatedBeans($bean, $relatedBean, $relationshipDef);
 
                                 // physically delete the record
-                                if($purge){
+                                if ($purge) {
                                     $relatedBean->db->query("DELETE FROM {$relatedBean->_tablename} WHERE id = '{$relatedBean->id}'");
                                 }
                             }
@@ -93,6 +101,58 @@ class SpiceGDPRManagerSchedulerJobTasks
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * deleting related Beans of the $relatedBean according to relationship type
+     * loops through links & retrieves related Beans to check if we can set deleted flag on relatedBean
+     * if $relatedBean has other not-deleted relationships then it should not be deleted
+     *
+     * @param SpiceBean $beanToBeDeleted
+     * @param SpiceBean $relatedBean
+     * @param string $relationshipDef
+     * @return void
+     */
+    private function deleteRelatedBeans(SpiceBean $beanToBeDeleted, SpiceBean $relatedBean, string $relationshipDef)
+    {
+
+        switch ($relationshipDef->type) {
+            case 'many-to-many':
+                $lhsLink = $relationshipDef->lhsLink;
+                if ($relatedBean->load_relationship($relationshipDef->lhsLink)) {
+                    $relatedBean->$lhsLink->delete($relatedBean->id, $beanToBeDeleted->id);
+                } else {
+                    LoggerManager::getLogger()->fatal('Deleting GDPR Beans', "error loading relationship $lhsLink in " . __FILE__);
+                }
+                break;
+
+            case 'one-to-many':
+                // get linked fields of the relatedBean
+                $linked_fields = array_filter(
+                    $relatedBean->get_linked_fields(),
+                    function ($key) {
+                        return !in_array($key, ['assigned_user_link', 'created_by_link', 'modified_user_link', 'mailboxes']);
+                    },
+                    ARRAY_FILTER_USE_KEY
+                );
+
+                // retrieve only links of the relatedBean
+                $linkNames = [];
+                foreach ($linked_fields as $linkedField) {
+                    $linkNames[] = $linkedField['name'];
+                }
+
+                if (count($linkNames) > 0) {
+                    // collection of all the beans related to the related Bean
+                    $beansRelatedToRelatedBean = $relatedBean->get_multiple_linked_beans($linkNames);
+
+                    if (empty($beansRelatedToRelatedBean)) {
+                        // delete related bean if we've not got any m-2-m relationships
+                        $relatedBean->mark_deleted($relatedBean->id);
+                    }
+                }
+                break;
         }
     }
 }
