10a88506ae42236f9b6ef8585f89ca93c2e9cb55
Christian Knoll <christian.g.knoll@gmail.com>
2024-03-07 11:59:15 +0100

    packaging

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2024.01.001.1709802806749",
+    buildNumber: "2024.01.001.1709805269657",
     copyright: "© 2015 -  2024 aac services k.s. All rights reserved."
     }
