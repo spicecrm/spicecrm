c3361351a8ace0fdd21ad1ed7c3404ead2f63be6
Maria Zakharova <maria.zakharova@twentyreasons.com>
2024-03-13 13:47:31 +0100

    CR1001272: mailbox footer bugfix, remove second expand in html editor

--- api/modules/Mailboxes/Handlers/TransportHandler.php
+++ api/modules/Mailboxes/Handlers/TransportHandler.php
@@ -131,13 +131,18 @@ abstract class TransportHandler
         $emailTemplate->body_html = $content;
         $parsedContent = $emailTemplate->parse($email)['body_html'];
         $doc = new DOMDocument();
-        $doc->loadHTML($parsedContent);
+        $doc->loadHTML('<?xml encoding="UTF-8">' . $parsedContent);
 
         # remove <!DOCTYPE
         $doc->removeChild($doc->doctype);
 
         # remove <html><body></body></html>
-        $doc->replaceChild($doc->firstChild->firstChild->firstChild, $doc->firstChild);
+//        $doc->replaceChild($doc->firstChild->firstChild->firstChild, $doc->firstChild);
+        # if there are still html tags get content
+        $doc->getElementsByTagName('body');
+        if($doc->textContent == ''){
+            $doc->getElementsByTagName('html');
+        }
 
         return $doc->saveHTML();
     }
