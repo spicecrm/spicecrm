a811dfc04a5ad34dbbc375c11aa14d6e8ee7c3b6
maretval <valerie.maret@twentyreasons.com>
2024-03-26 12:43:03 +0100

    Concatenated/reorganized some built queries to get a clean debug line.

--- api/include/database/DBManager.php
+++ api/include/database/DBManager.php
@@ -2039,9 +2039,7 @@ abstract class DBManager
             $where .= " AND deleted=0";
         }
 
-        return "UPDATE " . $bean->getTableName() . "
-					SET " . implode(",", $columns) . "
-					$where";
+        return "UPDATE " . $bean->getTableName() . " SET " . implode(",", $columns) . "	$where";
     }
 
     /**
