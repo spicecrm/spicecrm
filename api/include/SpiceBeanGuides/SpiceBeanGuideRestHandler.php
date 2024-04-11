ceb937823bab7e38815cf02612f1414ce60802a6
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2024-04-11 14:21:53 +0200

    Kanban Manager: Handler adjustment

--- api/include/SpiceBeanGuides/SpiceBeanGuideRestHandler.php
+++ api/include/SpiceBeanGuides/SpiceBeanGuideRestHandler.php
@@ -3,61 +3,143 @@
 namespace SpiceCRM\includes\SpiceBeanGuides;
 
 use SpiceCRM\data\BeanFactory;
+use SpiceCRM\data\SpiceBean;
 use SpiceCRM\includes\database\DBManagerFactory;
 use stdClass;
 
 class SpiceBeanGuideRestHandler
 {
-
-    public function getStageDefs($module)
+    /**
+     * get guides with stages
+     * @param $module
+     * @return array
+     * @throws \Exception
+     */
+    public function getStageDefs()
     {
-        global $current_language;
-$db = DBManagerFactory::getInstance();
 
-        $focus = BeanFactory::getBean($module);
+        $db = DBManagerFactory::getInstance();
 
-        // get the object
-        // get the object
-        $object = $db->fetchByAssoc($db->query("SELECT * FROM spicebeancustomguides WHERE module='$module'"));
-        if(empty($object)){
-            $object = $db->fetchByAssoc($db->query("SELECT * FROM spicebeanguides WHERE module='$module'"));
+        $fields = "id, name, status_field, systextid, module";
+        $query = $db->query("SELECT $fields, 'custom' as scope FROM spicebeancustomguides UNION SELECT $fields, 'global' as scope FROM spicebeanguides");
+
+        $guides = [];
+
+        while ($guide = $db->fetchByAssoc($query)) {
+
+            $bean = BeanFactory::getBean($guide['module']);
+
+            if (!$guides[$guide['module']]) $guides[$guide['module']] = [];
+
+            $guides[$guide['module']][] = [
+                'id' => $guide['id'],
+                'name' => $guide['name'],
+                'type' => $guide['scope'],
+                'statusfield' => $guide['status_field'],
+                'stages' => $this->getBeanGuideStages($guide, $bean)
+            ];
         }
 
-        $statusField = $object['status_field'];
+        return $guides;
+    }
 
-        // get the sales stages
-        $stagesObj = $db->query("SELECT st.*, stt.stage_name, stt.stage_secondaryname, stt.stage_description FROM spicebeanguidestages st , spicebeanguidestages_texts stt WHERE st.spicebeanguide_id = '" . $object['id'] . "' AND st.id = stt.stage_id AND stt.language = '$current_language' ORDER BY st.stage_sequence");
+    /**
+     * get bean guide stages
+     * @param array $guide
+     * @param SpiceBean $bean
+     * @return array
+     * @throws \Exception
+     */
+    private function getBeanGuideStages(array $guide, SpiceBean $bean): array
+    {
+        $db = DBManagerFactory::getInstance();
+
+        $statusField = $guide['status_field'];
+        $table = $guide['scope'] == 'global' ? 'spicebeanguidestages' : 'spicebeancustomguidestages';
+        $stagesObj = $db->query("SELECT * FROM $table WHERE spicebeanguide_id = '{$guide['id']}' ORDER BY stage_sequence;");
+
         $stages = [];
-        $stagePassed = false;
+
         while ($stage = $db->fetchByAssoc($stagesObj)) {
-            // set the stage - for multi stage take the first or the one that is active if it is not the first
-            if (!isset($stages[$stage['stage']]) || $stage['stage'] . $stage['secondary_stage'] == $focus->$statusField) {
-                $stages[$stage['stage']] = $stage;
-                $stages[$stage['stage']]['stage_description'] = html_entity_decode($stage['stage_description']);
 
+            // set the stage - for multi-stage take the first or the one that is active if it is not the first
+            if (!isset($stages[$stage['stage']]) || $stage['stage'] . $stage['secondary_stage'] == $bean->$statusField) {
 
-                // perform checks
-                $stages[$stage['stage']]['checks'] = [];
-                $stages[$stage['stage']]['checkcontent'] = '';
+                $stage['statusfield'] = $statusField;
+                $stage['checks'] = $this->getStageChecks($guide['id'], $stage['id'], $bean);
+                $stage['texts'] = $this->getStageTexts($stage['id'], $guide['systextid']);
+
+                $stages[] = [
+                    'stage' => $stage['stage'],
+                    'stagedata' => $stage
+                ];
             }
         }
 
-        $retStages = [];
-        foreach ($stages as $stage => $stageData) {
-            $retStages[] = $stageData;
+        return $stages;
+    }
+
+    /**
+     * get stage texts
+     * @param string $stageId
+     * @param string $sysTextId
+     * @return array
+     * @throws \Exception
+     */
+    private function getStageTexts(string $stageId, ?string $sysTextId = null): array
+    {
+        if (empty($sysTextId)) return [];
+
+        $db = DBManagerFactory::getInstance();
+
+        return $db->fetchAll("SELECT id FROM spicetexts WHERE parent_id = '$stageId' AND parent_type = 'SpiceBeanGuideStages' AND text_id = '$sysTextId'") ?: [];
+    }
+
+    private function getStageChecks(string $guidId, string $stageId, SpiceBean $bean): array
+    {
+        global $current_language;
+
+        $db = DBManagerFactory::getInstance();
+        $data = [];
+
+        $checks = $db->query("SELECT sc.*, sct.text FROM spicebeanguidestages_checks sc LEFT JOIN spicebeanguidestages_check_texts sct on sc.id = sct.stage_check_id AND sct.language='$current_language' WHERE sc.spicebeanguide_id = '$guidId' AND sc.stage_id = '$stageId' ORDER BY sc.check_sequence");
+
+        while ($check = $db->fetchByAssoc($checks)) {
+
+            $checkResult = null;
+
+            if(!empty($bean->id)){
+
+                if (!empty($check['check_include']) && file_exists($check['check_include'])) {
+                    require_once($check['check_include']);
+                    if(class_exists($check['check_class'])){
+                        $checkClass = new $check['check_class']();
+                        $checkMethod = $check['check_method'];
+                        $checkResult = $checkClass->$checkMethod($bean);
+                    }
+                }
+
+                elseif(!empty($check['check_method'])) {
+                    $checkResult = $this->runCheckResults($check['check_method'], $bean);
+                }
+            }
+
+            $data[] = [
+                'checkid' => $check['id'],
+                'name' => $check['text'],
+                'label' => $check['check_label'],
+                'result' => $checkResult
+            ];
         }
 
-        return [
-            'statusfield' => $object['status_field'],
-            'stages' => $retStages
-        ];
+        return $data;
     }
 
 
     public function getStages($module, $beanid = '')
     {
         global $current_language;
-$db = DBManagerFactory::getInstance();
+        $db = DBManagerFactory::getInstance();
 
         if (!empty($beanid))
             $focus = BeanFactory::getBean($module, $beanid);
@@ -81,6 +163,8 @@ $db = DBManagerFactory::getInstance();
         while ($stage = $db->fetchByAssoc($stagesObj)) {
             // set the stage - for multi stage take the first or the one that is active if it is not the first
             if (!isset($stages[$stage['stage']]) || $stage['stage'] . $stage['secondary_stage'] == $focus->$statusField) {
+
+
                 $stages[$stage['stage']] = $stage;
                 $stages[$stage['stage']]['stage_description'] = html_entity_decode($stage['stage_description']);
                 $stages[$stage['stage']]['stage_label'] = html_entity_decode($stage['stage_label']);
@@ -91,6 +175,9 @@ $db = DBManagerFactory::getInstance();
                 // perform checks
                 $stages[$stage['stage']]['checks'] = [];
                 $stages[$stage['stage']]['checkcontent'] = '';
+
+
+
                 $checks = $db->query("SELECT sc.*, sct.text FROM spicebeanguidestages_checks sc LEFT JOIN spicebeanguidestages_check_texts sct on sc.id = sct.stage_check_id AND sct.language='$current_language' WHERE sc.spicebeanguide_id = '" . $object['id'] . "' AND sc.stage_id = '" . $stage['id'] . "' ORDER BY sc.check_sequence");
 //                if (!empty($beanid)) {
                 while ($check = $db->fetchByAssoc($checks)) {
