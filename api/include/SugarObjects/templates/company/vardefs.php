94e1a7f77f69b2dada91855f973a4d072d4879b6
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2024-04-12 17:11:02 +0200

    vardefs: company, person: relationship with email_addresses refactored type to "email-address"

--- api/include/SugarObjects/templates/company/vardefs.php
+++ api/include/SugarObjects/templates/company/vardefs.php
@@ -471,7 +471,7 @@ $vardefs = [
             [
                 'lhs_module' => $module, 'lhs_table' => strtolower($module), 'lhs_key' => 'id',
                 'rhs_module' => 'EmailAddresses', 'rhs_table' => 'email_addresses', 'rhs_key' => 'id',
-                'relationship_type' => 'many-to-many',
+                'relationship_type' => 'email-address',
                 'join_table' => 'email_addr_bean_rel', 'join_key_lhs' => 'bean_id', 'join_key_rhs' => 'email_address_id',
                 'relationship_role_column' => 'bean_module',
                 'relationship_role_column_value' => $module
@@ -479,7 +479,7 @@ $vardefs = [
         strtolower($object_name) . '_email_addresses_primary' =>
             ['lhs_module' => $module, 'lhs_table' => strtolower($module), 'lhs_key' => 'id',
                 'rhs_module' => 'EmailAddresses', 'rhs_table' => 'email_addresses', 'rhs_key' => 'id',
-                'relationship_type' => 'many-to-many',
+                'relationship_type' => 'email-address',
                 'join_table' => 'email_addr_bean_rel', 'join_key_lhs' => 'bean_id', 'join_key_rhs' => 'email_address_id',
                 'relationship_role_column' => 'primary_address',
                 'relationship_role_column_value' => '1'
