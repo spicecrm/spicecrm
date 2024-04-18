94b50c2943f91c104a0da52e36bd975efbef212f
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2024-04-18 14:16:25 +0200

    SpicePath: adjusted support multiple kanbans

--- src/include/spicepath/components/spicepathwithcoaching.ts
+++ src/include/spicepath/components/spicepathwithcoaching.ts
@@ -64,7 +64,9 @@ export class SpicePathWithCoaching {
      */
     public beanStagesChecksResults: any[];
 
-    public componentconfig: any = {};
+    public componentconfig: {coachingVisible?: boolean, kanban?: string} = {};
+
+    public stages: any[] = [];
 
     constructor(public configuration: configurationService, public model: model, public language: language, public backend: backend, public metadata: metadata) {
         this.componentconfig = this.metadata.getComponentConfig('SpicePathWithCoaching', this.model.module);
@@ -77,6 +79,13 @@ export class SpicePathWithCoaching {
      * retrieve results for checks on load
      */
     public ngOnInit() {
+
+        if (this.componentconfig && this.componentconfig.coachingVisible) {
+            this.coachingVisible = this.componentconfig.coachingVisible;
+        }
+
+        this.setStages();
+
         this.backend.getRequest("common/spicebeanguide/" + this.model.module + "/" + this.model.id).subscribe(stages => {
             this.beanStagesChecksResults = stages;
         });
@@ -96,10 +105,21 @@ export class SpicePathWithCoaching {
     }
 
     /**
-     * returns the stages for the module from teh configuration service
+     * set the stages for the module from the configuration service
      */
-    get stages() {
-        return this.configuration.getData('spicebeanguides')[this.model.module].stages;
+    private setStages() {
+
+        const config = this.metadata.getComponentConfig('SpicePathWithCoaching', this.model.module);
+        const moduleKanbans = this.configuration.getData('spicebeanguides')[this.model.module];
+
+        if (!Array.isArray(moduleKanbans) || moduleKanbans.length == 0) return;
+
+        if (!config?.kanban) {
+            this.stages = moduleKanbans[0].stages;
+
+        } else {
+            this.stages = moduleKanbans.find(k => k.id == config.kanban)?.stages ?? [];
+        }
     }
 
     /**
