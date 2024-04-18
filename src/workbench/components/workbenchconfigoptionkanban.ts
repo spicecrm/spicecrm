642f982f7daa5b2e7ba03e409902619dff5ec87a
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2024-04-18 14:16:46 +0200

    WorkbenchConfig: pass module as input

--- src/workbench/components/workbenchconfigoptionkanban.ts
+++ src/workbench/components/workbenchconfigoptionkanban.ts
@@ -1,35 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {configurationService} from "../../services/configuration.service";
 import {view} from "../../services/view.service";
 import {language} from "../../services/language.service";
-import {WorkbenchService} from "../services/workbench.service";
 
 @Component({
     selector: 'workbench-config-option-kanban',
     templateUrl: '../templates/workbenchconfigoptionkanban.html'
 })
 
-export class WorkbenchConfigOptionKanban {
+export class WorkbenchConfigOptionKanban implements OnInit {
     public configValues: any = [];
     public option: any = {};
     public disabled: boolean = false;
     public moduleKanbans: any[] = [];
+    public module: string;
 
     constructor(private configurationService: configurationService,
                 public language: language,
-                public workbenchService: WorkbenchService,
                 public view: view) {
-        this.setModuleKanbans();
     }
 
-    get module(): string {
-        return this.workbenchService.activeModule;
+    public ngOnInit() {
+        this.setModuleKanbans();
     }
 
     /**
      * set module kanbans
      */
     public setModuleKanbans() {
-        this.moduleKanbans = this.configurationService.getData('spicebeanguides')[this.workbenchService.activeModule];
+        this.moduleKanbans = this.configurationService.getData('spicebeanguides')[this.module];
     }
 }
