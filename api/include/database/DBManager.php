64299d46b4e85401ce9e5b45443cf82b6202170f
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2024-04-23 17:34:44 +0200

    Revert "DBManager: save audit records only if the audit table exists"
    This reverts commit ae65829abbdf1a23573d64dc6e8434ba49eb8f2b.

--- api/include/database/DBManager.php
+++ api/include/database/DBManager.php
@@ -2915,13 +2915,7 @@ abstract class DBManager
      */
     public function save_audit_records(SpiceBean $bean, $changes)
     {
-        $auditTableName = $bean->get_audit_table_name();
-
-        if (!$bean->db->tableExists($auditTableName)) {
-            return false;
-        } else {
-            return $this->query($this->auditSQL($bean, $changes));
-        }
+        return $this->query($this->auditSQL($bean, $changes));
     }
 
     /**
