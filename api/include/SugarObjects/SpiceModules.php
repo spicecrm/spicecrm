0f859e2cc69658952c806a1c91400e27bb7ea45c
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2024-05-07 10:28:29 +0200

    Dictionary: audited set for legacy vardefs in module loader cast to bool

--- api/include/SugarObjects/SpiceModules.php
+++ api/include/SugarObjects/SpiceModules.php
@@ -82,7 +82,7 @@ class SpiceModules
                         $module['audited'] = (new SpiceDictionaryDefinition($module['sysdictionarydefinition_id']))->getDefinition()->audited == 1;
                     } catch (\Throwable) {}
                 } else {
-                    $module['audited'] = SpiceDictionary::getInstance()->getDefs($module['bean'])['audited'];
+                    $module['audited'] = (bool) SpiceDictionary::getInstance()->getDefs($module['bean'])['audited'];
                 }
 
                 $this->modules[$module['module']] = $module;
