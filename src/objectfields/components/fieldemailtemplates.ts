507e18a13de19ecf0cdc750e6159ae25caa2c5e3
maretval <valerie.maret@twentyreasons.com>
2024-05-24 14:03:02 +0200

    CR1001591 allow overwriting of email subject when changing Template

--- src/objectfields/components/fieldemailtemplates.ts
+++ src/objectfields/components/fieldemailtemplates.ts
@@ -101,11 +101,18 @@ export class fieldEmailTemplates extends fieldGeneric implements OnInit {
         if (this.value != '') {
             this.modal.openModal('SystemLoadingModal', false).subscribe(modalRef => {
                 this.backend.getRequest('module/EmailTemplates/' + this.value + '/parse/' + this.model.getFieldValue('parent_type') + '/' + this.model.getFieldValue('parent_id')).subscribe((data: any) => {
-                    // nur überschreiben wenn nicht bereits ein subject angegeben wurde.
+                    // overwrite if no subject or if action confirmed
                     if (!this.model.getField(this.subjectField)) {
                         this.model.setField(this.subjectField, data.subject);
                     }
-
+                    else {
+                        this.modal.confirm('LBL_OVERWRITE_SUBJECT', 'LBL_OVERWRITE_SUBJECT')
+                            .subscribe(answer => {
+                                if (answer) {
+                                    this.model.setField(this.subjectField, data.subject);
+                                }
+                            });
+                    }
                     // create a new document to manage the current html string (body)
                     let virtualDocument = document.implementation.createHTMLDocument("Virtual Document");
                     virtualDocument.documentElement.innerHTML = this.model.getFieldValue(this.bodyField);
