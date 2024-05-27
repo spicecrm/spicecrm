6a9024e48a282b8988fc0809287c477e6ef20676
maretval <valerie.maret@twentyreasons.com>
2024-05-27 21:55:38 +0200

    SpiceCRM version 2024.01.001

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2023.03.001.1704816688524",
+    buildNumber: "2023.03.001.1716839585631",
     copyright: "© 2015 -  2024 aac services k.s. All rights reserved."
     }
