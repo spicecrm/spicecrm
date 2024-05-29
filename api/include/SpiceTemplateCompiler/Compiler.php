30e6d64cf25043cdc7fb2d21e147a79e4be7d0ba
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2024-05-29 18:00:01 +0200

    Revert "CR1001325: Compiler: addNecessaryHeadTags process only with html tag."
    This reverts commit 12fc6e5bf64a7184bfd589bf678c00a224410157.

--- api/include/SpiceTemplateCompiler/Compiler.php
+++ api/include/SpiceTemplateCompiler/Compiler.php
@@ -149,10 +149,6 @@ class Compiler
      */
     private function addNecessaryHeadTags(): void
     {
-        $html = $this->doc->getElementsByTagName('html')[0];
-
-        if (!$html) return;
-
         $head = $this->root->getElementsByTagName('head')[0];
 
         if (!$head) {
