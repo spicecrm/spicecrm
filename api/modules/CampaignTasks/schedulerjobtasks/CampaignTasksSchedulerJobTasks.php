c2a7260343db0d4b2068fe9a4353dbc046f8039e
maretval <valerie.maret@twentyreasons.com>
2024-06-05 08:49:49 +0200

    CR1001248: updated use of the deprecated function

--- api/modules/CampaignTasks/schedulerjobtasks/CampaignTasksSchedulerJobTasks.php
+++ api/modules/CampaignTasks/schedulerjobtasks/CampaignTasksSchedulerJobTasks.php
@@ -18,6 +18,6 @@ class CampaignTasksSchedulerJobTasks
 
     public function sendCampaignTaskFeedbacks(): bool {
         $campaignTask = BeanFactory::getBean('CampaignTasks');
-        return $campaignTask->genereateServiceFeedbacks();
+        return $campaignTask->generateServiceFeedbacks();
     }
 }
