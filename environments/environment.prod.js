868eb1b3db276a2db9c78c8f42ff480d5762284a
strombo <andreas@gloeckl.name>
2024-07-05 15:59:29 +0200

    packaging

--- environments/environment.prod.js
+++ environments/environment.prod.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.environment = void 0;
 exports.environment = {
     production: true,
-    buildNumber: "2024.02.001.1719833948508",
+    buildNumber: "2024.02.001.1720187615489",
     copyright: "© 2015 -  2024 SpiceCRM FlexCo All rights reserved."
 };
