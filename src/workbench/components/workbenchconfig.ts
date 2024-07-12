d9efd108e6922bfcd4f5a052f4de049e1c771760
vasiljev <nenad.vasiljev@twentyreasons.com>
2024-07-12 11:53:58 +0200

    CR1001387: removed the options that are not corresponding to that field type

--- src/workbench/components/workbenchconfig.ts
+++ src/workbench/components/workbenchconfig.ts
@@ -37,8 +37,12 @@ export class WorkbenchConfig implements OnChanges {
 
     public ngOnChanges(changes: SimpleChanges) {
 
-        // remove any options elements in case some exist
+        // remove any options elements in case some exist and remove keys that are not in the corresponding field type options
         for (let option of this.optionsElements) {
+            let optionType = option.instance.option.option;
+            if(optionType in this.configValues) {
+                delete this.configValues[optionType]
+            }
             option.destroy();
         }
         this.optionsElements = [];
