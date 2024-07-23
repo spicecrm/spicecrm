9461db158e74c794b1b6d8b131a0db1d67b382bd
Christian Knoll <christian@spicecrm.com>
2024-07-23 13:47:16 +0200

    CR1001389 dropcontact and linkedinprofiles

---
+++ src/modules/linkedinprofiles/components/linkedinprofileheader.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import {modal} from "../../../services/modal.service";
+import {ModuleLinkedInProfiles} from "../linkedinprofiles";
+
+/* @ignore */
+
+@Component({
+    selector: "linkedinprofiles-header",
+    templateUrl: "../templates/inkedinprofilesheader.html"
+})
+
+export class LinkedInProfilesHeader  {
+
+
+}
