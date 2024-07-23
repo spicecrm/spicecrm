f0b067815db6180bc298fd6988ed60cb8e4def82
vasiljev <nenad.vasiljev@twentyreasons.com>
2024-07-23 16:12:35 +0200

    template linkedin

--- src/modules/linkedinprofiles/components/linkedinprofileheader.ts
+++ src/modules/linkedinprofiles/components/linkedinprofileheader.ts
@@ -11,9 +11,20 @@ import {model} from "../../../services/model.service";
 
 export class LinkedInProfilesHeader  {
 
-
     constructor(
         public model: model
     ) {
+    }
+
+    public cssStyles() {
+        return {
+            'border-radius': '50%',
+            'margin-top': '-100px',
+            'margin-left': '30px',
+            'width': '150px',
+            'border-width': '5px',
+            'border-color': 'white',
+            'border-style': 'solid'
+        }
     }
 }
