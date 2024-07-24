3db2a03a70ab2d552a3436dd9a8dc0567239cf6c
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2024-07-24 11:38:08 +0200

    packaging

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2024.02.001.1720608743928",
+    buildNumber: "2024.02.001.1721813730007",
     copyright: "© 2015 -  2024 SpiceCRM FlexCo All rights reserved."
     }
