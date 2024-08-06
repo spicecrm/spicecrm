38ca3603adcd7d1ca752bdcfabddacb680de26c6
strombo <andreas@gloeckl.name>
2024-08-06 12:28:53 +0200

    cleanup: removed unnecessary "use definitions"

--- api/modules/Events/api/controllers/EventsController.php
+++ api/modules/Events/api/controllers/EventsController.php
@@ -3,18 +3,12 @@
 
 namespace SpiceCRM\modules\Events\api\controllers;
 
-use SpiceCRM\includes\database\DBManagerFactory;
 use SpiceCRM\includes\ErrorHandlers\Exception;
-use SpiceCRM\includes\ErrorHandlers\ForbiddenException;
-use SpiceCRM\includes\utils\DBUtils;
 use SpiceCRM\data\BeanFactory;
 use SpiceCRM\includes\ErrorHandlers\NotFoundException;
-use SpiceCRM\data\api\handlers\SpiceBeanHandler;
 use SpiceCRM\includes\authentication\AuthenticationController;
-use SpiceCRM\modules\SpiceACL\SpiceACL;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use SpiceCRM\includes\SpiceSlim\SpiceResponse as Response;
-use SpiceCRM\includes\TimeDate;
 
 class EventsController
 {
