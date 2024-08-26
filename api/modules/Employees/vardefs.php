8fe9f2c37a33ad6884bcfc068b23d7d1fd70308d
maretval <valerie.maret@twentyreasons.com>
2024-08-26 19:31:56 +0200

    CR1001401: migrated HCMMeasures, HCMSkills, HCMTrainings vardefs

--- api/modules/Employees/vardefs.php
+++ api/modules/Employees/vardefs.php
@@ -14,30 +14,31 @@ SpiceDictionaryHandler::getInstance()->dictionary['Employee'] = [
             'source' => 'non-db',
             'module' => 'Users'
         ],
-        'hcmmeasures' => [
-            'name' => 'hcmmeasures',
-            'vname' => 'LBL_HCMMEASURES',
-            'type' => 'link',
-            'relationship' => 'employees_hcmmeasures',
-            'source' => 'non-db',
-            'module' => 'HCMMeasures'
-        ],
-        'hcmskills' => [
-            'name' => 'hcmskills',
-            'vname' => 'LBL_HCMSKILLS',
-            'type' => 'link',
-            'relationship' => 'employees_hcmskills',
-            'source' => 'non-db',
-            'module' => 'HCMSkills'
-        ],
-        'hcmtrainings' => [
-            'name' => 'hcmtrainings',
-            'vname' => 'LBL_HCMTRAININGS',
-            'type' => 'link',
-            'relationship' => 'employees_hcmtrainings',
-            'source' => 'non-db',
-            'module' => 'HCMTrainings'
-        ],
+// migrated to dictionary
+//        'hcmmeasures' => [
+//            'name' => 'hcmmeasures',
+//            'vname' => 'LBL_HCMMEASURES',
+//            'type' => 'link',
+//            'relationship' => 'employees_hcmmeasures',
+//            'source' => 'non-db',
+//            'module' => 'HCMMeasures'
+//        ],
+//        'hcmskills' => [
+//            'name' => 'hcmskills',
+//            'vname' => 'LBL_HCMSKILLS',
+//            'type' => 'link',
+//            'relationship' => 'employees_hcmskills',
+//            'source' => 'non-db',
+//            'module' => 'HCMSkills'
+//        ],
+//        'hcmtrainings' => [
+//            'name' => 'hcmtrainings',
+//            'vname' => 'LBL_HCMTRAININGS',
+//            'type' => 'link',
+//            'relationship' => 'employees_hcmtrainings',
+//            'source' => 'non-db',
+//            'module' => 'HCMTrainings'
+//        ],
         'orgunit_id' => [
             'vname' => 'LBL_ORGUNIT_ID',
             'name' => 'orgunit_id',
