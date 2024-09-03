95894be587db8e0c9c194edbc31873aa5fcc7543
Dawidbestrzynski <dawid.bestrzynski@twentyreasons.com>
2024-09-03 12:56:40 +0200

    Kanban migration route

--- api/include/SpiceBeanGuides/api/controllers/SpiceBeanGuidesKanbanMigrationController.php
+++ api/include/SpiceBeanGuides/api/controllers/SpiceBeanGuidesKanbanMigrationController.php
@@ -2,10 +2,13 @@
 
 namespace SpiceCRM\includes\SpiceBeanGuides\api\controllers;
 
+use Psr\Http\Message\ServerRequestInterface as Request;
+use SpiceCRM\includes\SpiceSlim\SpiceResponse as Response;
+
 class SpiceBeanGuidesKanbanMigrationController
 {
-    public function migrateKanban(): bool
+    public function migrateKanban(Request $req, Response $res, $args): Response
     {
-        return 'test';
+        return $res->withJson('tests');
     }
 }
