26b9f359c0b467e112186995a8cfe64b03807fcf
maretval <valerie.maret@twentyreasons.com>
2024-09-04 11:53:23 +0200

    HCMEmployeeObjectives: migrated vardefs partially

--- api/modules/Employees/vardefs.php
+++ api/modules/Employees/vardefs.php
@@ -152,22 +152,23 @@ SpiceDictionaryHandler::getInstance()->dictionary['Employee'] = [
             'source' => 'non-db',
             'vname' => 'LBL_TASKS',
         ],
-        'hcmemployeeobjectiveagreements' => [
-            'name' => 'hcmemployeeobjectiveagreements',
-            'vname' => 'LBL_HCMEMPLOYEE_OBJECTIVE_AGREEMENTS',
-            'type' => 'link',
-            'relationship' => 'employees_hcmemployeeobjectiveagreements',
-            'source' => 'non-db',
-            'module' => 'HCMEmployeeObjectiveAgreements'
-        ],
-        'hcmemployeeobjectives' => [
-            'name' => 'hcmemployeeobjectives',
-            'vname' => 'LBL_HCMEMPLOYEE_OBJECTIVES',
-            'type' => 'link',
-            'relationship' => 'employees_hcmemployeeobjectives',
-            'source' => 'non-db',
-            'module' => 'HCMEmployeeObjectives'
-        ],
+// migrated
+//        'hcmemployeeobjectiveagreements' => [
+//            'name' => 'hcmemployeeobjectiveagreements',
+//            'vname' => 'LBL_HCMEMPLOYEE_OBJECTIVE_AGREEMENTS',
+//            'type' => 'link',
+//            'relationship' => 'employees_hcmemployeeobjectiveagreements',
+//            'source' => 'non-db',
+//            'module' => 'HCMEmployeeObjectiveAgreements'
+//        ],
+//        'hcmemployeeobjectives' => [
+//            'name' => 'hcmemployeeobjectives',
+//            'vname' => 'LBL_HCMEMPLOYEE_OBJECTIVES',
+//            'type' => 'link',
+//            'relationship' => 'employees_hcmemployeeobjectives',
+//            'source' => 'non-db',
+//            'module' => 'HCMEmployeeObjectives'
+//        ],
     ],
     'indices' => [],
     'relationships' => [
