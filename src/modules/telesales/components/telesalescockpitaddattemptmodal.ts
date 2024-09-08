83746c327b682f7ecd0b7ad148b4c0d6249bce42
Christian Knoll <christian@spicecrm.com>
2024-09-08 08:49:41 +0200

    minor fixes TeleCockpit

--- src/modules/telesales/components/telesalescockpitaddattemptmodal.ts
+++ src/modules/telesales/components/telesalescockpitaddattemptmodal.ts
@@ -11,6 +11,7 @@ import {backend} from "../../../services/backend.service";
 import {Observable, Subject} from 'rxjs';
 import {metadata} from "../../../services/metadata.service";
 import {telecockpitservice} from "../services/telecockpit.service";
+import {modal} from "../../../services/modal.service";
 
 /**
  * @ignore
@@ -34,6 +35,7 @@ export class TeleSalesCockpitAddAttemptModal implements OnInit {
     constructor(
         public language: language,
         public model: model,
+        public modal: modal,
         public modelutilities: modelutilities,
         public toast: toast,
         public backend: backend,
@@ -51,9 +53,13 @@ export class TeleSalesCockpitAddAttemptModal implements OnInit {
         this.setEditMode();
     }
 
+    /**
+     * initilizes the model
+     */
     public initializeModel() {
         this.model.module = 'CampaignLog';
         this.model.id = this.selectedListItem.id;
+        this.model.initializeModel();
         this.model.setData({
             hits: this.selectedListItem.hits,
             planned_activity_date: new moment().add(1, 'days'),
@@ -79,6 +85,8 @@ export class TeleSalesCockpitAddAttemptModal implements OnInit {
     }
 
     public save() {
+
+        let awaitModal = this.modal.await('LBL_SAVING')
         let planned_activity_date = this.modelutilities.spice2backend(this.model.module, 'planned_activity_date', this.model.getField('planned_activity_date'));
         let params = {planned_activity_date: planned_activity_date};
 
@@ -89,10 +97,16 @@ export class TeleSalesCockpitAddAttemptModal implements OnInit {
                     this.responseSubject.next(true);
                     this.responseSubject.complete();
                     this.telecockpit.loadStats();
+
+                    awaitModal.emit(true);
+
                     this.self.destroy();
 
                 } else {
                     this.toast.sendToast(this.language.getLabel('ERR_FAILED_TO_EXECUTE'), 'error');
+
+                    awaitModal.emit(true);
+
                     this.self.destroy();
                 }
             },
@@ -104,8 +118,8 @@ export class TeleSalesCockpitAddAttemptModal implements OnInit {
 
     public remove() {
         this.backend.postRequest(`module/CampaignLog/${this.model.id}/completed`)
-            .subscribe(
-                status => {
+            .subscribe({
+                next: (status) => {
                     if (status.success) {
                         this.toast.sendToast(this.language.getLabel('LBL_DATA_SAVED'), 'success');
                         this.responseSubject.next(true);
@@ -114,6 +128,9 @@ export class TeleSalesCockpitAddAttemptModal implements OnInit {
                         this.toast.sendToast(this.language.getLabel('ERR_FAILED_TO_EXECUTE'), 'error');
                     }
                 },
-                err => this.toast.sendToast(this.language.getLabel('ERR_NETWORK'), 'error'));
+                error: (err) => {
+                    this.toast.sendToast(this.language.getLabel('ERR_NETWORK'), 'error');
+                }
+            });
     }
 }
