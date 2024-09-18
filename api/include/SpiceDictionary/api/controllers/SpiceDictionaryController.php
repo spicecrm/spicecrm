4172ec30efdfab1790aaa647b1153757d0da9078
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2024-09-18 16:35:52 +0200

    CR1001349: Dictionary Repair all: fix loading template relationships

--- api/include/SpiceDictionary/api/controllers/SpiceDictionaryController.php
+++ api/include/SpiceDictionary/api/controllers/SpiceDictionaryController.php
@@ -373,7 +373,21 @@ class SpiceDictionaryController
                 if($isDuplicate) break;
 
             }
-            if($isDuplicate || !SpiceModules::getInstance()->getModuleByDictionaryDefinitionId($rel['lhs_sysdictionarydefinition_id']) || !SpiceModules::getInstance()->getModuleByDictionaryDefinitionId($rel['rhs_sysdictionarydefinition_id'])) continue;
+
+            if($isDuplicate) continue;
+
+            $leftId = $rel['lhs_sysdictionarydefinition_id'];
+            $rightId = $rel['rhs_sysdictionarydefinition_id'];
+
+            # if one of the sides is a template, set the side id as the reference dictionary id
+            if ((new SpiceDictionaryDefinition($rel['lhs_sysdictionarydefinition_id']))->type == 'template') {
+                $leftId = $rel['referencing_sysdictionarydefinition_id'];
+            } else if ((new SpiceDictionaryDefinition($rel['rhs_sysdictionarydefinition_id']))->type == 'template') {
+                $rightId = $rel['referencing_sysdictionarydefinition_id'];
+            }
+
+            # check if both sides modules exist, otherwise ignore the relationship
+            if (!SpiceModules::getInstance()->getModuleByDictionaryDefinitionId($leftId) || !SpiceModules::getInstance()->getModuleByDictionaryDefinitionId($rightId)) continue;
 
             // if this is considered unique ... go for it
             $spiceDictionaryRelationships[] = $rel;
