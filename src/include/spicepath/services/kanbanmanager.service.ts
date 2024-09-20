90ca1df5c22566e26434f93095a66098c66fa089
BorisVlaovic <boris.vlaovic.kovacev@twentyreasons.com>
2024-09-20 16:29:14 +0200

    CR1001450 Created a setDefault method for the Kanban Manager that sets a Default Kanban for the selected Module

--- src/include/spicepath/services/kanbanmanager.service.ts
+++ src/include/spicepath/services/kanbanmanager.service.ts
@@ -42,6 +42,10 @@ export class KanbanManagerService {
     public minimized: boolean = false;
 
     public selectedStage: SpiceBeanGuideStageI;
+    /**
+     * workbench edit mode
+     */
+    public editMode: 'all' | 'custom' | 'none';
 
     public constructor(
         public backend: backend,
@@ -50,6 +54,7 @@ export class KanbanManagerService {
         public changeService: ChangeHistoryService,
         private configurationService: configurationService
     ) {
+        this.editMode = this.configurationService.getCapabilityConfig('core').edit_mode;
         this.loadItems();
         this.loadChecks();
         this.loadValidations();
