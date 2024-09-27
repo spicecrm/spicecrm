fdf2c3e9134aa1c83c81f597795493a131e4a79e
Maria Zakharova <maria.zakharova@twentyreasons.com>
2024-09-27 14:43:39 +0200

    CR1001457: lock time to minutes, remove debug line

--- src/services/navigation.service.ts
+++ src/services/navigation.service.ts
@@ -625,7 +625,6 @@ export class navigation {
         this.modelregister.push({id: id, model: model, tabid: this.activeTab});
 
         this.socket.joinRoom('module', Md5.hashStr(`${model.module}:${model.id}`).toString());
-        console.log('socket join room', model.module, model.id, Md5.hashStr(`${model.module}:${model.id}`).toString());
 
         return id;
     }
