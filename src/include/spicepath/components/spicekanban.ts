b153f503eab96e09e67206fa7e6a8e896f7e9937
Christian Knoll <christian@spicecrm.com>
2024-10-04 11:08:23 +0200

    kanban fix prevent attempt unsubscribe from undefined

--- src/include/spicepath/components/spicekanban.ts
+++ src/include/spicepath/components/spicekanban.ts
@@ -277,7 +277,7 @@ export class SpiceKanban implements OnInit, OnDestroy {
      */
     public ngOnDestroy() {
         // unsubscribe
-        this.modellistsubscribe.unsubscribe();
+        this.modellistsubscribe?.unsubscribe();
 
         // reset buckets
         this.modellist.buckets = {};
