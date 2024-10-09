037bf10f95b3ba2c7640e77cc371f8d7c13bb024
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2024-10-09 15:49:41 +0200

    CR1001465: Template compiler category tree field display value.

--- api/include/SpiceTemplateCompiler/Compiler.php
+++ api/include/SpiceTemplateCompiler/Compiler.php
@@ -725,6 +725,9 @@ class Compiler
                             $value = $this->app_list_strings[$obj->field_defs[$part]['options']][$obj->{$part}];
                         }
                         break;
+                    case 'categories':
+                        $value = SpiceUtils::renderCategoryTreeEntry($obj, $obj->_module, $part, $this->lang);
+                        break;
                     case 'multienum':
                         $value = $obj->{$part};
                         if(!$keepFetchedRowValue) {
@@ -886,6 +889,9 @@ class Compiler
                         break;
                     case 'enum':
                         $value = $raw ? $obj->{$part} : $this->app_list_strings[$obj->field_defs[$part]['options']][$obj->{$part}];
+                        break;
+                    case 'categories':
+                        $value = SpiceUtils::renderCategoryTreeEntry($obj, $obj->_module, $part, $this->lang);
                         break;
                     case 'multienum':
                         $values = explode(',', $obj->{$part});
