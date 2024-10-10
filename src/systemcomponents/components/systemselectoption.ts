75a97eec362a13d402e91dbed7ec1a129eb788a9
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2024-10-10 18:05:35 +0200

    CR1001470: Field Enum: use system select. Show inactive options. System select implemented inactive.

--- src/systemcomponents/components/systemselectoption.ts
+++ src/systemcomponents/components/systemselectoption.ts
@@ -18,5 +18,8 @@ export class SystemSelectOption {
      * value of the option
      */
     @Input() public group: string;
-
+    /**
+     * to highlight the inactive option in the dropdown
+     */
+    @Input() public inactive: boolean = false;
 }
