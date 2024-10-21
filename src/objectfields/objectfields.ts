e1ccb48565d34ca71b8a9f0bfa00083f0f7bf722
Christian Knoll <christian@spicecrm.com>
2024-10-21 16:10:28 +0200

    CR1001480 - added a probability field

--- src/objectfields/objectfields.ts
+++ src/objectfields/objectfields.ts
@@ -21,6 +21,7 @@ import {fieldContainer} from './components/fieldcontainer';
 import {fieldGenericDisplay} from './components/fieldgenericdisplay';
 import {fieldText} from './components/fieldtext';
 import {fieldNumber} from './components/fieldnumber';
+import {fieldProbability} from "./components/fieldprobability";
 import {fieldHtml} from './components/fieldhtml';
 import {fieldHtmlEditor} from './components/fieldhtmleditor';
 import {fieldWYSIWYGEditor} from './components/fieldwysiwygeditor';
@@ -138,6 +139,7 @@ import {fieldPackage} from "./components/fieldpackage";
 import {fieldActivityStatus} from "./components/fieldactivitystatus";
 
 
+
 /**
  * @ignore
  */
@@ -166,6 +168,7 @@ declare var _: any;
         fieldGenericDisplay,
         fieldText,
         fieldNumber,
+        fieldProbability,
         fieldHtml,
         fieldHtmlEditor,
         fieldWYSIWYGEditor,
