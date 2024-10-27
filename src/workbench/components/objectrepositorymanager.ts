756a439b1f472ab7439d051b67c22013f931d7f1
Christian Knoll <christian@spicecrm.com>
2024-10-27 09:59:32 +0100

    fix default set in objectrepo manager

--- src/workbench/components/objectrepositorymanager.ts
+++ src/workbench/components/objectrepositorymanager.ts
@@ -47,7 +47,7 @@ export class ObjectRepositoryManager {
     public crNoneActive: boolean = false;
     public moduleReposSelect: {id: string, name: string, group: string}[] = [];
     public moduleRepos: any[] = [];
-    public modulereposselecteditem: any = {};
+    public modulereposselecteditem: any;
     public objrepoList: any[] = [];
     public configList: any = {};
     public currentConfigArray: any[] = [];
