4f3070df6e4df54b10eb25aa70b9192d28afc713
Christian Knoll <christian@spicecrm.com>
2024-10-29 11:56:13 +0100

    sort field currencies by currency name

--- src/objectfields/components/fieldcurrencies.ts
+++ src/objectfields/components/fieldcurrencies.ts
@@ -24,7 +24,7 @@ export class fieldCurrencies extends fieldGeneric {
     constructor(public model: model, public view: view, public language: language, public metadata: metadata, public router: Router, public currency: currency) {
         super(model, view, language, metadata, router);
 
-        this.currencies = this.currency.getCurrencies();
+        this.currencies = this.currency.getCurrencies().sort((a, b) => a.name.localeCompare(b.name));
     }
 
     public getCurrencySymbol() {
