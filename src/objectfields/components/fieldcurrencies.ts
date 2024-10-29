368e883f13fc19a4c195b8e7f552d8292ded6dbc
Christian Knoll <christian@spicecrm.com>
2024-10-29 12:42:50 +0100

    CR1001488 systemcurrencies .. update currency field and remove currency module vardefs

--- src/objectfields/components/fieldcurrencies.ts
+++ src/objectfields/components/fieldcurrencies.ts
@@ -27,6 +27,22 @@ export class fieldCurrencies extends fieldGeneric {
         this.currencies = this.currency.getCurrencies();
     }
 
+    /**
+     * a getter for the value bound top the model
+     */
+    get value() {
+        return this.model.getField(this.fieldname);
+    }
+
+    /**
+     * a setter that returns the value to the model and triggers the validation
+     *
+     * @param val the new value
+     */
+    set value(val) {
+        this.model.setField(this.fieldname, val.id);
+    }
+
     public getCurrencySymbol() {
         let currencyid = -99;
 
