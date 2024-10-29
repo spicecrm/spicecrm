b3891a55513faf0dd5c3f64b61ae6bfb7dac1a9f
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2024-10-29 12:17:18 +0100

    CR1001459: require package pear.

--- api/include/SpiceInstaller/SpiceInstaller.php
+++ api/include/SpiceInstaller/SpiceInstaller.php
@@ -149,6 +149,10 @@ class SpiceInstaller
 
         $requirements['bcmath'] = extension_loaded('bcmath');
 
+        # check package pear
+        require_once 'System.php';
+        $requirements['pear'] = class_exists('System', false);
+
         // db check
         $drivers = $this->dbManagerFactory::getDbDrivers();
 
