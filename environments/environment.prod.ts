17a3e8f64c10f842ed3aec48af7790befe2a000d
natoku <natalia.okutansoy@twentyreasons.com>
2024-10-31 15:01:18 +0100

    packaging

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2024.02.001.1730360354169",
+    buildNumber: "2024.02.001.1730382378435",
     copyright: "© 2015 -  2024 SpiceCRM FlexCo All rights reserved."
     }
