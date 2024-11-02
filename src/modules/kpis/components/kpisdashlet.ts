ca76762be4541d35facda8c54a8e3bff6446c184
Christian Knoll <christian@spicecrm.com>
2024-11-02 08:45:07 +0100

    KPI Changes

--- src/modules/kpis/components/kpisdashlet.ts
+++ src/modules/kpis/components/kpisdashlet.ts
@@ -9,6 +9,8 @@ import {language} from "../../../services/language.service";
 import {toast} from "../../../services/toast.service";
 import {metadata} from "../../../services/metadata.service";
 import {view} from "../../../services/view.service";
+import {session} from "../../../services/session.service";
+import {layout} from "../../../services/layout.service";
 
 @Component({
     selector: 'kpis-dashlet',
@@ -18,9 +20,14 @@ import {view} from "../../../services/view.service";
 export class KPIsDashlet implements OnInit {
 
     /**
+     * indicates that we are loading
+     */
+    public loading: boolean = true;
+
+    /**
      * holds kpis for logged in User
      */
-    public kpis: any[] = [];
+    public kpiTargets: any[] = [];
 
     /**
      * parentId the KPITarget is related to
@@ -34,18 +41,15 @@ export class KPIsDashlet implements OnInit {
     parentType: string = 'Users';
 
     constructor(
-        public model: model,
-        public language: language,
-        private metadata: metadata,
-        private backend: backend,
-        private toast: toast,
-        private view: view
+        public session: session,
+        public backend: backend,
+        public toast: toast,
+        public layout: layout
     ) {
     }
 
     ngOnInit() {
-        this.parentId = this.metadata.session.authData.user.id;
-        this.model._module = 'KPIs';
+        this.parentId = this.session.authData.user.id;
         this.loadKPIs();
     }
 
@@ -53,24 +57,28 @@ export class KPIsDashlet implements OnInit {
      * load KPIs for logged-in User
      * @private
      */
-    private loadKPIs() {
-        this.backend.getRequest('module/KPIs/' + this.parentType + '/' + this.parentId).subscribe({
+    public loadKPIs() {
+        this.kpiTargets = [];
+        this.loading = true;
+        this.backend.getRequest(`module/KPIs/byparent/${this.parentType}/${this.parentId}`).subscribe({
             next: (data) => {
-                this.kpis = [...data];
+                this.kpiTargets = data;
+                this.loading = false;
             }, error: () => {
-                this.toast.sendToast(this.language.getLabel('LBL_ERR_LOADING_KPIS'), 'error');
+                this.toast.sendToast('LBL_ERR_LOADING_KPIS', 'error');
+                this.loading = false;
             }
         })
     }
 
     /**
-     * calcualate amount of tiles displayed
+     * calculate amount of tiles displayed
      * depending on screen width
      */
     get getTileWidthClass() {
-        if (this.view.layout.screenwidth == 'small') {
+        if (this.layout.screenwidth == 'small') {
             return 'slds-size--1-of-1';
-        } else if (this.view.layout.screenwidth == 'medium') {
+        } else if (this.layout.screenwidth == 'medium') {
             return 'slds-size--1-of-2';
         } else {
             return 'slds-size--1-of-3';
--- src/modules/kpis/components/kpitile.ts
+++ src/modules/kpis/components/kpitile.ts
@@ -3,6 +3,7 @@ import {model} from "../../../services/model.service";
 import {backend} from "../../../services/backend.service";
 import {toast} from "../../../services/toast.service";
 import {language} from "../../../services/language.service";
+import moment from "moment";
 
 @Component({
     selector: 'kpi-tile',
@@ -14,28 +15,17 @@ export class KpiTile implements OnInit {
     /**
      * id of KPI related to KPITarget
      */
-    @Input() kpi: any = {};
-
-    /**
-     * parentId the KPITarget is related to
-     */
-    @Input() parentId: string = '';
-
-    /**
-     * parentType the KPITarget is related to
-     * i.e. Users, CompanyCodes etc.
-     */
-    @Input() parentType: string = '';
-
-    /**
-     * holds KPITarget's data
-     */
-    public kpiTargetData: any = [];
+    @Input() kpiTarget: any = {};
 
     /**
      * holds KPITargetValue's data
      */
-    public kpiTargetValue: any = [];
+    public kpiTargetValue: any = {};
+
+    /**
+     * holds the tend data
+     */
+    public kpiTrendData: any = {};
 
     /**
      * if tile is being loaded
@@ -62,6 +52,11 @@ export class KpiTile implements OnInit {
      */
     public percentage: string = '';
 
+    /**
+     * a now moment object
+     */
+    public now = moment();
+
     constructor(
         public model: model,
         private backend: backend,
@@ -79,13 +74,11 @@ export class KpiTile implements OnInit {
      */
     public loadKPITargets() {
         this.loading = true;
-        this.backend.getRequest('module/KPIs/' + this.kpi.id + '/' + this.parentType + '/' + this.parentId).subscribe({
+        this.backend.getRequest(`module/KPITargets/${this.kpiTarget.id}/getvalues`).subscribe({
             next: (data) => {
-                this.kpiTargetData = data;
                 this.kpiTargetValue = data.kpiTargetValue;
+                this.kpiTrendData = data.kpiTrendData;
                 this.getKPIColor();
-                this.getTrend();
-
                 this.loading = false;
             }, error: () => {
                 this.toast.sendToast(this.language.getLabel('LBL_ERR_LOADING_KPITARGETS'), 'error');
@@ -98,9 +91,10 @@ export class KpiTile implements OnInit {
      * gets text color for the KPIValue
      */
     public getKPIColor() {
+        if(!this.kpiTargetValue) return;
         let kpiValue = this.kpiTargetValue.kpi_value;
-        let kpiTargetDataLow = this.kpiTargetData.kpiTarget?.lower_boundary;
-        let kpiTargetDataUp = this.kpiTargetData.kpiTarget?.upper_boundary;
+        let kpiTargetDataLow = this.kpiTarget.kpiTarget?.lower_boundary;
+        let kpiTargetDataUp = this.kpiTarget.kpiTarget?.upper_boundary;
 
         if (kpiTargetDataLow && kpiValue <= kpiTargetDataLow) {
             this.kpiColor = "slds-text-color_error";
@@ -111,26 +105,24 @@ export class KpiTile implements OnInit {
         }
     }
 
-    /**
-     * sets arrow Icon and its color
-     * depending on trend results from Backend
-     */
-    public getTrend() {
-        switch (this.kpiTargetData.trendData.trend) {
-            case 'positive':
-                this.arrowIcon = "arrowup";
-                this.iconColor = 'slds-icon-text-success';
-                this.percentage = this.kpiTargetData.trendData.percentage + '% ' + this.language.getLabel('LBL_KPI_BETTER');
-                break;
-            case 'negative':
-                this.arrowIcon = "arrowdown";
-                this.iconColor = "slds-icon-text-error";
-                this.percentage = this.kpiTargetData.trendData.percentage + '% ' + this.language.getLabel('LBL_KPI_WORSE');
-                break;
-            default:
-                this.arrowIcon = "sort";
-                this.iconColor = "slds-icon-text-default";
-                this.percentage = this.kpiTargetData.trendData.percentage + '% ' + this.language.getLabel('LBL_KPI_UNCHANGED');
-        }
+    get slope(){
+        return this.kpiTrendData?.trend?.slope ?? 0;
     }
+
+    get slopeIcon (){
+        if(this.slope > 0) return 'arrowup';
+        if(this.slope < 0) return 'arrowdown';
+        return 'sort'
+    }
+
+    get slopeColor(){
+        if(this.slope > 0) return 'slds-icon-text-success';
+        if(this.slope < 0) return 'slds-icon-text-error';
+        return 'slds-icon-text-default'
+    }
+
+    get trendValue(){
+        return this.kpiTrendData.percentage;
+    }
+
 }
