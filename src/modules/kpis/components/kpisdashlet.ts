6ee64cc9eb200279de1ce7f3f3efe9e8195594ff
Christian Knoll <christian@spicecrm.com>
2024-11-04 15:47:57 +0100

    KPI allow user selection and minor fix in history component

--- src/modules/kpis/components/kpisdashlet.ts
+++ src/modules/kpis/components/kpisdashlet.ts
@@ -3,12 +3,9 @@
  */
 
 import {Component, OnInit} from '@angular/core';
-import {model} from "../../../services/model.service";
 import {backend} from "../../../services/backend.service";
-import {language} from "../../../services/language.service";
 import {toast} from "../../../services/toast.service";
-import {metadata} from "../../../services/metadata.service";
-import {view} from "../../../services/view.service";
+import {modal} from "../../../services/modal.service";
 import {session} from "../../../services/session.service";
 import {layout} from "../../../services/layout.service";
 
@@ -40,17 +37,33 @@ export class KPIsDashlet implements OnInit {
      */
     parentType: string = 'Users';
 
+    /**
+     * the selected item
+     */
+    public selectedItem: any;
+
     constructor(
         public session: session,
         public backend: backend,
         public toast: toast,
-        public layout: layout
+        public layout: layout,
+        public modal: modal
     ) {
     }
 
     ngOnInit() {
         this.parentId = this.session.authData.user.id;
+        this.selectedItem = {
+            id: this.session.authData.user.id,
+            summary_text: this.session.authData.user.full_name,
+            module: 'Users',
+            data: this.session.authData.user
+        };
         this.loadKPIs();
+    }
+
+    get isAdmin(){
+        return this.session.authData.admin;
     }
 
     /**
@@ -83,5 +96,33 @@ export class KPIsDashlet implements OnInit {
         } else {
             return 'slds-size--1-of-3';
         }
+    }
+
+    public clearField() {
+        this.selectedItem = undefined;
+        this.parentId = undefined;
+        this.kpiTargets = [];
+    }
+
+    /**
+     * opens a model search modal
+     */
+    public searchWithModal() {
+        this.modal.openModal('ObjectModalModuleLookup').subscribe(selectModal => {
+            selectModal.instance.module = 'Users'
+            selectModal.instance.multiselect = false;
+                selectModal.instance.selectedItems.subscribe(items => {
+                    if (items.length) {
+                        this.selectedItem = {
+                            id: items[0].id,
+                            summary_text: items[0].summary_text,
+                            module: 'Users',
+                            data: items[0]
+                        };
+                        this.parentId = this.selectedItem.id;
+                        this.loadKPIs();
+                    }
+                });
+        });
     }
 }
