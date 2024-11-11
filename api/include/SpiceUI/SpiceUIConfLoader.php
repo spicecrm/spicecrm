df40739a633b49bf0b9f645d95f2943085bc36c7
Christian Knoll <christian@spicecrm.com>
2024-11-11 16:44:36 +0100

    break UNION select when atte,pting to find packages .. breaks when one table does n ot yet have a package field and stops reload

--- api/include/SpiceUI/SpiceUIConfLoader.php
+++ api/include/SpiceUI/SpiceUIConfLoader.php
@@ -551,30 +551,29 @@ class SpiceUIConfLoader
     public function getCurrentConf()
     {
         $db = DBManagerFactory::getInstance();
-        $qArray = [];
+
         $excludePackageCheck = ['systemdeploymentrpdbentrys'];
-        foreach($this->conftables as $conftable) {
-            if(!in_array($conftable, $excludePackageCheck) && $db->tableExists($conftable)){
-                $qArray[] = "(SELECT package, version FROM $conftable WHERE version is not null AND version <> '')";
-            }
-        }
-        $q = implode(" UNION ", $qArray) . " ORDER BY package, version";
-        $res = $db->query($q);
+
         $packages = [];
         $versions = [];
 
-        while ($row = $db->fetchByAssoc($res)) {
-            // skip system package
-            if($row['package'] == 'system') continue;
+        foreach($this->conftables as $conftable) {
+            if (!in_array($conftable, $excludePackageCheck) && $db->tableExists($conftable)) {
+                $res = $db->query("SELECT package, version FROM $conftable WHERE version is not null AND version <> ''");
+                while ($row = $db->fetchByAssoc($res)) {
+                    // skip system package
+                    if ($row['package'] == 'system') continue;
 
-            // check if it is loaded
-            if (!empty($row['package']) && !in_array($row['package'], $packages)) {
-                $packages[] = $row['package'];
-            } elseif (!in_array('core', $packages) && !in_array($row['package'], $packages)) {
-                $packages[] = 'core';
+                    // check if it is loaded
+                    if (!empty($row['package']) && !in_array($row['package'], $packages)) {
+                        $packages[] = $row['package'];
+                    } elseif (!in_array('core', $packages) && !in_array($row['package'], $packages)) {
+                        $packages[] = 'core';
+                    }
+                    if (!empty($row['version']) && !in_array($row['version'], $versions))
+                        $versions[] = $row['version'];
+                }
             }
-            if (!empty($row['version']) && !in_array($row['version'], $versions))
-                $versions[] = $row['version'];
         }
         return ['packages' => $packages, 'versions' => $versions];
     }
