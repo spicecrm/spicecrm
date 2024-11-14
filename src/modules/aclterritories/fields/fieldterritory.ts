6625a5bec0729c1e0ab67f8ac6c564db234a0cd1
Christian Knoll <christian@spicecrm.com>
2024-11-14 08:50:08 +0100

    minor core fixes territory field

--- src/modules/aclterritories/fields/fieldterritory.ts
+++ src/modules/aclterritories/fields/fieldterritory.ts
@@ -48,7 +48,7 @@ export class fieldTerritory extends fieldGeneric implements OnInit, OnDestroy {
 
         // if we have a new model determine a default territory
         if (this.model.isNew) {
-            let searchterritories = this.territories.searchTerritories(this.model.module, '', 2, [], 'create');
+            let searchterritories = this.territories.searchTerritories(this.model.module, '', undefined, [], 'create');
             if (searchterritories.length == 1) {
                 this.value = searchterritories[0].id;
                 this.model.setField(this.fieldname + '_name', searchterritories[0].name);
