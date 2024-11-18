e0d2ed569de39cbbe693070f1111927525b84a21
natoku <natalia.okutansoy@twentyreasons.com>
2024-11-18 15:22:20 +0100

    CR1001479: kpitile: remove error message when kpiTargetValue not found for the kpiTarget & user

--- src/modules/kpis/components/kpitile.ts
+++ src/modules/kpis/components/kpitile.ts
@@ -92,7 +92,6 @@ export class KpiTile implements OnInit {
                 this.getKPIColor();
                 this.loading = false;
             }, error: () => {
-                this.toast.sendToast(this.language.getLabel('LBL_ERR_LOADING_KPITARGETS'), 'error');
                 this.loading = false;
             }
         });
