5e1e588b01c25c718ae64fcf51d157974747e82b
Christian Knoll <christian@spicecrm.com>
2024-11-19 15:59:19 +0100

    packaging

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2024.02.001.1731999359268",
+    buildNumber: "2024.02.001.1732027535797",
     copyright: "© 2015 -  2024 SpiceCRM FlexCo All rights reserved."
     }
