5b5966808a57511a1d1a814a6307923e66a8880c
maretval <valerie.maret@twentyreasons.com>
2024-11-22 12:07:53 +0100

    packaging

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2024.02.001.1732266167589",
+    buildNumber: "2024.02.001.1732271619902",
     copyright: "© 2015 -  2024 SpiceCRM FlexCo All rights reserved."
     }
