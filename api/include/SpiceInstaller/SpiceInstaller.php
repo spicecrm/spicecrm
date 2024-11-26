f5316dfc22e0232e8181089bef7bce43c9f8bf28
Christian Knoll <christian@spicecrm.com>
2024-11-26 18:06:17 +0100

    HOTFIX set charset otherwise will throw error with collation

--- api/include/SpiceInstaller/SpiceInstaller.php
+++ api/include/SpiceInstaller/SpiceInstaller.php
@@ -453,6 +453,7 @@ class SpiceInstaller
 
         $db = $this->dbManagerFactory::getTypeInstance($postData['database']['db_type'], ['dbconfig' => ['db_manager' => $postData['database']['db_manager']]]);
         $postData['dboptions']['collation'] = "utf8mb4_unicode_ci";
+        $postData['dboptions']['charset'] = "utf8mb4";
         $db->setOptions($postData['dboptions']);
         if ($dbconfig['db_type'] == 'oci8') {
             $dbconfig['db_schema'] = $postData['database']['db_schema'];
