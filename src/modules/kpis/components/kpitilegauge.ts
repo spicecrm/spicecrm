9370b2a3624653a57dbe88a4dbd54877d3b45826
Christian Knoll <christian@spicecrm.com>
2024-12-07 07:58:08 +0100

    add color ranges to chartgauge for kpis

--- src/modules/kpis/components/kpitilegauge.ts
+++ src/modules/kpis/components/kpitilegauge.ts
@@ -62,4 +62,46 @@ export class KpiTileGauge extends KpiTile implements OnInit {
         super(model, modal, backend, injector)
     }
 
+    get maxValue(){
+        let max = this.kpiTarget.max_value ?? 0;
+
+        // see if we have a kpi value
+        if(this.kpiTargetValue.kpi_value > max) max = this.kpiTargetValue.kpi_value;
+
+        // see if we have an upper boundary
+        if(this.kpiTarget.upper_boundary > max) max = this.kpiTarget.upper_boundary;
+
+        return max;
+    }
+
+    public getRange(color: 'green'|'yellow'|'red'){
+        let from = 0;
+        let to = 0;
+
+        switch(color){
+            case 'red':
+                if(this.kpiTarget.lower_boundary != 0){
+                    from = this.kpiTarget.min_value ?? 0;
+                    to = this.kpiTarget.lower_boundary;
+                }
+                break;
+            case 'yellow':
+                if(this.kpiTarget.upper_boundary != 0){
+                    from = this.kpiTarget.lower_boundary ?? this.kpiTarget.min_value ?? 0;
+                    to = this.kpiTarget.upper_boundary;
+                }
+                break;
+            case 'green':
+                if(this.kpiTarget.upper_boundary != 0){
+                    from = this.kpiTarget.upper_boundary;
+                    to = this.maxValue ? this.maxValue : undefined;
+                }
+                break;
+        }
+
+
+        return {from, to};
+
+    }
+
 }
