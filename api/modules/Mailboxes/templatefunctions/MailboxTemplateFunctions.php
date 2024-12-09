f0ea8b6ae4ff47649d707146f330d6f2fe850f0f
Maria Zakharova <maria.zakharova@twentyreasons.com>
2024-12-09 14:14:39 +0100

    CR1001541: newsletter module

--- api/modules/Mailboxes/templatefunctions/MailboxTemplateFunctions.php
+++ api/modules/Mailboxes/templatefunctions/MailboxTemplateFunctions.php
@@ -29,6 +29,13 @@ class MailboxTemplateFunctions
         return "<a href=\"{$unsubUrl}\" class=\"spice-marketing-unsubscribe-link\">$inputString</a>";
     }
 
+    /**
+     * parse double opt in url and generate an double opt in link
+     * @param $compiler
+     * @param $beans
+     * @param $inputString
+     * @return string
+     */
     public static function generateDOILink($compiler, $beans, $inputString): string
     {
         if (empty($beans['Emails']) && empty($beans['bean']) && $beans['bean']->_module != 'Emails') return '';
@@ -40,5 +47,24 @@ class MailboxTemplateFunctions
         $trackData = EmailTracking::encodeTrackingID("ParentType:$parentType:ParentId:$parentId");
         $doubleOptinUrl = str_replace('{refid}', $trackData, SpiceConfig::getInstance()->get('emailtracking.double_optin_url'));
         return "<a href=\"{$doubleOptinUrl}\" class=\"spice-marketing-double-optin-link\">$inputString</a>";
+    }
+
+    /** opt out url and generate an opt out link
+     * @param $compiler
+     * @param $beans
+     * @param $inputString
+     * @return string
+     */
+    public static function generateOptOutLink($compiler, $beans, $inputString): string
+    {
+        if (empty($beans['Emails']) && empty($beans['bean']) && $beans['bean']->_module != 'Emails') return '';
+
+        $emails = isset($beans['Emails']) ? $beans['Emails'] : $beans['bean'];
+
+        [$parentType, $parentId] = $emails->getTrackingParentData();
+
+        $trackData = EmailTracking::encodeTrackingID("ParentType:$parentType:ParentId:$parentId");
+        $optoutUrl = str_replace('{refid}', $trackData, SpiceConfig::getInstance()->get('emailtracking.optout_url'));
+        return "<a href=\"{$optoutUrl}\" class=\"spice-marketing-optout-link\">$inputString</a>";
     }
 }
