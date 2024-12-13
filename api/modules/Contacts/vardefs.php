fa21e7be8f044e285b908a392fea870c34158b1d
Christian Knoll <christian@spicecrm.com>
2024-12-13 16:24:28 +0100

    partneragreement relationship metadata migration

--- api/modules/Contacts/vardefs.php
+++ api/modules/Contacts/vardefs.php
@@ -1113,30 +1113,30 @@ SpiceDictionaryHandler::getInstance()->dictionary['Contact']['fields']['alt_addr
 ];
 
 // CR1000661
-if(file_exists('extensions/modules/PartnerAgreements')) {
-    SpiceDictionaryHandler::getInstance()->dictionary['Contact']['fields']['partneragreements'] = [
-        'name' => 'partneragreements',
-        'vname' => 'LBL_PARTNERAGREEMENTS',
-        'type' => 'link',
-        'relationship' => 'partneragreements_contacts',
-        'module' => 'PartnerAgreements',
-        'bean_name' => 'PartnerAgreement',
-        'source' => 'non-db',
-    ];
-    SpiceDictionaryHandler::getInstance()->dictionary['Contact']['fields']['partneragreement_role'] = [
-        'name' => 'partneragreement_role',
-        'vname' => 'LBL_ROLE',
-        'type' => 'enum',
-        'options' => 'partneragreement_contact_role_dom',
-        'source' => 'non-db',
-        'comment' => 'for relationship field partneragreement_role'
-    ];
-    SpiceDictionaryHandler::getInstance()->dictionary['Contact']['fields']['propensity_to_partnership'] = [
-        'name' => 'propensity_to_partnership',
-        'vname' => 'LBL_PROPENSITY_TO_PARTNERSHIP',
-        'type' => 'enum',
-        'options' => 'partneragreement_propensity_role_dom',
-        'source' => 'non-db',
-        'comment' => 'for relationship field propensity_to_partnership'
-    ];
-}
+//if(file_exists('extensions/modules/PartnerAgreements')) {
+//    SpiceDictionaryHandler::getInstance()->dictionary['Contact']['fields']['partneragreements'] = [
+//        'name' => 'partneragreements',
+//        'vname' => 'LBL_PARTNERAGREEMENTS',
+//        'type' => 'link',
+//        'relationship' => 'partneragreements_contacts',
+//        'module' => 'PartnerAgreements',
+//        'bean_name' => 'PartnerAgreement',
+//        'source' => 'non-db',
+//    ];
+//    SpiceDictionaryHandler::getInstance()->dictionary['Contact']['fields']['partneragreement_role'] = [
+//        'name' => 'partneragreement_role',
+//        'vname' => 'LBL_ROLE',
+//        'type' => 'enum',
+//        'options' => 'partneragreement_contact_role_dom',
+//        'source' => 'non-db',
+//        'comment' => 'for relationship field partneragreement_role'
+//    ];
+//    SpiceDictionaryHandler::getInstance()->dictionary['Contact']['fields']['propensity_to_partnership'] = [
+//        'name' => 'propensity_to_partnership',
+//        'vname' => 'LBL_PROPENSITY_TO_PARTNERSHIP',
+//        'type' => 'enum',
+//        'options' => 'partneragreement_propensity_role_dom',
+//        'source' => 'non-db',
+//        'comment' => 'for relationship field propensity_to_partnership'
+//    ];
+//}
