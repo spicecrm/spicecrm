c83c6f00277c96ff89e2adb8209bdc3d4e9bab88
Christian Knoll <christian@spicecrm.com>
2025-01-10 17:51:17 +0100

    minor code fix prep for angular19 in pipe definition

--- src/objectcomponents/pipes/objectkeyvalue.pipe.ts
+++ src/objectcomponents/pipes/objectkeyvalue.pipe.ts
@@ -6,7 +6,9 @@ import {Pipe, PipeTransform} from "@angular/core";
 /**
  * transforms an object with an attribute and a value into an aray with objects with attributes key and value
  */
-@Pipe({name: 'objectkeyvalues'})
+@Pipe({
+    name: 'objectkeyvalues'
+})
 export class ObjectKeyValuesPipe implements PipeTransform {
     public transform(value): any {
         let keys = [];
