054d3d08e7dae73f808fb2724a8a223831b58999
Christian Knoll <christian@spicecrm.com>
2025-03-01 17:00:38 +0100

    CR1001686 add usage modal to Domain Manager

--- src/workbench/workbench.module.ts
+++ src/workbench/workbench.module.ts
@@ -186,6 +186,7 @@ import {HL7Manager} from './components/hl7manager';
 import {Hl7ManagerType} from './components/hl7managertype';
 import {HL7ManagerRule} from './components/hl7managerrule';
 import {WorkbenchConfigOptionKanban} from "./components/workbenchconfigoptionkanban";
+import {DomainManagerDefinitionUsageModal} from "./components/domainmanagerdefinitionusagemodal";
 
 
 @NgModule({
@@ -206,6 +207,7 @@ import {WorkbenchConfigOptionKanban} from "./components/workbenchconfigoptionkan
         CategoryTreeAddModal,
         DomainManager,
         DomainManagerDefinitions,
+        DomainManagerDefinitionUsageModal,
         DomainManagerFields,
         DomainManagerFieldTabs,
         DomainManagerFieldDetails,
