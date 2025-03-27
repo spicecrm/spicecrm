2b11955b82fc3e2c1c6a1b63919d3fd6fc6d9da2
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-03-27 12:05:44 +0100

    CR1001696: DocX Editor: Implemented parsing bean.

--- api/include/TXControlEditor/api/extensions/TXControlEditorExtensions.php
+++ api/include/TXControlEditor/api/extensions/TXControlEditorExtensions.php
@@ -1,5 +1,6 @@
 <?php
 
+use SpiceCRM\includes\Middleware\ValidationMiddleware;
 use SpiceCRM\includes\RESTManager;
 use SpiceCRM\includes\TXControlEditor\api\controllers\TXControlEditorController;
 
@@ -16,6 +17,28 @@ $routes = [
         'function'    => 'getSettings',
         'description' => 'Returns the tx control settings with the token',
         'options'     => ['noAuth' => false, 'adminOnly' => false],
+    ],
+    [
+        'method'      => 'post',
+        'route'       => '/common/TXControl/parse/module/{module}/{beanId}',
+        'class'       => TXControlEditorController::class,
+        'function'    => 'parseContent',
+        'description' => 'Parse TX control content',
+        'options'     => ['noAuth' => false, 'adminOnly' => false],
+        'parameters'  => [
+            'content' => [
+                'content' => 'body',
+                'description' => 'DocX content',
+                'type' => ValidationMiddleware::TYPE_STRING,
+                'required' => true
+            ],
+            'format' => [
+                'content' => 'body',
+                'description' => 'Return format options: "PDF", "PDFA", "RTF", "DOC", "DOCX", "HTML", "TX"',
+                'type' => ValidationMiddleware::TYPE_STRING,
+                'required' => false
+            ],
+        ],
     ]
 ];
 
