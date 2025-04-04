7ba31e47c3ea7ff1d150868c8a44916dbca26a97
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-04-04 22:29:22 +0200

    CR1001768: PageBuilder: render stylesheet on preview. Edit page color and width.

--- src/include/spicepagebuilder/fields/fieldpagebuilder.ts
+++ src/include/spicepagebuilder/fields/fieldpagebuilder.ts
@@ -12,6 +12,7 @@ import {backend} from "../../../services/backend.service";
 import {metadata} from "../../../services/metadata.service";
 import {language} from "../../../services/language.service";
 import {view} from "../../../services/view.service";
+import * as less from 'less';
 
 @Component({
     selector: 'field-page-builder',
@@ -29,6 +30,10 @@ export class fieldPageBuilder extends fieldGeneric implements OnInit, AfterViewI
      * @private
      */
     public iframeHeight: number = 250;
+    /**
+     * holds the stylesheet data
+     */
+    public stylesheet: {fieldName: string; id: string; content?: string; contentResourceUrl?: string; contentSafeResourceUrl?: SafeResourceUrl};
 
     constructor(public model: model,
                 public view: view,
@@ -64,8 +69,10 @@ export class fieldPageBuilder extends fieldGeneric implements OnInit, AfterViewI
      */
     public ngAfterViewInit() {
         super.ngAfterViewInit();
-        this.setHtmlValue();
-        this.modelChangesSubscriber();
+        this.loadStylesheetData().then(() => {
+            this.setHtmlValue();
+            this.modelChangesSubscriber();
+        });
     }
 
     /**
@@ -100,7 +107,9 @@ export class fieldPageBuilder extends fieldGeneric implements OnInit, AfterViewI
      */
     public setHtmlValue() {
         if (!this.value) return;
-        this.parsedHtml = this.sanitizer.bypassSecurityTrustResourceUrl('data:text/html;charset=UTF-8,' + encodeURIComponent(this.value));
+        const htmlDocument = !this.stylesheet?.content ? this.value : this.value
+            .replace('</head>', `<style>${this.stylesheet.content}"</style></head>`);
+        this.parsedHtml = this.sanitizer.bypassSecurityTrustResourceUrl('data:text/html;charset=UTF-8,' + encodeURIComponent(htmlDocument));
         this.cdRef.detectChanges();
     }
 
@@ -127,6 +136,31 @@ export class fieldPageBuilder extends fieldGeneric implements OnInit, AfterViewI
                 if (!res.html) this.toast.sendToast('ERR_FAILED_TO_EXECUTE', 'error');
                 this.model.setField(this.fieldname, res.html);
             }
+        });
+    }
+
+    /**
+     * load css stylesheet and wrap it with a prefix with less library
+     */
+    public async loadStylesheetData(): Promise<void> {
+
+        const stylesheetField = Object.values(this.model.fields).find(f => f.name == 'style' || f.name.endsWith('stylesheet_id'));
+
+        if (!stylesheetField) return Promise.resolve();
+
+        this.stylesheet = {
+            fieldName: stylesheetField,
+            id: this.model.getField(stylesheetField.name)
+        };
+
+        let styleContent = !!this.stylesheet.id ? this.metadata.getHtmlStylesheetCode(this.stylesheet.id) : '';
+
+        if (!styleContent) return Promise.resolve();
+
+        return less.render(`.spice-page-builder-${this.stylesheet.id} {${styleContent}}`).then(res => {
+            this.stylesheet.content = styleContent;
+            this.stylesheet.contentResourceUrl = 'data:text/css;base64,' + btoa(res.css);
+            this.stylesheet.contentSafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.stylesheet.contentResourceUrl);
         });
     }
 }
