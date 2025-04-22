e1ca8a57e7faa887741a56c027c7949187138030
vasiljev <nenad.vasiljev@twentyreasons.com>
2025-04-22 09:57:07 +0200

    Revert "CR1001813: navigate to the clicked cloned tab"
    This reverts commit d985d7df210057883b7f8dae19d063dd26ec6702.

--- src/services/navigation.service.ts
+++ src/services/navigation.service.ts
@@ -785,9 +785,6 @@ export class navigation {
             if (this.layout.screenwidth == 'small') this.objectTabs = [];
 
             for (let objectTab of this.objectTabs) {
-
-                if (this.activeTab != objectTab.id) continue;
-
                 if (this.matchPath(objectTab, routeData) && this.matchRouteParams(objectTab, routeParams)) {
                     // set the path since the path might be changed dues to the reference path of routes for the tabbed navigation
                     // but do not change it when the only difference is the tabid
