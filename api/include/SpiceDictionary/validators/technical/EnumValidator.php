3ed8333d4b99f425c26eec4376e23fcabf9faf40
DawidBestrzynski <dawid.bestrzynski@twentyreasons.com>
2025-05-09 12:53:02 +0200

    CR1001802 added missing EnumValidator

---
+++ api/include/SpiceDictionary/validators/technical/EnumValidator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SpiceCRM\includes\SpiceDictionary\validators\technical;
+
+/**
+ * There's just one domainfield with the type enum. Is that even correct?
+ */
+class EnumValidator extends TechnicalValidator
+{
+    protected function validate(mixed $value, array $definition): void
+    {
+        // TODO: Implement validate() method.
+    }
+}
