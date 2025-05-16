f909e505d36919db2e3405a669835d3ef7188d1a
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-05-16 11:25:03 +0200

    CR1001877: Calendar: Bug Fix: Participants do not show when editing an event in the calendar.

---
+++ src/modules/calendar/components/calendareditbutton.ts
@@ -0,0 +1,30 @@
+import {ChangeDetectorRef, Component, inject} from '@angular/core';
+import {ObjectActionEditButton} from "../../../objectcomponents/components/objectactioneditbutton";
+import {modal} from "../../../services/modal.service";
+
+@Component({
+    selector: 'calendar-edit-button',
+    templateUrl: '../templates/calendareditbutton.html'
+})
+
+export class CalendarEditButton extends ObjectActionEditButton {
+    /**
+     * modal service instance
+     * @private
+     */
+    private modal: modal = inject(modal);
+    private cdRef: ChangeDetectorRef = inject(ChangeDetectorRef);
+
+    /**
+     * reload the bean data before editing
+     */
+    public execute(): void {
+        const loading = this.modal.await('LBL_LOADING');
+        this.model.getData(true).subscribe(() => {
+            this.cdRef.markForCheck();
+            super.execute();
+            loading.next(true);
+            loading.complete();
+        });
+    }
+}
