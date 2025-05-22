51292ce2fe8d96d0b9611a1cd4c383a4cc9b3298
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-05-22 10:39:07 +0200

    CR1001899: Added a new flag "exclude image fields" for the list view api requests.

--- api/data/SpiceBean.php
+++ api/data/SpiceBean.php
@@ -5,6 +5,7 @@ namespace SpiceCRM\data;
 
 use SpiceCRM\includes\ErrorHandlers\Exception;
 use SpiceCRM\includes\ErrorHandlers\ValidationException;
+use SpiceCRM\includes\RESTManager;
 use SpiceCRM\includes\SpiceDictionary\SpiceDictionary;
 use SpiceCRM\includes\SpiceDictionary\SpiceDictionaryDefinition;
 use SpiceCRM\includes\SpiceDictionary\SpiceDictionaryDomain;
@@ -2110,12 +2111,12 @@ class SpiceBean
             $id = $this->id;
         }
 
-        $query = "SELECT $this->_tablename.*" . " FROM $this->_tablename ";
-        $query .= " WHERE $this->_tablename.id = " . $this->db->quoted($id);
+        $fields = array_filter($this->field_defs, fn($e) => (RESTManager::getInstance()->excludeImageFields || $e['type'] != 'image') && $e['source'] !== 'non-db');
+        $fields = join(',', array_map(fn($e) => $e['name'], $fields));
+        $query = "SELECT $fields FROM $this->_tablename WHERE id = " . $this->db->quoted($id);
 
-        // don't retrieve Bean with deleted flag true
-        if ($deleted) $query .= " AND $this->_tablename.deleted=0";
-        // LoggerManager::getLogger()->debug("Retrieve $this->_objectname : " . $query);
+        # exclude deleted if the deleted flag check is true
+        if ($deleted) $query .= " AND deleted = 0";
 
         $result = $this->db->query($query, true, "Retrieving record by id $this->_tablename:$id found ");
         if (empty($result)) {
