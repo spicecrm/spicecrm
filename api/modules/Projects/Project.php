25780a479e157890bf10111f7bd04756674a71e4
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-05-22 10:42:16 +0200

    CR1001899: Project: moved calculation on retrieve to retrieveViewDetails

--- api/modules/Projects/Project.php
+++ api/modules/Projects/Project.php
@@ -2,8 +2,9 @@
 /***** SPICE-SUGAR-HEADER-SPACEHOLDER *****/
 namespace SpiceCRM\modules\Projects;
 
-use SpiceCRM\data\BeanFactory;
 use SpiceCRM\data\SpiceBean;
+use SpiceCRM\includes\database\DBManagerFactory;
+use SpiceCRM\includes\ErrorHandlers\DatabaseException;
 use SpiceCRM\includes\utils\SpiceUtils;
 
 class Project extends SpiceBean {
@@ -13,40 +14,36 @@ class Project extends SpiceBean {
     public $estimated_start_date;
     public $estimated_end_date;
 
-
     /**
-     * add some calculated values on retrieve
-     * @param int $id
-     * @param false $encode
-     * @param bool $deleted
-     * @param bool $relationships
-     * @return Project|null
+     * Retrieves and calculates detailed view information for the object, including
+     * planned and actual efforts, estimated start and end dates. Efforts are aggregated
+     * using linked WBS elements, and start/end dates are derived based on those elements.
+     *
+     * @return void
      */
-    public function retrieve($id = -1, $encode = false, $deleted = true, $relationships = true)
+    public function retrieveViewDetails(): void
     {
-        $bean =  parent::retrieve($id, $encode, $deleted, $relationships);
+        parent::retrieveViewDetails();
 
-        // calculations
-        if($bean){
-            // calculate planned & actual efforts using wbs elements
-            $this->total_estimated_effort = 0;
-            $this->total_actual_effort = 0;
-            $wbsElements = $this->get_linked_beans('projectwbss');
-            foreach($wbsElements as $wbs){
-                // handle planned efforts
-                if(is_string($wbs->planned_effort)) $wbs->planned_effort = intval($wbs->planned_effort);
-                $this->total_estimated_effort += $wbs->planned_effort;
-                // handle actual efforts
-                if(is_string($wbs->consumed_effort)) $wbs->consumed_effort = intval($wbs->consumed_effort);
-                $this->total_actual_effort += $wbs->consumed_effort;
-            }
+        // calculate planned & actual efforts using wbs elements
+        $this->total_estimated_effort = 0;
+        $this->total_actual_effort = 0;
+        $wbsElements = $this->get_linked_beans('projectwbss');
+        foreach($wbsElements as $wbs){
+            // handle planned efforts
+            if(is_string($wbs->planned_effort)) $wbs->planned_effort = intval($wbs->planned_effort);
+            $this->total_estimated_effort += $wbs->planned_effort;
+            // handle actual efforts
+            if(is_string($wbs->consumed_effort)) $wbs->consumed_effort = intval($wbs->consumed_effort);
+            $this->total_actual_effort += $wbs->consumed_effort;
+        }
 
-            // calculate start and end dates according to earliest WBS element start date and lastest WBS end date
-            if(count($wbsElements) > 0) {
-                $this->estimated_start_date = SpiceUtils::getMinDate($wbsElements, 'date_start');
-                $this->estimated_end_date = SpiceUtils::getMaxDate($wbsElements, 'date_end');
-            }
-                    }
+        // calculate start and end dates according to earliest WBS element start date and lastest WBS end date
+        if(count($wbsElements) > 0) {
+            $this->estimated_start_date = SpiceUtils::getMinDate($wbsElements, 'date_start');
+            $this->estimated_end_date = SpiceUtils::getMaxDate($wbsElements, 'date_end');
+        }
+    }
 
         return $bean;
     }
