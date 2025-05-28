a4eb78121f628d1312f14c4c780a4c23df9ae0b8
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-05-28 14:15:17 +0200

    packaging restored app folder

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2025.01.001.1748262575882",
+    buildNumber: "2025.01.001.1748433246850",
     copyright: "© 2015 -  2025 SpiceCRM FlexCo All rights reserved."
     }
