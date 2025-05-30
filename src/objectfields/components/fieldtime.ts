3e31ca1b0b6203a72a30b79d059223f132c54409
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-05-30 13:19:26 +0200

    CR1001924: FieldTime logic improved. Added useUtc input property to the SystemInputTime.

--- src/objectfields/components/fieldtime.ts
+++ src/objectfields/components/fieldtime.ts
@@ -1,61 +1,19 @@
 /**
  * @module ObjectFields
  */
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {userpreferences} from '../../services/userpreferences.service';
 import {fieldGeneric} from './fieldgeneric';
-
-
-/**
- * @ignore
- */
-/**
- * @ignore
- */
-declare var moment: any;
 
 @Component({
     selector: 'field-time',
     templateUrl: '../templates/fieldtime.html',
     standalone: false
 })
-export class fieldTime extends fieldGeneric implements OnInit {
+export class fieldTime extends fieldGeneric {
 
     /**
      * injected instance of the user perference service
      */
     public userPreferences: userpreferences = inject(userpreferences);
-    /**
-     * holds the utc value of the original value
-     */
-    public valueUtc;
-
-    ngOnInit() {
-        super.ngOnInit();
-
-        this.setValueUtc(this.value);
-
-        this.subscriptions.add(
-            this.model.observeFieldChanges(this.fieldname).subscribe(value => {
-                this.setValueUtc(value);
-            })
-        );
-    }
-
-    /**
-     * set utc value to prevent changing the hour on timezone change
-     * @param value
-     * @private
-     */
-    private setValueUtc(value) {
-        this.valueUtc = !value ? null : moment.utc(value);
-    }
-
-    /**
-     * returns the time in the users time format
-     */
-    get displayTime() {
-        return this.valueUtc?.format(this.userPreferences.getTimeFormat());
-    }
-
 }
