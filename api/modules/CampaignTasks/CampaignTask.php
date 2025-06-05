557fe3db74f0a34a6c98c6c70ef34fd8020478b6
Christian Knoll <christian@spicecrm.com>
2025-06-05 07:30:21 +0200

    CR1001940 prevent indexing of emails and SMS in Mass send

--- api/modules/CampaignTasks/CampaignTask.php
+++ api/modules/CampaignTasks/CampaignTask.php
@@ -675,7 +675,7 @@ class CampaignTask extends SpiceBean
             if (isset($addBeans['CampaignLog'])) {
                 $email->registerTrackingParentData('CampaignLog', $addBeans['CampaignLog']->id);
             }
-            $email->save();
+            $email->save(false, false);
 
         } else {
 
@@ -730,7 +730,7 @@ class CampaignTask extends SpiceBean
                 $textMessage->parent_type = $seed->_module;
                 $textMessage->parent_id = $seed->id;
                 $textMessage->to_be_sent = true;
-                $textMessage->save();
+                $textMessage->save(false, false);
             } else {
                 $textMessage->send();
             }
