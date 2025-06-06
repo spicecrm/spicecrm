2ec29830c58e1dba330b18c05235976508888e99
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-06-06 13:55:50 +0200

    CR1001946: StatusNetwork: Added acl check on field required_model_acl_action

--- src/objectfields/components/fieldstatusnetwork.ts
+++ src/objectfields/components/fieldstatusnetwork.ts
@@ -72,7 +72,10 @@ export class fieldStatusNetwork extends fieldEnum {
     get networkItems() {
         let retArray = [];
         for (let statusnetworkitem of this.statusNetwork) {
-            if (statusnetworkitem.status_from == this.value && (!statusnetworkitem.required_model_state || this.model.checkModelState(statusnetworkitem.required_model_state))) {
+
+            const aclCheck = (!statusnetworkitem.required_model_acl_action || this.model.checkAccess(statusnetworkitem.required_model_acl_action));
+
+            if (aclCheck && statusnetworkitem.status_from == this.value && (!statusnetworkitem.required_model_state || this.model.checkModelState(statusnetworkitem.required_model_state))) {
                 retArray.push(statusnetworkitem);
             }
         }
