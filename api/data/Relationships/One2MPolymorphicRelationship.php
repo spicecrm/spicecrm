9222e061d43bc75ef7109cf616e7721a5d88e57c
DawidBestrzynski <dawid.bestrzynski@twentyreasons.com>
2025-06-10 10:16:15 +0200

    CR1001852 Added alias files for moved classes

---
+++ api/data/Relationships/One2MPolymorphicRelationship.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This alias is needed for backwards compatibility with custom code.
+ * Once all the occurrences of the old full namespace class names are gone, this file can be safely removed.
+ * Don't forget to composer dump-autoload
+ */
+namespace SpiceCRM\data\Relationships;
+
+class_alias(\SpiceCRM\includes\SpiceDictionary\relationships\One2MPolymorphicRelationship::class, 'SpiceCRM\data\Relationships\One2MPolymorphicRelationship');
---
+++ api/data/api/handlers/SpiceBeanHandler.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This alias is needed for backwards compatibility with custom code.
+ * Once all the occurrences of the old full namespace class names are gone, this file can be safely removed.
+ * Don't forget to composer dump-autoload
+ */
+namespace SpiceCRM\includes\SugarObjects\api\handlers;
+
+class_alias(\SpiceCRM\includes\SpiceBeans\api\handlers\SpiceBeanHandler::class, 'SpiceCRM\includes\SugarObjects\api\handlers\SpiceBeanHandler');
