ea8086c5413086927f5c511af3274d03caf28e68
DawidBestrzynski <dawid.bestrzynski@twentyreasons.com>
2025-06-10 10:16:15 +0200

    CR1001852 Added alias files for moved classes

---
+++ api/include/database/DBManagerFactory.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This alias is needed for backwards compatibility with custom code.
+ * Once all the occurrences of the old full namespace class names are gone, this file can be safely removed.
+ * Don't forget to composer dump-autoload
+ */
+namespace SpiceCRM\includes\database;
+
+class_alias(\SpiceCRM\includes\SpiceDictionary\database\DBManagerFactory::class, 'SpiceCRM\includes\database\DBManagerFactory');
