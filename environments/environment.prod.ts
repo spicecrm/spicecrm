53c052767ded72710a80b815eea5a7087f07a481
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-06-26 16:24:54 +0200

    CR1001950: MediaArticles: Added share to social media components

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2025.01.001.1749210961242",
+    buildNumber: "2025.01.001.1750945580949",
     copyright: "© 2015 -  2025 SpiceCRM FlexCo All rights reserved."
     }
