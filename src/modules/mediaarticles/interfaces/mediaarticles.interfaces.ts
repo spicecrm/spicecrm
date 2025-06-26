7d46a34b805012ceb3533ad897ab73cb2764714e
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-06-26 15:43:26 +0200

    CR1001950: MediaArticles: Added share to social media components

---
+++ src/modules/mediaarticles/interfaces/mediaarticles.interfaces.ts
@@ -0,0 +1,9 @@
+export interface MediaFileI {
+    id: string;
+    selected: boolean;
+    filetype: string;
+    thumbnail: string;
+    thumbnail_file: string;
+    file_size_readable: string;
+    filesize: string;
+}
