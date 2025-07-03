ca5547e1f19d0f70ce0f6d82ec8ea57d6c988643
Maret Valerie <valerie.maret@twentyreasons.com>
2025-07-03 17:30:03 +0200

    CR1001981 New component to add a module shortcut in the global header

---
+++ src/globalcomponents/components/globalheadermoduleshortcut.ts
@@ -0,0 +1,59 @@
+/**
+ * @module GlobalComponents
+ */
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {language} from "../../services/language.service";
+import {metadata} from "../../services/metadata.service";
+
+/**
+ * renders a componentn in the global toolbar on top to display if a cr is active
+ */
+@Component({
+    selector: 'global-header-module-shortcut',
+    templateUrl: "../templates/globalheadermoduleshortcut.html",
+    standalone: false
+})
+export class GlobalHeaderModuleShortcut implements OnInit {
+
+    public componentconfig: any = {};
+    public moduledefs: any = {};
+
+    constructor(public language: language,
+                public metadata: metadata,
+                public router: Router) {
+    }
+
+    get module(){
+        return this?.componentconfig?.module ?? '';
+    }
+
+    get moduleNameTranslated(){
+        return this.language.getModuleName(this?.componentconfig?.module ?? '');
+    }
+
+    get moduleIcon() {
+        return this?.moduledefs?.icon ?? '';
+    }
+
+    get hasAccess() {
+        if(!this.componentconfig) return false;
+        return true;
+    }
+
+    public ngOnInit() {
+        this.loadConfig();
+    }
+
+    public loadConfig(){
+        this.moduledefs = this.metadata.getModuleDefs(this.componentconfig.module);
+    }
+
+
+    /**
+     * navigates to the module
+     */
+    public goTo() {
+        this.router.navigate(["/module/" + this.componentconfig.module]);
+    }
+}
