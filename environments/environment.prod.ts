6500d43e0d0f0863c419bf0914b3ce05f5b46650
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-07-08 17:38:35 +0200

    Revert "CR1001961 option to delete AccountCCDetails from within tab on accounts"
    This reverts commit 0d971667c427add4aaac811eab582c1715dbb0a0.

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2025.01.001.1751988846604",
+    buildNumber: "2025.01.001.1751638553325",
     copyright: "© 2015 -  2025 SpiceCRM FlexCo All rights reserved."
     }
--- src/modules/accounts/components/accountterritorydetails.ts
+++ src/modules/accounts/components/accountterritorydetails.ts
@@ -7,8 +7,6 @@ import {view} from '../../../services/view.service';
 import {backend} from '../../../services/backend.service';
 import {model} from '../../../services/model.service';
 import {metadata} from "../../../services/metadata.service";
-import {modal} from "../../../services/modal.service";
-import {toast} from "../../../services/toast.service";
 
 declare var _: any;
 
@@ -24,20 +22,14 @@ export class AccountTerritoryDetails implements OnInit {
 
     constructor(public language: language,
                 public model: model,
-                public modal: modal,
                 public backend: backend,
                 public metadata: metadata,
-                public toast: toast,
                 public view: view) {
 
     }
 
     public ngOnInit() {
         this.view.isEditable = true;
-    }
-
-    get canDelete(){
-        return this.getCCDetails()[this.activeTab]?.acl.delete;
     }
 
     /*
@@ -79,36 +71,5 @@ export class AccountTerritoryDetails implements OnInit {
     * */
     public trackByFn(index, item) {
         return index;
-    }
-
-    /**
-     * deletes the acc detail record
-     */
-    public deleteACCDetail(){
-        if(this.canDelete){
-            this.modal.prompt('confirm', 'LBL_DELETE_ACCOUNTCCDETAIL','LBL_DELETE_RECORD').subscribe({
-                next: (res) => {
-                    if(res){
-                        let record = this.getCCDetails()[this.activeTab];
-                        let awaitModal = this.modal.await('LBL_DELETING');
-                        this.backend.deleteRequest(`module/AccountCCDetails/${record.id}`).subscribe({
-                            next: () => {
-                                delete this.model.data.accountccdetails.beans[record.id];
-                                if(this.getCCDetails().length > 0){
-                                    this.activeTab = 0;
-                                } else {
-                                    this.activeTab = null;
-                                }
-                                awaitModal.emit(true);
-                            },
-                            error: (e) => {
-                                this.toast.sendToast('LBL_ERROR_DELETING_RECORD', 'error');
-                                awaitModal.emit(true);
-                            }
-                        })
-                    }
-                }
-            })
-        }
     }
 }
