27b138885223bc337d0087afafefb0aa5b43ca58
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-07-15 15:29:49 +0200

    CR1002004: SpiceUIConfLoader: Bug Fix: relationships for modules were not repaired after package load

--- api/include/SpiceUI/SpiceUIConfLoader.php
+++ api/include/SpiceUI/SpiceUIConfLoader.php
@@ -481,7 +481,7 @@ class SpiceUIConfLoader
                 if ($dictionaryDef['status'] != 'a') continue;
 
                 try {
-                    $definitions->repair($dictionaryDef['id']);
+                    $definitions->repair($dictionaryDef['id'], false, true, true);
                 } catch (\Throwable|Exception $exception) {
                     unset($response[$dictionaryDef['tablename']]);
 
