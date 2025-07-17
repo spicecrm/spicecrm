f54f7a407dde12cdd5672ec473deb16da05eee22
BorisVlaovic <boris.vlaovic.kovacev@twentyreasons.com>
2025-07-17 14:28:12 +0200

    CR1002013 Duplicate button date fix

--- src/objectcomponents/components/objectactionduplicatebutton.ts
+++ src/objectcomponents/components/objectactionduplicatebutton.ts
@@ -50,7 +50,7 @@ export class ObjectActionDuplicateButton {
         modelData.date_entered = new Date();
         modelData.date_modified = new Date();
 
-        this.model.setData(modelData, false);
+        this.model.setData(modelData, true);
 
         for (let field in this.parent.fields) {
             if (this.parent.fields[field].type == 'link' && this.model.data[field] && this.model.data[field].beans) {
@@ -78,6 +78,11 @@ export class ObjectActionDuplicateButton {
         // set as duplicate
         this.model.duplicate = true;
         this.model.templateId = this.parent.id;
+        //
+        // let dateClosed = this.parent.getField('date_closed');
+        // if (dateClosed) {
+        //     this.model.setField('date_closed', dateClosed);
+        // }
 
         this.model.edit();
     }
