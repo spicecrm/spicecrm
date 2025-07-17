5f9c8429ad79fad950865169922d2b46c76bb316
vasiljev <nenad.vasiljev@twentyreasons.com>
2025-07-17 14:24:29 +0200

    CR1002007: ProspecLists: display only the most recent date modified from the given prospect list

--- src/modules/prospectlists/fields/prospectlistslastmembermodificationfield.ts
+++ src/modules/prospectlists/fields/prospectlistslastmembermodificationfield.ts
@@ -22,15 +22,7 @@ export class ProspectlistsLastMemberModificationField extends fieldGeneric imple
 
     public isLoading: boolean = true;
 
-    public lastMemberModificationModule: string;
-
-    public lastMemberModificationId: string;
-
-    public modificatedOnDate: string;
-
-    public isDeleted: string;
-
-    public lastMemberModificationName: string;
+    public modificationDate: string;
 
     constructor(public model: model,
                 public view: view,
@@ -45,31 +37,15 @@ export class ProspectlistsLastMemberModificationField extends fieldGeneric imple
     public ngOnInit() {
         this.backend.getRequest(`module/ProspectLists/${this.model.id}/lastmembermodification`).subscribe({
             next: (res) => {
-                this.lastMemberModificationModule = res.prospect_list_data.modificated_memeber_module;
-                this.lastMemberModificationId = res.prospect_list_data.modificated_memeber_id;
-                this.modificatedOnDate = res.prospect_list_data.date_modified;
-                this.isDeleted = res.prospect_list_data.deleted;
-                this.lastMemberModificationName = res.modified_member_name;
+                this.modificationDate = res;
                 this.isLoading = false;
             }
         })
     }
 
-    get modificationDescriptionLabel() {
-        if (this.isDeleted == '1') {
-            return 'LBL_TARGETLIST_MEMBER_REMOVED'
-        }
-
-        return 'LBL_TARGETLIST_MEMBER_ADDED'
-    }
-
     get formatedDate() {
         let timeZone = this.session.getSessionData('timezone');
-        let pDateTime = typeof timeZone == 'string' && timeZone.length > 0 ? moment.utc(this.modificatedOnDate).tz(timeZone) : moment(this.modificatedOnDate);
+        let pDateTime = typeof timeZone == 'string' && timeZone.length > 0 ? moment.utc(this.modificationDate).tz(timeZone) : moment(this.modificationDate);
         return pDateTime.format('DD.MM.YYYY HH:mm');
-    }
-
-    get moduleLabel() {
-        return 'LBL_' + this.lastMemberModificationModule.toUpperCase();
     }
 }
