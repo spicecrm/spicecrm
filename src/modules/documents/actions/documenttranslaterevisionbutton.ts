9e01b19740adecd2d6358b094d109332e86a1ba2
Mehyar Sawas-Donauer <mehyar.sawas@twentyreasons.com>
2025-07-25 14:24:58 +0200

    CR1002033: DocumentRevision: Google Translate API Service added

---
+++ src/modules/documents/actions/documenttranslaterevisionbutton.ts
@@ -0,0 +1,42 @@
+import {Component, inject} from '@angular/core';
+import {backend} from "../../../services/backend.service";
+import {modal} from "../../../services/modal.service";
+import {toast} from "../../../services/toast.service";
+import {ObjectActionSetItemBase} from "../../../objectcomponents/interfaces/objectactionsetitembase";
+
+@Component({
+    selector: 'document-translate-revision-button',
+    template: `
+        <system-label label="LBL_TRANSLATE"></system-label>`,
+    standalone: false
+})
+export class DocumentTranslateRevisionButton extends ObjectActionSetItemBase {
+
+    private backend = inject(backend);
+    private modal = inject(modal);
+    private toast = inject(toast);
+
+    get hidden(): boolean {
+        return this.model.getField('documentrevisionstatus') != 'r' || !!this.model.getField('parent_revision_id');
+    }
+
+    /**
+     * prompt to select a language to translate to and create a new translated revision
+     */
+    public execute() {
+
+        this.modal.prompt('input', '', 'LBL_LANGUAGE').subscribe(toLanguage => {
+
+            if (!toLanguage) return;
+
+            this.backend.postRequest(`module/DocumentRevisions/${this.model.id}/translate/${toLanguage}`).subscribe({
+                next: () => {
+                    this.toast.sendToast('MSG_SUCCESSFULLY_EXECUTED', 'success');
+                },
+                error: () => {
+                    this.toast.sendToast('ERR_FAILED_TO_EXECUTE', 'error');
+                }
+            });
+        });
+    }
+}
