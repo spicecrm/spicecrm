2ba427d0406501903c2914bf241326bd71b27680
DawidBestrzynski <dawid.bestrzynski@twentyreasons.com>
2025-08-19 10:42:40 +0200

    preparing release

---
+++ app/moduleprojects.js
@@ -0,0 +1,12 @@
+/*!
+ * 
+ *                     SpiceCRM
+ *
+ *                     release: 2025.01.002
+ *
+ *                     date: 2025-08-19 10:32:01
+ *
+ *                     build: 2025.01.002.1755592321814
+ *
+ */
+"use strict";(self.webpackChunkcore=self.webpackChunkcore||[]).push([["src_modules_projects_moduleprojects_ts"],{97910:(e,t,s)=>{s.r(t),s.d(t,{ModuleProjects:()=>p});var o=s(14572),c=s(10936),n=s(40432),r=s(40412),i=s(92470),d=s(87371),u=s(8848),l=s(44557);let p=(()=>{class ModuleProjects{static{this.ɵfac=function(e){return new(e||ModuleProjects)}}static{this.ɵmod=l.$C({type:ModuleProjects})}static{this.ɵinj=l.G2t({imports:[o.MD,c.YN,r.ObjectFields,i.ObjectComponents,d.SystemComponents,n.h]})}}return ModuleProjects})();("undefined"==typeof ngJitMode||ngJitMode)&&l.Obh(p,{declarations:[u.G],imports:[o.MD,c.YN,r.ObjectFields,i.ObjectComponents,d.SystemComponents,n.h],exports:[u.G]})}}]);
--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2025.01.002.1754049629187",
+    buildNumber: "2025.01.002.1755592875771",
     copyright: "© 2015 -  2025 SpiceCRM FlexCo All rights reserved."
     }
