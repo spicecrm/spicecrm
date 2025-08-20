e9b1ffefcaa5601ea7715876a25b8ee7b4722dc5
Maria Zakharova <maria.zakharova@twentyreasons.com>
2025-08-20 15:54:45 +0200

    CR1002056: return true for job

--- api/modules/EmailTrackingActions/schedulerjobtasks/EmailTrackingActionsSchedulerJobTasks.php
+++ api/modules/EmailTrackingActions/schedulerjobtasks/EmailTrackingActionsSchedulerJobTasks.php
@@ -16,9 +16,9 @@ class EmailTrackingActionsSchedulerJobTasks
         $emailTrackingActions = $db->fetchAll("SELECT * from emailtrackingactions where update_bean = 1 and deleted = 0 ORDER BY date_entered");
 
         foreach ($emailTrackingActions as $emailTrackingAction) {
-            $records = array_merge($db->fetchAll("SELECT 'CampaignLog' module, id FROM campaign_log WHERE external_id = '{$emailTrackingAction['message_id']}' AND deleted = 0") ?: [],
-                $db->fetchAll("SELECT 'NewsletterLogs' module, id FROM newsletterlogs WHERE external_id = '{$emailTrackingAction['message_id']}' AND deleted = 0") ?: [],
-                $db->fetchAll("SELECT 'Emails' module, id FROM emails WHERE message_id='{$emailTrackingAction['message_id']}' AND deleted = 0") ?: []);
+            $records = array_merge($db->fetchAll("SELECT 'CampaignLog' module, id FROM campaign_log WHERE external_id = '<{$emailTrackingAction['message_id']}>' AND deleted = 0") ?: [],
+                $db->fetchAll("SELECT 'NewsletterLogs' module, id FROM newsletterlogs WHERE external_id = '<{$emailTrackingAction['message_id']}>' AND deleted = 0") ?: [],
+                $db->fetchAll("SELECT 'Emails' module, id FROM emails WHERE message_id='<{$emailTrackingAction['message_id']}>' AND deleted = 0") ?: []);
 
             $emailTrackingActionBean = BeanFactory::getBean('EmailTrackingActions', $emailTrackingAction['id']);
 
@@ -69,7 +69,7 @@ class EmailTrackingActionsSchedulerJobTasks
                 $emailTrackingActionBean->update_bean = 0;
                 $emailTrackingActionBean->save();
             }
-
+            return true;
         }
     }
 }
