34c6b7273919e6734b517b12b4e86e08fd05b508
Maret Valerie <valerie.maret@twentyreasons.com>
2025-09-04 15:51:48 +0200

    CR1002081 Moved SpiceCRM more components to extensions

--- api/include/SpiceUI/api/controllers/SpiceUIRepositoryController.php
+++ api/include/SpiceUI/api/controllers/SpiceUIRepositoryController.php
@@ -25,7 +25,24 @@ class SpiceUIRepositoryController
 
         $retArray = [];
 
-        $modules = $db->query("SELECT {$sysuimodulerepositoryColumns} FROM sysuimodulerepository UNION ALL SELECT {$sysuimodulerepositoryColumns} FROM sysuicustommodulerepository");
+        // the query will only return the newest record for each module
+        $modules = $db->query("SELECT $sysuimodulerepositoryColumns
+FROM (
+  SELECT s.*, 
+         ROW_NUMBER() OVER (
+           PARTITION BY module
+           ORDER BY CAST(REPLACE(version,'.','') AS UNSIGNED) DESC
+         ) AS rn
+  FROM (
+    SELECT $sysuimodulerepositoryColumns
+    FROM sysuimodulerepository
+    UNION ALL
+    SELECT $sysuimodulerepositoryColumns
+    FROM sysuicustommodulerepository
+  ) AS s
+) AS x
+WHERE rn = 1; ");
+
         while ($module = $db->fetchByAssoc($modules)) {
             $retArray[$module['id']] = [
                 'id' => $module['id'],
