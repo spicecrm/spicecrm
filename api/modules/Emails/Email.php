ec45b18214a67b4da0f8b1b1d56423c89d8f494e
Maret Valerie <valerie.maret@twentyreasons.com>
2025-09-19 17:25:29 +0200

    CR1002108 Fix e-mail draft: handle removed recipients

--- api/modules/Emails/Email.php
+++ api/modules/Emails/Email.php
@@ -553,6 +553,36 @@ class Email extends SpiceBean
         }
     }
 
+    /**
+     * compares the recipients before the save and removes the ones that are not in the POST data
+     * can happen when a draft is edited
+     * @return void
+     */
+    public function removeRecipientAdresses()
+    {
+        $removedIds = [];
+        $beforeSaveRecipients = $this->db->fetchAll(
+            "SELECT id FROM emails_email_addr_rel WHERE email_id = '{$this->id}'"
+        );
+
+       if($beforeSaveRecipients) {
+            foreach ($beforeSaveRecipients as $beforeSaveRecipient) {
+                if (array_search($beforeSaveRecipient['id'], array_column($this->recipient_addresses, 'id')) === false) {
+                    $removedIds[] = $beforeSaveRecipient['id'];
+                }
+            }
+
+            if (count($removedIds) > 0) {
+                $this->db->query(
+                    "UPDATE emails_email_addr_rel SET
+                            deleted = 1
+                            WHERE id IN ('" . implode("','", $removedIds) . "')
+                        "
+                );
+            }
+        }
+    }
+
     function saveRecipientAddresses($ignoreInvalid = true)
     {
         if (!is_array($this->recipient_addresses) || empty($this->recipient_addresses)) {
@@ -564,6 +594,9 @@ class Email extends SpiceBean
             'cc_addrs' => [],
             'bcc_addrs' => [],
         ];
+
+        // handle removed recipients
+        $this->removeRecipientAdresses();
 
         foreach ($this->recipient_addresses as $recipient_address) {
             $record = $this->db->fetchByAssoc($this->db->query(
