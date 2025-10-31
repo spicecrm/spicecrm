713f6842cbfd2c387bd50eebc6fe159421184cd4
Maret Valerie <valerie.maret@twentyreasons.com>
2025-10-31 14:55:37 +0100

    CR1002092 vardefs cleanup migrated basic, assignable, activities

--- api/include/SugarObjects/implements/assignable/vardefs.php
+++ api/include/SugarObjects/implements/assignable/vardefs.php
@@ -33,77 +33,77 @@
  * technical reasons, the Appropriate Legal Notices must display the words
  * "Powered by SugarCRM".
  ********************************************************************************/
-
-$vardefs = [
-    'fields' => [
-        'assigned_user_id' => [
-            'name' => 'assigned_user_id',
-            'rname' => 'user_name',
-            'id_name' => 'assigned_user_id',
-            'vname' => 'LBL_ASSIGNED_TO_ID',
-            'group' => 'assigned_user_name',
-            'type' => 'varchar',
-            'dbtype' => 'char',
-            'table' => 'users',
-            'module' => 'Users',
-            'reportable' => true,
-            'isnull' => 'false',
-            'len' => '36',
-            'audited' => true,
-            'comment' => 'User ID assigned to record',
-            'duplicate_merge' => true,
-            'link' => 'assigned_user_link'
-        ],
-        'assigned_user_name' => [
-            'name' => 'assigned_user_name',
-            'link' => 'assigned_user_link',
-            'vname' => 'LBL_ASSIGNED_TO',
-            'rname' => 'user_name',
-            'type' => 'relate',
-            'reportable' => false,
-            'source' => 'non-db',
-            'table' => 'users',
-            'id_name' => 'assigned_user_id',
-            'module' => 'Users',
-            'duplicate_merge' => false
-        ],
-        'assigned_user' => [
-            'name' => 'assigned_user',
-            'link' => 'assigned_user_link',
-            'vname' => 'LBL_ASSIGNED_TO',
-            'rname' => 'user_name',
-            'type' => 'linked',
-            'reportable' => false,
-            'source' => 'non-db',
-            'id_name' => 'assigned_user_id',
-            'module' => 'Users',
-            'duplicate_merge' => true
-        ],
-        'assigned_user_link' => [
-            'name' => 'assigned_user_link',
-            'type' => 'link',
-            'relationship' => strtolower($module) . '_assigned_user',
-            'vname' => 'LBL_ASSIGNED_TO',
-            'link_type' => 'one',
-            'module' => 'Users',
-            'bean_name' => 'User',
-            'source' => 'non-db',
-            'rname' => 'user_name',
-            'id_name' => 'assigned_user_id',
-            'table' => 'users',
-            'recover' => false,
-            'duplicate_merge' => true
-        ]
-    ],
-    'relationships' => [
-        strtolower($module) . '_assigned_user' => [
-            'lhs_module' => 'Users',
-            'lhs_table' => 'users',
-            'lhs_key' => 'id',
-            'rhs_module' => $module,
-            'rhs_table' => strtolower($module),
-            'rhs_key' => 'assigned_user_id',
-            'relationship_type' => 'one-to-many'
-        ]
-    ]
-];
+//
+//$vardefs = [
+//    'fields' => [
+//        'assigned_user_id' => [
+//            'name' => 'assigned_user_id',
+//            'rname' => 'user_name',
+//            'id_name' => 'assigned_user_id',
+//            'vname' => 'LBL_ASSIGNED_TO_ID',
+//            'group' => 'assigned_user_name',
+//            'type' => 'varchar',
+//            'dbtype' => 'char',
+//            'table' => 'users',
+//            'module' => 'Users',
+//            'reportable' => true,
+//            'isnull' => 'false',
+//            'len' => '36',
+//            'audited' => true,
+//            'comment' => 'User ID assigned to record',
+//            'duplicate_merge' => true,
+//            'link' => 'assigned_user_link'
+//        ],
+//        'assigned_user_name' => [
+//            'name' => 'assigned_user_name',
+//            'link' => 'assigned_user_link',
+//            'vname' => 'LBL_ASSIGNED_TO',
+//            'rname' => 'user_name',
+//            'type' => 'relate',
+//            'reportable' => false,
+//            'source' => 'non-db',
+//            'table' => 'users',
+//            'id_name' => 'assigned_user_id',
+//            'module' => 'Users',
+//            'duplicate_merge' => false
+//        ],
+//        'assigned_user' => [
+//            'name' => 'assigned_user',
+//            'link' => 'assigned_user_link',
+//            'vname' => 'LBL_ASSIGNED_TO',
+//            'rname' => 'user_name',
+//            'type' => 'linked',
+//            'reportable' => false,
+//            'source' => 'non-db',
+//            'id_name' => 'assigned_user_id',
+//            'module' => 'Users',
+//            'duplicate_merge' => true
+//        ],
+//        'assigned_user_link' => [
+//            'name' => 'assigned_user_link',
+//            'type' => 'link',
+//            'relationship' => strtolower($module) . '_assigned_user',
+//            'vname' => 'LBL_ASSIGNED_TO',
+//            'link_type' => 'one',
+//            'module' => 'Users',
+//            'bean_name' => 'User',
+//            'source' => 'non-db',
+//            'rname' => 'user_name',
+//            'id_name' => 'assigned_user_id',
+//            'table' => 'users',
+//            'recover' => false,
+//            'duplicate_merge' => true
+//        ]
+//    ],
+//    'relationships' => [
+//        strtolower($module) . '_assigned_user' => [
+//            'lhs_module' => 'Users',
+//            'lhs_table' => 'users',
+//            'lhs_key' => 'id',
+//            'rhs_module' => $module,
+//            'rhs_table' => strtolower($module),
+//            'rhs_key' => 'assigned_user_id',
+//            'relationship_type' => 'one-to-many'
+//        ]
+//    ]
+//];
--- api/include/SugarObjects/templates/basic/vardefs.php
+++ api/include/SugarObjects/templates/basic/vardefs.php
@@ -33,222 +33,222 @@
  * technical reasons, the Appropriate Legal Notices must display the words
  * "Powered by SugarCRM".
  ********************************************************************************/
-
-$vardefs = [
-    'fields' => [
-        'id' => [
-            'name' => 'id',
-            'vname' => 'LBL_ID',
-            'type' => 'id',
-            'required' => true,
-            'reportable' => true,
-            'comment' => 'Unique identifier',
-            'duplicate_merge' => false,
-            'audited' => false
-        ],
-        'name' => [
-            'name' => 'name',
-            'vname' => 'LBL_NAME',
-            'type' => 'name',
-            'link' => true, // bug 39288
-            'dbType' => 'varchar',
-            'len' => 255,
-            'unified_search' => true,
-            'full_text_search' => ['boost' => 3],
-            'required' => true,
-            'importable' => 'required',
-            'duplicate_merge' => true,
-            //'duplicate_merge_dom_value' => '3',
-            'merge_filter' => 'selected',
-        ],
-        'date_entered' => [
-            'name' => 'date_entered',
-            'vname' => 'LBL_DATE_ENTERED',
-            'type' => 'datetime',
-            'group' => 'created_by_name',
-            'comment' => 'Date record created',
-            'enable_range_search' => true,
-            'options' => 'date_range_search_dom',
-            'duplicate_merge' => false,
-            'audited' => false
-        ],
-        'date_modified' => [
-            'name' => 'date_modified',
-            'vname' => 'LBL_DATE_MODIFIED',
-            'type' => 'datetime',
-            'group' => 'modified_by_name',
-            'comment' => 'Date record last modified',
-            'enable_range_search' => true,
-            'options' => 'date_range_search_dom',
-            'duplicate_merge' => false,
-            'audited' => false
-        ],
-        'date_indexed' => [
-            'name' => 'date_indexed',
-            'vname' => 'LBL_DATE_INDEXED',
-            'type' => 'datetime',
-            'comment' => 'Date record last indexed',
-            'enable_range_search' => true,
-            'options' => 'date_range_search_dom',
-            'duplicate_merge' => false,
-            'audited' => false
-        ],
-        'modified_user_id' => [
-            'name' => 'modified_user_id',
-            'rname' => 'user_name',
-            'id_name' => 'modified_user_id',
-            'vname' => 'LBL_MODIFIED_BY_ID',
-            'type' => 'assigned_user_name',
-            'table' => 'users',
-            'isnull' => 'false',
-            'group' => 'modified_by_name',
-            'dbType' => 'id',
-            'reportable' => true,
-            'comment' => 'User who last modified record',
-            'massupdate' => false,
-            'duplicate_merge' => false,
-            'audited' => false
-        ],
-        'modified_by_user' => [
-            'name' => 'modified_by_user',
-            'rname' => 'user_name',
-            'id_name' => 'modified_user_id',
-            'vname' => 'LBL_MODIFIED_BY',
-            'type' => 'linked',
-            'module' => 'Users',
-            'source' => 'non-db',
-            'link' => 'modified_user_link',
-            'reportable' => false,
-            'comment' => 'User who last modified record',
-            'audited' => false,
-            'duplicate_merge' => false
-        ],
-        'modified_by_name' => [
-            'name' => 'modified_by_name',
-            'vname' => 'LBL_MODIFIED_BY',
-            'type' => 'relate',
-            'reportable' => false,
-            'source' => 'non-db',
-            'rname' => 'user_name',
-            'table' => 'users',
-            'id_name' => 'modified_user_id',
-            'module' => 'Users',
-            'link' => 'modified_user_link',
-            'duplicate_merge' => false,
-            'massupdate' => false,
-            'audited' => false
-        ],
-        'created_by' => [
-            'name' => 'created_by',
-            'rname' => 'user_name',
-            'id_name' => 'modified_user_id',
-            'vname' => 'LBL_CREATED_BY',
-            'type' => 'assigned_user_name',
-            'table' => 'users',
-            'isnull' => 'false',
-            'dbType' => 'id',
-            'group' => 'created_by_name',
-            'comment' => 'User who created record',
-            'massupdate' => false,
-            'duplicate_merge' => false,
-            'audited' => false
-        ],
-        'created_by_user' => [
-            'name' => 'created_by_user',
-            'vname' => 'LBL_CREATED_BY',
-            'type' => 'linked',
-            'link' => 'created_by_link',
-            'rname' => 'user_name',
-            'source' => 'non-db',
-            'id_name' => 'created_by',
-            'module' => 'Users',
-            'duplicate_merge' => false
-        ],
-        'created_by_name' => [
-            'name' => 'created_by_name',
-            'vname' => 'LBL_CREATED_BY',
-            'type' => 'relate',
-            'reportable' => false,
-            'link' => 'created_by_link',
-            'rname' => 'user_name',
-            'source' => 'non-db',
-            'table' => 'users',
-            'id_name' => 'created_by',
-            'module' => 'Users',
-            'importable' => 'false',
-            'massupdate' => false,
-            'duplicate_merge' => false
-        ],
-        'description' => [
-            'name' => 'description',
-            'vname' => 'LBL_DESCRIPTION',
-            'type' => 'text',
-            'comment' => 'Full text of the note'
-        ],
-        'deleted' => [
-            'name' => 'deleted',
-            'vname' => 'LBL_DELETED',
-            'type' => 'bool',
-            'default' => '0',
-            'reportable' => false,
-            'comment' => 'Record deletion indicator',
-            'duplicate_merge' => false,
-            'audited' => false
-        ],
-        'tags' => [
-            'name' => 'tags',
-            'vname' => 'LBL_TAGS',
-            'type' => 'tags',
-            'dbType' => 'text',
-            'duplicate_merge' => false
-        ],
-        'created_by_link' => [
-            'name' => 'created_by_link',
-            'type' => 'link',
-            'relationship' => (isset($table_name) ? $table_name : strtolower($module)) . '_created_by',
-            'vname' => 'LBL_CREATED_BY',
-            'link_type' => 'one',
-            'module' => 'Users',
-            'bean_name' => 'User',
-            'source' => 'non-db',
-            'recover' => false,
-            'duplicate_merge' => false
-        ],
-        'modified_user_link' => [
-            'name' => 'modified_user_link',
-            'type' => 'link',
-            'relationship' => (isset($table_name) ? $table_name : strtolower($module)) . '_modified_user',
-            'vname' => 'LBL_MODIFIED_BY',
-            'link_type' => 'one',
-            'module' => 'Users',
-            'bean_name' => 'User',
-            'source' => 'non-db',
-            'recover' => false,
-            'duplicate_merge' => false
-        ]
-    ],
-    'indices' => [
-        'id' => ['name' => (isset($table_name) ? $table_name : strtolower($module)) . 'pk', 'type' => 'primary', 'fields' => ['id']],
-        'fts_idx' => ['name' => 'idx_' . (isset($table_name) ? $table_name : strtolower($module)) . '_fts', 'type' => 'index', 'fields' => ['date_modified', 'date_indexed']]
-    ],
-    'relationships' => [
-        (isset($table_name) ? $table_name : strtolower($module)) . '_modified_user' => [
-            'lhs_module' => 'Users',
-            'lhs_table' => 'users',
-            'lhs_key' => 'id',
-            'rhs_module' => $module,
-            'rhs_table' => (isset($table_name) ? $table_name : strtolower($module)),
-            'rhs_key' => 'modified_user_id',
-            'relationship_type' => 'one-to-many'
-        ],
-        (isset($table_name) ? $table_name : strtolower($module)) . '_created_by' => [
-            'lhs_module' => 'Users',
-            'lhs_table' => 'users',
-            'lhs_key' => 'id',
-            'rhs_module' => $module,
-            'rhs_table' => (isset($table_name) ? $table_name : strtolower($module)),
-            'rhs_key' => 'created_by',
-            'relationship_type' => 'one-to-many'
-        ]
-    ]
-];
+//
+//$vardefs = [
+//    'fields' => [
+//        'id' => [
+//            'name' => 'id',
+//            'vname' => 'LBL_ID',
+//            'type' => 'id',
+//            'required' => true,
+//            'reportable' => true,
+//            'comment' => 'Unique identifier',
+//            'duplicate_merge' => false,
+//            'audited' => false
+//        ],
+//        'name' => [
+//            'name' => 'name',
+//            'vname' => 'LBL_NAME',
+//            'type' => 'name',
+//            'link' => true, // bug 39288
+//            'dbType' => 'varchar',
+//            'len' => 255,
+//            'unified_search' => true,
+//            'full_text_search' => ['boost' => 3],
+//            'required' => true,
+//            'importable' => 'required',
+//            'duplicate_merge' => true,
+//            //'duplicate_merge_dom_value' => '3',
+//            'merge_filter' => 'selected',
+//        ],
+//        'date_entered' => [
+//            'name' => 'date_entered',
+//            'vname' => 'LBL_DATE_ENTERED',
+//            'type' => 'datetime',
+//            'group' => 'created_by_name',
+//            'comment' => 'Date record created',
+//            'enable_range_search' => true,
+//            'options' => 'date_range_search_dom',
+//            'duplicate_merge' => false,
+//            'audited' => false
+//        ],
+//        'date_modified' => [
+//            'name' => 'date_modified',
+//            'vname' => 'LBL_DATE_MODIFIED',
+//            'type' => 'datetime',
+//            'group' => 'modified_by_name',
+//            'comment' => 'Date record last modified',
+//            'enable_range_search' => true,
+//            'options' => 'date_range_search_dom',
+//            'duplicate_merge' => false,
+//            'audited' => false
+//        ],
+//        'date_indexed' => [
+//            'name' => 'date_indexed',
+//            'vname' => 'LBL_DATE_INDEXED',
+//            'type' => 'datetime',
+//            'comment' => 'Date record last indexed',
+//            'enable_range_search' => true,
+//            'options' => 'date_range_search_dom',
+//            'duplicate_merge' => false,
+//            'audited' => false
+//        ],
+//        'modified_user_id' => [
+//            'name' => 'modified_user_id',
+//            'rname' => 'user_name',
+//            'id_name' => 'modified_user_id',
+//            'vname' => 'LBL_MODIFIED_BY_ID',
+//            'type' => 'assigned_user_name',
+//            'table' => 'users',
+//            'isnull' => 'false',
+//            'group' => 'modified_by_name',
+//            'dbType' => 'id',
+//            'reportable' => true,
+//            'comment' => 'User who last modified record',
+//            'massupdate' => false,
+//            'duplicate_merge' => false,
+//            'audited' => false
+//        ],
+//        'modified_by_user' => [
+//            'name' => 'modified_by_user',
+//            'rname' => 'user_name',
+//            'id_name' => 'modified_user_id',
+//            'vname' => 'LBL_MODIFIED_BY',
+//            'type' => 'linked',
+//            'module' => 'Users',
+//            'source' => 'non-db',
+//            'link' => 'modified_user_link',
+//            'reportable' => false,
+//            'comment' => 'User who last modified record',
+//            'audited' => false,
+//            'duplicate_merge' => false
+//        ],
+//        'modified_by_name' => [
+//            'name' => 'modified_by_name',
+//            'vname' => 'LBL_MODIFIED_BY',
+//            'type' => 'relate',
+//            'reportable' => false,
+//            'source' => 'non-db',
+//            'rname' => 'user_name',
+//            'table' => 'users',
+//            'id_name' => 'modified_user_id',
+//            'module' => 'Users',
+//            'link' => 'modified_user_link',
+//            'duplicate_merge' => false,
+//            'massupdate' => false,
+//            'audited' => false
+//        ],
+//        'created_by' => [
+//            'name' => 'created_by',
+//            'rname' => 'user_name',
+//            'id_name' => 'modified_user_id',
+//            'vname' => 'LBL_CREATED_BY',
+//            'type' => 'assigned_user_name',
+//            'table' => 'users',
+//            'isnull' => 'false',
+//            'dbType' => 'id',
+//            'group' => 'created_by_name',
+//            'comment' => 'User who created record',
+//            'massupdate' => false,
+//            'duplicate_merge' => false,
+//            'audited' => false
+//        ],
+//        'created_by_user' => [
+//            'name' => 'created_by_user',
+//            'vname' => 'LBL_CREATED_BY',
+//            'type' => 'linked',
+//            'link' => 'created_by_link',
+//            'rname' => 'user_name',
+//            'source' => 'non-db',
+//            'id_name' => 'created_by',
+//            'module' => 'Users',
+//            'duplicate_merge' => false
+//        ],
+//        'created_by_name' => [
+//            'name' => 'created_by_name',
+//            'vname' => 'LBL_CREATED_BY',
+//            'type' => 'relate',
+//            'reportable' => false,
+//            'link' => 'created_by_link',
+//            'rname' => 'user_name',
+//            'source' => 'non-db',
+//            'table' => 'users',
+//            'id_name' => 'created_by',
+//            'module' => 'Users',
+//            'importable' => 'false',
+//            'massupdate' => false,
+//            'duplicate_merge' => false
+//        ],
+//        'description' => [
+//            'name' => 'description',
+//            'vname' => 'LBL_DESCRIPTION',
+//            'type' => 'text',
+//            'comment' => 'Full text of the note'
+//        ],
+//        'deleted' => [
+//            'name' => 'deleted',
+//            'vname' => 'LBL_DELETED',
+//            'type' => 'bool',
+//            'default' => '0',
+//            'reportable' => false,
+//            'comment' => 'Record deletion indicator',
+//            'duplicate_merge' => false,
+//            'audited' => false
+//        ],
+//        'tags' => [
+//            'name' => 'tags',
+//            'vname' => 'LBL_TAGS',
+//            'type' => 'tags',
+//            'dbType' => 'text',
+//            'duplicate_merge' => false
+//        ],
+//        'created_by_link' => [
+//            'name' => 'created_by_link',
+//            'type' => 'link',
+//            'relationship' => (isset($table_name) ? $table_name : strtolower($module)) . '_created_by',
+//            'vname' => 'LBL_CREATED_BY',
+//            'link_type' => 'one',
+//            'module' => 'Users',
+//            'bean_name' => 'User',
+//            'source' => 'non-db',
+//            'recover' => false,
+//            'duplicate_merge' => false
+//        ],
+//        'modified_user_link' => [
+//            'name' => 'modified_user_link',
+//            'type' => 'link',
+//            'relationship' => (isset($table_name) ? $table_name : strtolower($module)) . '_modified_user',
+//            'vname' => 'LBL_MODIFIED_BY',
+//            'link_type' => 'one',
+//            'module' => 'Users',
+//            'bean_name' => 'User',
+//            'source' => 'non-db',
+//            'recover' => false,
+//            'duplicate_merge' => false
+//        ]
+//    ],
+//    'indices' => [
+//        'id' => ['name' => (isset($table_name) ? $table_name : strtolower($module)) . 'pk', 'type' => 'primary', 'fields' => ['id']],
+//        'fts_idx' => ['name' => 'idx_' . (isset($table_name) ? $table_name : strtolower($module)) . '_fts', 'type' => 'index', 'fields' => ['date_modified', 'date_indexed']]
+//    ],
+//    'relationships' => [
+//        (isset($table_name) ? $table_name : strtolower($module)) . '_modified_user' => [
+//            'lhs_module' => 'Users',
+//            'lhs_table' => 'users',
+//            'lhs_key' => 'id',
+//            'rhs_module' => $module,
+//            'rhs_table' => (isset($table_name) ? $table_name : strtolower($module)),
+//            'rhs_key' => 'modified_user_id',
+//            'relationship_type' => 'one-to-many'
+//        ],
+//        (isset($table_name) ? $table_name : strtolower($module)) . '_created_by' => [
+//            'lhs_module' => 'Users',
+//            'lhs_table' => 'users',
+//            'lhs_key' => 'id',
+//            'rhs_module' => $module,
+//            'rhs_table' => (isset($table_name) ? $table_name : strtolower($module)),
+//            'rhs_key' => 'created_by',
+//            'relationship_type' => 'one-to-many'
+//        ]
+//    ]
+//];
