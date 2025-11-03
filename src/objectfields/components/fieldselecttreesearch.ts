9d3a2c5c9d4f4f6924ac12d7ca9e143c4040284b
Maret Valerie <valerie.maret@twentyreasons.com>
2025-11-03 18:18:04 +0100

    CR1001908 deprecated fieldselecttree

--- src/objectfields/components/fieldselecttreesearch.ts
+++ src/objectfields/components/fieldselecttreesearch.ts
@@ -7,11 +7,16 @@ import {language} from '../../services/language.service';
 import {backend} from "../../services/backend.service";
 import {configurationService} from "../../services/configuration.service";
 
+/**
+ * @deprecated in 2025.02.001
+ */
 @Component({
     selector: 'select-tree-search',
     templateUrl: '../templates/fieldselecttreesearch.html',
     standalone: false
 })
+
+
 export class fieldSelectTreeSearch
 {
     @Input() search:string;
