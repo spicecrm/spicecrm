bce884833dbdcab6e363efa4c3d7e3bde9cbd7cf
vasiljev <nenad.vasiljev@twentyreasons.com>
2025-11-05 12:43:27 +0100

    CR1002193: added a new param for the get /related route called "filteron"

--- api/include/SpiceBeans/api/extensions/spicebean.php
+++ api/include/SpiceBeans/api/extensions/spicebean.php
@@ -843,7 +843,11 @@ $routes = [
                 'description' => 'a searchterm to search by',
                 'type' => ValidationMiddleware::TYPE_STRING,
             ],
-
+            'filteron' => [
+                'in' => 'query',
+                'description' => 'if true, filter the items based on assigned user id',
+                'type' => ValidationMiddleware::TYPE_BOOL,
+            ]
         ],
         'responses' => [
             '200' => [
