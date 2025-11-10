15ce8cfd24c3c0c23c819e255244c63b97f40ba6
Christian Knoll <christian@spicecrm.com>
2025-11-10 10:51:21 +0100

    CR1002201 new panel on records that can be linked to users

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2025.02.001.1764944106771",
+    buildNumber: "2025.02.001.1765466607674",
     copyright: "© 2015 -  2025 SpiceCRM FlexCo All rights reserved."
     }
