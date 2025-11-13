a620a5012a6f08a43eb5c2cca1c4bd8a733003d7
Christian Knoll <christian@spicecrm.com>
2025-11-13 13:43:53 +0100

    CR1002226 new Logic Hook attachment_added

--- api/include/SpiceAttachments/api/controllers/SpiceAttachmentsController.php
+++ api/include/SpiceAttachments/api/controllers/SpiceAttachmentsController.php
@@ -113,6 +113,9 @@ class SpiceAttachmentsController
 
         $postBody = $req->getParsedBody();
         $postParams = $req->getQueryParams();
+
+        $seed->call_custom_logic('attachment_added');
+
         return $res->withJson(SpiceAttachments::saveAttachmentHashFiles($args['beanName'], $args['beanId'], array_merge($postBody, $postParams)));
     }
 
