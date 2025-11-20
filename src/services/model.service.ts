9f42e638e56dc9974bd7649f3d4b9f77c3025745
Christian Knoll <christian@spicecrm.com>
2025-11-20 07:00:33 +0100

    CR1002245 BUG Error in validation rules

--- src/services/model.service.ts
+++ src/services/model.service.ts
@@ -818,11 +818,11 @@ export class model implements OnDestroy {
 
         let val_left = this.data[condition.fieldname];
         let val_right: null;
-        if (condition.comparator.match(/regex/g)) {
+        //if (condition.comparator.match(/regex/g)) {
             val_right = condition.valuations;
-        } else {
-            val_right = this.evaluateValidationParams(condition.valuations);
-        }
+        //} else {
+        //    val_right = this.evaluateValidationParams(condition.valuations);
+        //}
 
         check = modelutilities.compare(val_left, condition.comparator, val_right);
 
