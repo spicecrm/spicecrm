67d6bd60649e6fabbb9b36bf9966fdf54930fdc3
Christian Knoll <christian@spicecrm.com>
2026-01-04 07:51:26 +0100

    CR1002302 new field type "select"

---
+++ src/objectfields/components/fieldselect.ts
@@ -0,0 +1,71 @@
+/**
+ * @module ObjectFields
+ */
+import {Component} from '@angular/core';
+import {model} from '../../services/model.service';
+import {view} from '../../services/view.service';
+import {EnumDisplayOptionArray, language} from '../../services/language.service';
+import {metadata} from '../../services/metadata.service';
+import {fieldGeneric} from './fieldgeneric';
+import {Router} from '@angular/router';
+
+@Component({
+    selector: 'field-select',
+    templateUrl: '../templates/fieldselect.html',
+    standalone: false
+})
+export class fieldSelect extends fieldGeneric {
+
+    public options: EnumDisplayOptionArray = [];
+
+    constructor(public model: model, public view: view, public language: language, public metadata: metadata, public router: Router) {
+        super(model, view, language, metadata, router);
+
+        this.subscriptions.add(
+            this.language.currentlanguage$.subscribe((language) => {
+                this.getOptions();
+            })
+        );
+    }
+
+    public ngOnInit() {
+        super.ngOnInit();
+
+        this.getOptions();
+    }
+
+    /**
+     * a getter for the value bound top the model
+     */
+    get value() {
+        return this.model.getField(this.fieldname);
+    }
+
+    /**
+     * a setter that returns the value to the model and triggers the validation
+     *
+     * @param val the new value
+     */
+    set value(val) {
+        this.model.setField(this.fieldname, val ?? '');
+    }
+
+    public getValue(): string {
+        return this.language.getFieldDisplayOptionValue(this.model.module, this.fieldname, this.value);
+    }
+
+    public getOptions() {
+
+        this.options = this.language.getFieldDisplayOptions(this.model.module, this.fieldname, true, this.fieldconfig.hideinactive ? false : true);
+
+        if (this.fieldconfig.sortdirection) {
+            switch (this.fieldconfig.sortdirection.toLowerCase()) {
+                case 'desc':
+                    this.options.sort((a,b) => a.display.toLowerCase() < b.display.toLowerCase() ? 1 : -1);
+                    break;
+                case 'asc':
+                    this.options.sort((a,b) => a.display.toLowerCase() > b.display.toLowerCase() ? 1 : -1);
+            }
+        }
+    }
+}
