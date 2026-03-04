6df468c98e85fd4fe3cf3cba5be2e1b35d2554ce
strombo <andreas@gloeckl.name>
2026-03-04 18:36:38 +0100

    CR1002413 Flexible property type in Mailbox to facilitate SpiceCRM upgrades (SMTP Port)

--- api/modules/Mailboxes/Mailbox.php
+++ api/modules/Mailboxes/Mailbox.php
@@ -95,7 +95,7 @@ class Mailbox extends SpiceBean {
     /**
      * @var int TODO check if that should go into the dictionary
      */
-    public int $smtp_port;
+    public int|string $smtp_port;
 
     /**
      * @var bool TODO check if that should go into the dictionary
