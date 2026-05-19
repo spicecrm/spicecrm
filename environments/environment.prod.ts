0c05b8d134ce368dac3948968c1556ae199be129
DawidBestrzynski <dawid.bestrzynski@twentyreasons.com>
2026-05-19 10:15:42 +0200

    packaging

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    buildNumber: "2026.01.001.1778669420568",
+    buildNumber: "2026.01.001.1779177946500",
     copyright: "© 2015 -  2026 SpiceCRM FlexCo All rights reserved."
     }
