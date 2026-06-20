0863460160fd20e7ac8be39afade3b9c96d5db91
Christian Knoll <christian@spicecrm.com>
2026-06-20 08:29:59 +0200

    CR1002554 properly handle sequence field in subpanel

--- src/services/relatedmodels.service.ts
+++ src/services/relatedmodels.service.ts
@@ -327,6 +327,10 @@ export class relatedmodels implements OnDestroy {
 
                     this.items = response.list.map(item => ({...this.modelutilities.backendModel2spice(this.relatedModule, item), relid: item.relid}));
 
+                    if(this.sequencefield){
+                        this.items.sort((a, b) => a[this.sequencefield] > b[this.sequencefield] ? 1 : -1);
+                    }
+
                     // set loaded
                     this.isloading = false;
 
